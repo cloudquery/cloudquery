2188f9e46928fe3d8b060bd981bba62940e8ad6c
Ekaterina Korsun <korsun.pats@gmail.com>
2021-01-10 18:22:12 +0200

    Add support for CQ_ env variable via viper

--- cmd/query.go
+++ cmd/query.go
@@ -3,18 +3,18 @@ package cmd
 import (
 	"github.com/cloudquery/cloudquery/cloudqueryclient"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
-
-var queryDSN string
-var queryDriver string
-var queryConfigPath string
-var queryOutputJsonPath string
 
 var queryCmd = &cobra.Command{
 	Use:     "query",
 	Short:   "Run queries specified in a policy file",
 	Version: Version,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		driver := viper.GetString("driver")
+		dsn := viper.GetString("dsn")
+		queryConfigPath := viper.GetString("policy_path")
+		queryOutputJsonPath := viper.GetString("output")
 		client, err := cloudqueryclient.New(driver, dsn, verbose)
 		if err != nil {
 			return err
@@ -25,9 +25,14 @@ var queryCmd = &cobra.Command{
 }
 
 func init() {
+	queryCmd.Flags().String("dsn", "./cloudquery.db", "database connection string or filepath if driver is sqlite (env CQ_DSN)")
+	queryCmd.Flags().String("driver", "sqlite", "database driver sqlite/postgresql/mysql/sqlserver (env CQ_DRIVER)")
+	queryCmd.Flags().String("path", "./policy.yml", "path to a policy file. can be generated with 'gen policy' command (env CQ_POLICY_PATH)")
+	queryCmd.Flags().String("output", "", "output path to store results as json file (env CQ_OUTPUT)")
+	viper.BindPFlag("dsn", queryCmd.Flags().Lookup("dsn"))
+	viper.BindPFlag("driver", queryCmd.Flags().Lookup("driver"))
+	viper.BindPFlag("policy_path", queryCmd.Flags().Lookup("path"))
+	viper.BindPFlag("output", queryCmd.Flags().Lookup("output"))
+
 	rootCmd.AddCommand(queryCmd)
-	queryCmd.Flags().StringVar(&queryDSN, "dsn", "./cloudquery.db", "database connection string or filepath if driver is sqlite")
-	queryCmd.Flags().StringVar(&queryDriver, "driver", "sqlite", "database driver sqlite/postgresql/mysql/sqlserver")
-	queryCmd.Flags().StringVar(&queryConfigPath, "path", "./policy.yml", "path to a policy file. can be generated with 'gen policy' command")
-	queryCmd.Flags().StringVar(&queryOutputJsonPath, "output", "", "output path to store results as json file")
 }
