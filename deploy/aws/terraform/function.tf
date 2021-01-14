38315627f8b1615ecd5cd2ae7bbe311e43c4ed01
zscholl <zak.scholl1@gmail.com>
2021-01-13 21:13:37 -0600

    update terraform lambda function

--- deploy/aws/terraform/function.tf
+++ deploy/aws/terraform/function.tf
@@ -28,5 +28,13 @@ resource "aws_lambda_function" "cloudquery" {
   handler       = "cloudquery"
   function_name = "cloudquery"
   filename      = "cloudquery.zip"
+  runtime       = "go1.x"
   role          = aws_iam_role.iam_for_lambda.arn
+
+  environment {
+    variables = {
+      CLOUDQUERY_DRIVER = "mysql",
+      CLOUDQUERY_DATABASE_STRING = "${aws_rds_cluster.cloudquery.master_username}:${aws_rds_cluster.cloudquery.master_password}@tcp(${aws_rds_cluster.cloudquery.endpoint}:3306)/${aws_rds_cluster.cloudquery.database_name}"
+    }
+  }
 }
