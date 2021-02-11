87dc08dd9fc997b0c2b711c7c6453a00f29dba5c
Yevgeny Pats <yev.pats@gmail.com>
2021-02-11 10:42:48 +0200

    Fix integration tests after split to plugins

--- cloudqueryclient/client_test.go
+++ cloudqueryclient/client_test.go
@@ -5,35 +5,22 @@ package cloudqueryclient
 import (
 	"fmt"
 	"github.com/ory/dockertest/v3"
-	"io/ioutil"
 	"log"
 	"testing"
 	"time"
 )
-
-func TestMigrationSQLite(t *testing.T) {
-	tmpFile, err := ioutil.TempFile("", "*.cloudquery.db")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if err = tmpFile.Close(); err != nil {
-		t.Fatal(err)
-	}
-	client, err := New("sqlite", tmpFile.Name(), true)
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = client.Run("./testdata/config.yml")
-	if err != nil {
-		t.Fatal(err)
-	}
-}
 
 func TestMigrationSQLServers(t *testing.T) {
 	pool, err := dockertest.NewPool("")
 	if err != nil {
 		log.Fatalf("Could not connect to docker: %s", err)
 	}
+
+	err = Init("./testdata/config.yml")
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	tests := []struct {
 		dockerName string
 		dockerVersion string
@@ -42,6 +29,13 @@ func TestMigrationSQLServers(t *testing.T) {
 		dsn string
 		port string
 	}{
+		{"sqlite",
+			"5",
+			[]string{"MYSQL_ROOT_PASSWORD=pass", "MYSQL_DATABASE=dbname"},
+			"sqlite",
+			"%s",
+			"cloudquery.db",
+		},
 		{"mysql",
 			"5",
 			[]string{"MYSQL_ROOT_PASSWORD=pass", "MYSQL_DATABASE=dbname"},
@@ -67,24 +61,32 @@ func TestMigrationSQLServers(t *testing.T) {
 
 	for _, tc := range tests {
 		t.Run(tc.driver, func(t *testing.T) {
-			resource, err := pool.Run(tc.dockerName, tc.dockerVersion, tc.env)
-			if err != nil {
-				log.Fatalf("Could not start resource: %s", err)
+			var resource *dockertest.Resource
+			var port string
+			if tc.dockerName != "sqlite" {
+				resource, err = pool.Run(tc.dockerName, tc.dockerVersion, tc.env)
+				if err != nil {
+					log.Fatalf("Could not start resource: %s", err)
+				}
+				time.Sleep(20 * time.Second)
+				port = resource.GetPort(tc.port)
 			}
-			time.Sleep(20 * time.Second)
-			port := resource.GetPort(tc.port)
+
 			client, err := New(tc.driver, fmt.Sprintf(tc.dsn, port), true)
 			if err != nil {
 				t.Fatal(err)
 			}
+
 			testErr := client.Run("./testdata/config.yml")
 
-			if err := pool.Purge(resource); err != nil {
-				log.Fatalf("Could not purge resource: %s", err)
+			if tc.dockerName != "sqlite" {
+				if err := pool.Purge(resource); err != nil {
+					log.Fatalf("Could not purge resource: %s", err)
+				}
 			}
 
 			if testErr != nil {
-				t.Fatal(err)
+				t.Fatal(testErr)
 			}
 		})
 	}
