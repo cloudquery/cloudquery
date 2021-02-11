010e6edd40f89ca0a086f0086d6db41cd9ed0784
Yevgeny Pats <yev.pats@gmail.com>
2021-02-11 10:14:50 +0200

    Remove providers from CloudQuery core
    Providers are now located at:
    
    https://github.com/cloudquery/cq-provider-aws
    https://github.com/cloudquery/cq-provider-azure
    https://github.com/cloudquery/cq-provider-gcp
    https://github.com/cloudquery/cq-provider-okta
    https://github.com/cloudquery/cq-provider-ok8s

--- providers/gcp/resource/client.go
+++
@@ -1,5 +0,0 @@
-package resource
-
-type ClientInterface interface {
-	CollectResource(resource string, config interface{}) error
-}
--- providers/k8s/config.go
+++
@@ -1,7 +0,0 @@
-package main
-
-var configYaml = `
-  - name: k8s
-    resources:
-      - name: pods
-      - name: services`
