337ba55e73241556049cd833d03fbf17a50f3af7
zscholl <zak.scholl1@gmail.com>
2021-03-06 16:41:53 -0600

    add custom unmarshaler for json config

--- config/config.go
+++ config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/creasty/defaults"
 )
 
@@ -35,4 +37,49 @@ func (c *Config) ProviderExists(providerName string) bool {
 		}
 	}
 	return false
+}
+
+// Default JSON unmarshaler will not populate the Rest field
+// and there are no type annotations that make this happen.
+// See https://github.com/golang/go/issues/6213
+//
+// Function assumes config keys are lowercase
+func (c *Config) UnmarshalJSON(data []byte) error {
+	var v map[string][]map[string]interface{}
+
+	if err := json.Unmarshal(data, &v); err != nil {
+		return err
+	}
+
+	for _, d := range v["providers"] {
+		prov := Provider{}
+		var ok bool
+		numKnownKeys := 0
+
+		if _, ok := d["name"]; ok {
+			numKnownKeys++
+		}
+		if _, ok := d["version"]; ok {
+			numKnownKeys++
+		}
+
+		rest := make(map[string]interface{}, len(d) - numKnownKeys)
+
+		if prov.Name, ok = d["name"].(string); !ok {
+			return fmt.Errorf("Could not parse provider config")
+		}
+		if prov.Version, ok = d["version"].(string); !ok {
+			prov.Version = "latest"
+		}
+
+		for key, value := range d {
+			if key == "name" || key == "version" { continue }
+			rest[key] = value
+		}
+		prov.Rest = rest
+		c.Providers = append(c.Providers, prov)
+
+	}
+
+	return nil
 }
