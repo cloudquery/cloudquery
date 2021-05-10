0f3306aef76dffc4cdc08481b25d2bd1bee4a222
Andrii Romanenko <andrejjrom@gmail.com>
2021-05-10 13:07:12 +0300

    Cloudfront client hotfix (#61)
    Co-authored-by: Andrii Romanenko <arforgethedev@gmail.com>

--- plugins/aws/client/client.go
+++ plugins/aws/client/client.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/service/cloudfront"
+
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/retry"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -250,6 +252,7 @@ func Configure(logger hclog.Logger, providerConfig interface{}) (schema.ClientMe
 func initServices(awsCfg aws.Config) Services {
 	return Services{
 		Autoscaling:      autoscaling.NewFromConfig(awsCfg),
+		Cloudfront:       cloudfront.NewFromConfig(awsCfg),
 		Cloudtrail:       cloudtrail.NewFromConfig(awsCfg),
 		Cloudwatch:       cloudwatch.NewFromConfig(awsCfg),
 		CloudwatchLogs:   cloudwatchlogs.NewFromConfig(awsCfg),
