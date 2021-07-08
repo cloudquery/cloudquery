2511fd8311fde05ed67a9a6afe0b2d78d3738cd0
Andrii Romanenko <andrejjrom@gmail.com>
2021-07-08 11:34:57 +0300

    fixed config examples (#23)
    * fixed config examples
    
    * fix config annotations

--- plugins/azure/client/config.go
+++ plugins/azure/client/config.go
@@ -3,12 +3,12 @@ package client
 // Provider Configuration
 
 type Config struct {
-	Subscriptions []string `yaml:"subscriptions"`
+	Subscriptions []string `hcl:"subscriptions"`
 }
 
 func (c Config) Example() string {
 	return `configuration {
 		//  Optional. if you not specified, cloudquery tries to access all subscriptions available to tenant
-		subscriptions = ["<YOU_SUBSCRIPTION_ID_HERE">]
+		subscriptions = ["<YOU_SUBSCRIPTION_ID_HERE>"]
 }`
 }
