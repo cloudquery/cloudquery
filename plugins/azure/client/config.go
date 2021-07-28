6e8fc3d01107144bf8d844ee067297c5e4be7cf5
Ron <38083777+roneli@users.noreply.github.com>
2021-07-28 17:21:19 +0300

    update config to make subscriptions optional (#58)

--- plugins/azure/client/config.go
+++ plugins/azure/client/config.go
@@ -3,12 +3,12 @@ package client
 // Provider Configuration
 
 type Config struct {
-	Subscriptions []string `hcl:"subscriptions"`
+	Subscriptions []string `hcl:"subscriptions,optional"`
 }
 
 func (c Config) Example() string {
 	return `configuration {
 		//  Optional. if you not specified, cloudquery tries to access all subscriptions available to tenant
-		subscriptions = ["<YOU_SUBSCRIPTION_ID_HERE>"]
+		//  subscriptions = ["<YOU_SUBSCRIPTION_ID_HERE>"]
 }`
 }
