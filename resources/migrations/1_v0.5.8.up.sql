67cd2eef72c54e8bda280a5ad97e153513c71034
Ron <38083777+roneli@users.noreply.github.com>
2021-08-19 19:47:00 +0300

    Bugfix/add_missing_semicolons (#160)
    * add missing ;

--- resources/migrations/1_v0.5.8.up.sql
+++ resources/migrations/1_v0.5.8.up.sql
@@ -1,4 +1,5 @@
 ALTER TABLE "aws_cloudfront_distributions"
-    DROP COLUMN IF EXISTS region
+    DROP COLUMN IF EXISTS region;
 
-ALTER TABLE IF EXISTS "aws_s3_bucket_cors_rules" DROP CONSTRAINT IF EXISTS "aws_s3_bucket_cors_rules_pk"
+ALTER TABLE IF EXISTS "aws_s3_bucket_cors_rules" DROP CONSTRAINT IF EXISTS "aws_s3_bucket_cors_rules_pk";
+
