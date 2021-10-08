a8eb7dfe871771ce09e3e2d0035de8e45f6123e7
roneli <38083777+roneli@users.noreply.github.com>
2021-10-08 11:57:32 +0300

    Fix migrate

--- plugins/gcp/resources/migrations/2_v0.4.8.down.sql
+++ plugins/gcp/resources/migrations/2_v0.4.8.down.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "gcp_compute_instance"
+ALTER TABLE "gcp_compute_instances"
     ADD COLUMN IF NOT EXISTS post_key_revocation_action_type text;
--- plugins/gcp/resources/migrations/2_v0.4.8.up.sql
+++ plugins/gcp/resources/migrations/2_v0.4.8.up.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "gcp_compute_instance"
+ALTER TABLE "gcp_compute_instances"
     DROP COLUMN IF EXISTS post_key_revocation_action_type;
