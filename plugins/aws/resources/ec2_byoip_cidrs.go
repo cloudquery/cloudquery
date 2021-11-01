62fa27498a4c6242298b706597e1cbea4fcae3b8
Andrii Romanenko <andrejjrom@gmail.com>
2021-11-01 16:36:20 +0200

    Ec2 byoip cidrs regions (#239)
    * aws_ec2_byoip_cidrs added regions to ignore
    
    * added more ignored regions
    
    Co-authored-by: Ron <38083777+roneli@users.noreply.github.com>

--- plugins/aws/resources/ec2_byoip_cidrs.go
+++ plugins/aws/resources/ec2_byoip_cidrs.go
@@ -66,9 +66,14 @@ func fetchEc2ByoipCidrs(ctx context.Context, meta schema.ClientMeta, parent *sch
 	c := meta.(*client.Client)
 	// DescribeByoipCidrs does not work in next regions, so we ignore them.
 	if _, ok := map[string]struct{}{
-		"eu-west-3":      {},
-		"eu-north-1":     {},
 		"ap-northeast-2": {},
+		"af-south-1":     {},
+		"cn-north-1":     {},
+		"cn-northwest-1": {},
+		"eu-west-3":      {},
+		"eu-south-1":     {},
+		"eu-north-1":     {},
+		"me-south-1":     {},
 	}[c.Region]; ok {
 		return nil
 	}
