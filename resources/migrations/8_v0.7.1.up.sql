c59d01a6250551ae1ca1283870b28f42a95f2222
Andrii Romanenko <andrejjrom@gmail.com>
2021-12-02 18:39:09 +0200

    Ec2 instances regenerated and manual field added (#325)
    * regenerated and added transition timestamp
    
    * changelog adjusted
    
    Co-authored-by: Ron <38083777+roneli@users.noreply.github.com>

--- resources/migrations/8_v0.7.1.up.sql
+++ resources/migrations/8_v0.7.1.up.sql
@@ -11,3 +11,10 @@ ALTER TABLE IF EXISTS "aws_wafv2_web_acls" ADD COLUMN logging_configuration text
 ALTER TABLE IF EXISTS "aws_waf_web_acls" ADD COLUMN logging_configuration text[];
 
 ALTER TABLE IF EXISTS "aws_redshift_clusters" ADD COLUMN logging_status jsonb;
+
+--ec2-instances
+ALTER TABLE IF EXISTS "aws_ec2_instances" ADD COLUMN state_transition_reason_time timestamp;
+ALTER TABLE IF EXISTS "aws_ec2_instances" ADD COLUMN boot_mode text;
+ALTER TABLE IF EXISTS "aws_ec2_instances" ADD COLUMN metadata_options_http_protocol_ipv6 text;
+ALTER TABLE IF EXISTS "aws_ec2_instance_network_interfaces" ADD COLUMN ipv4_prefixes _text;
+ALTER TABLE IF EXISTS "aws_ec2_instance_network_interfaces" ADD COLUMN ipv6_prefixes _text;
