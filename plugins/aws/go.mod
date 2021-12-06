e13f2e67ff352b35c08f313f79acb8f7ff9ad50b
Ron <38083777+roneli@users.noreply.github.com>
2021-12-06 19:04:48 +0200

    close v0.8.0 (#334)
    * close v0.8.0

--- plugins/aws/go.mod
+++ plugins/aws/go.mod
@@ -51,7 +51,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/waf v1.2.1
 	github.com/aws/aws-sdk-go-v2/service/wafv2 v1.5.1
 	github.com/aws/smithy-go v1.9.0
-	github.com/cloudquery/cq-provider-sdk v0.5.2
+	github.com/cloudquery/cq-provider-sdk v0.5.3
 	github.com/cloudquery/faker/v3 v3.7.5
 	github.com/gocarina/gocsv v0.0.0-20210516172204-ca9e8a8ddea8
 	github.com/golang/mock v1.6.0
@@ -126,12 +126,12 @@ require (
 	github.com/zclconf/go-cty v1.9.1 // indirect
 	go.uber.org/atomic v1.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
-	golang.org/x/net v0.0.0-20210520170846-37e1c6afe023 // indirect
+	golang.org/x/net v0.0.0-20210825183410-e898025ed96a // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+	golang.org/x/sys v0.0.0-20211025201205-69cdffdb9359 // indirect
 	golang.org/x/text v0.3.6 // indirect
-	google.golang.org/genproto v0.0.0-20211005153810-c76a74d43a8e // indirect
-	google.golang.org/grpc v1.41.0 // indirect
+	google.golang.org/genproto v0.0.0-20211129164237-f09f9a12af12 // indirect
+	google.golang.org/grpc v1.42.0 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- plugins/aws/resources/migrations/8_v0.8.0.down.sql
+++ plugins/aws/resources/migrations/8_v0.8.0.down.sql
@@ -163,3 +163,31 @@ ALTER TABLE IF EXISTS "aws_cloudfront_distribution_default_behaviour_lambda_func
 --aws_cloudfront_distribution_origin_groups
 ALTER TABLE IF EXISTS "aws_cloudfront_distribution_origin_groups"
     RENAME COLUMN "failover_criteria_status_codes" TO "failover_criteria_status_codes_items";
+
+
+DROP TABLE IF EXISTS public.aws_emr_clusters;
+
+CREATE TABLE public.aws_emr_clusters (
+                                         cq_id uuid,
+                                         meta jsonb,
+                                         account_id text NOT NULL,
+                                         region text,
+                                         arn text,
+                                         id text NOT NULL,
+                                         name text,
+                                         normalized_instance_hours integer,
+                                         outpost_arn text,
+                                         status_state text,
+                                         status_state_change_reason_code text,
+                                         status_state_change_reason_message text,
+                                         status_timeline_creation_date_time timestamp without time zone,
+                                         status_timeline_end_date_time timestamp without time zone,
+                                         status_timeline_ready_date_time timestamp without time zone,
+                                         vpc_id text
+);
+
+ALTER TABLE ONLY public.aws_emr_clusters
+    ADD CONSTRAINT aws_emr_clusters_cq_id_key UNIQUE (cq_id);
+
+ALTER TABLE ONLY public.aws_emr_clusters
+    ADD CONSTRAINT aws_emr_clusters_pk PRIMARY KEY (account_id, id);
