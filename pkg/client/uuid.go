235d364cca8df6ce62d092273ca88f538145563b
bbernays <ben@bernays.me>
2021-12-08 15:03:46 -0500

    Update uuid.go

--- pkg/client/uuid.go
+++ pkg/client/uuid.go
@@ -225,7 +225,7 @@ func (src UUID) MarshalJSON() ([]byte, error) {
 }
 
 func (dst *UUID) UnmarshalJSON(src []byte) error {
-	if bytes.Compare(src, []byte("null")) == 0 {
+	if bytes.Equal(src, []byte("null")) {
 		return dst.Set(nil)
 	}
 	if len(src) != 38 {
@@ -237,8 +237,7 @@ func (dst *UUID) UnmarshalJSON(src []byte) error {
 func underlyingUUIDType(val interface{}) (interface{}, bool) {
 	refVal := reflect.ValueOf(val)
 
-	switch refVal.Kind() {
-	case reflect.Ptr:
+	if refVal.Kind() == reflect.Ptr {
 		if refVal.IsNil() {
 			return time.Time{}, false
 		}
