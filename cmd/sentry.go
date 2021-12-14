0cb786f68ef5c4d591732eda123700938ff7f1d0
Kemal Hadimli <disq@users.noreply.github.com>
2021-12-14 15:31:52 +0000

    Add `-no-sentry` to opt-out, remove version flags, hide DSN flag

--- cmd/sentry.go
+++ cmd/sentry.go
@@ -13,23 +13,27 @@ import (
 const defaultSentryFlushTimeout = 2 * time.Second
 
 func registerSentryFlags(cmd *cobra.Command) {
+	cmd.PersistentFlags().Bool("no-sentry", false, "Disable Sentry")
 	cmd.PersistentFlags().Bool("sentry-debug", false, "Enable Sentry APIDebug")
 	cmd.PersistentFlags().String("sentry-dsn", "https://5ff9e378a79d4ba2821f540b036286e9@o912044.ingest.sentry.io/6106324", "Sentry DSN")
-	// cmd.PersistentFlags().String("sentry-environment", "development", "Sentry Environment")
-	cmd.PersistentFlags().String("sentry-release", client.Version, "Sentry Release")
 
+	_ = cmd.PersistentFlags().MarkHidden("sentry-dsn")
+
+	_ = viper.BindPFlag("no-sentry", cmd.PersistentFlags().Lookup("no-sentry"))
 	_ = viper.BindPFlag("sentry-debug", cmd.PersistentFlags().Lookup("sentry-debug"))
 	_ = viper.BindPFlag("sentry-dsn", cmd.PersistentFlags().Lookup("sentry-dsn"))
-	// _ = viper.BindPFlag("sentry-environment", cmd.PersistentFlags().Lookup("sentry-environment"))
-	_ = viper.BindPFlag("sentry-release", cmd.PersistentFlags().Lookup("sentry-release"))
 }
 
 func initSentry() {
+	dsn := viper.GetString("sentry-dsn")
+	if viper.GetBool("no-sentry") {
+		dsn = "" // "To drop all events, set the DSN to the empty string."
+	}
+
 	if err := sentry.Init(sentry.ClientOptions{
-		Debug: viper.GetBool("sentry-debug"),
-		Dsn:   viper.GetString("sentry-dsn"),
-		// Environment: viper.GetString("sentry-environment"),
-		Release: viper.GetString("sentry-release"),
+		Debug:   viper.GetBool("sentry-debug"),
+		Dsn:     dsn,
+		Release: client.Version,
 	}); err != nil {
 		zerolog.Info().Err(err).Msg("sentry.Init failed")
 	}
