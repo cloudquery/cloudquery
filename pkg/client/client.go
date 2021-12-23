905be6fc49a94ef93397ac27cd76de0dbe294b61
Kemal Hadimli <disq@users.noreply.github.com>
2021-12-23 11:04:36 +0000

    Don't log req.PolicyName, use policyConfig.name instead

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -787,12 +787,11 @@ func (c *Client) LoadPolicies(ctx context.Context, req *PoliciesRunRequest) (pol
 
 func (c *Client) runPolicy(ctx context.Context, policyConfig *config.Policy, req *PoliciesRunRequest) (res *policy.ExecutionResult, retErr error) {
 	spanAttrs := []attribute.KeyValue{
-		attribute.String("policy", req.PolicyName),
+		attribute.String("policy_name", policyConfig.Name),
 	}
 
 	if strings.HasPrefix(policyConfig.Name, policy.CloudQueryOrg) {
 		spanAttrs = append(spanAttrs,
-			attribute.String("policy_name", policyConfig.Name),
 			attribute.String("policy_version", policyConfig.Version),
 			attribute.String("policy_subpath", policyConfig.SubPath),
 		)
