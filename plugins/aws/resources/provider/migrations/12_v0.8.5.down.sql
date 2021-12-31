edc530d216924ee18e8c44911fceaffba6f1370f
Kemal <223029+disq@users.noreply.github.com>
2021-12-31 09:11:08 +0000

    Fix elasticsearch domains PK (#384)
    * Fix elasticsearch domains PK
    
    * Update CHANGELOG
    
    * Fix PK order
    
    Co-authored-by: Kemal Hadimli <disq@users.noreply.github.com>

---
+++ plugins/aws/resources/provider/migrations/12_v0.8.5.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE IF EXISTS "aws_elasticsearch_domains" DROP CONSTRAINT IF EXISTS "aws_elasticsearch_domains_pk";
+
+ALTER TABLE "aws_elasticsearch_domains"
+    ADD CONSTRAINT "aws_elasticsearch_domains_pk"
+        PRIMARY KEY(account_id, id);
