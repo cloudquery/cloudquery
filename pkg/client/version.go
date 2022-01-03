728cef58f26ea5811199c30b9ffebc714e29310b
Kemal Hadimli <disq@users.noreply.github.com>
2022-01-03 12:45:24 +0000

    Don't check for update if development build

--- pkg/client/version.go
+++ pkg/client/version.go
@@ -58,9 +58,13 @@ var getLatestRelease = doGetLatestRelease
 // * "last update check" content and that version is newer than current
 // * Github latest release version and it's newer than current
 func MaybeCheckForUpdate(ctx context.Context, fs afero.Afero, nowUnix, period int64) (*version.Version, error) {
+	if Version == DefaultVersion {
+		// development version or something with local changes, skip update checks
+		return nil, nil
+	}
+
 	currentVersion, err := version.NewSemver(Version)
 	if err != nil {
-		// must be a development version or something with local changes
 		return nil, err
 	}
 
