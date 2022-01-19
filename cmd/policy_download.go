e5c796064cf66467fc59355c5e5831ff523b2b8a
Itay Zagron <zagron@gmail.com>
2022-01-19 18:25:53 +0100

    updated policy commands examples

--- cmd/policy_download.go
+++ cmd/policy_download.go
@@ -22,6 +22,9 @@ var (
   # The following will be the same as above
   # Official policies are hosted here: https://github.com/cloudquery-policies
   cloudquery policy download aws//cis-1.2.0
+	
+  # Download community policy
+  cloudquery policy download github.com/COMMUNITY_GITHUB_ORG/aws
 
   # See https://hub.cloudquery.io for additional policies.`,
 		Args: cobra.ExactArgs(1),
