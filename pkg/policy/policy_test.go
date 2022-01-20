fc374025ed9abfa5cbf6f32baa74f02fe0f97d09
bbernays <ben@bernays.me>
2022-01-20 12:07:12 -0500

    feat: Support selector on describe (#425)
    * Support Filtering on describe
    
    * Add Support for Check level filter
    
    * Add support and tests for empty selector

---
+++ pkg/policy/policy_test.go
@@ -0,0 +1,296 @@
+package policy
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+)
+
+func TestFilterPolicies(t *testing.T) {
+	filterTests := []struct {
+		p              Policy
+		path           string
+		expectError    bool
+		expectedPolicy Policy
+	}{
+		{
+			expectError: false,
+			p: Policy{
+				Name: "aws",
+				Policies: Policies{
+					&Policy{
+						Name: "test2",
+						Policies: Policies{
+							&Policy{
+								Name: "test",
+							},
+						},
+					},
+				},
+			},
+			path: "",
+			expectedPolicy: Policy{
+				Name: "aws",
+				Policies: Policies{
+					&Policy{
+						Name: "test2",
+						Policies: Policies{
+							&Policy{
+								Name: "test",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			expectError: false,
+			p: Policy{
+				Name: "aws",
+				Policies: Policies{
+					&Policy{
+						Name: "test2",
+						Policies: Policies{
+							&Policy{
+								Name: "test",
+							},
+						},
+					},
+				},
+			},
+			path:           "aws/test1",
+			expectedPolicy: Policy{},
+		}, {
+			expectError: true,
+			p: Policy{
+				Name: "aws",
+				Policies: Policies{
+					&Policy{
+						Name: "test",
+					},
+				},
+			},
+			path: "aws/test1",
+			expectedPolicy: Policy{
+				Name: "test",
+			},
+		},
+		{
+			p: Policy{
+				Name: "aws",
+				Policies: Policies{
+					&Policy{
+						Name: "level-1",
+						Policies: Policies{
+							&Policy{
+								Name: "level-2",
+								Policies: Policies{
+									&Policy{
+										Name: "level-3",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			path: "aws/level-1/level-2/level-3",
+			expectedPolicy: Policy{
+				Name: "level-3",
+			},
+		}, {
+			p: Policy{
+				Name: "aws",
+				Policies: Policies{
+					&Policy{
+						Name: "level-1",
+						Policies: Policies{
+							&Policy{
+								Name: "level-2",
+								Policies: Policies{
+									&Policy{
+										Name: "level-3",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			path: "aws/level-1/level-2",
+			expectedPolicy: Policy{
+				Name: "level-2",
+				Policies: Policies{
+					&Policy{
+						Name: "level-3",
+					},
+				},
+			},
+		},
+		{
+			p: Policy{
+				Name: "aws",
+				Policies: Policies{
+					&Policy{
+						Name: "level-1",
+						Policies: Policies{
+							&Policy{
+								Name: "level-2",
+								Policies: Policies{
+									&Policy{
+										Name: "level-3",
+										Checks: []*Check{
+											{
+												Name: "Control-1",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			path: "aws/level-1/level-2/level-3/Control-1",
+			expectedPolicy: Policy{
+				Name: "level-3",
+				Checks: []*Check{
+					{
+						Name: "Control-1",
+					},
+				},
+			},
+		},
+		{
+			p: Policy{
+				Name: "aws",
+				Policies: Policies{
+					&Policy{
+						Name: "level-1",
+						Policies: Policies{
+							&Policy{
+								Name: "level-2",
+								Policies: Policies{
+									&Policy{
+										Name: "level-3",
+										Checks: []*Check{
+											{
+												Name: "Control-1",
+											},
+											{
+												Name: "Control-2",
+											},
+											{
+												Name: "Control-3",
+											},
+											{
+												Name: "Control-4",
+											},
+											{
+												Name: "Control-5",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			path: "aws/level-1/level-2/level-3",
+			expectedPolicy: Policy{
+				Name: "level-3",
+				Checks: []*Check{
+					{
+						Name: "Control-1",
+					},
+					{
+						Name: "Control-2",
+					},
+					{
+						Name: "Control-3",
+					},
+					{
+						Name: "Control-4",
+					},
+					{
+						Name: "Control-5",
+					},
+				},
+			},
+		},
+		{
+			p: Policy{
+				Name: "aws",
+				Policies: Policies{
+					&Policy{
+						Name: "level-1",
+						Policies: Policies{
+							&Policy{
+								Name: "level-2",
+								Policies: Policies{
+									&Policy{
+										Name: "level-3",
+										Checks: []*Check{
+											{
+												Name: "Control-1",
+											},
+											{
+												Name: "Control-2",
+											},
+											{
+												Name: "Control-3",
+											},
+											{
+												Name: "Control-4",
+											},
+											{
+												Name: "Control-5",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			path: "aws/level-1/level-2",
+			expectedPolicy: Policy{
+				Name: "level-2",
+				Policies: Policies{
+					&Policy{
+						Name: "level-3",
+						Checks: []*Check{
+							{
+								Name: "Control-1",
+							},
+							{
+								Name: "Control-2",
+							},
+							{
+								Name: "Control-3",
+							},
+							{
+								Name: "Control-4",
+							},
+							{
+								Name: "Control-5",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for i, tt := range filterTests {
+		t.Run(fmt.Sprintf("case-%d", i), func(t *testing.T) {
+			diff := cmp.Diff(tt.expectedPolicy, tt.p.Filter(tt.path), cmpopts.IgnoreUnexported(Policy{}))
+			if diff != "" && !tt.expectError {
+				t.Fatalf("values are not the same %s", diff)
+			}
+		})
+	}
+}
