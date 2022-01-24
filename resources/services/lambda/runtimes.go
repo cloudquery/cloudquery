afbd3e5f9724fb956f4ea45c7068680a9e181de6
Ron <38083777+roneli@users.noreply.github.com>
2022-01-24 13:25:53 +0200

    fix: Make lambda runtimes global table (#424)
    Closes #382

--- resources/services/lambda/runtimes.go
+++ resources/services/lambda/runtimes.go
@@ -16,6 +16,7 @@ func LambdaRuntimes() *schema.Table {
 		Resolver:     fetchLambdaRuntimes,
 		DeleteFilter: client.DeleteAllFilter,
 		Options:      schema.TableCreationOptions{PrimaryKeys: []string{"name"}},
+		Global:       true,
 		Columns: []schema.Column{
 			{
 				Name:        "name",
@@ -30,11 +31,14 @@ func LambdaRuntimes() *schema.Table {
 //                                               Table Resolver Functions
 // ====================================================================================================================
 func fetchLambdaRuntimes(_ context.Context, _ schema.ClientMeta, _ *schema.Resource, res chan<- interface{}) error {
-	for _, runtime := range types.RuntimeProvidedal2.Values() {
-		res <- &RuntimeWrapper{
+
+	runtimes := make([]RuntimeWrapper, len(types.RuntimeProvidedal2.Values()))
+	for i, runtime := range types.RuntimeProvidedal2.Values() {
+		runtimes[i] = RuntimeWrapper{
 			Name: string(runtime),
 		}
 	}
+	res <- runtimes
 	return nil
 }
 
