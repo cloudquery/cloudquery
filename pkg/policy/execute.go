5b8969855e29f568097e7dba4931b723a343dd46
bbernays <ben@bernays.me>
2022-01-24 11:33:14 -0500

    Update execute.go

--- pkg/policy/execute.go
+++ pkg/policy/execute.go
@@ -5,15 +5,12 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"log"
 	"path/filepath"
-	"regexp"
 	"strings"
 
 	"github.com/cloudquery/cq-provider-sdk/provider/schema"
 	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/go-version"
-	"github.com/jeremywohl/flatten"
 	"github.com/spf13/afero"
 )
 
@@ -261,57 +258,4 @@ func GenerateExecutionResultFile(result *ExecutionResult, outputDir string) erro
 		return err
 	}
 	return nil
-}
-
-func cleanQuery(query string) string {
-	var re = regexp.MustCompile(`(?s)\/\*.*?\*\/|--.*?\n`)
-	query = re.ReplaceAllString(query, "")
-	// query = strings.ReplaceAll(query, "\n", " ")
-	query = strings.TrimSuffix(query, ";")
-	query = strings.TrimSpace(query)
-	query = strings.TrimSuffix(query, ";")
-
-	return strings.TrimSpace(query)
-}
-func (e *Executor) ExtractTableNames(ctx context.Context, query string) (tableNames []string, err error) {
-	e.log.Debug("extracting Table names-raw", "raw query", query)
-	cleanedQuery := cleanQuery(query)
-	e.log.Debug("extracting Table names-cleaned", "cleaned query", cleanedQuery)
-	explainQuery := fmt.Sprintf("EXPLAIN (FORMAT JSON) %s", cleanedQuery)
-
-	rows, err := e.conn.Query(ctx, explainQuery)
-	if err != nil {
-		return tableNames, err
-	}
-
-	for rows.Next() {
-		var s string
-		if err := rows.Scan(&s); err != nil {
-			log.Fatal(err)
-		}
-		var arrayJsonMap []map[string](interface{})
-		err := json.Unmarshal([]byte(s), &arrayJsonMap)
-		if err != nil {
-			e.log.Error("failed to unmarshal json", "err", err)
-			return tableNames, err
-		}
-
-		flat, err := flatten.Flatten(arrayJsonMap[0], "", flatten.DotStyle)
-		if err != nil {
-			e.log.Error("failed to flatten json", "err", err)
-			return tableNames, err
-		}
-		for key, val := range flat {
-			if strings.HasSuffix(key, "Relation Name") {
-				tableNames = append(tableNames, val.(string))
-			}
-
-		}
-	}
-	if err := rows.Err(); err != nil {
-		log.Println(query)
-		log.Fatal(err)
-	}
-
-	return tableNames, err
 }
