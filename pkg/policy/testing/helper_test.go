b90099e40edc92a7e21099b1829b9bda2552ad82
bbernays <ben@bernays.me>
2022-01-28 15:46:06 -0500

    Update helper_test.go

--- pkg/policy/testing/helper_test.go
+++ pkg/policy/testing/helper_test.go
@@ -1,26 +1,11 @@
 package testing
 
 import (
-	"log"
 	"testing"
 )
 
-func TestManager_Load(t *testing.T) {
-	// Skip test for now since github is annoying
-	files, err := FilePathWalkDir("../../../database-data")
-	log.Println(files, err)
-
-}
-
-func TestManager_Group(t *testing.T) {
-	// Skip test for now since github is annoying
-	files, _ := FilePathWalkDir("../../../database-data")
-	log.Printf("%+v", FilterFiles(files))
-
-}
-
 func TestManager_Execute(t *testing.T) {
 	// Skip test for now since github is annoying
-	TestPolicy(t, ".cq/policies/github.com/cloudquery-policies/aws", "ec2/EC2.18")
+	TestPolicy(t, "../../../.cq/policies/github.com/cloudquery-policies/aws", "foundational_security/ec2/EC2.18", "../../../database-data")
 
 }
