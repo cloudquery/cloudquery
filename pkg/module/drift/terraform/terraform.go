595fe4af1444bee3c441ac35e2ccc24cbc4e28d9
Kemal Hadimli <disq@users.noreply.github.com>
2022-02-01 17:50:05 +0000

    fix: Relax tfstate version check

--- pkg/module/drift/terraform/terraform.go
+++ pkg/module/drift/terraform/terraform.go
@@ -1,19 +1,32 @@
 package terraform
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 )
 
-// LoadState loads the given reader into tfstate and validates the state version
+// LoadState loads the given reader into tfstate
 func LoadState(reader io.Reader) (*Data, error) {
 	var s Data
 	if err := json.NewDecoder(reader).Decode(&s.State); err != nil {
 		return nil, fmt.Errorf("invalid tf state file: %w", err)
 	}
-	if s.State.Version != StateVersion {
-		return nil, fmt.Errorf("unsupported state version %d", s.State.Version)
-	}
 	return &s, nil
+}
+
+// ValidateStateVersion validates the given tfstate version to be version 4
+func ValidateStateVersion(s *Data) (bool, error) {
+	if s.State.Version == nil {
+		return true, errors.New("unspecified tfstate version, allowing")
+	}
+	if bytes.Equal(*s.State.Version, []byte{'4'}) {
+		return true, nil
+	}
+	if bytes.Equal(*s.State.Version, []byte{'2'}) || bytes.Equal(*s.State.Version, []byte{'3'}) {
+		return false, fmt.Errorf("unsupported tfstate version %s", string(*s.State.Version))
+	}
+	return true, fmt.Errorf("unknown tfstate version %s, allowing", string(*s.State.Version))
 }
