7a8999d116ec76c6a1f3ca00e0bf6ec2961e875a
bbernays <ben@bernays.me>
2022-02-01 15:08:37 -0500

    Update helper_test.go

--- pkg/policy/testing/helper_test.go
+++ pkg/policy/testing/helper_test.go
@@ -1,7 +1,9 @@
 package testing
 
-// func TestManager_Execute(t *testing.T) {
-// 	// Skip test for now since github is annoying
-// 	TestPolicy(t, "../../../.cq/policies/github.com/cloudquery-policies/aws", "foundational_security/ec2/EC2.18", "../../../database-data")
+import "testing"
 
-// }
+func TestManager_Execute(t *testing.T) {
+	// Skip test for now since github is annoying
+	TestPolicy(t, "../../../.cq/policies/github.com/cloudquery-policies/aws", "foundational_security/ec2/EC2.18", "../../../dest")
+
+}
