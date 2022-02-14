9cab380add0d4b44f3e33e45d8a44e4cbe4ebdca
Kemal Hadimli <disq@users.noreply.github.com>
2022-02-14 16:56:06 +0000

    Fix tests

--- pkg/module/drift/drift_test.go
+++ pkg/module/drift/drift_test.go
@@ -5,12 +5,56 @@ import (
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/cloudquery/cloudquery/pkg/module"
+	"github.com/cloudquery/cq-provider-sdk/cqproto"
 	"github.com/doug-martin/goqu/v9"
 	"github.com/doug-martin/goqu/v9/exp"
 	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/hcl/v2/hclparse"
 	"github.com/stretchr/testify/assert"
 )
+
+func setupDrift(t *testing.T) (*Drift, *BaseConfig) {
+	d := &Drift{
+		logger: hclog.NewNullLogger(),
+	}
+	val, err := d.readBaseConfig(1, map[string]module.ProviderData{
+		"aws": {
+			"info": []*cqproto.ModuleFile{
+				{
+					Name: "file.hcl",
+					Contents: []byte(`
+provider "aws" {
+  resource "*" {
+    ignore_attributes = [ "unknown_fields" ]
+  }
+
+  resource "accessanalyzer.analyzers" {
+    iac {
+      terraform {
+        type = "aws_accessanalyzer_analyzer"
+      }
+    }
+  }
+
+  resource "ec2.instances" {
+    ignore_attributes = ["launch_time"]
+
+    iac {
+      terraform {
+        type = "aws_instance"
+      }
+    }
+  }
+}
+`),
+				},
+			},
+		},
+	})
+	assert.NoError(t, err)
+	return d, val
+}
 
 func TestReadBuiltinConfig(t *testing.T) {
 	t.Parallel()
@@ -20,6 +64,15 @@ func TestReadBuiltinConfig(t *testing.T) {
 	}
 	val, err := d.readBaseConfig(1, nil)
 	assert.NoError(t, err)
+	assert.Nil(t, val.Terraform)
+	assert.NotNil(t, val.WildProvider)
+	assert.Equal(t, 0, len(val.Providers))
+}
+
+func TestReadBuiltinConfigWithSuppliedProviderData(t *testing.T) {
+	t.Parallel()
+
+	_, val := setupDrift(t)
 	assert.Nil(t, val.Terraform)
 	assert.NotNil(t, val.WildProvider)
 	assert.Equal(t, 1, len(val.Providers))
@@ -45,11 +98,7 @@ func TestEmptyProfileConfig(t *testing.T) {
 func TestProfileConfig(t *testing.T) {
 	t.Parallel()
 
-	d := &Drift{
-		logger: hclog.NewNullLogger(),
-	}
-	base, err := d.readBaseConfig(1, nil)
-	assert.NoError(t, err)
+	d, base := setupDrift(t)
 	assert.NotNil(t, base)
 
 	configRaw, diags := hclparse.NewParser().ParseHCL([]byte(`
@@ -75,7 +124,7 @@ provider "aws" {
 	assert.NotNil(t, a)
 
 	{
-		r := a.Resources["kms.keys"]
+		r := a.Resources["accessanalyzer.analyzers"]
 		assert.NotNil(t, r)
 		assert.EqualValues(t, aws.Bool(true), r.Deep)
 	}
