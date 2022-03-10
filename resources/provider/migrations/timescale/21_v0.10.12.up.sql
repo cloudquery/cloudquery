598f13ab6c1dedbf2737ee786cb8c89008c3f3a7
Itay Zagron <zagronitay@users.noreply.github.com>
2022-03-10 10:58:46 +0200

    fix: Update EC2 EIPs Primary key (#546)

---
+++ resources/provider/migrations/timescale/21_v0.10.12.up.sql
@@ -0,0 +1,7 @@
+-- Resource: ec2.eips
+
+ALTER TABLE IF EXISTS aws_ec2_eips
+    DROP CONSTRAINT aws_ec2_eips_pk;
+ALTER TABLE IF EXISTS aws_ec2_eips
+    ADD CONSTRAINT aws_ec2_eips_pk PRIMARY KEY (cq_fetch_date,account_id,allocation_id);
+
