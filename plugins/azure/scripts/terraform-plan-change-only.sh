7c5ca76ea18a2cb3bcbb373f1188e4aa4138418c
Yevgeny Pats <yev.pats@gmail.com>
2022-03-13 19:30:47 +0200

    chore: Add infracost (#193)
    * chore: Add infracost
    
    * skip unit tests for scripts folder
    
    * chore: Test terraform workflow
    
    * fix terraform -chdir
    
    * fix /tmp/infracost.yml generated file

--- plugins/azure/scripts/terraform-plan-change-only.sh
+++ plugins/azure/scripts/terraform-plan-change-only.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env bash
 
+# The infracost logic is from here # https://www.infracost.io/docs/troubleshooting/#multi-projects
+
 set -e
 
+plans=()
+planjsons=()
 for f in terraform/*; do
     if [ -d $f ]; then
-        cd $f/prod
-        CHANGES=$(git diff --name-only HEAD..origin/main ../)
+        CHANGES=$(git diff --name-only HEAD..origin/main ${f})
         # if there are any changes run terraform apply
         if [ "$CHANGES" != "" ]; then
             echo "detected changes in $f. Running terraform plan..."
-            terraform init -no-color
-            terraform plan -lock=false -no-color
+            terraform -chdir="${f}/prod" plan -lock=false -no-color -out tfplan.binary
+            
+            echo "Running terraform show in $f"
+            terraform -chdir="${f}/prod" show -json tfplan.binary > "${f}/prod/plan.json"
+            plans=(${plans[@]} "${f}/prod/tfplan.binary")
+            planjsons=(${planjsons[@]} "${f}/prod/plan.json")
         fi
-        cd -
     fi
+done
+
+echo -e "version: 0.1\n\nprojects:\n" > /tmp/infracost.yml
+for planjson in "${planjsons[@]}"; do
+  echo -e "  - path: $planjson" >> /tmp/infracost.yml
 done
