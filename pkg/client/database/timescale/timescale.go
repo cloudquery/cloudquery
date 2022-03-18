27bad104bebe73f8eaf1fbb34696de860706d430
bbernays <ben@bernays.me>
2022-03-18 15:55:43 -0400

    Update timescale.go

--- pkg/client/database/timescale/timescale.go
+++ pkg/client/database/timescale/timescale.go
@@ -20,9 +20,8 @@ import (
 const (
 	validateTimescaleInstalled = `SELECT EXISTS(SELECT 1 FROM pg_extension where extname = 'timescaledb')`
 	timescaleVersionQuery      = `SELECT extversion FROM pg_catalog.pg_extension WHERE extname='timescaledb';`
+	MinTimescaleVersion        = version.Must(version.NewVersion("2.0"))
 )
-
-var MinTimescaleVersion = version.Must(version.NewVersion("2.0"))
 
 // queryRower helps with unit tests
 type queryRower interface {
@@ -145,7 +144,7 @@ func doValidateTimescaleVersion(ctx context.Context, q queryRower, want *version
 		return fmt.Errorf("error getting Timescale version: %w", err)
 	}
 	if got.LessThan(want) {
-		return fmt.Errorf("unsupported Timescale version: %v. (should be >= %v)", got, want)
+		return fmt.Errorf("unsupported Timescale version: %s. (should be >= %s)", got.String(), want.String())
 	}
 	return nil
 }
