2ad4167ba4e7f0abf3b7eb50d7634e10d70d140d
Ron <38083777+roneli@users.noreply.github.com>
2022-04-04 16:56:02 +0300

    feat: Merge migrations (#650)
    * feat: Merge migrations
    
    * Update lint_golang.yml

--- resources/provider/migrations/timescale/25_v0.10.17.down.sql
+++
@@ -1,6 +0,0 @@
--- Resource: redshift.event_subscriptions
-DROP TABLE IF EXISTS aws_redshift_event_subscriptions;
-
--- Resource: redshift.clusters
-DROP TABLE IF EXISTS aws_redshift_snapshot_accounts_with_restore_access;
-DROP TABLE IF EXISTS aws_redshift_snapshots;
