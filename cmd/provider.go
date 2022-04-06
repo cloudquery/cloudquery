d9e923524390149186f8cdcc8636fab4756358f2
roneli <38083777+roneli@users.noreply.github.com>
2022-04-06 18:05:55 +0300

    update cmd docs

--- cmd/provider.go
+++ cmd/provider.go
@@ -113,7 +113,7 @@ var (
 )
 
 func init() {
-	providerRemoveStaleCmd.Flags().DurationVar(&lastUpdate, "last-update", time.Hour*1, "")
+	providerRemoveStaleCmd.Flags().DurationVar(&lastUpdate, "last-update", time.Hour*1, "Duration should be a string is a possibly signed sequence of decimal numbers, each with optional fraction and a unit suffix, such as \"300ms\", \"-1.5h\" or \"2h45m\". Valid time units are \"ns\", \"us\" (or \"µs\"), \"ms\", \"s\", \"m\", \"h\".")
 	providerRemoveStaleCmd.Flags().BoolVar(&dryRun, "dry-run", true, "")
 	providerCmd.AddCommand(providerDownloadCmd, providerUpgradeCmd, providerDowngradeCmd, providerDropCmd, providerBuildSchemaCmd, providerRemoveStaleCmd)
 	rootCmd.AddCommand(providerCmd)
