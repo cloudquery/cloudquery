be650e68028d80a0458ad4efa8919c8d0abaa87b
bbernays <ben@bernays.me>
2022-04-11 16:45:59 -0400

    Update load_config.go

--- pkg/config/load_config.go
+++ pkg/config/load_config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	"io"
 	"net/url"
 	"strings"
@@ -11,26 +10,18 @@ import (
 	"github.com/hairyhenderson/go-fsimpl/filefs"
 )
 
-// srvURL := setupTestS3Bucket(t)
-
-// 	os.Setenv("AWS_ANON", "true")
-// 	defer os.Unsetenv("AWS_ANON")
-
-// 	fsys, err := New(tests.MustURL("s3://mybucket/?region=us-east-1&disableSSL=true&s3ForcePathStyle=true&endpoint=" + srvURL.Host))
-// 	assert.NoError(t, err)
-
-// 	de, err := fs.ReadDir(fsys, "dir1")
-// 	assert.NoError(t, err)
-
 func loadRemoteFile(path string) ([]byte, error) {
 	mux := fsimpl.NewMux()
 	mux.Add(filefs.FS)
 	mux.Add(blobfs.FS)
 
 	sanitizedPath, _ := url.Parse(path)
+
+	// go-fsimpl is looking for a "directory" where it is the full path without the actual file
+	// but must have all of the query strings
 	directory := path[:strings.LastIndex(path, "/")] + "?" + sanitizedPath.RawQuery
 	fileName := sanitizedPath.Path[strings.LastIndex(sanitizedPath.Path, "/")+1:]
-	fmt.Println(directory, fileName)
+
 	fsys, err := mux.Lookup(directory)
 	if err != nil {
 		return nil, err
