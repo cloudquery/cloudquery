92e19314df0f32764eba02c184b3033c5feb1a0b
bbernays <ben@bernays.me>
2022-04-11 19:17:53 -0400

    Update load_config_test.go

--- pkg/config/load_config_test.go
+++ pkg/config/load_config_test.go
@@ -10,10 +10,12 @@ import (
 
 	"github.com/johannesboyne/gofakes3"
 	"github.com/johannesboyne/gofakes3/backend/s3mem"
+	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 )
 
 const bucketName = "myBucket"
+const defaultPermissions = 0644
 
 func putFile(backend gofakes3.Backend, file, mime, content string) error {
 	_, err := backend.PutObject(
@@ -52,7 +54,25 @@ func TestLoadRemoteFile(t *testing.T) {
 	body, diags := p.LoadConfigFile(fmt.Sprintf("s3://%s/config.hcl?region=us-east-1&disableSSL=true&s3ForcePathStyle=true&endpoint=%s", bucketName, srvURL.Host))
 	assert.Equal(t, 0, len(diags))
 
-	cfg, diags := p.LoadConfigFromSource("test.hcl", []byte(testConfig))
+	p2 := NewParser()
+	cfg, diags := p2.LoadConfigFromSource("test.hcl", []byte(testConfig))
+	assert.Nil(t, diags)
+	assert.Equal(t, cfg, body)
+}
+
+func TestLoadLocalFile(t *testing.T) {
+	appFS := afero.NewMemMapFs()
+	p := NewParser(func(p *Parser) {
+		p.fs = afero.Afero{Fs: appFS}
+	})
+	path := "./asdf/asdf/asfd/teddstcdonfig.hcl"
+	p.fs.WriteFile(path, []byte(testConfig), defaultPermissions)
+	body, diags := p.LoadConfigFile("./asdf/asdf/asfd/teddstcdonfig.hcl")
+	assert.Equal(t, 0, len(diags))
+
+	p2 := NewParser()
+	cfg, diags := p2.LoadConfigFromSource("test.hcl", []byte(testConfig))
+
 	assert.Nil(t, diags)
 	assert.Equal(t, cfg, body)
 }
