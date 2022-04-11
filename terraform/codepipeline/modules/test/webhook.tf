455f0784aa63f0d4c1d3247512722be8e8259ce3
Itay Zagron <zagronitay@users.noreply.github.com>
2022-04-11 11:55:08 +0300

    chore: Added codepipeline::webhook tf (#677)

---
+++ terraform/codepipeline/modules/test/webhook.tf
@@ -0,0 +1,11 @@
+resource "aws_codepipeline_webhook" "codepipeline_webhook" {
+  name            = "${var.prefix}-codepipeline-webhook"
+  authentication  = "UNAUTHENTICATED"
+  target_action   = "Source"
+  target_pipeline = aws_codepipeline.codepipeline.name
+
+  filter {
+    json_path    = "$.ref"
+    match_equals = "refs/heads/{Branch}"
+  }
+}
