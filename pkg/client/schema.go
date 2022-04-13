3fd8c32048f699a0f54bedc40447e462e79716e4
Ron <38083777+roneli@users.noreply.github.com>
2022-04-13 12:19:36 +0300

    feat: Update plugin manager to support versions (#577)
    feat: Update plugin manager to support versions

--- pkg/client/schema.go
+++ pkg/client/schema.go
@@ -3,6 +3,8 @@ package client
 import (
 	"context"
 
+	"github.com/cloudquery/cloudquery/pkg/plugin/registry"
+
 	"github.com/rs/zerolog/log"
 
 	"github.com/cloudquery/cloudquery/pkg/plugin"
@@ -10,7 +12,7 @@ import (
 )
 
 type GetProviderSchemaOptions struct {
-	Provider string
+	Provider registry.Provider
 }
 
 type ProviderSchema struct {
@@ -18,19 +20,19 @@ type ProviderSchema struct {
 	ProtocolVersion int
 }
 
-func GetProviderSchema(ctx context.Context, plugin *plugin.Manager, request *GetProviderSchemaOptions) (*ProviderSchema, error) {
-	providerPlugin, err := plugin.CreatePlugin(request.Provider, "", nil)
+func GetProviderSchema(ctx context.Context, manager *plugin.Manager, request *GetProviderSchemaOptions) (*ProviderSchema, error) {
+	providerPlugin, err := manager.CreatePlugin(&plugin.CreationOptions{Provider: request.Provider})
 	if err != nil {
-		log.Error().Str("provider", request.Provider).Err(err).Msg("failed to create provider plugin")
+		log.Error().Stringer("provider", request.Provider).Err(err).Msg("failed to create provider plugin")
 		return nil, err // TODO: should be a diag
 	}
-	defer plugin.ClosePlugin(providerPlugin)
+	defer manager.ClosePlugin(providerPlugin)
 
 	schema, err := providerPlugin.Provider().GetProviderSchema(ctx, &cqproto.GetProviderSchemaRequest{})
 	if err != nil {
 		return nil, err // TODO: make a diag
 	}
-	log.Debug().Str("provider", request.Provider).Msg("retrieved provider schema successfully")
+	log.Debug().Stringer("provider", request.Provider).Msg("retrieved provider schema successfully")
 	return &ProviderSchema{
 		GetProviderSchemaResponse: schema,
 		ProtocolVersion:           providerPlugin.ProtocolVersion(),
--- pkg/client/schema_test.go
+++ pkg/client/schema_test.go
@@ -7,19 +7,22 @@ import (
 
 	"github.com/cloudquery/cloudquery/pkg/plugin"
 	"github.com/cloudquery/cloudquery/pkg/plugin/registry"
-	"github.com/hashicorp/go-hclog"
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_GetProviderSchema(t *testing.T) {
 	cancelServe := setupTestPlugin(t)
 	defer cancelServe()
-
-	pManager, err := plugin.NewManager(hclog.Default(), filepath.Join(".", ".cq", "providers"), registry.CloudQueryRegistryURL, nil)
+	pManager, err := plugin.NewManager(registry.NewRegistryHub(registry.CloudQueryRegistryURL,
+		registry.WithPluginDirectory(filepath.Join(".", ".cq", "providers"))), plugin.WithAllowReattach())
 	if !assert.NoError(t, err) {
 		t.FailNow()
 	}
-	s, err := GetProviderSchema(context.Background(), pManager, &GetProviderSchemaOptions{"test"})
+	s, err := GetProviderSchema(context.Background(), pManager, &GetProviderSchemaOptions{Provider: registry.Provider{
+		Name:    "test",
+		Version: "latest",
+		Source:  registry.DefaultOrganization,
+	}})
 	if s == nil {
 		t.FailNow()
 	}
