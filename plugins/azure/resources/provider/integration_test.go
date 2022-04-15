dff39d24815ac6dc96f2c418f6089df3a0298c84
shimonp21 <37939765+shimonp21@users.noreply.github.com>
2022-04-15 15:37:10 +0300

    fix: Fetch from default subscriptions for integration tests (#230)
    This fixes an issue where it was impossible to run azure integration tests locally,
    because cloudquery's integration-environment's subscription id was hardcoded.

--- plugins/azure/resources/provider/integration_test.go
+++ plugins/azure/resources/provider/integration_test.go
@@ -9,11 +9,5 @@ import (
 )
 
 func TestIntegration(t *testing.T) {
-	config := `
-		subscriptions = ["78f26f10-0e60-4293-8a7e-122584ccb40d"]
-	`
-	providertest.TestResource(t, providertest.ResourceTestCase{
-		Provider: Provider(),
-		Config:   config,
-	})
+	providertest.TestResource(t, providertest.ResourceTestCase{Provider: Provider()})
 }
