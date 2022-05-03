4cbc2e2db1d1f99eb253b7c2713f4d7249183a0d
Itay Zagron <zagronitay@users.noreply.github.com>
2022-05-03 17:32:44 +0300

    chore: Update waf/wafv2 tf files (#836)

--- plugins/aws/terraform/wafv2/modules/test/web_acl.tf
+++ plugins/aws/terraform/wafv2/modules/test/web_acl.tf
@@ -1,5 +1,15 @@
 #
 
+resource "aws_wafv2_regex_pattern_set" "example" {
+  name        = "example"
+  description = "Example regex pattern set"
+  scope       = "REGIONAL"
+
+  regular_expression {
+    regex_string = "one"
+  }
+}
+
 resource "aws_wafv2_web_acl" "wafv2_web_acl" {
   name        = "${var.prefix}-web-acl-managed-rule"
   description = "Example of a managed rule."
@@ -9,9 +19,15 @@ resource "aws_wafv2_web_acl" "wafv2_web_acl" {
     allow {}
   }
 
+  custom_response_body {
+    key          = "blocked_request_custom_response"
+    content      = "Rate Limit Exceeded"
+    content_type = "TEXT_PLAIN"
+  }
+
   rule {
     name     = "${var.prefix}-rule-1"
-    priority = 1
+    priority = 2
 
     override_action {
       count {}
@@ -45,18 +61,51 @@ resource "aws_wafv2_web_acl" "wafv2_web_acl" {
     }
   }
 
-  visibility_config {
-    cloudwatch_metrics_enabled = false
-    metric_name                = "${var.prefix}-web-acl-metric-1"
-    sampled_requests_enabled   = false
+  rule {
+    name     = "rate_limit_mt_external_webhook"
+    priority = 1
+
+    action {
+      block {
+        custom_response {
+          custom_response_body_key  = "blocked_request_custom_response"
+          response_code             = 429
+        }
+      }
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 100   # 100 per span of 5 mins - minimum value
+        aggregate_key_type = "IP"
+
+        scope_down_statement {
+          regex_pattern_set_reference_statement {
+            arn = aws_wafv2_regex_pattern_set.example.arn
+            text_transformation {
+              priority = 1
+              type     = "NONE"
+            }
+            field_to_match {
+              uri_path {}
+            }
+          }
+        }
+      }
+
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "wafv2_alb_mt_external_webhook_acl_rule_vis"
+      sampled_requests_enabled   = false
+    }
   }
 
-  tags = merge(
-    var.tags,
-    {
-      Name = "${var.prefix}-web-acl-managed-rule"
-    }
-  )
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "wafv2_alb_mt_external_webhook_acl_vis"
+    sampled_requests_enabled   = false
+  }
 }
 
 module "wafv2-web-acl-logging" {
@@ -72,6 +121,34 @@ resource "aws_wafv2_web_acl_logging_configuration" "web_acl_logging_configuratio
   redacted_fields {
     single_header {
       name = "user-agent"
+    }
+  }
+
+  logging_filter {
+    default_behavior = "KEEP"
+
+    filter {
+      behavior = "DROP"
+
+      condition {
+        action_condition {
+          action = "COUNT"
+        }
+      }
+
+      requirement = "MEETS_ALL"
+    }
+
+    filter {
+      behavior = "KEEP"
+
+      condition {
+        action_condition {
+          action = "ALLOW"
+        }
+      }
+
+      requirement = "MEETS_ANY"
     }
   }
 
