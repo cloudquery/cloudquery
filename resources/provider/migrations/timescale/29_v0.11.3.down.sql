1a33af9e24ec37ee8bf9a1bbc5eb260ef0298671
Timur Irmatov <irmatov@gmail.com>
2022-05-05 13:54:09 +0500

    chore: Truncate redshift table during migrations (#850)

--- resources/provider/migrations/timescale/29_v0.11.3.down.sql
+++ resources/provider/migrations/timescale/29_v0.11.3.down.sql
@@ -1,4 +1,5 @@
 -- aws_redshift_clusters: revert PK
+TRUNCATE TABLE aws_redshift_clusters CASCADE;
 ALTER TABLE IF EXISTS aws_redshift_clusters DROP CONSTRAINT aws_redshift_clusters_pk;
 ALTER TABLE IF EXISTS aws_redshift_clusters ADD CONSTRAINT aws_redshift_clusters_pk PRIMARY KEY (cq_fetch_date,account_id,id);
 
@@ -10,10 +11,12 @@ ALTER TABLE IF EXISTS aws_redshift_snapshots DROP COLUMN IF EXISTS cluster_cq_id
 ALTER TABLE IF EXISTS aws_redshift_snapshots DROP COLUMN IF EXISTS arn;
 
 -- aws_redshift_event_subscriptions: revert PK, drop arn
+TRUNCATE TABLE aws_redshift_event_subscriptions CASCADE;
 ALTER TABLE IF EXISTS aws_redshift_event_subscriptions DROP CONSTRAINT aws_redshift_event_subscriptions_pk;
 ALTER TABLE IF EXISTS aws_redshift_event_subscriptions ADD CONSTRAINT aws_redshift_event_subscriptions_pk PRIMARY KEY (cq_fetch_date,account_id,id);
 ALTER TABLE IF EXISTS aws_redshift_event_subscriptions DROP COLUMN IF EXISTS arn;
 
 -- aws_redshift_subnet_groups: revert PK
+TRUNCATE TABLE aws_redshift_subnet_groups CASCADE;
 ALTER TABLE IF EXISTS aws_redshift_subnet_groups DROP CONSTRAINT aws_redshift_subnet_groups_pk;
 ALTER TABLE IF EXISTS aws_redshift_subnet_groups ADD CONSTRAINT aws_redshift_subnet_groups_pk PRIMARY KEY (cq_fetch_date,account_id, region, cluster_subnet_group_name);
