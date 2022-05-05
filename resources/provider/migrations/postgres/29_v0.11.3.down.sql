1f3aac7fe73a17fc36c6c1d512e71ee6694d6d6f
Daniel Spangenberg <daniel@cloudquery.io>
2022-05-05 17:25:36 +0200

    fix: PK fix for conformance_pack_rule_compliances (#809)
    The PK was set on the parent but should be on default `cq_id`.
    Also the filter for the Details was not applied.
    
    ```
    cloudquery $ cloudquery fetch aws:config.conformance_packs
    An update to CloudQuery core is available: 0.22.10!
    
    Initializing CloudQuery Providers...
    
    Finished provider initialization...
    
    Upgrading CloudQuery providers aws
    
    ✓ Upgraded provider aws to latest successfully.
    
    Finished upgrading providers...
    
    Starting provider fetch...
    
    ✓ cq-provider-aws@latest fetch complete    3s   Finished Resources: 1/1
    
    Fetch Diagnostics for provider aws:
    
    Resource: config.conformance_packs Type: Database Severity: Error
    	Summary: failed to insert to table "aws_config_conformance_pack_rule_compliances": ERROR: duplicate key value violates unique constraint "aws_config_conformance_pack_rule_compliances_pk" (SQLSTATE 23505)
    	Detail: duplicate key value violates unique constraint "aws_config_conformance_pack_rule_compliances_pk". [Repeated:36]
    
    Provider fetch complete.
    
    Provider aws fetch summary: ✓ Total Resources fetched: 1	 ⚠️ Warnings: 0	 ❌ Errors: 1
    ```
    
    With fix:
    
    ```
    cloudquery $ cloudquery fetch aws:config.conformance_packs
    An update to CloudQuery core is available: 0.22.10!
    
    Initializing CloudQuery Providers...
    
    Finished provider initialization...
    
    Upgrading CloudQuery providers aws
    
    ✓ Upgraded provider aws to latest successfully.
    
    Finished upgrading providers...
    
    Starting provider fetch...
    
    ✓ cq-provider-aws@latest fetch complete    3s   Finished Resources: 1/1
    
    Provider fetch complete.
    
    Provider aws fetch summary: ✓ Total Resources fetched: 1	 ⚠️ Warnings: 0	 ❌ Errors: 0
    cloudquery $
    ```

--- resources/provider/migrations/postgres/29_v0.11.3.down.sql
+++ resources/provider/migrations/postgres/29_v0.11.3.down.sql
@@ -37,3 +37,8 @@ ALTER TABLE IF EXISTS "aws_wafv2_rule_groups" DROP COLUMN IF EXISTS "scope";
 -- Resource: wafv2.web_acls
 TRUNCATE TABLE aws_wafv2_web_acls CASCADE;
 ALTER TABLE IF EXISTS "aws_wafv2_web_acls" DROP COLUMN IF EXISTS "scope";
+
+-- aws_config_conformance_pack_rule_compliances: PK change
+ALTER TABLE IF EXISTS "aws_config_conformance_pack_rule_compliances"
+  DROP CONSTRAINT aws_config_conformance_pack_rule_compliances_pk,
+  ADD CONSTRAINT aws_config_conformance_pack_rule_compliances_pk PRIMARY KEY (conformance_pack_cq_id);
