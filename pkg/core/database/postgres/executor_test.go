fe9ca3b6d119760bba4324c741a9d6d123d9630e
Kemal Hadimli <disq@users.noreply.github.com>
2022-05-06 10:38:35 +0100

    Give up on the mock

--- pkg/core/database/postgres/executor_test.go
+++ pkg/core/database/postgres/executor_test.go
@@ -5,138 +5,79 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/georgysavva/scany/pgxscan"
+	"github.com/driftprogramming/pgxpoolmock"
+	"github.com/golang/mock/gomock"
 	"github.com/hashicorp/go-version"
-	"github.com/jackc/pgconn"
-	"github.com/jackc/pgproto3/v2"
-	"github.com/jackc/pgx/v4"
 	"github.com/stretchr/testify/assert"
 )
 
-type mockConn struct {
-	rows pgx.Rows
-}
-
-func (m mockConn) Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error) {
-	return m.rows, nil
-}
-
-type mockScanner struct {
-	t   *testing.T
-	val string
-	err error
-
-	nextCounter int
-}
-
-func NewMockScanner(t *testing.T, val string, err error) pgx.Rows {
-	return &mockScanner{
-		t:   t,
-		val: val,
-		err: err,
-	}
-}
-
-func (m *mockScanner) Close() {
-}
-
-func (m *mockScanner) Err() error {
-	if m.nextCounter > 0 {
-		return pgx.ErrNoRows
-	}
-	return nil
-}
-
-func (m *mockScanner) CommandTag() pgconn.CommandTag {
-	return nil
-}
-
-func (m *mockScanner) FieldDescriptions() []pgproto3.FieldDescription {
-	return []pgproto3.FieldDescription{
-		{
-			Name: []byte("data"),
-		},
-	}
-}
-
-func (m *mockScanner) Next() bool {
-	m.nextCounter++
-	return m.nextCounter == 1
-}
-
-func (m *mockScanner) Values() ([]interface{}, error) {
-	return []interface{}{m.val}, nil
-}
-
-func (m *mockScanner) RawValues() [][]byte {
-	return [][]byte{[]byte(m.val)}
-}
-
-func (m *mockScanner) Scan(dst ...interface{}) error {
-	if len(dst) != 1 {
-		m.t.Fatalf("called with %d args, want exactly one", len(dst))
-	}
-	ptr, ok := dst[0].(*string)
-	if !ok {
-		m.t.Fatalf("received %T, expected *string", dst[0])
-	}
-	*ptr = m.val
-	return m.err
-}
-
-var (
-	_ pgxscan.Querier = (*mockConn)(nil)
-	_ pgx.Rows        = (*mockScanner)(nil)
-)
-
 func Test_doValidatePostgresVersion(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
 	tests := []struct {
 		name       string
-		q          mockConn
+		value      string
 		minVersion string
+		mockErr    error
 		wantErr    error
 	}{
 		{
 			"scan error",
-			mockConn{rows: NewMockScanner(t, "", errors.New("scan"))},
+			"",
 			"10.0",
-			errors.New("error getting PostgreSQL version: scan"),
+			errors.New("scan"),
+			errors.New("error getting PostgreSQL version: scany: query one result row: scan"),
 		},
 		{
 			"strange version output",
-			mockConn{rows: NewMockScanner(t, "MSSQL", nil)},
+			"MSSQL",
 			"10.0",
+			nil,
 			errors.New("error getting PostgreSQL version: failed to parse version: MSSQL"),
 		},
 		{
 			"unparsable version",
-			mockConn{rows: NewMockScanner(t, "PostgreSQL 10.a.1", nil)},
+			"PostgreSQL 10.a.1",
 			"10.0",
+			nil,
 			errors.New("error getting PostgreSQL version: Malformed version: 10.a.1"),
 		},
 		{
 			"lower than needed",
-			mockConn{rows: NewMockScanner(t, "PostgreSQL 9.5 blah blah", nil)},
+			"PostgreSQL 9.5 blah blah",
 			"10.0",
+			nil,
 			errors.New("unsupported PostgreSQL version: 9.5.0. (should be >= 10.0.0)"),
 		},
 		{
 			"equal",
-			mockConn{rows: NewMockScanner(t, "PostgreSQL 10.0 blah blah", nil)},
+			"PostgreSQL 10.0 blah blah",
 			"10.0",
+			nil,
 			nil,
 		},
 		{
 			"greater than needed",
-			mockConn{rows: NewMockScanner(t, "PostgreSQL 12.5 blah blah", nil)},
+			"PostgreSQL 12.5 blah blah",
 			"10.0",
+			nil,
 			nil,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			mockPool := pgxpoolmock.NewMockPgxPool(ctrl)
+			pgxRows := pgxpoolmock.NewRows([]string{"value"}).AddRow(tt.value).ToPgxRows()
+
+			if tt.mockErr == nil {
+				mockPool.EXPECT().Query(gomock.Any(), gomock.Any(), gomock.Any()).Return(pgxRows, nil)
+			} else {
+				mockPool.EXPECT().Query(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, tt.mockErr)
+			}
+
 			want := version.Must(version.NewVersion(tt.minVersion))
-			err := doValidatePostgresVersion(context.Background(), tt.q, want)
+			err := doValidatePostgresVersion(context.Background(), mockPool, want)
 			if (tt.wantErr == nil) != (err == nil) {
 				t.Errorf("wantErr is %v, returned error is %v", tt.wantErr, err)
 			}
