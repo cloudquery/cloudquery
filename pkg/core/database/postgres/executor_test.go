184f97984cb90ba8b235e96b85a1f07098cb76d9
Kemal Hadimli <disq@users.noreply.github.com>
2022-05-06 09:58:52 +0100

    Fix the mock

--- pkg/core/database/postgres/executor_test.go
+++ pkg/core/database/postgres/executor_test.go
@@ -5,26 +5,70 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/georgysavva/scany/pgxscan"
 	"github.com/hashicorp/go-version"
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgproto3/v2"
 	"github.com/jackc/pgx/v4"
 	"github.com/stretchr/testify/assert"
 )
 
 type mockConn struct {
-	row pgx.Row
+	rows pgx.Rows
 }
 
-func (m mockConn) QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row {
-	return m.row
+func (m mockConn) Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error) {
+	return m.rows, nil
 }
 
 type mockScanner struct {
 	t   *testing.T
 	val string
 	err error
+
+	nextCounter int
 }
 
-func (m mockScanner) Scan(dst ...interface{}) error {
+func NewMockScanner(t *testing.T, val string, err error) pgx.Rows {
+	return &mockScanner{
+		t:   t,
+		val: val,
+		err: err,
+	}
+}
+
+func (m *mockScanner) Close() {
+}
+
+func (m *mockScanner) Err() error {
+	if m.nextCounter > 0 {
+		return pgx.ErrNoRows
+	}
+	return nil
+}
+
+func (m *mockScanner) CommandTag() pgconn.CommandTag {
+	return nil
+}
+
+func (m *mockScanner) FieldDescriptions() []pgproto3.FieldDescription {
+	return nil
+}
+
+func (m *mockScanner) Next() bool {
+	m.nextCounter++
+	return m.nextCounter == 1
+}
+
+func (m *mockScanner) Values() ([]interface{}, error) {
+	return nil, nil
+}
+
+func (m *mockScanner) RawValues() [][]byte {
+	return nil
+}
+
+func (m *mockScanner) Scan(dst ...interface{}) error {
 	if len(dst) != 1 {
 		m.t.Fatalf("called with %d args, want exactly one", len(dst))
 	}
@@ -36,6 +80,11 @@ func (m mockScanner) Scan(dst ...interface{}) error {
 	return m.err
 }
 
+var (
+	_ pgxscan.Querier = (*mockConn)(nil)
+	_ pgx.Rows        = (*mockScanner)(nil)
+)
+
 func Test_doValidatePostgresVersion(t *testing.T) {
 	tests := []struct {
 		name       string
@@ -45,37 +94,37 @@ func Test_doValidatePostgresVersion(t *testing.T) {
 	}{
 		{
 			"scan error",
-			mockConn{row: mockScanner{err: errors.New("scan")}},
+			mockConn{rows: NewMockScanner(t, "", errors.New("scan"))},
 			"10.0",
 			errors.New("error getting PostgreSQL version: scan"),
 		},
 		{
 			"strange version output",
-			mockConn{row: mockScanner{t, "MSSQL", nil}},
+			mockConn{rows: NewMockScanner(t, "MSSQL", nil)},
 			"10.0",
 			errors.New("error getting PostgreSQL version: failed to parse version: MSSQL"),
 		},
 		{
 			"unparsable version",
-			mockConn{row: mockScanner{t, "PostgreSQL 10.a.1", nil}},
+			mockConn{rows: NewMockScanner(t, "PostgreSQL 10.a.1", nil)},
 			"10.0",
 			errors.New("error getting PostgreSQL version: Malformed version: 10.a.1"),
 		},
 		{
 			"lower than needed",
-			mockConn{row: mockScanner{t, "PostgreSQL 9.5 blah blah", nil}},
+			mockConn{rows: NewMockScanner(t, "PostgreSQL 9.5 blah blah", nil)},
 			"10.0",
 			errors.New("unsupported PostgreSQL version: 9.5.0. (should be >= 10.0.0)"),
 		},
 		{
 			"equal",
-			mockConn{row: mockScanner{t, "PostgreSQL 10.0 blah blah", nil}},
+			mockConn{rows: NewMockScanner(t, "PostgreSQL 10.0 blah blah", nil)},
 			"10.0",
 			nil,
 		},
 		{
 			"greater than needed",
-			mockConn{row: mockScanner{t, "PostgreSQL 12.5 blah blah", nil}},
+			mockConn{rows: NewMockScanner(t, "PostgreSQL 12.5 blah blah", nil)},
 			"10.0",
 			nil,
 		},
