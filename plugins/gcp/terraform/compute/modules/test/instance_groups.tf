dabc8cd167bbd2d108baf139344213ee721ce264
Andrii Romanenko <andrejjrom@gmail.com>
2022-05-09 12:24:17 +0300

    chore: Fixed e2e tf (#242)

--- plugins/gcp/terraform/compute/modules/test/instance_groups.tf
+++ plugins/gcp/terraform/compute/modules/test/instance_groups.tf
@@ -1,6 +1,9 @@
 resource "google_compute_instance_group" "test" {
   name        = "${var.prefix}-instance-group"
   description = "Integration test instance group"
-  zone        = "${var.region}-a"
+  zone        = data.google_compute_zones.available.names[0]
   network     = module.vpc.network_id
+}
+
+data "google_compute_zones" "available" {
 }
