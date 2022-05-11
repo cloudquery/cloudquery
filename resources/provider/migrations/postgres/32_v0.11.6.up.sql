0009d0a55ac3d12bb4294ccfa3261c90d51e4bf0
Timur Irmatov <irmatov@gmail.com>
2022-05-11 12:01:41 +0500

    fix: Add ON DELETE CASCADE to redshift snapshots. (#880)

---
+++ resources/provider/migrations/postgres/32_v0.11.6.up.sql
@@ -0,0 +1,7 @@
+-- aws_redshift_snapshots
+ALTER TABLE IF EXISTS aws_redshift_snapshots DROP CONSTRAINT aws_redshift_snapshots_cluster_cq_id_fkey;
+ALTER TABLE IF EXISTS aws_redshift_snapshots
+    ADD CONSTRAINT aws_redshift_snapshots_cluster_cq_id_fkey
+    FOREIGN KEY (cluster_cq_id)
+    REFERENCES aws_redshift_clusters(cq_id)
+    ON DELETE CASCADE;
