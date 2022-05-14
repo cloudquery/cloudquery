5254f34f30f96b27d82e627a2be6c302bcb174af
Yevgeny Pats <yev.pats@gmail.com>
2022-05-14 12:55:12 +0300

    fix: Remove lambda support (#710)
    Lambda is limited by 15min and thus is not a good fit for CQ.
    
    Currently it is advised to deploy CQ on k8s via helm
    
    https://github.com/cloudquery/helm-charts
    https://github.com/cloudquery/terraform-aws-cloudquery

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"os"
-
-	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/cloudquery/cloudquery/cmd"
-	"github.com/cloudquery/cloudquery/deploy"
 )
 
 func main() {
-	if env := os.Getenv("AWS_LAMBDA_FUNCTION_NAME"); env != "" {
-		lambda.Start(deploy.LambdaHandler)
-		return
-	}
 	cmd.Execute()
 }
