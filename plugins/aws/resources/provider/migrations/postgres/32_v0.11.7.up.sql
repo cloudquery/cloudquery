1376b926e2ae3f30a689e86cf045f6f70a649f0c
shimonp21 <37939765+shimonp21@users.noreply.github.com>
2022-05-17 16:37:53 +0300

    fix: Merge migrations (#913)

--- plugins/aws/resources/provider/migrations/postgres/32_v0.11.7.up.sql
+++
@@ -1,2 +0,0 @@
--- Resource: aws.regions
-ALTER TABLE IF EXISTS "aws_regions" ADD COLUMN IF NOT EXISTS "partition" text;
--- plugins/aws/resources/provider/migrations/timescale/32_v0.11.7.down.sql
+++
@@ -1,2 +0,0 @@
--- Resource: aws.regions
-ALTER TABLE IF EXISTS "aws_regions" DROP COLUMN IF EXISTS "partition";
