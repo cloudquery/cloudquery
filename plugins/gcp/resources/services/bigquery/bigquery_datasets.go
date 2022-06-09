1961ccebe69632112911e5f97b8e1ab0bb8580cc
Andrii Romanenko <andrejjrom@gmail.com>
2022-06-09 17:12:18 +0300

    fix: Fixed error detail when bigquery is disabled (#323)
    * fix: Fixed error detail when bigquery is disabled
    
    * fix

--- plugins/gcp/resources/services/bigquery/bigquery_datasets.go
+++ plugins/gcp/resources/services/bigquery/bigquery_datasets.go
@@ -123,8 +123,8 @@ func fetchBigqueryDatasets(ctx context.Context, meta schema.ClientMeta, parent *
 		list, err := c.RetryingDo(ctx, call)
 		if err != nil {
 			if isAccessErrorToIgnore(err, c.ProjectId) {
-				return diag.FromError(err, diag.USER, diag.WithType(diag.ACCESS),
-					diag.WithDetails("Please verify the project id was configured correctly or BigQuery API is enabled in current project. Project names can't be used when fetching BigQuery resources"))
+				return diag.FromError(err, diag.RESOLVING, diag.WithSeverity(diag.IGNORE),
+					diag.WithDetails("Please verify the BigQuery API is enabled in current project."))
 			}
 			return diag.WrapError(err)
 		}
