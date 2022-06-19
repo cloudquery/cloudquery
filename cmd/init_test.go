5d07d9c646cfe4c31e94f658ae2df274ed719d32
erezrokah <erezrokah@users.noreply.github.com>
2022-06-19 13:04:47 +0300

    test: Add missing test

--- cmd/init_test.go
+++ cmd/init_test.go
@@ -7,6 +7,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func Test_Init(t *testing.T) {
+	testCases := []CommandTestCases{
+		{
+			Name:           "init-no-args",
+			Command:        "init",
+			ExpectError:    true,
+			ExpectedOutput: "Error: requires at least 1 arg(s), only received 0",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			testCommand(t, tc)
+		})
+	}
+}
+
 func Test_parseProviderCLIArg(t *testing.T) {
 	type args struct {
 		providerCLIArg string
