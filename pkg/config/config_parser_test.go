546aee4d7fce1cece7743d34fab9c9c5318d8261
erezrokah <erezrokah@users.noreply.github.com>
2022-06-19 14:52:26 +0300

    test: Move test from parse_test.go to config_parser_test.go

--- pkg/config/config_parser_test.go
+++ pkg/config/config_parser_test.go
@@ -60,3 +60,82 @@ func TestHandleConnectionBlock(t *testing.T) {
 		})
 	}
 }
+
+func TestParser_LoadConfigFromSourceConnectionOptionality(t *testing.T) {
+	cases := []struct {
+		cfg           string
+		expectedDSN   string
+		expectedError bool
+	}{
+		{
+			`
+cloudquery {
+  connection {
+    dsn =  "postgres://postgres:pass@localhost:5432/postgres"
+  }
+}
+`,
+			"postgres://postgres:pass@localhost:5432/postgres",
+			false,
+		},
+		{
+			`
+cloudquery {
+  connection {
+    dsn =  "postgres://postgres:pass@localhost:5432/postgres"
+    database = "cq"
+  }
+}
+`,
+			"",
+			true,
+		},
+		{
+			`
+cloudquery {
+  connection {
+    username = "postgres"
+    password = "pass"
+    host = "localhost"
+    port = 15432
+    database = "cq"
+    sslmode = "disable"
+  }
+}
+`,
+			"postgres://postgres:pass@localhost:15432/cq?sslmode=disable",
+			false,
+		},
+		{
+			`
+cloudquery {
+  connection {
+    username = "postgres"
+    password = "pass"
+    type = "postgres"
+    host = "localhost"
+    port = 15432
+    database = "cq"
+    sslmode = "disable"
+	extras = [ "search_path=myschema" ]
+  }
+}
+`,
+			"postgres://postgres:pass@localhost:15432/cq?search_path=myschema&sslmode=disable",
+			false,
+		},
+	}
+	for i := range cases {
+		tc := cases[i]
+		t.Run("case #"+strconv.Itoa(i+1), func(t *testing.T) {
+			p := NewParser()
+			parsedCfg, diags := p.LoadConfigFromSource("test.hcl", []byte(tc.cfg))
+			if tc.expectedError {
+				assert.True(t, diags.HasErrors())
+			} else {
+				assert.Len(t, diags.Errs(), 0)
+				assert.Equal(t, tc.expectedDSN, parsedCfg.CloudQuery.Connection.DSN)
+			}
+		})
+	}
+}
