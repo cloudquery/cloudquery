df3d07872032c320b1bcf28b71692451e8c2a37a
erezrokah <erezrokah@users.noreply.github.com>
2022-06-20 17:55:05 +0300

    fix: Don't print duplicate errors

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/cloudquery/cloudquery/cmd"
@@ -9,7 +8,6 @@ import (
 
 func main() {
 	if err := cmd.Execute(); err != nil {
-		log.Println(err)
 		os.Exit(1)
 	}
 }
