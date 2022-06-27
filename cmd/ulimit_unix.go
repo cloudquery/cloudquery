ec3d4dd6d6b2d7c7b6de97e1ad05ba587ec483a9
shimonp21 <shimonp21@gmail.com>
2022-06-27 14:10:44 +0200

    fix: Don't decrease ulimit. Change ulimit logs from 'debug' to 'info'
    1. We should never decrease ulimit, if it's big enough.
    2. The logs can definitely be 'info', since this is only logged once per 'cloudquery' invocation. Will also assist in debugging when we get customer logs.

--- cmd/ulimit_unix.go
+++ cmd/ulimit_unix.go
@@ -26,14 +26,21 @@ func setUlimit(ulimit uint64) error {
 	logger := zerolog.Logger
 	rLimit, err := GetUlimit()
 	if err != nil {
-		logger.Err(fmt.Errorf("error getting ulimit: %w", err))
+		return fmt.Errorf("error getting ulimit: %w", err)
 	}
+
+	logger.Info().Uint64("hard_ulimit", rLimit.Max).Uint64("soft_ulimit", rLimit.Cur).Msg("limits (before adjustment)")
+
 	if rLimit.Max < ulimit {
-		logger.Debug().Uint64("previous_ulimit", rLimit.Max).Uint64("new_ulimit", ulimit).Msg("adjusting max ulimit")
+		logger.Info().Uint64("previous_ulimit", rLimit.Max).Uint64("new_ulimit", ulimit).Msg("adjusting max ulimit")
 		rLimit.Max = ulimit
 	}
-	logger.Debug().Uint64("previous_ulimit", rLimit.Cur).Uint64("new_ulimit", ulimit).Msg("adjusting current ulimit")
-	rLimit.Cur = ulimit
+
+	if rLimit.Cur < ulimit {
+		logger.Info().Uint64("previous_ulimit", rLimit.Cur).Uint64("new_ulimit", ulimit).Msg("adjusting current ulimit")
+		rLimit.Cur = ulimit
+	}
+
 	return syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit)
 }
 
