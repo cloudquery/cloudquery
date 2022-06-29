f7ae08a4803adc69a11e26eddd8bfe5db5648f30
Yevgeny Pats <yev.pats@gmail.com>
2022-06-29 09:29:54 +0300

    feat: Generate metadata for CLI docs (#1034)
    * doc: Generate metadata for CLI docs
    
    * Add metadata to CLI docs

--- cmd/doc.go
+++ cmd/doc.go
@@ -1,6 +1,11 @@
 package cmd
 
 import (
+	"fmt"
+	"path"
+	"path/filepath"
+	"strings"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 )
@@ -13,8 +18,24 @@ var docCmd = &cobra.Command{
 	Args:   cobra.ExactValidArgs(1),
 	Hidden: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return doc.GenMarkdownTree(rootCmd, args[0])
+		return doc.GenMarkdownTreeCustom(rootCmd, args[0], filePrepender, linkHandler)
 	},
+}
+
+func linkHandler(s string) string { return s }
+
+func filePrepender(filename string) string {
+	const fmTemplate = `---
+id: "%s"
+hide_title: true
+sidebar_label: "%s"
+---
+`
+	name := filepath.Base(filename)
+	base := strings.TrimSuffix(name, path.Ext(name))
+	id := strings.ReplaceAll(base, "_", " ")
+	sidebarLabel := strings.TrimPrefix(id, "cloudquery ")
+	return fmt.Sprintf(fmTemplate, id, sidebarLabel)
 }
 
 func init() {
