52884b685afebcbf9885880ce481475029d66ab6
erezrokah <erezrokah@users.noreply.github.com>
2022-06-30 13:56:52 +0300

    test: Make it fail

--- cmd/ulimit_unix_test.go
+++ cmd/ulimit_unix_test.go
@@ -9,27 +9,11 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_setUlimit(t *testing.T) {
-	var initialLimit syscall.Rlimit
-	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &initialLimit)
+func Test_setUlimit_when_low(t *testing.T) {
+	// set to low values
+	err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &syscall.Rlimit{Cur: 10, Max: 20})
 	require.NoError(t, err)
 
 	err = setUlimit(ulimitUnix)
 	require.NoError(t, err)
-
-	var actualLimit syscall.Rlimit
-	err = syscall.Getrlimit(syscall.RLIMIT_NOFILE, &actualLimit)
-	require.NoError(t, err)
-
-	if initialLimit.Max < ulimitUnix {
-		require.Equal(t, ulimitUnix, actualLimit.Max)
-	} else {
-		require.Equal(t, initialLimit.Max, actualLimit.Max)
-	}
-
-	if initialLimit.Cur < ulimitUnix {
-		require.Equal(t, ulimitUnix, actualLimit.Cur)
-	} else {
-		require.Equal(t, initialLimit.Cur, actualLimit.Cur)
-	}
 }
