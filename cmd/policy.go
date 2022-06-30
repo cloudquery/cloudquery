2235e826c956ea1873d18f3d4155f6e08b5edfc2
shimonp21 <37939765+shimonp21@users.noreply.github.com>
2022-06-30 12:42:02 +0200

    fix: Update 'policy run' help message (#1037)
    * fix: Update 'policy run' help message
    
    * Update cmd/policy.go
    
    Co-authored-by: Erez Rokah <erezrokah@users.noreply.github.com>
    
    * Update cmd/policy.go
    
    Co-authored-by: Erez Rokah <erezrokah@users.noreply.github.com>
    
    * Update cmd/policy.go
    
    Co-authored-by: Erez Rokah <erezrokah@users.noreply.github.com>
    
    * Update cmd/policy.go
    
    Co-authored-by: Erez Rokah <erezrokah@users.noreply.github.com>
    
    * Update cmd/policy.go
    
    Co-authored-by: Erez Rokah <erezrokah@users.noreply.github.com>
    
    * Update cmd/policy.go
    
    Co-authored-by: Erez Rokah <erezrokah@users.noreply.github.com>
    
    * Update cmd/policy.go
    
    Co-authored-by: Erez Rokah <erezrokah@users.noreply.github.com>
    
    * Update cmd/policy.go
    
    Co-authored-by: Erez Rokah <erezrokah@users.noreply.github.com>
    
    Co-authored-by: Erez Rokah <erezrokah@users.noreply.github.com>

--- cmd/policy.go
+++ cmd/policy.go
@@ -63,25 +63,25 @@ var (
 	noResults    bool
 	storeResults bool
 	policyRunCmd = &cobra.Command{
-		Use:   "run",
+		Use:   "run <policy>",
 		Short: policyRunHelpMsg,
 		Long:  policyRunHelpMsg,
 		Example: `
-  # Download & Run the policies defined in your config
-  cloudquery policy run
+	# Run an official policy
+	# Official policies are available on our hub: https://hub.cloudquery.io/policies
+	cloudquery policy run aws
 
-  # Run a specific policy that is not defined in the config.hcl
-  # Run official policy
-  cloudquery policy run aws
+	# Run a sub-policy of an official policy
+	cloudquery policy run aws//cis_v1.2.0
 
-  # The following will be the same as above
-  # Official policies are hosted here: https://github.com/cloudquery-policies
-  cloudquery policy run aws//cis_v1.2.0
-	
-  # Run community policy
-  cloudquery policy run github.com/COMMUNITY_GITHUB_ORG/aws
+	# Run a policy from a GitHub repository
+	cloudquery policy run github.com/<repo-owner>/<repo-name>
 
-  # See https://hub.cloudquery.io for additional policies.`,
+	# Run a policy from a local directory
+	cloudquery policy run ./<path-to-local-directory>
+
+	# See https://hub.cloudquery.io for additional policies
+	# See https://docs.cloudquery.io/docs/tutorials/policies/policies-overview for instructions on writing policies`,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			source := args[0]
 			c, err := console.CreateClient(cmd.Context(), getConfigFile(), false, nil, instanceId)
