bb451a7bcf137bb0cfe36dee5afbfaf1a7a018c5
Herman Schaaf <hermanschaaf@gmail.com>
2022-07-04 08:39:08 +0100

    fix(tests): Update Lightsail terraform for Integration testing of Instances (#1137)

--- plugins/aws/terraform/lightsail/modules/test/instances.tf
+++ plugins/aws/terraform/lightsail/modules/test/instances.tf
@@ -1,10 +1,14 @@
-# Create a new GitLab Lightsail Instance
+# Create a new Lightsail Instance
 resource "aws_lightsail_instance" "aws_lightsail_instance" {
-  name              = "${var.prefix}-lightsaleinstance"
+  name              = "${var.prefix}-lightsailinstance"
   availability_zone = "us-east-1b"
   blueprint_id      = "amazon_linux_2"
   bundle_id         = "nano_2_0"
   key_pair_name     = aws_lightsail_key_pair.aws_lightsail_key_pair.name
+  tags              = {
+    foo1 = "bar1"
+    foo2 = ""
+  }
 }
 
 resource "aws_lightsail_static_ip_attachment" "test" {
@@ -12,8 +16,13 @@ resource "aws_lightsail_static_ip_attachment" "test" {
   instance_name  = aws_lightsail_instance.aws_lightsail_instance.id
 }
 
-
 resource "awslightsail_lb_attachment" "test" {
   load_balancer_name = awslightsail_lb.awslightsail_lb.name
   instance_name      = aws_lightsail_instance.aws_lightsail_instance.name
+}
+
+resource "awslightsail_disk_attachment" "test" {
+  disk_name     = awslightsail_disk.awslightsail_disk.name
+  instance_name = aws_lightsail_instance.aws_lightsail_instance.name
+  disk_path     = "/dev/xvdf"
 }
--- plugins/aws/terraform/lightsail/modules/test/lb.tf
+++ plugins/aws/terraform/lightsail/modules/test/lb.tf
@@ -1,5 +1,5 @@
 resource "awslightsail_lb" "awslightsail_lb" {
-  name              = "${var.prefix}_load_ballancer"
+  name              = "${var.prefix}_load_balancer"
   health_check_path = "/"
   instance_port     = "80"
 }
