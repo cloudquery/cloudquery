1a41bea9394436a953bf9d08185f0cddb716cada
candiduslynx <candiduslynx@users.noreply.github.com>
2022-07-13 10:32:19 +0300

    test(terraform): Add Front Door CDN Terraform resource (#413)
    * add tf part for Front Door CDN

---
+++ plugins/azure/terraform/frondoor/modules/test/frontdoors.tf
@@ -0,0 +1,50 @@
+resource "azurerm_resource_group" "frontdoor" {
+  name     = "${var.prefix}-frontdoor"
+  location = "East US"
+}
+
+resource "azurerm_frontdoor" "test" {
+  name                = "${var.prefix}-frontdoor"
+  tags                = var.tags
+  location            = azurerm_resource_group.frontdoor.location
+  resource_group_name = azurerm_resource_group.frontdoor.name
+
+  enforce_backend_pools_certificate_name_check = false
+
+  routing_rule {
+    name               = "exampleRoutingRule1"
+    accepted_protocols = ["Http", "Https"]
+    patterns_to_match  = ["/*"]
+    frontend_endpoints = ["exampleFrontendEndpoint1"]
+    forwarding_configuration {
+      forwarding_protocol = "MatchRequest"
+      backend_pool_name   = "exampleBackendBing"
+    }
+  }
+
+  backend_pool_load_balancing {
+    name = "exampleLoadBalancingSettings1"
+  }
+
+  backend_pool_health_probe {
+    name = "exampleHealthProbeSetting1"
+  }
+
+  backend_pool {
+    name = "exampleBackendBing"
+    backend {
+      host_header = "www.bing.com"
+      address     = "www.bing.com"
+      http_port   = 80
+      https_port  = 443
+    }
+
+    load_balancing_name = "exampleLoadBalancingSettings1"
+    health_probe_name   = "exampleHealthProbeSetting1"
+  }
+
+  frontend_endpoint {
+    name      = "exampleFrontendEndpoint1"
+    host_name = "example-FrontDoor.azurefd.net"
+  }
+}
--- plugins/azure/terraform/frondoor/prod/terraform.tf
+++ plugins/azure/terraform/frondoor/prod/terraform.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "default"
     storage_account_name = "cqproviderazureterraform"
     container_name       = "tfstate"
-    key                  = "cq_frontdoor.terraform.tfstate"
+    key                  = "cq_keyvault.terraform.tfstate"
   }
 }
