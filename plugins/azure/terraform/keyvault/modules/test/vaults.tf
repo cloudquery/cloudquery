7e2b11b7f87c56671fd98e13f126cb6d00714159
Andrii Romanenko <andrejjrom@gmail.com>
2022-07-19 15:29:45 +0100

    chore: Keyvault e2e fix (#420)
    * chore: Keyvault e2e fix
    
    * fix
    
    * fix
    
    * fix

--- plugins/azure/terraform/keyvault/modules/test/vaults.tf
+++ plugins/azure/terraform/keyvault/modules/test/vaults.tf
@@ -3,13 +3,13 @@ resource "azurerm_resource_group" "keyvault" {
   location = "East US"
 }
 
-resource "azurerm_key_vault" "test" {
+resource "azurerm_key_vault" "azurerm_key_vault" {
   name                        = "${var.prefix}vault"
   location                    = azurerm_resource_group.keyvault.location
   resource_group_name         = azurerm_resource_group.keyvault.name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = "standard"
-  soft_delete_retention_days  = 7
+  soft_delete_retention_days  = 8
   enabled_for_disk_encryption = true
   purge_protection_enabled    = false
   enabled_for_template_deployment = true
@@ -21,7 +21,7 @@ resource "azurerm_key_vault" "test" {
 
 resource "azurerm_key_vault_key" "generated" {
   name         = "${var.prefix}key"
-  key_vault_id = azurerm_key_vault.test.id
+  key_vault_id = azurerm_key_vault.azurerm_key_vault.id
   key_type     = "RSA"
   key_size     = 2048
 
