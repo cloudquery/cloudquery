bb4e4e7a9a79b84daf96628df1c160469e00448e
Yevgeny Pats <yev.pats@gmail.com>
2022-07-22 14:08:26 +0300

    fix: Default execution_time in policies (#239)

--- plugins/k8s/policies/nsa_cisa_v1/policy.sql
+++ plugins/k8s/policies/nsa_cisa_v1/policy.sql
@@ -1,5 +1,4 @@
 \set ON_ERROR_STOP on
-
 SET TIME ZONE 'UTC';
 -- neat trick to set execution_time if not already set
 -- https://stackoverflow.com/questions/32582600/only-set-variable-in-psql-script-if-not-specified-on-the-command-line
--- plugins/k8s/policies/policy.sql
+++ plugins/k8s/policies/policy.sql
@@ -1,12 +1,10 @@
 \set ON_ERROR_STOP on
-
 SET TIME ZONE 'UTC';
--- trick to set execution_time if not already set
+-- neat trick to set execution_time if not already set
 -- https://stackoverflow.com/questions/32582600/only-set-variable-in-psql-script-if-not-specified-on-the-command-line
-\set execution_time '2022-07-20 16:16:30.298982+00'
+\set execution_time :execution_time
 SELECT CASE 
-  WHEN :'execution_time' = '2022-07-20 16:16:30.298982+00'
-  THEN now()
+  WHEN :'execution_time' = ':execution_time' THEN to_char(now(), 'YYYY-MM-dd HH24:MI:SS.US')
   ELSE :'execution_time'
 END AS "execution_time"  \gset
 
