c2aa375cacb236fc376f8b804bcdf1e42682e9c5
Andrii Romanenko <andrejjrom@gmail.com>
2022-07-29 04:53:57 -0700

    chore: Rollback terraform and infracost action (#1343)

--- plugins/aws/terraform/glue/modules/test/iam.tf
+++ plugins/aws/terraform/glue/modules/test/iam.tf
@@ -1,8 +1,3 @@
-resource "aws_glue_dev_endpoint" "aws_glue_dev_endpoint" {
-  name     = "${var.prefix}-dev-endpoint"
-  role_arn = aws_iam_role.aws_iam_role.arn
-}
-
 resource "aws_iam_role" "aws_iam_role" {
   name               = "AWSGlueServiceRole-${var.prefix}-role"
   assume_role_policy = data.aws_iam_policy_document.aws_iam_policy_document.json
