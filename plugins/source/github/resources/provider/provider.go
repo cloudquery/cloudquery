cad4e7764cc538ddfa2264512db0a408c72499af
Herman Schaaf <hermanschaaf@gmail.com>
2022-08-09 13:09:13 +0100

    chore(build): Add support for drift detection of generated code (#22)
    * chore(build): Add support for drift detection of generated code
    
    This adds a new workflow that checks that generated code remains the same when go generate is run again. It currently needs to be specifically enabled by adding //check-for-changes to the top of the generator .hcl file.
    
    It also adds drift detection to all existing files and fixes any drift that had already occurred. To get the script to work also involved bringing the directory structure in line with other providers.

--- plugins/source/github/resources/provider/provider.go
+++ plugins/source/github/resources/provider/provider.go
@@ -2,7 +2,14 @@ package provider
 
 import (
 	"github.com/cloudquery/cloudquery/plugins/source/github/client"
-	"github.com/cloudquery/cloudquery/plugins/source/github/resources"
+	"github.com/cloudquery/cloudquery/plugins/source/github/resources/services/billing"
+	"github.com/cloudquery/cloudquery/plugins/source/github/resources/services/external_groups"
+	"github.com/cloudquery/cloudquery/plugins/source/github/resources/services/hooks"
+	"github.com/cloudquery/cloudquery/plugins/source/github/resources/services/installations"
+	"github.com/cloudquery/cloudquery/plugins/source/github/resources/services/issues"
+	"github.com/cloudquery/cloudquery/plugins/source/github/resources/services/organizations"
+	"github.com/cloudquery/cloudquery/plugins/source/github/resources/services/repositories"
+	"github.com/cloudquery/cloudquery/plugins/source/github/resources/services/teams"
 	sdkprovider "github.com/cloudquery/cq-provider-sdk/provider"
 	"github.com/cloudquery/cq-provider-sdk/provider/schema"
 )
@@ -17,16 +24,16 @@ func Provider() *sdkprovider.Provider {
 		Name:      "github",
 		Configure: client.Configure,
 		ResourceMap: map[string]*schema.Table{
-			"billing.actions":  resources.ActionBillings(),
-			"billing.storage":  resources.StorageBillings(),
-			"billing.packages": resources.PackageBillings(),
-			"issues":           resources.Issues(),
-			"hooks":            resources.Hooks(),
-			"installations":    resources.Installations(),
-			"organizations":    resources.Organizations(),
-			"repositories":     resources.Repositories(),
-			"teams":            resources.Teams(),
-			"external_groups":  resources.ExternalGroups(),
+			"billing.actions":  billing.ActionBillings(),
+			"billing.storage":  billing.StorageBillings(),
+			"billing.packages": billing.PackageBillings(),
+			"issues":           issues.Issues(),
+			"hooks":            hooks.Hooks(),
+			"installations":    installations.Installations(),
+			"organizations":    organizations.Organizations(),
+			"repositories":     repositories.Repositories(),
+			"teams":            teams.Teams(),
+			"external_groups":  external_groups.ExternalGroups(),
 		},
 		Config: func() sdkprovider.Config {
 			return &client.Config{}
---
+++ plugins/source/github/resources/services/organizations/organizations_test.go
@@ -0,0 +1,41 @@
+package organizations
+
+import (
+	"testing"
+
+	"github.com/cloudquery/cloudquery/plugins/source/github/client"
+	"github.com/cloudquery/cloudquery/plugins/source/github/client/mocks"
+	"github.com/cloudquery/faker/v3"
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-github/v45/github"
+)
+
+func buildOrganizations(t *testing.T, ctrl *gomock.Controller) client.GithubServices {
+	mock := mocks.NewMockOrganizationsService(ctrl)
+
+	var cs *github.Organization
+	if err := faker.FakeData(&cs); err != nil {
+		t.Fatal(err)
+	}
+	mock.EXPECT().Get(gomock.Any(), gomock.Any()).Return(cs, &github.Response{}, nil)
+
+	var u github.User
+	if err := faker.FakeDataSkipFields(&u, []string{"Parent"}); err != nil {
+		t.Fatal(err)
+	}
+	mock.EXPECT().ListMembers(gomock.Any(), gomock.Any(), gomock.Any()).Return(
+		[]*github.User{&u}, &github.Response{}, nil)
+
+	var m github.Membership
+	if err := faker.FakeDataSkipFields(&m, []string{}); err != nil {
+		t.Fatal(err)
+	}
+	mock.EXPECT().GetOrgMembership(gomock.Any(), *u.Login, gomock.Any()).Return(
+		&m, &github.Response{}, nil)
+
+	return client.GithubServices{Organizations: mock}
+}
+
+func TestOrganizations(t *testing.T) {
+	client.GithubMockTestHelper(t, Organizations(), buildOrganizations, client.TestOptions{})
+}
