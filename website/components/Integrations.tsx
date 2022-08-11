f361ee8f3c3b98b17dedf425b88a0e7234e6f500
Nikita Kabardin <nikita@kabardin.com>
2022-08-11 21:29:51 +0200

    feat: Add integration logos (#1181)
    * Add integration logos
    
    * Correct spacing for integrations
    
    * Align integration header to center on mobile too

---
+++ website/components/Integrations.tsx
@@ -0,0 +1,25 @@
+import AWSLogo from "./logos/integrations/AWSLogo";
+import AzureLogo from "./logos/integrations/AzureLogo";
+import CloudflareLogo from "./logos/integrations/CloudflareLogo";
+import DigitalOceanLogo from "./logos/integrations/DigitalOceanLogo";
+import GCPLogo from "./logos/integrations/GCPLogo";
+import KubernetesLogo from "./logos/integrations/KubernetesLogo";
+import OktaLogo from "./logos/integrations/OktaLogo";
+import TerraformLogo from "./logos/integrations/TerraformLogo";
+import YandexCloudLogo from "./logos/integrations/YandexCloudLogo";
+
+export function Integrations() {
+    return (
+        <div className="flex justify-center items-center flex-wrap gap-8 mt-8 sm:mt-4">
+            <AWSLogo />
+            <AzureLogo />
+            <CloudflareLogo />
+            <DigitalOceanLogo />
+            <GCPLogo />
+            <KubernetesLogo />
+            <OktaLogo />
+            <TerraformLogo />
+            <YandexCloudLogo />
+        </div>
+    )
+}
