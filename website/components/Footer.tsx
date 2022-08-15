3ae9f3504aebed4a5285c3351aab94bd6b3b45e7
Ilya Kuznetsov <ilya@cloudquery.io>
2022-08-15 14:41:05 +0400

    feat: Subscribe

--- website/components/Footer.tsx
+++ website/components/Footer.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
-import { useState, ReactNode } from "react";
+import { useState, ReactNode, FormEvent } from "react";
 import CloudQueryLogo from "./logos/CloudQuery";
 
 function FooterLink({ href, children }: { href: string; children: ReactNode }) {
@@ -29,7 +29,10 @@ const navigation = {
     { name: "Documentation", href: "/docs" },
     { name: "CLI reference", href: "/docs/cli-reference" },
     { name: "Blog", href: "/blog" },
-    { name: "Releases", href: "https://github.com/cloudquery/cloudquery/releases" },
+    {
+      name: "Releases",
+      href: "https://github.com/cloudquery/cloudquery/releases",
+    },
     { name: "FAQ", href: "/docs/faq" },
   ],
   support: [
@@ -110,8 +113,8 @@ export function Footer() {
           <div className="mt-12 xl:!mt-0">
             <FooterHeader>Subscribe to our newsletter</FooterHeader>
             <p className="mt-4 text-sm text-gray-600 dark:text-gray-500">
-              Join the CloudQuery newsletter and stay updated on new releases and
-              features, guides, and case studies.
+              Join the CloudQuery newsletter and stay updated on new releases
+              and features, guides, and case studies.
             </p>
             <SubmitForm />
           </div>
@@ -141,40 +144,48 @@ export function Footer() {
 function SubmitForm() {
   const [email, setEmail] = useState("");
   const router = useRouter();
+
+  const [message, setMessage] = useState("");
+
+  const subscribe = async (e: FormEvent) => {
+    e.preventDefault();
+    // 3. Send a request to our API with the user's email address.
+    const res = await fetch(`/api/subscribe?email=${encodeURI(email)}`);
+
+    const { error } = await res.json();
+
+    if (error) {
+      // 4. If there was an error, update the message in state.
+      setMessage(error);
+
+      return;
+    }
+
+    router.push("/confirm");
+  };
+
   return (
-    <form
-      className="mt-4 sm:flex sm:max-w-md"
-      onSubmit={(e) => {
-        fetch("/api/signup", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email }),
-        })
-          .then((res) => res.json())
-          .then((res) => {
-            return router.push("/confirm");
-          });
-        e.preventDefault();
-      }}
-    >
+    <form className="mt-4 sm:flex sm:max-w-md" onSubmit={subscribe}>
       <label htmlFor="email-address" className="sr-only">
         Email address
       </label>
-      <input
-        type="email"
-        name="email-address"
-        id="email-address"
-        autoComplete="email"
-        required
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        className="w-full min-w-0 px-4 py-2 text-base text-gray-900 placeholder-gray-500 bg-white border border-transparent rounded-md appearance-none dark:text-white sm:text-sm dark:border-gray-700 dark:bg-transparent focus:outline-none focus:ring-2 focus:ring-gray-800 dark:focus:border-white focus:placeholder-gray-400"
-        placeholder="you@domain.com"
-      />
+      <div>
+        <input
+          type="email"
+          name="email-address"
+          id="email-address"
+          autoComplete="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full min-w-0 px-4 py-2 text-base text-gray-900 placeholder-gray-500 bg-white border border-transparent rounded-md appearance-none dark:text-white sm:text-sm dark:border-gray-700 dark:bg-transparent focus:outline-none focus:ring-2 focus:ring-gray-800 dark:focus:border-white focus:placeholder-gray-400"
+          placeholder="you@domain.com"
+        />
+        <div className="text-sm text-red-500 mt-4">{message}</div>
+      </div>
       <div className="mt-3 rounded-md sm:mt-0 sm:ml-3 sm:flex-shrink-0">
         <button
+          data-bs-target="tooltip"
           type="submit"
           className="flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white bg-black dark:bg-white dark:text-black border border-transparent rounded-md sm:text-sm betterhover:hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:focus:ring-white dark:betterhover:hover:bg-gray-300"
         >
