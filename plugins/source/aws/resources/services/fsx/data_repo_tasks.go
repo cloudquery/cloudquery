fcd4866ad6795a8b636b550fb027dd368f448930
Timur Irmatov <irmatov@gmail.com>
2022-08-15 13:09:02 +0500

    feat: Add fsx data repo tasks

---
+++ plugins/source/aws/resources/services/fsx/data_repo_tasks.go
@@ -0,0 +1,170 @@
+package fsx
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/fsx"
+	"github.com/aws/aws-sdk-go-v2/service/fsx/types"
+	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
+	"github.com/cloudquery/cq-provider-sdk/provider/diag"
+	"github.com/cloudquery/cq-provider-sdk/provider/schema"
+)
+
+//go:generate cq-gen --resource data_repo_tasks --config data_repo_tasks.hcl --output .
+func DataRepoTasks() *schema.Table {
+	return &schema.Table{
+		Name:         "aws_fsx_data_repo_tasks",
+		Description:  "A description of the data repository task",
+		Resolver:     fetchFsxDataRepoTasks,
+		Multiplex:    client.ServiceAccountRegionMultiplexer("glue"),
+		IgnoreError:  client.IgnoreAccessDeniedServiceDisabled,
+		DeleteFilter: client.DeleteAccountRegionFilter,
+		Options:      schema.TableCreationOptions{PrimaryKeys: []string{"arn"}},
+		Columns: []schema.Column{
+			{
+				Name:        "account_id",
+				Description: "The AWS Account ID of the resource.",
+				Type:        schema.TypeString,
+				Resolver:    client.ResolveAWSAccount,
+			},
+			{
+				Name:        "region",
+				Description: "The AWS Region of the resource.",
+				Type:        schema.TypeString,
+				Resolver:    client.ResolveAWSRegion,
+			},
+			{
+				Name:        "creation_time",
+				Description: "The time that the resource was created, in seconds (since 1970-01-01T00:00:00Z), also known as Unix time",
+				Type:        schema.TypeTimestamp,
+			},
+			{
+				Name:        "file_system_id",
+				Description: "The globally unique ID of the file system, assigned by Amazon FSx",
+				Type:        schema.TypeString,
+			},
+			{
+				Name:        "lifecycle",
+				Description: "The lifecycle status of the data repository task, as follows:  * PENDING - Amazon FSx has not started the task  * EXECUTING - Amazon FSx is processing the task  * FAILED - Amazon FSx was not able to complete the task",
+				Type:        schema.TypeString,
+			},
+			{
+				Name:        "task_id",
+				Description: "The system-generated, unique 17-digit ID of the data repository task",
+				Type:        schema.TypeString,
+			},
+			{
+				Name:        "type",
+				Description: "The type of data repository task  * The EXPORT_TO_REPOSITORY data repository task exports from your Lustre file system from to a linked S3 bucket  * The IMPORT_METADATA_FROM_REPOSITORY data repository task imports metadata changes from a linked S3 bucket to your Lustre file system",
+				Type:        schema.TypeString,
+			},
+			{
+				Name:        "end_time",
+				Description: "The time that Amazon FSx completed processing the task, populated after the task is complete",
+				Type:        schema.TypeTimestamp,
+			},
+			{
+				Name:        "failure_details_message",
+				Description: "A detailed error message",
+				Type:        schema.TypeString,
+				Resolver:    schema.PathResolver("FailureDetails.Message"),
+			},
+			{
+				Name:        "paths",
+				Description: "An array of paths on the Amazon FSx for Lustre file system that specify the data for the data repository task to process",
+				Type:        schema.TypeStringArray,
+			},
+			{
+				Name:        "report_enabled",
+				Description: "Set Enabled to True to generate a CompletionReport when the task completes",
+				Type:        schema.TypeBool,
+				Resolver:    schema.PathResolver("Report.Enabled"),
+			},
+			{
+				Name:        "report_format",
+				Description: "Required if Enabled is set to true",
+				Type:        schema.TypeString,
+				Resolver:    schema.PathResolver("Report.Format"),
+			},
+			{
+				Name:        "report_path",
+				Description: "Required if Enabled is set to true",
+				Type:        schema.TypeString,
+				Resolver:    schema.PathResolver("Report.Path"),
+			},
+			{
+				Name:        "report_scope",
+				Description: "Required if Enabled is set to true",
+				Type:        schema.TypeString,
+				Resolver:    schema.PathResolver("Report.Scope"),
+			},
+			{
+				Name:        "arn",
+				Description: "The Amazon Resource Name (ARN) for a given resource",
+				Type:        schema.TypeString,
+				Resolver:    schema.PathResolver("ResourceARN"),
+			},
+			{
+				Name:        "start_time",
+				Description: "The time that Amazon FSx began processing the task",
+				Type:        schema.TypeTimestamp,
+			},
+			{
+				Name:        "status_failed_count",
+				Description: "A running total of the number of files that the task failed to process",
+				Type:        schema.TypeBigInt,
+				Resolver:    schema.PathResolver("Status.FailedCount"),
+			},
+			{
+				Name:        "status_last_updated_time",
+				Description: "The time at which the task status was last updated",
+				Type:        schema.TypeTimestamp,
+				Resolver:    schema.PathResolver("Status.LastUpdatedTime"),
+			},
+			{
+				Name:        "status_succeeded_count",
+				Description: "A running total of the number of files that the task has successfully processed",
+				Type:        schema.TypeBigInt,
+				Resolver:    schema.PathResolver("Status.SucceededCount"),
+			},
+			{
+				Name:        "status_total_count",
+				Description: "The total number of files that the task will process",
+				Type:        schema.TypeBigInt,
+				Resolver:    schema.PathResolver("Status.TotalCount"),
+			},
+			{
+				Name:        "tags",
+				Description: "A list of Tag values, with a maximum of 50 elements",
+				Type:        schema.TypeJSON,
+				Resolver:    resolveDataRepoTasksTags,
+			},
+		},
+	}
+}
+
+// ====================================================================================================================
+//                                               Table Resolver Functions
+// ====================================================================================================================
+
+func fetchFsxDataRepoTasks(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- interface{}) error {
+	cl := meta.(*client.Client)
+	svc := cl.Services().FSX
+	input := fsx.DescribeDataRepositoryTasksInput{MaxResults: aws.Int32(1000)}
+	for {
+		result, err := svc.DescribeDataRepositoryTasks(ctx, &input)
+		if err != nil {
+			return diag.WrapError(err)
+		}
+		res <- result.DataRepositoryTasks
+		if aws.ToString(result.NextToken) == "" {
+			break
+		}
+		input.NextToken = result.NextToken
+	}
+	return nil
+}
+func resolveDataRepoTasksTags(ctx context.Context, meta schema.ClientMeta, resource *schema.Resource, c schema.Column) error {
+	return diag.WrapError(resource.Set(c.Name, client.TagsToMap(resource.Item.(types.DataRepositoryTask).Tags)))
+}
---
+++ plugins/source/aws/resources/services/fsx/data_repo_tasks.hcl
@@ -0,0 +1,48 @@
+//check-for-changes
+service          = "aws"
+output_directory = "."
+add_generate     = true
+
+description_modifier "remove_read_only" {
+  words = ["  This member is required."]
+}
+
+resource "aws" "fsx" "data_repo_tasks" {
+  path = "github.com/aws/aws-sdk-go-v2/service/fsx/types.DataRepositoryTask"
+  ignoreError "IgnoreAccessDenied" {
+    path = "github.com/cloudquery/cloudquery/plugins/source/aws/client.IgnoreAccessDeniedServiceDisabled"
+  }
+  deleteFilter "AccountRegionFilter" {
+    path = "github.com/cloudquery/cloudquery/plugins/source/aws/client.DeleteAccountRegionFilter"
+  }
+  multiplex "AwsAccountRegion" {
+    path   = "github.com/cloudquery/cloudquery/plugins/source/aws/client.ServiceAccountRegionMultiplexer"
+    params = ["glue"]
+  }
+  options {
+    primary_keys = ["arn"]
+  }
+  userDefinedColumn "account_id" {
+    description = "The AWS Account ID of the resource."
+    type        = "string"
+    resolver "resolveAWSAccount" {
+      path = "github.com/cloudquery/cloudquery/plugins/source/aws/client.ResolveAWSAccount"
+    }
+  }
+  userDefinedColumn "region" {
+    type        = "string"
+    description = "The AWS Region of the resource."
+    resolver "resolveAWSRegion" {
+      path = "github.com/cloudquery/cloudquery/plugins/source/aws/client.ResolveAWSRegion"
+    }
+  }
+
+  column "resource_arn" {
+    rename = "arn"
+  }
+
+  column "tags" {
+    type = "json"
+    generate_resolver = true
+  }
+}
