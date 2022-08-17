b531e3d394bbbc8937b46ed5ed77e19db79fba3c
bbernays <ben@bernays.me>
2022-08-17 16:50:40 -0500

    Update roles.go

--- plugins/source/aws/resources/services/iam/roles.go
+++ plugins/source/aws/resources/services/iam/roles.go
@@ -148,6 +148,10 @@ func Roles() *schema.Table {
 // ====================================================================================================================
 
 func fetchIamRoles(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- interface{}) error {
+	return diag.WrapError(client.ListAndDetailResolver(ctx, meta, res, listRoles, roleDetail))
+}
+
+func listRoles(ctx context.Context, meta schema.ClientMeta, detailChan chan<- interface{}) error {
 	var config iam.ListRolesInput
 	svc := meta.(*client.Client).Services().IAM
 	for {
@@ -155,7 +159,9 @@ func fetchIamRoles(ctx context.Context, meta schema.ClientMeta, parent *schema.R
 		if err != nil {
 			return diag.WrapError(err)
 		}
-		res <- response.Roles
+		for _, role := range response.Roles {
+			detailChan <- role
+		}
 		if aws.ToString(response.Marker) == "" {
 			break
 		}
@@ -163,8 +169,25 @@ func fetchIamRoles(ctx context.Context, meta schema.ClientMeta, parent *schema.R
 	}
 	return nil
 }
+
+func roleDetail(ctx context.Context, meta schema.ClientMeta, resultsChan chan<- interface{}, errorChan chan<- error, listInfo interface{}) {
+	c := meta.(*client.Client)
+	role := listInfo.(types.Role)
+	svc := meta.(*client.Client).Services().IAM
+	roleDetails, err := svc.GetRole(ctx, &iam.GetRoleInput{
+		RoleName: role.RoleName,
+	})
+	if err != nil {
+		if c.IsNotFoundError(err) {
+			return
+		}
+		errorChan <- diag.WrapError(err)
+		return
+	}
+	resultsChan <- roleDetails.Role
+}
 func resolveIamRolePolicies(ctx context.Context, meta schema.ClientMeta, resource *schema.Resource, c schema.Column) error {
-	r := resource.Item.(types.Role)
+	r := resource.Item.(*types.Role)
 	cl := meta.(*client.Client)
 	svc := cl.Services().IAM
 	input := iam.ListAttachedRolePoliciesInput{
@@ -190,7 +213,7 @@ func resolveIamRolePolicies(ctx context.Context, meta schema.ClientMeta, resourc
 	return diag.WrapError(resource.Set("policies", policies))
 }
 func resolveRolesAssumeRolePolicyDocument(ctx context.Context, meta schema.ClientMeta, resource *schema.Resource, c schema.Column) error {
-	r := resource.Item.(types.Role)
+	r := resource.Item.(*types.Role)
 	if r.AssumeRolePolicyDocument == nil {
 		return nil
 	}
@@ -201,7 +224,7 @@ func resolveRolesAssumeRolePolicyDocument(ctx context.Context, meta schema.Clien
 	return diag.WrapError(resource.Set("assume_role_policy_document", decodedDocument))
 }
 func resolveRolesTags(ctx context.Context, meta schema.ClientMeta, resource *schema.Resource, c schema.Column) error {
-	r := resource.Item.(types.Role)
+	r := resource.Item.(*types.Role)
 	cl := meta.(*client.Client)
 	svc := cl.Services().IAM
 	response, err := svc.ListRoleTags(ctx, &iam.ListRoleTagsInput{RoleName: r.RoleName})
@@ -221,7 +244,7 @@ func resolveRolesTags(ctx context.Context, meta schema.ClientMeta, resource *sch
 func fetchIamRolePolicies(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- interface{}) error {
 	c := meta.(*client.Client)
 	svc := c.Services().IAM
-	role := parent.Item.(types.Role)
+	role := parent.Item.(*types.Role)
 	config := iam.ListRolePoliciesInput{
 		RoleName: role.RoleName,
 	}
