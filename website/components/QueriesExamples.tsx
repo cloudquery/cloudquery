f9a75d0c8b8b25f3224559775962b941e3fc9177
Anton Kuptsov <39290991+anton-kuptsov@users.noreply.github.com>
2022-08-18 15:45:17 +0400

    chore: Format query examples on the landing page (#1489)

--- website/components/QueriesExamples.tsx
+++ website/components/QueriesExamples.tsx
@@ -2,45 +2,39 @@ import { DuplicateIcon } from "@heroicons/react/outline"
 
 const SecurityQuery = () => (
     <>
-        <div>
-            <span style={{ color: "var(--shiki-token-keyword)" }}>SELECT</span>
-            <span style={{ color: "var(--shiki-token-keyword)" }}> * </span>
-            <span style={{ color: "var(--shiki-token-keyword)" }}>FROM </span>aws_elbv2_load_balancers
-        </div>
-        <div>
-            <span style={{ color: "var(--shiki-token-keyword)" }}>WHERE </span>scheme
-            <span style={{ color: "var(--shiki-token-keyword)" }}> = </span>
-            <span style={{ color: "var(--shiki-token-string-expression)" }}>'internet-facing'</span>
-        </div>
+        <div><span style={{ color: "var(--shiki-token-keyword)" }}>SELECT</span></div>
+        <div><span>    *</span></div>
+        <div><span style={{ color: "var(--shiki-token-keyword)" }}>FROM</span></div>
+        <div><span>    aws_elbv2_load_balancers</span></div>
+        <div><span style={{ color: "var(--shiki-token-keyword)" }}>WHERE</span></div>
+        <div>    scheme<span style={{ color: "var(--shiki-token-keyword)" }}> = </span><span style={{ color: "var(--shiki-token-string-expression)" }}>'internet-facing'</span></div>
     </>
 )
 
 const ComplianceQuery = () => (
     <>
-        <div className="whitespace-pre-line">
-            <span style={{ color: "var(--shiki-token-keyword)" }}>SELECT</span> account_id, require_uppercase_characters
-        </div>
-        <div>
-            <span style={{ color: "var(--shiki-token-keyword)" }}>FROM</span> aws_iam_password_policies
-        </div>
-        <div>
-            <span style={{ color: "var(--shiki-token-keyword)" }}>WHERE</span> require_uppercase_characters =
-            <span style={{ color: "var(--shiki-token-keyword)" }}> FALSE</span>
-        </div>
+        <div><span style={{ color: "var(--shiki-token-keyword)" }}>SELECT</span></div>
+        <div><span>    account_id,</span></div>
+        <div><span>    require_uppercase_characters</span></div>
+        <div><span style={{ color: "var(--shiki-token-keyword)" }}>FROM</span></div>
+        <div><span>    aws_iam_password_policies</span></div>
+        <div><span style={{ color: "var(--shiki-token-keyword)" }}>WHERE</span></div>
+        <div>    require_uppercase_characters = aws_iam_password_policies<span style={{ color: "var(--shiki-token-keyword)" }}> FALSE</span></div>
     </>
 )
 
 const QueryAcrossApps = () => (
     <>
+        <div><span style={{ color: "var(--shiki-token-keyword)" }}>SELECT</span></div>
+        <div><span>    arn</span></div>
+        <div><span style={{ color: "var(--shiki-token-keyword)" }}>FROM</span></div>
+        <div><span>    aws_iam_users</span></div>
         <div>
-            <span style={{ color: "var(--shiki-token-keyword)" }}>SELECT</span> arn
-            <span style={{ color: "var(--shiki-token-keyword)" }}> FROM</span> aws_iam_users
+            <span style={{ color: "var(--shiki-token-keyword)" }}>    JOIN</span> aws_iam_user_tags <span style={{ color: "var(--shiki-token-keyword)" }}>ON</span> aws_iam_users.id = aws_iam_user_tags.user_id
         </div>
-        <div><span style={{ color: "var(--shiki-token-keyword)" }}>JOIN</span> aws_iam_user_tags</div>
-        <div className="whitespace-pre-line"><span style={{ color: "var(--shiki-token-keyword)" }}>ON</span> aws_iam_users.id = aws_iam_user_tags.user_id</div>
-        <div><span style={{ color: "var(--shiki-token-keyword)" }}>JOIN</span> okta_users</div>
-        <div className="whitespace-pre-line"><span style={{ color: "var(--shiki-token-keyword)" }}>ON</span> aws_iam_users.tags.value = okta_users.profile_email</div>
-        <div><span style={{ color: "var(--shiki-token-keyword)" }}>WHERE</span> aws_iam_users.tags_key = <span style={{ color: "var(--shiki-token-string-expression)" }}>&quot;email&quot;</span></div>
+        <div><span style={{ color: "var(--shiki-token-keyword)" }}>    JOIN</span> okta_users <span style={{ color: "var(--shiki-token-keyword)" }}>ON</span> aws_iam_users.tags.value = okta_users.profile_email</div>
+        <div><span style={{ color: "var(--shiki-token-keyword)" }}>WHERE</span></div>
+        <div>    aws_iam_users.tags_key = <span style={{ color: "var(--shiki-token-string-expression)" }}>&quot;email&quot;</span></div>
     </>
 )
 
@@ -88,14 +82,14 @@ const QueryItem = ({ children, onClick, description, title }) => {
     return (
         <div className='flex flex-col'>
             <div className="relative">
-                <pre className="h-[260px] bg-transparent dark:bg-transparent m-0 p-0 relative whitespace-pre border border-transparent border-gray-200 rounded-md dark:border-gray-700">
+                <pre className="h-[260px] overflow-y-hidden overflow-x-hidden hover:overflow-x-auto bg-transparent dark:bg-transparent m-0 p-0 relative border border-transparent border-gray-200 rounded-md dark:border-gray-700">
                     <code className={codeClasses}>
                         {children}
                     </code>
                 </pre>
                 <button
                     onClick={onClick}
-                    className="absolute bottom-6 right-6 px-2 py-2 text-gray-600 bg-black rounded-md bg-opacity-5 dark:bg-white dark:text-gray-300 dark:border-gray-700 dark:bg-opacity-5 betterhover:hover:bg-gray-50 betterhover:dark:hover:bg-gray-900 md:py-3 md:text-base md:leading-6 md:px-10"
+                    className="absolute bottom-6 right-3 px-2 py-2 text-gray-600 bg-black rounded-md bg-opacity-5 dark:bg-white dark:text-gray-300 dark:border-gray-700 dark:bg-opacity-5 betterhover:hover:bg-gray-50 betterhover:dark:hover:bg-gray-900 md:py-3 md:text-base md:leading-6 md:px-10"
                 >
                     <DuplicateIcon className="w-6 h-6 text-gray-400" />
                 </button>
