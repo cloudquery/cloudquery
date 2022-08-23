9893bce2622166842c9744a1731f0d5369bb2fe7
Ilya Kuznetsov <ilya@cloudquery.io>
2022-08-23 19:53:06 +0400

    feat(website): Plugins layout (#1548)
    * feat(website): Plugins layout
    
    * Update website/scripts/generate-hub.js
    
    Co-authored-by: Erez Rokah <erezrokah@users.noreply.github.com>
    
    * Update generate-hub.js
    
    * Update generate-hub.js
    
    Co-authored-by: Erez Rokah <erezrokah@users.noreply.github.com>

--- website/scripts/generate-hub.js
+++ website/scripts/generate-hub.js
@@ -12,13 +12,24 @@ const PLUGINS_DATA = {
   k8s: { name: "Kubernetes" },
   okta: { name: "Okta" },
   terraform: { name: "Terraform" },
+  yandexcloud: { name: "Yandex Cloud" },
 };
+
+const EXTERNAL_PLUGINS = [
+  {
+    external: true,
+    id: "yandexcloud",
+    name: "Yandex Cloud",
+    url: "https://github.com/yandex-cloud/cq-provider-yandex",
+  },
+];
 
 const PLUGINS_SOURCE = `${__dirname}/../../plugins/source`;
 const PLUGINS_PATH = `${__dirname}/../pages/plugins`;
 
 const NAME_PLACEHOLDER = "NAME_PLACEHOLDER";
 const CONTENT_PLACEHOLDER = "CONTENT_PLACEHOLDER";
+const EXTERNAL_PLUGINS_LINK_PLACEHOLDER = "EXTERNAL_PLUGINS_LINK_PLACEHOLDER";
 
 const PLUGIN_TEMPLATE = `---
 title: ${NAME_PLACEHOLDER} Plugin
@@ -27,17 +38,17 @@ title: ${NAME_PLACEHOLDER} Plugin
 ${CONTENT_PLACEHOLDER}
 `;
 
-const PLUGINS_PLACEHOLDER = "PLUGINS_PLACEHOLDER";
-
 const PLUGINS_TEMPLATE = `---
 title: Overview
 ---
 
+import { Plugins } from '../../components/Plugins'
+
+# CloudQuery Plugins
+
 Discover plugins that power CloudQuery, the open source high performance data integration platform designed for security and infrastructure teams.
 
-|Plugins|
-|---|
-${PLUGINS_PLACEHOLDER}
+<Plugins />
 `;
 
 const TABLES_TEMPLATE = `---
@@ -47,6 +58,14 @@ title: ${NAME_PLACEHOLDER} Plugin Tables
 |${NAME_PLACEHOLDER} Plugin Tables|
 |---|
 ${CONTENT_PLACEHOLDER}
+`;
+
+const EXTERNAL_PLUGIN_TEMPLATE = `
+# ${NAME_PLACEHOLDER} Plugin
+
+The CloudQuery ${NAME_PLACEHOLDER} plugin pulls configuration out of ${NAME_PLACEHOLDER} resources, normalizes them and stores them in PostgreSQL database.
+
+For more detailes see [${NAME_PLACEHOLDER} Plugin repository](${EXTERNAL_PLUGINS_LINK_PLACEHOLDER})
 `;
 
 const getPlugins = async () => {
@@ -60,13 +79,22 @@ const getPlugins = async () => {
     .map((dirent) => ({
       id: dirent.name,
       name: PLUGINS_DATA[dirent.name].name,
-    }));
+    }))
+    .concat(...EXTERNAL_PLUGINS);
+
   return plugins.sort((plugin1, plugin2) =>
     plugin1.id.localeCompare(plugin2.id)
   );
 };
 
 const getPluginOverview = async (plugin) => {
+  if (plugin.external) {
+    return EXTERNAL_PLUGIN_TEMPLATE.replace(
+      EXTERNAL_PLUGINS_LINK_PLACEHOLDER,
+      plugin.url
+    ).replaceAll(NAME_PLACEHOLDER, plugin.name);
+  }
+
   const overview = await fs.readFile(
     `${PLUGINS_SOURCE}/${plugin.id}/docs/index.md`,
     { encoding: "utf8" }
@@ -104,12 +132,9 @@ const generatePluginsPages = async (plugins) => {
   await generatePluginsPagesMeta(plugins);
 };
 
-const generatePluginsIndexPage = async (plugins) => {
-  const pluginsTable = plugins
-    .map((plugin) => `|[${plugin.name}](plugins/${plugin.id})|`)
-    .join("\n");
+const generatePluginsIndexPage = async () => {
   const page = {
-    content: PLUGINS_TEMPLATE.replace(PLUGINS_PLACEHOLDER, pluginsTable),
+    content: PLUGINS_TEMPLATE,
     path: `${PLUGINS_PATH}/index.mdx`,
   };
   await fs.mkdir(path.dirname(page.path), { recursive: true });
@@ -139,7 +164,9 @@ const generatePluginTablePages = async (plugin) => {
 
 const generatePluginsTablesPages = async (plugins) => {
   await Promise.all(
-    plugins.map(async (plugin) => generatePluginTablePages(plugin))
+    plugins
+      .filter((plugin) => !plugin.external)
+      .map(async (plugin) => generatePluginTablePages(plugin))
   );
 };
 
@@ -147,7 +174,7 @@ const main = async () => {
   await fs.rm(PLUGINS_PATH, { recursive: true, force: true });
 
   const plugins = await getPlugins();
-  await generatePluginsIndexPage(plugins);
+  await generatePluginsIndexPage();
   await generatePluginsPages(plugins);
   await generatePluginsTablesPages(plugins);
 };
