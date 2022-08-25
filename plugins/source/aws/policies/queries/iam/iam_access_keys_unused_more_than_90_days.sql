3e2a5eb3797598a328bf2feb1e459e98b3eb945c
bbernays <ben@bernays.me>
2022-08-25 13:35:21 -0500

    spec says only active keys should be flagged

--- plugins/source/aws/policies/queries/iam/iam_access_keys_unused_more_than_90_days.sql
+++ plugins/source/aws/policies/queries/iam/iam_access_keys_unused_more_than_90_days.sql
@@ -6,10 +6,11 @@ select
     'Unused IAM user credentials should be removed' AS title,
     account_id,
     aws_iam_user_access_keys.access_key_id AS resource_id,
-    case when date_part('day', now() - last_used) > 90 then 'fail'
-         else 'pass'
+    case when (aws_iam_user_access_keys.status = 'Active') and date_part('day', now() - aws_iam_user_access_keys.last_used_date) > 90 then 'fail'
+        else 'pass'
     end as status
 from aws_iam_users
      left join
      aws_iam_user_access_keys on
      aws_iam_users.cq_id = aws_iam_user_access_keys.user_cq_id
+where access_key_id is not null
