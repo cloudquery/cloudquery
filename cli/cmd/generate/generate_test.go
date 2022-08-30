853c212a16800be1a27697aeba1ff11f6083802d
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2022-08-30 19:18:42 +0300

    working on more tests

---
+++ cli/cmd/generate/generate_test.go
@@ -0,0 +1,19 @@
+package generate
+
+import (
+	"testing"
+)
+
+func TestGenerate(t *testing.T) {
+	// dir, err := ioutil.TempDir(os.TempDir(), "test_generate")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer os.RemoveAll(dir)
+	// sourceConfigPath := path.Join(dir, "source.cq.yaml")
+	// cmd := NewCmdGenerate()
+	// cmd.SetArgs([]string{"source", "aws", sourceConfigPath})
+	// if err = cmd.Execute(); err != nil {
+	// 	t.Fatal(err)
+	// }
+}
