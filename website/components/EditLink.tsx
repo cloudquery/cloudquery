999e735312bfcdf5f7c152b1f8f9b1e01e3239fe
erezrokah <erezrokah@users.noreply.github.com>
2022-09-01 17:25:12 +0300

    chore: Fix lint to Yandex repo

--- website/components/EditLink.tsx
+++ website/components/EditLink.tsx
@@ -1,6 +1,29 @@
 import react from "react";
 
 const excludedPaths = ["pages/plugins/index.mdx"];
+const externalPaths = {
+  "pages/plugins/yandexcloud.md":
+    "https://github.com/yandex-cloud/cq-provider-yandex/blob/main/docs/index.md",
+};
+
+const getGitHubEditLink = (filePath: string) => {
+  if (externalPaths[filePath]) {
+    return externalPaths[filePath];
+  }
+  const owner = process.env.VERCEL_GIT_REPO_OWNER || "cloudquery";
+  const repo = process.env.VERCEL_GIT_REPO_SLUG || "cloudquery";
+  const branch = process.env.VERCEL_GIT_COMMIT_REF || "main";
+  const repoPath = `blob/${branch}/${filePath}`;
+  const href = `https://github.com/${owner}/${repo}/${repoPath}`
+    .replace(/pages\/plugins\/([^/]+?).md/, "plugins/source/$1/docs/index.md")
+    .replace(
+      /pages\/plugins\/(.*?)\/tables.mdx/,
+      "plugins/source/$1/docs/tables"
+    )
+    .replace("pages/", "website/pages/");
+
+  return href;
+};
 
 export function EditLink({
   className,
@@ -18,18 +41,8 @@ export function EditLink({
   if (excludedPaths.includes(filePath)) {
     return null;
   }
-  const owner = process.env.VERCEL_GIT_REPO_OWNER || "cloudquery";
-  const repo = process.env.VERCEL_GIT_REPO_SLUG || "cloudquery";
-  const branch = process.env.VERCEL_GIT_COMMIT_REF || "main";
-  const repoPath = `blob/${branch}/${filePath}`;
-  const href = `https://github.com/${owner}/${repo}/${repoPath}`
-    .replace(/pages\/plugins\/([^/]+?).md/, "plugins/source/$1/docs/index.md")
-    .replace(
-      /pages\/plugins\/(.*?)\/tables.mdx/,
-      "plugins/source/$1/docs/tables"
-    )
-    .replace("pages/", "website/pages/");
 
+  const href = getGitHubEditLink(filePath);
   return (
     <a href={href} className={className} target="_blank" rel="noreferrer">
       Edit this page on GitHub
