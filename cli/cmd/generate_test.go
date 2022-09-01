d2352f18ac001bc05aec1004d81798cda515965a
Herman Schaaf <hermanschaaf@gmail.com>
2022-09-01 13:02:42 +0200

    feat: Write to config file instead of stdout (v2) (#1645)
    * feat: Write to config file instead of stdout
    
    * Update SDK to v0.0.10 and remove unique constraints
    
    * Add more assertions to generate test
    
    * Write to stdout
    
    * clarify if statement

--- cli/cmd/generate_test.go
+++ cli/cmd/generate_test.go
@@ -1,14 +1,39 @@
 package cmd
 
 import (
+	"github.com/google/go-cmp/cmp"
+	"io/ioutil"
+	"os"
+	"path"
 	"testing"
 )
 
 func TestGenerate(t *testing.T) {
-	// this works but some funny stuff is going on
-	// cmd := NewCmdGenerate()
-	// cmd.SetArgs([]string{"source", "test"})
-	// if err := cmd.Execute(); err != nil {
-	// 	t.Fatal(err)
-	// }
+	tmpdir, tmpErr := os.MkdirTemp("", "generate_test_*")
+	if tmpErr != nil {
+		t.Fatalf("failed to create temporary directory: %v", tmpErr)
+	}
+	defer os.RemoveAll(tmpdir)
+
+	t.Run("with output file", func(t *testing.T) {
+		output := path.Join(tmpdir, "test.yml")
+		cmd := NewCmdGenerate()
+		cmd.SetArgs([]string{"source", "test", "--output", output})
+		if err := cmd.Execute(); err != nil {
+			t.Fatal(err)
+		}
+
+		// check the generated config
+		cfg, err := ioutil.ReadFile(output)
+		if err != nil {
+			t.Fatalf("error reading config file output: %v ", err)
+		}
+		wantConfig := `
+# This is an example config file for the test plugin.
+account_ids: []
+`
+		if diff := cmp.Diff(string(cfg), wantConfig); diff != "" {
+			t.Errorf("generated config not as expected (+got, -want): %v", diff)
+		}
+	})
 }
