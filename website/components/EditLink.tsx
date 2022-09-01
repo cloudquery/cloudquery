b1d5c55cdc2c12fc92f9a0aee94b7e4e7fa4cac3
erezrokah <erezrokah@users.noreply.github.com>
2022-09-01 14:54:32 +0300

    chore: Migrate nextra, fix broken links

---
+++ website/components/EditLink.tsx
@@ -0,0 +1,33 @@
+import react from "react";
+import Link from "next/link";
+
+export function EditLink({
+  className,
+  filePath,
+  children,
+}: {
+  className?: string;
+  filePath?: string;
+  children: react.ReactNode;
+}) {
+  debugger;
+  if (!filePath) {
+    return;
+  }
+  const owner = process.env.VERCEL_GIT_REPO_OWNER || "cloudquery";
+  const repo = process.env.VERCEL_GIT_REPO_SLUG || "cloudquery";
+  const branch = process.env.VERCEL_GIT_COMMIT_REF || "main";
+  const repoPath = `blob/${branch}/${filePath}`;
+  const href = `https://github.com/${owner}/${repo}/${repoPath}`
+    .replace(/pages\/plugins\/([^/]+?).md/, "plugins/source/$1/docs/index.md")
+    .replace(
+      /pages\/plugins\/(.*?)\/tables.mdx/,
+      "plugins/source/$1/docs/tables"
+    );
+
+  return (
+    <a href={href} className={className} target="_blank" rel="noreferrer">
+      Edit this page on GitHub
+    </a>
+  );
+}
