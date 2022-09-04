28bd9c42c24ba63d4d825c4ad23ffd3d689c6dc6
erezrokah <erezrokah@users.noreply.github.com>
2022-09-04 19:09:15 +0300

    chore: Add Vnet connections

--- plugins/source/azure/codegen/web.go
+++ plugins/source/azure/codegen/web.go
@@ -2,7 +2,42 @@ package codegen
 
 import (
 	"github.com/Azure/azure-sdk-for-go/services/web/mgmt/2020-12-01/web"
+	"github.com/cloudquery/plugin-sdk/codegen"
+	"github.com/cloudquery/plugin-sdk/schema"
 )
+
+const fetchVnetConnections = `func fetchVnetConnections(ctx context.Context, meta schema.ClientMeta, resource *schema.Resource, c schema.Column) error {
+	site := resource.Item.(web.Site)
+	svc := meta.(*client.Client).Services().Web.Apps
+
+	if site.SiteConfig == nil || site.SiteConfig.VnetName == nil {
+		return nil
+	}
+
+	response, err := svc.GetVnetConnection(ctx, *site.ResourceGroup, *site.Name, *site.SiteConfig.VnetName)
+	if err != nil {
+		return diag.WrapError(err)
+	}
+	if response.VnetInfoProperties != nil {
+		vnetConnection := make(map[string]interface{})
+		if response.Name != nil {
+			vnetConnection["name"] = response.Name
+		}
+		if response.ID != nil {
+			vnetConnection["id"] = response.ID
+		}
+		if response.Type != nil {
+			vnetConnection["type"] = response.Type
+		}
+		vnetConnection["properties"] = response.VnetInfoProperties
+		b, err := json.Marshal(vnetConnection)
+		if err != nil {
+			return diag.WrapError(err)
+		}
+		return diag.WrapError(resource.Set(c.Name, b))
+	}
+	return nil
+}`
 
 func Web() []Resource {
 	var resourcesByTemplates = []byTemplates{
@@ -21,6 +56,8 @@ func Web() []Resource {
 					subServiceOverride: "Apps",
 					mockListResult:     "AppCollection",
 					relations:          []string{"SiteAuthSettings()"},
+					helpers:            []string{fetchVnetConnections},
+					customColumns:      codegen.ColumnDefinitions{codegen.ColumnDefinition{Name: "vnet_connection", Type: schema.TypeJSON, Resolver: "fetchVnetConnections"}},
 				},
 				{
 					azureStruct:          &web.SiteAuthSettings{},
