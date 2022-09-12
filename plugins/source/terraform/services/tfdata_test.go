730cdaa6188d1ea286bc0063190b00214470c4fd
Herman Schaaf <hermanschaaf@gmail.com>
2022-09-12 12:02:41 +0100

    Port TestTfData to new SDK

--- plugins/source/terraform/services/tfdata_test.go
+++ plugins/source/terraform/services/tfdata_test.go
@@ -1,23 +1,37 @@
 package services_test
 
-//import (
-//	"testing"
-//
-//	"github.com/cloudquery/cq-provider-sdk/provider"
-//	providertest "github.com/cloudquery/cq-provider-sdk/provider/testing"
-//)
-//
-//func TestTfData(t *testing.T) {
-//	cfg := `
-//    config:
-//      - name: mylocal
-//        local:
-//          path: "../examples/terraform.tfstate"
-//`
-//
-//	// TODO
-//	providertest.TestResource(t, providertest.ResourceTestCase{
-//		Provider: provider.Provider(),
-//		Config:   cfg,
-//	})
-//}
+import (
+	"github.com/cloudquery/cloudquery/plugins/source/terraform/client"
+	"github.com/cloudquery/cloudquery/plugins/source/terraform/services"
+	"github.com/cloudquery/plugin-sdk/plugins"
+	"github.com/cloudquery/plugin-sdk/schema"
+	"github.com/cloudquery/plugin-sdk/specs"
+	"testing"
+)
+
+func TestTfData(t *testing.T) {
+	p := plugins.NewSourcePlugin(
+		"terraform",
+		"test",
+		[]*schema.Table{
+			services.TFData(),
+		},
+		client.Configure,
+	)
+	plugins.TestSourcePluginSync(t, p, specs.Source{
+		Name:         "dev",
+		Tables:       []string{"*"},
+		Destinations: []string{},
+		Spec: map[string]interface{}{
+			"backends": []map[string]interface{}{
+				{
+					"name": "mylocal",
+					"type": "local",
+					"config": map[string]string{
+						"path": "testdata/terraform.tfstate",
+					},
+				},
+			},
+		},
+	})
+}
