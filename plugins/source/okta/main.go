f19625c6c32df1b8ba4d83bd73a9ae5e37468d7f
Erez Rokah <erezrokah@users.noreply.github.com>
2022-09-15 12:19:44 +0300

    feat: Add Sentry DSN (#1818)
    #### Summary
    
    Adds the Sentry DSN for the Okta project
    
    ---

--- plugins/source/okta/main.go
+++ plugins/source/okta/main.go
@@ -5,8 +5,11 @@ import (
 	"github.com/cloudquery/plugin-sdk/serve"
 )
 
+const sentryDSN = "https://e43b6becdda446e6aedb4539cbc7cc83@o1396617.ingest.sentry.io/6747629"
+
 func main() {
 	serve.Serve(serve.Options{
 		SourcePlugin: plugin.Plugin(),
+		SentryDsn:    sentryDSN,
 	})
 }
