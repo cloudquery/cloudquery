8b8ed8846f1066b77976ddfdd78056a5e3d560da
Kemal Hadimli <disq@users.noreply.github.com>
2022-09-15 13:47:35 +0100

    Add Sentry DSN, update SDK

--- plugins/source/terraform/main.go
+++ plugins/source/terraform/main.go
@@ -5,8 +5,11 @@ import (
 	"github.com/cloudquery/plugin-sdk/serve"
 )
 
+const sentryDSN = "https://8a66aa7c550b46258f2391cbae261fe2@o1396617.ingest.sentry.io/6747630"
+
 func main() {
 	serve.Serve(serve.Options{
 		SourcePlugin: plugin.Plugin(),
+		SentryDsn:    sentryDSN,
 	})
 }
