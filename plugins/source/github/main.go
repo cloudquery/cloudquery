5405ebafa62e4e10056f384a314fac8e12f760d1
Erez Rokah <erezrokah@users.noreply.github.com>
2022-09-15 14:25:27 +0300

    fix: Add Sentry DSN (#1814)

--- plugins/source/github/main.go
+++ plugins/source/github/main.go
@@ -5,6 +5,11 @@ import (
 	"github.com/cloudquery/plugin-sdk/serve"
 )
 
+const sentryDSN = "https://99f66f1a627f48deb66e49a25d6028a6@o1396617.ingest.sentry.io/6747628"
+
 func main() {
-	serve.Serve(serve.Options{SourcePlugin: plugin.Plugin()})
+	serve.Serve(serve.Options{
+		SourcePlugin: plugin.Plugin(),
+		SentryDsn:    sentryDSN,
+	})
 }
