69607a3371b08b3f14b51b0e51fa1929350d5f01
Herman Schaaf <hermanschaaf@gmail.com>
2022-09-15 16:19:20 +0100

    Add codegen for autoscaling

--- plugins/source/aws/codegen/recipes/autoscaling.go
+++ plugins/source/aws/codegen/recipes/autoscaling.go
@@ -1,39 +1,113 @@
 package recipes
 
 import (
-	"github.com/aws/aws-sdk-go-v2/service/acm/types"
+	"github.com/aws/aws-sdk-go-v2/service/autoscaling/types"
+	"github.com/cloudquery/cloudquery/plugins/source/aws/resources/services/autoscaling"
 	"github.com/cloudquery/plugin-sdk/codegen"
 	"github.com/cloudquery/plugin-sdk/schema"
+	"reflect"
+	"strings"
 )
 
 func AutoscalingResources() []*Resource {
 	resources := []*Resource{
 		{
-			SubService:          "certificates",
-			Struct:              &types.CertificateDetail{},
-			SkipFields:          []string{"CertificateArn"},
-			PreResourceResolver: "getCertificate",
-			Multiplex:           `client.ServiceAccountRegionMultiplexer("acm")`,
+			SubService: "launch_configurations",
+			Struct:     &types.LaunchConfiguration{},
+			SkipFields: []string{"LaunchConfigurationARN"},
 			ExtraColumns: append(
 				defaultRegionalColumns,
 				[]codegen.ColumnDefinition{
 					{
 						Name:     "arn",
 						Type:     schema.TypeString,
-						Resolver: `schema.PathResolver("CertificateArn")`,
+						Resolver: `schema.PathResolver("LaunchConfigurationARN")`,
 						Options:  schema.ColumnCreationOptions{PrimaryKey: true},
 					},
+				}...),
+		},
+		{
+			SubService: "groups",
+			Struct:     &autoscaling.AutoScalingGroupWrapper{},
+			SkipFields: []string{"AutoScalingGroupARN"},
+			ExtraColumns: append(
+				defaultRegionalColumns,
+				[]codegen.ColumnDefinition{
 					{
-						Name:     "tags",
+						Name:     "load_balancers",
 						Type:     schema.TypeJSON,
-						Resolver: `resolveCertificateTags`,
+						Resolver: `resolveAutoscalingGroupLoadBalancers`,
+					},
+					{
+						Name:     "load_balancer_target_groups",
+						Type:     schema.TypeJSON,
+						Resolver: `resolveAutoscalingGroupLoadBalancerTargetGroups`,
+					},
+					{
+						Name:     "arn",
+						Type:     schema.TypeString,
+						Resolver: `schema.PathResolver("AutoScalingGroupARN")`,
+						Options:  schema.ColumnCreationOptions{PrimaryKey: true},
+					},
+				}...),
+			Relations: []string{
+				"GroupScalingPolicies()",
+				"GroupLifecycleHooks()",
+			},
+		},
+		{
+			SubService: "group_scaling_policies",
+			Struct:     &types.ScalingPolicy{},
+			SkipFields: []string{},
+			ExtraColumns: append(
+				defaultRegionalColumns,
+				[]codegen.ColumnDefinition{
+					{
+						Name:     "group_arn",
+						Type:     schema.TypeString,
+						Resolver: `schema.ParentResourceFieldResolver("arn")`,
+					},
+				}...),
+		},
+		{
+			SubService: "group_lifecycle_hooks",
+			Struct:     &types.LifecycleHook{},
+			SkipFields: []string{},
+			ExtraColumns: append(
+				defaultRegionalColumns,
+				[]codegen.ColumnDefinition{
+					{
+						Name:     "group_arn",
+						Type:     schema.TypeString,
+						Resolver: `schema.ParentResourceFieldResolver("arn")`,
+					},
+				}...),
+		},
+		{
+			SubService: "scheduled_actions",
+			Struct:     &types.ScheduledUpdateGroupAction{},
+			SkipFields: []string{"ScheduledActionARN"},
+			ExtraColumns: append(
+				defaultRegionalColumns,
+				[]codegen.ColumnDefinition{
+					{
+						Name:     "arn",
+						Type:     schema.TypeString,
+						Resolver: `schema.PathResolver("ScheduledActionARN")`,
+						Options:  schema.ColumnCreationOptions{PrimaryKey: true},
 					},
 				}...),
 		},
 	}
 
+	// set default values
 	for _, r := range resources {
 		r.Service = "autoscaling"
+		r.Multiplex = `client.ServiceAccountRegionMultiplexer("autoscaling")`
+		structName := reflect.ValueOf(r.Struct).Elem().Type().Name()
+		if strings.Contains(structName, "Wrapper") {
+			r.UnwrapEmbeddedStructs = true
+		}
 	}
-	return nil
+	return resources
 }
