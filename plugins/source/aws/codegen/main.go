d3d699e4bc4a4e5f78938fe30bce49bdd19e9e84
Herman Schaaf <hermanschaaf@gmail.com>
2022-09-15 10:58:23 +0100

    Add codegen for firehose

--- plugins/source/aws/codegen/main.go
+++ plugins/source/aws/codegen/main.go
@@ -24,6 +24,7 @@ func main() {
 	resources = append(resources, recipes.DaxResources()...)
 	resources = append(resources, recipes.ElastiCacheResources()...)
 
+	resources = append(resources, recipes.FirehoseResources()...)
 	resources = append(resources, recipes.FSXResources()...)
 	resources = append(resources, recipes.GlueResources()...)
 	resources = append(resources, recipes.GuarddutyResources()...)
