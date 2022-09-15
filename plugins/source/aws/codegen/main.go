a2d9c1b9ed837652393534b3eacd2e1c716e24d7
Herman Schaaf <hermanschaaf@gmail.com>
2022-09-15 11:01:43 +0100

    Add codegen for eventbridge

--- plugins/source/aws/codegen/main.go
+++ plugins/source/aws/codegen/main.go
@@ -24,6 +24,7 @@ func main() {
 	resources = append(resources, recipes.DaxResources()...)
 	resources = append(resources, recipes.ElastiCacheResources()...)
 
+	resources = append(resources, recipes.EventbridgeResources()...)
 	resources = append(resources, recipes.FirehoseResources()...)
 	resources = append(resources, recipes.FSXResources()...)
 	resources = append(resources, recipes.GlueResources()...)
