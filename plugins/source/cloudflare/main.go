8594d00719d63aa08bbfc9d8300f60ceeaf9ea91
Kemal Hadimli <disq@users.noreply.github.com>
2022-09-15 13:45:45 +0100

    Add Sentry DSN

--- plugins/source/cloudflare/main.go
+++ plugins/source/cloudflare/main.go
@@ -5,8 +5,11 @@ import (
 	"github.com/cloudquery/plugin-sdk/serve"
 )
 
+const sentryDSN = "https://6beffd303fbd4661a7b7c6c2d546b580@o1396617.ingest.sentry.io/6747634"
+
 func main() {
 	serve.Serve(serve.Options{
 		SourcePlugin: plugin.Plugin(),
+		SentryDsn:    sentryDSN,
 	})
 }
