4f9c8de59ac2e9d6c88e6dd9c4ae6cf457359e61
Erez Rokah <erezrokah@users.noreply.github.com>
2022-09-18 18:00:31 +0300

    feat: Create directory structure when generating configuration (#1845)
    #### Summary
    
    Related to https://github.com/cloudquery/cloudquery/issues/1788.
    This PR ensures that if a user runs `gen source aws examples/aws.yml` the command succeeds.
    
    ---

--- cli/cmd/generate.go
+++ cli/cmd/generate.go
@@ -5,6 +5,7 @@ import (
 	"embed"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 	"text/template"
 
@@ -169,6 +170,10 @@ func writeDestination(path string, destinationSpec specs.Destination) error {
 }
 
 func writeConfig(path, cfgTemplate string, spec interface{}) error {
+	err := os.MkdirAll(filepath.Dir(path), 0744)
+	if err != nil {
+		return fmt.Errorf("failed to directory for file: %w", err)
+	}
 	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_EXCL, 0644)
 	if err != nil {
 		return fmt.Errorf("failed to create file: %w", err)
