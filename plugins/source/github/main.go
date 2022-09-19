3157817941a74f860c7b17049a39172c49863f46
candiduslynx <candiduslynx@gmail.com>
2022-09-19 15:16:40 +0300

    new serve

--- plugins/source/github/main.go
+++ plugins/source/github/main.go
@@ -8,8 +8,5 @@ import (
 const sentryDSN = "https://99f66f1a627f48deb66e49a25d6028a6@o1396617.ingest.sentry.io/6747628"
 
 func main() {
-	serve.Serve(serve.Options{
-		SourcePlugin: plugin.Plugin(),
-		SentryDsn:    sentryDSN,
-	})
+	serve.Source(plugin.Plugin(), serve.WithSourceSentryDSN(sentryDSN))
 }
