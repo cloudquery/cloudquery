6481a8c4e63c5dd15d7a717f0845a4ad0f32080c
erezrokah <erezrokah@users.noreply.github.com>
2022-09-19 12:48:03 +0300

    chore: Cleanup test code

--- cli/commands_tests/sync_test.go
+++ cli/commands_tests/sync_test.go
@@ -1,23 +1,11 @@
 package commands_tests
 
 import (
-	"bytes"
 	"fmt"
-	"os"
-	"path"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
-
-func addDestination(t *testing.T, configFile string) {
-	input, err := os.ReadFile(configFile)
-	require.NoError(t, err)
-
-	output := bytes.ReplaceAll(input, []byte("destinations: []"), []byte("destinations: [postgresql]"))
-	err = os.WriteFile(configFile, output, 0666)
-	require.NoError(t, err)
-}
 
 func TestSync(t *testing.T) {
 	i := newIntegrationTest(t)
@@ -27,8 +15,6 @@ func TestSync(t *testing.T) {
 	output, err = i.runCommand(t, "gen", "destination", "postgresql")
 	fmt.Println(output)
 	require.NoError(t, err)
-
-	addDestination(t, path.Join(i.dir, "test.yml"))
 
 	output, err = i.runCommand(t, "sync", ".")
 	fmt.Println(output)
