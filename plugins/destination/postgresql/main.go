6be2c265d630d744524b8a91b26b43651fbe15ed
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2022-09-25 00:14:12 +0300

    feat(postgresql): Move to standalone plugin

--- plugins/destination/postgresql/main.go
+++ plugins/destination/postgresql/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"github.com/cloudquery/plugin-sdk/serve"
+	"github.com/cloudquery/plugins/destinations/postgresql/client"
+)
 
 func main() {
-	fmt.Println("Not implemented yet!")
+	serve.Destination(client.New())
 }
+
+// https://github.com/cloudquery/cloudquery/releases/download/plugins-source-test-v1.1.0/test_darwin_arm64.zip
+// https://github.com/cloudquery/cloudquery/releases/download/plugins-source-test-v1.1.5/test_darwin_arm64.zip
