ed361b712385f0ada490f37436d63892a0682143
Yevgeny Pats <yev.pats@gmail.com>
2022-09-27 15:18:08 +0300

    chore: Remove deprecated terraform files (#2040)
    Those files are not used and it confuses first time contributors and/or
    users who looks for guidelines on how to build new plugins.

--- plugins/source/aws/terraform/lightsail/modules/test/lb.tf
+++
@@ -1,5 +0,0 @@
-resource "awslightsail_lb" "awslightsail_lb" {
-  name              = "${var.prefix}_load_balancer"
-  health_check_path = "/"
-  instance_port     = "80"
-}
--- plugins/source/aws/terraform/ses/modules/test/template.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_ses_template" "ses-template" {
-  name    = "${var.prefix}-ses-template"
-  subject = "Greetings, {{name}}!"
-  html    = "<h1>Hello {{name}},</h1><p>Your favorite animal is {{favoriteanimal}}.</p>"
-  text    = "Hello {{name}},\r\nYour favorite animal is {{favoriteanimal}}."
-}
