53a5414682053a35d78c13c325da83e59f24c534
Kemal Hadimli <disq@users.noreply.github.com>
2022-09-28 12:20:13 +0100

    Use client's ProjectId

--- plugins/source/gcp/resources/services/resourcemanager/project_policies_fetch.go
+++ plugins/source/gcp/resources/services/resourcemanager/project_policies_fetch.go
@@ -6,18 +6,16 @@ import (
 	"github.com/cloudquery/plugin-sdk/schema"
 	"github.com/cloudquery/plugins/source/gcp/client"
 	"github.com/pkg/errors"
-	"google.golang.org/api/cloudresourcemanager/v3"
 	iampb "google.golang.org/genproto/googleapis/iam/v1"
 )
 
 func fetchProjectPolicies(ctx context.Context, meta schema.ClientMeta, r *schema.Resource, res chan<- interface{}) error {
 	c := meta.(*client.Client)
 
-	p := r.Parent.Item.(*cloudresourcemanager.Project)
 	output, err := c.Services.ResourcemanagerProjectsClient.GetIamPolicy(
 		ctx,
 		&iampb.GetIamPolicyRequest{
-			Resource: "projects/" + p.ProjectId,
+			Resource: "projects/" + c.ProjectId,
 		},
 	)
 	if err != nil {
