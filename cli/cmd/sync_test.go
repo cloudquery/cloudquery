1c688d8d8eb5a597013f17d8b2bb31b46faca126
Herman Schaaf <hermanschaaf@gmail.com>
2022-10-04 15:47:10 +0100

    Add test for log file

--- cli/cmd/sync_test.go
+++ cli/cmd/sync_test.go
@@ -1,12 +1,18 @@
 package cmd
 
 import (
+	"os"
 	"path"
 	"runtime"
+	"strings"
 	"testing"
 )
 
 func TestSync(t *testing.T) {
+	t.Cleanup(func() {
+		os.RemoveAll("cloudquery.log")
+	})
+
 	// this works but some funny stuff is going on
 	_, filename, _, _ := runtime.Caller(0)
 	currentDir := path.Dir(filename)
@@ -15,5 +21,18 @@ func TestSync(t *testing.T) {
 	cmd.SetArgs([]string{"sync", testDataDir})
 	if err := cmd.Execute(); err != nil {
 		t.Fatal(err)
+	}
+
+	// check that log was written and contains some lines from the plugin
+	b, err := os.ReadFile("cloudquery.log")
+	if err != nil {
+		t.Fatal("failed to read cloudquery.log:", err)
+	}
+	content := string(b)
+	if len(content) == 0 {
+		t.Fatalf("cloudquery.log empty, but expected some logs")
+	}
+	if !strings.Contains(content, "plugin=") {
+		t.Errorf("cloudquery.log is expected to contain lines with string plugin=, but none found. Logs were: \n%v", content)
 	}
 }
