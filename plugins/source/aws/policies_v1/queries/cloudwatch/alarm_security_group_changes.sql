0f641966ab704cf82fa0bdb1f3cbf18ca015f40f
Andrii Romanenko <andrejjrom@gmail.com>
2022-10-05 11:41:40 +0300

    feat(aws): Update cis 1.5.0 policy (#1615)
    #### Summary
    closes #1559 
    
    some cloudwatch alerts checks are refined but they are still not perfect. Formatting of rules can these affect checks result.
    <!--
    Explain what problem this PR addresses
    -->
    
    ---

--- plugins/source/aws/policies_v1/queries/cloudwatch/alarm_security_group_changes.sql
+++ plugins/source/aws/policies_v1/queries/cloudwatch/alarm_security_group_changes.sql
@@ -6,13 +6,13 @@ select
   'Ensure a log metric filter and alarm exist for security group changes (Scored)' as title,
   account_id,
   cloud_watch_logs_log_group_arn as resource_id,
-  case when
-      pattern = '{ ($.eventName = AuthorizeSecurityGroupIngress) '
-      || '|| ($.eventName = AuthorizeSecurityGroupEgress) '
-      || '|| ($.eventName = RevokeSecurityGroupIngress) '
-      || '|| ($.eventName = RevokeSecurityGroupEgress) '
-      || '|| ($.eventName = CreateSecurityGroup) '
-      || '|| ($.eventName = DeleteSecurityGroup) }' then 'pass'
+  case when pattern NOT LIKE '%NOT%'
+           AND pattern LIKE '%($.eventName = AuthorizeSecurityGroupIngress)%'
+           AND pattern LIKE '%($.eventName = AuthorizeSecurityGroupEgress)%'
+           AND pattern LIKE '%($.eventName = RevokeSecurityGroupIngress)%'
+           AND pattern LIKE '%($.eventName = RevokeSecurityGroupEgress)%'
+           AND pattern LIKE '%($.eventName = CreateSecurityGroup)%'
+           AND pattern LIKE '%($.eventName = DeleteSecurityGroup)%' then 'pass'
       else 'fail'
   end as status
 from view_aws_log_metric_filter_and_alarm
