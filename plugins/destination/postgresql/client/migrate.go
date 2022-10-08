e8a7fa85626a2a9e4047d1aa228c49ce0091243a
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2022-10-09 00:45:41 +0300

    feat(pg): Support CockroachDB

--- plugins/destination/postgresql/client/migrate.go
+++ plugins/destination/postgresql/client/migrate.go
@@ -78,7 +78,7 @@ func (c *Client) autoMigrateTable(ctx context.Context, table *schema.Table) erro
 
 	for _, col := range table.Columns {
 		columnName := pgx.Identifier{col.Name}.Sanitize()
-		columnType := SchemaTypeToPg(col.Type)
+		columnType := c.SchemaTypeToPg(col.Type)
 		pgColumn := pgColumns.getPgColumn(col.Name)
 
 		switch {
@@ -142,7 +142,7 @@ func (c *Client) createTableIfNotExist(ctx context.Context, table *schema.Table)
 
 	primaryKeys := []string{}
 	for i, col := range table.Columns {
-		pgType := SchemaTypeToPg(col.Type)
+		pgType := c.SchemaTypeToPg(col.Type)
 		columnName := pgx.Identifier{col.Name}.Sanitize()
 		fieldDef := columnName + " " + pgType
 		sb.WriteString(fieldDef)
