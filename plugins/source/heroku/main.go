13f5642eabc664552c9a6b4111c57482bc2113e3
erezrokah <erezrokah@users.noreply.github.com>
2022-10-11 17:51:16 +0300

    fix: Move plugin and resources to correct directories

--- plugins/source/heroku/main.go
+++ plugins/source/heroku/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/cloudquery/cloudquery/plugins/source/heroku/plugin"
+	"github.com/cloudquery/cloudquery/plugins/source/heroku/resources/plugin"
 	"github.com/cloudquery/plugin-sdk/serve"
 )
 
