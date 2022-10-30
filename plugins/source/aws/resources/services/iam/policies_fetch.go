a35b8e89d625287a9b9406ff18cfac78ffdb1241
Yevgeny Pats <yev.pats@gmail.com>
2022-10-30 18:09:18 +0200

    feat: Update all plugins to SDK with metrics and DFS scheduler (#3286)
    Should be merged after this SDK PR https://github.com/cloudquery/plugin-sdk/pull/318

--- plugins/source/aws/resources/services/iam/policies_fetch.go
+++ plugins/source/aws/resources/services/iam/policies_fetch.go
@@ -39,7 +39,6 @@ func resolveIamPolicyTags(ctx context.Context, meta schema.ClientMeta, resource 
 	response, err := svc.ListPolicyTags(ctx, &iam.ListPolicyTagsInput{PolicyArn: r.Arn})
 	if err != nil {
 		if cl.IsNotFoundError(err) {
-			meta.Logger().Debug().Err(err).Msg("ListPolicyTags: Policy does not exist")
 			return nil
 		}
 		return err
@@ -52,8 +51,6 @@ func resolveIamPolicyVersionList(ctx context.Context, meta schema.ClientMeta, re
 	for i := range r.PolicyVersionList {
 		if v, err := url.PathUnescape(aws.ToString(r.PolicyVersionList[i].Document)); err == nil {
 			r.PolicyVersionList[i].Document = &v
-		} else {
-			meta.Logger().Warn().Err(err).Str("policy_id", aws.ToString(r.PolicyId)).Msg("Failed to unescape policy document, leaving as-is")
 		}
 	}
 	return resource.Set(c.Name, r.PolicyVersionList)
