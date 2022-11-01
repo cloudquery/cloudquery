94e7fae35fcfb3d43481dd1ed103d9a0432d2106
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2022-11-01 12:48:25 +0200

    add sentry DSN

--- plugins/destination/csv/main.go
+++ plugins/destination/csv/main.go
@@ -8,8 +8,7 @@ import (
 )
 
 const (
-	// TODO: add sentry DSN
-	sentryDSN = ""
+	sentryDSN = "https://79d5e237dafe45e1a4ec0785bc528280@o1396617.ingest.sentry.io/4504083471335424"
 )
 
 func main() {
