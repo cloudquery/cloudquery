d3baabdc228d0fc4a33b4767aab54ea22545b03f
bbernays <ben@bernays.me>
2022-11-03 11:22:03 -0500

    Add filtering

--- plugins/source/aws/resources/services/docdb/event_categories_fetch.go
+++ plugins/source/aws/resources/services/docdb/event_categories_fetch.go
@@ -3,7 +3,9 @@ package docdb
 import (
 	"context"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/docdb"
+	"github.com/aws/aws-sdk-go-v2/service/docdb/types"
 	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
 	"github.com/cloudquery/plugin-sdk/schema"
 )
@@ -12,7 +14,9 @@ func fetchDocdbEventCategories(ctx context.Context, meta schema.ClientMeta, _ *s
 	c := meta.(*client.Client)
 	svc := c.Services().Docdb
 
-	input := &docdb.DescribeEventCategoriesInput{}
+	input := &docdb.DescribeEventCategoriesInput{
+		Filters: []types.Filter{{Name: aws.String("engine"), Values: []string{"docdb"}}},
+	}
 
 	response, err := svc.DescribeEventCategories(ctx, input)
 	if err != nil {
--- plugins/source/aws/resources/services/docdb/global_clusters_fetch.go
+++ plugins/source/aws/resources/services/docdb/global_clusters_fetch.go
@@ -3,7 +3,9 @@ package docdb
 import (
 	"context"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/docdb"
+	"github.com/aws/aws-sdk-go-v2/service/docdb/types"
 	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
 	"github.com/cloudquery/plugin-sdk/schema"
 )
@@ -12,7 +14,9 @@ func fetchDocdbGlobalClusters(ctx context.Context, meta schema.ClientMeta, paren
 	c := meta.(*client.Client)
 	svc := c.Services().Docdb
 
-	input := &docdb.DescribeGlobalClustersInput{}
+	input := &docdb.DescribeGlobalClustersInput{
+		Filters: []types.Filter{{Name: aws.String("engine"), Values: []string{"docdb"}}},
+	}
 	p := docdb.NewDescribeGlobalClustersPaginator(svc, input)
 	for p.HasMorePages() {
 		response, err := p.NextPage(ctx)
