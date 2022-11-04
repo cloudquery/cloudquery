f5cd42c308f99dc62d3e476316dd6a4b69a5a8b7
bbernays <ben@cloudquery.io>
2022-11-04 13:00:15 -0500

    feat(aws): AppRunner Resources: VPC Connector, VPC Ingress Connection And Autotscaling (#3450)
    #### Summary
    
    <!--
    Explain what problem this PR addresses
    -->
    
    <!--

--- plugins/source/aws/codegen/recipes/apprunner.go
+++ plugins/source/aws/codegen/recipes/apprunner.go
@@ -45,6 +45,65 @@ func ApprunnerResources() []*Resource {
 				"Operations()",
 				"CustomDomains()"},
 		}, {
+			SubService:          "auto_scaling_configuration",
+			Struct:              &types.AutoScalingConfiguration{},
+			Description:         "https://docs.aws.amazon.com/apprunner/latest/api/API_AutoScalingConfiguration.html",
+			SkipFields:          []string{"AutoScalingConfigurationArn"},
+			Multiplex:           `client.ServiceAccountRegionMultiplexer("apprunner")`,
+			PreResourceResolver: "getAutoScalingConfiguration",
+			ExtraColumns: append(
+				defaultRegionalColumns,
+				[]codegen.ColumnDefinition{
+					{
+						Name:     "arn",
+						Type:     schema.TypeString,
+						Resolver: `schema.PathResolver("AutoScalingConfigurationArn")`,
+						Options:  schema.ColumnCreationOptions{PrimaryKey: true},
+					},
+				}...),
+		},
+		{
+			SubService:  "vpc_connector",
+			Struct:      &types.VpcConnector{},
+			Description: "https://docs.aws.amazon.com/apprunner/latest/api/API_VpcConnector.html",
+			SkipFields:  []string{"VpcConnectorArn"},
+			Multiplex:   `client.ServiceAccountRegionMultiplexer("apprunner")`,
+			ExtraColumns: append(
+				defaultRegionalColumns,
+				[]codegen.ColumnDefinition{
+					{
+						Name:     "arn",
+						Type:     schema.TypeString,
+						Resolver: `schema.PathResolver("VpcConnectorArn")`,
+						Options:  schema.ColumnCreationOptions{PrimaryKey: true},
+					},
+				}...),
+		}, {
+			SubService: "vpc_ingress_connection",
+			Struct:     &types.VpcIngressConnection{},
+			Description: `https://docs.aws.amazon.com/apprunner/latest/api/API_VpcIngressConnection.html
+
+Notes:
+- 'account_id' has been renamed to 'source_account_id' to avoid conflict with the 'account_id' column that indicates what account this was synced from.`,
+			SkipFields:          []string{"VpcIngressConnectionArn", "AccountId"},
+			Multiplex:           `client.ServiceAccountRegionMultiplexer("apprunner")`,
+			PreResourceResolver: "getVpcIngressConnection",
+			ExtraColumns: append(
+				defaultRegionalColumns,
+				[]codegen.ColumnDefinition{
+					{
+						Name:     "arn",
+						Type:     schema.TypeString,
+						Resolver: `schema.PathResolver("VpcIngressConnectionArn")`,
+						Options:  schema.ColumnCreationOptions{PrimaryKey: true},
+					},
+					{
+						Name:     "source_account_id",
+						Type:     schema.TypeString,
+						Resolver: `schema.PathResolver("AccountId")`,
+					},
+				}...),
+		}, {
 			SubService:   "operations",
 			Struct:       &types.OperationSummary{},
 			Description:  "https://docs.aws.amazon.com/apprunner/latest/api/API_OperationSummary.html",
