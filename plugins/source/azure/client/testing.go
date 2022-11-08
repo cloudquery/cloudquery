5c29f2df2135d8bd091f8377ac8d90660757b4d6
Herman Schaaf <hermanschaaf@gmail.com>
2022-11-08 10:04:29 +0000

    Update plugins

--- plugins/source/azure/client/testing.go
+++ plugins/source/azure/client/testing.go
@@ -38,6 +38,7 @@ func MockTestHelper(t *testing.T, table *schema.Table, createServices func(t *te
 	p.SetLogger(l)
 	plugins.TestSourcePluginSync(t, p, specs.Source{
 		Name:         "dev",
+		Path:         "cloudquery/dev",
 		Version:      version,
 		Tables:       []string{table.Name},
 		Destinations: []string{"mock-destination"},
--- plugins/source/digitalocean/client/testing.go
+++ plugins/source/digitalocean/client/testing.go
@@ -54,6 +54,7 @@ func MockTestHelper(t *testing.T, table *schema.Table, createService func(t *tes
 	p.SetLogger(l)
 	plugins.TestSourcePluginSync(t, p, specs.Source{
 		Name:         "dev",
+		Path:         "cloudquery/dev",
 		Version:      version,
 		Tables:       []string{table.Name},
 		Destinations: []string{"mock-destination"},
