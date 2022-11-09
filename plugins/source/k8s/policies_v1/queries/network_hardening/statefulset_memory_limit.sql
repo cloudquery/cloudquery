d04da202447ed06343a2f45e76178481ac7eb7f6
Samuel Kelemen <samuel@kelemen.us>
2022-11-09 17:23:56 +0100

    statefulset checks

---
+++ plugins/source/k8s/policies_v1/queries/network_hardening/statefulset_memory_limit.sql
@@ -0,0 +1,23 @@
+WITH stateful_set_containers AS (SELECT uid, value AS container 
+                               FROM k8s_apps_stateful_sets
+                               CROSS JOIN jsonb_array_elements(spec_template->'spec'->'containers') AS value)
+
+INSERT INTO k8s_policy_results (resource_id, execution_time, framework, check_id, title, context, namespace,
+                                resource_name, status)
+select uid                              AS resource_id,
+        :'execution_time'::timestamp     AS execution_time,
+        :'framework'                     AS framework,
+        :'check_id'                      AS check_id,
+        'Statefulset enforce memory limits' AS title,
+        context                          AS context,
+        namespace                        AS namespace,
+        name                             AS resource_name,
+        CASE
+            WHEN
+                  -- Every container needs to have a memory limit for the check to pass
+                  (SELECT COUNT(*) FROM stateful_set_containers WHERE stateful_set_containers.uid = k8s_apps_stateful_sets.uid AND
+                  stateful_set_containers.container->'resources'->'limits'->>'memory' IS NULL) > 0
+                THEN 'fail'
+                ELSE 'pass'
+            END                          AS status
+FROM k8s_apps_stateful_sets
