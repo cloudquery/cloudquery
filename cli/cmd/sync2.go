9958b4dd6a0f58ca07ff4f0144de31c0eabda4ed
Herman Schaaf <hermanschaaf@gmail.com>
2022-11-09 10:21:28 +0000

    fix: Respect --no-migrate option in sync2 (#3979)
    When we introduced sync2, the `--no-migrate` option wasn't ported along, so currently newer versions of the CLI aren't respecting it.

--- cli/cmd/sync2.go
+++ cli/cmd/sync2.go
@@ -12,7 +12,7 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-func syncConnectionV2(ctx context.Context, cqDir string, sourceClient *clients.SourceClient, sourceSpec specs.Source, destinationsSpecs []specs.Destination, uid string) error {
+func syncConnectionV2(ctx context.Context, cqDir string, sourceClient *clients.SourceClient, sourceSpec specs.Source, destinationsSpecs []specs.Destination, uid string, noMigrate bool) error {
 	var err error
 	destinationNames := make([]string, len(destinationsSpecs))
 	for i := range destinationsSpecs {
@@ -23,40 +23,35 @@ func syncConnectionV2(ctx context.Context, cqDir string, sourceClient *clients.S
 	log.Info().Str("source", sourceSpec.Name).Strs("destinations", destinationNames).Time("sync_time", syncTime).Msg("Start sync")
 	defer log.Info().Str("source", sourceSpec.Name).Strs("destinations", destinationNames).Time("sync_time", syncTime).Msg("End sync")
 
-	destClients := make([]*clients.DestinationClient, len(sourceSpec.Destinations))
-	destSubscriptions := make([]chan []byte, len(sourceSpec.Destinations))
-	for i := range destSubscriptions {
-		destSubscriptions[i] = make(chan []byte)
+	destClients, err := newDestinationClients(ctx, sourceSpec, destinationsSpecs, cqDir)
+	if err != nil {
+		return err
 	}
-	defer func() {
-		for _, destClient := range destClients {
-			if destClient != nil {
-				if err := destClient.Terminate(); err != nil {
-					log.Error().Err(err).Msg("Failed to terminate destination client")
-					fmt.Println("failed to terminate destination client: ", err)
-				}
-			}
-		}
-	}()
+	defer destClients.Close()
+
 	tables, err := sourceClient.GetTables(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to get tables for source %s: %w", sourceSpec.Name, err)
 	}
-	for i, destinationSpec := range destinationsSpecs {
-		destClients[i], err = clients.NewDestinationClient(ctx, destinationSpec.Registry, destinationSpec.Path, destinationSpec.Version,
-			clients.WithDestinationLogger(log.Logger),
-			clients.WithDestinationDirectory(cqDir),
-		)
-		if err != nil {
-			return fmt.Errorf("failed to create destination plugin client for %s: %w", destinationSpec.Name, err)
-		}
-		if err := destClients[i].Initialize(ctx, destinationSpec); err != nil {
-			return fmt.Errorf("failed to initialize destination plugin client for %s: %w", destinationSpec.Name, err)
-		}
 
-		if err := destClients[i].Migrate(ctx, tables); err != nil {
-			return fmt.Errorf("failed to migrate source %s on destination %s : %w", sourceSpec.Name, destinationSpec.Name, err)
+	if !noMigrate {
+		fmt.Println("Starting migration for:", sourceSpec.Name, "->", sourceSpec.Destinations)
+		log.Info().Str("source", sourceSpec.Name).Strs("destinations", sourceSpec.Destinations).Msg("Start migration")
+		migrateStart := time.Now()
+
+		for i, destinationSpec := range destinationsSpecs {
+			if err := destClients[i].Migrate(ctx, tables); err != nil {
+				return fmt.Errorf("failed to migrate source %s on destination %s : %w", sourceSpec.Name, destinationSpec.Name, err)
+			}
 		}
+		migrateTimeTook := time.Since(migrateStart)
+		fmt.Printf("Migration completed successfully.\n")
+		log.Info().
+			Str("source", sourceSpec.Name).
+			Strs("destinations", sourceSpec.Destinations).
+			Int("num_tables", len(tables)).
+			Float64("time_took", migrateTimeTook.Seconds()).
+			Msg("End migration")
 	}
 
 	resources := make(chan []byte)
@@ -74,6 +69,10 @@ func syncConnectionV2(ctx context.Context, cqDir string, sourceClient *clients.S
 		return nil
 	})
 
+	destSubscriptions := make([]chan []byte, len(sourceSpec.Destinations))
+	for i := range destSubscriptions {
+		destSubscriptions[i] = make(chan []byte)
+	}
 	bar := progressbar.NewOptions(-1,
 		progressbar.OptionSetDescription("Syncing resources..."),
 		progressbar.OptionSetItsString("resources"),
