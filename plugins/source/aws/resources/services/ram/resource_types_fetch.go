0c16addf0534147f5e23cf34d1f9286c681e4aa4
candiduslynx <candiduslynx@users.noreply.github.com>
2022-11-10 11:49:05 +0300

    feat(aws): Add AWS RAM resources (#3961)
    Add AWS RAM resources (#1984)

---
+++ plugins/source/aws/resources/services/ram/resource_types_fetch.go
@@ -0,0 +1,23 @@
+package ram
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ram"
+	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
+	"github.com/cloudquery/plugin-sdk/schema"
+)
+
+func fetchRamResourceTypes(ctx context.Context, meta schema.ClientMeta, _ *schema.Resource, res chan<- interface{}) error {
+	input := &ram.ListResourceTypesInput{MaxResults: aws.Int32(500)}
+	paginator := ram.NewListResourceTypesPaginator(meta.(*client.Client).Services().Ram, input)
+	for paginator.HasMorePages() {
+		response, err := paginator.NextPage(ctx)
+		if err != nil {
+			return err
+		}
+		res <- response.ResourceTypes
+	}
+	return nil
+}
