7b6e18bdcaade0015bea65208a1c9ae5197a0696
Andrii Romanenko <andrejjrom@gmail.com>
2022-11-25 19:26:57 +0200

    Update plugins/source/datadog/main.go
    Co-authored-by: Herman Schaaf <hermanschaaf@gmail.com>

--- plugins/source/datadog/main.go
+++ plugins/source/datadog/main.go
@@ -5,7 +5,7 @@ import (
 	"github.com/cloudquery/plugin-sdk/serve"
 )
 
-const sentryDSN = "https://99f66f1a627f48deb66e49a25d6028a6@o1396617.ingest.sentry.io/6747628"
+const sentryDSN = "https://a0c2570b96264748a6759bb62e8cdef5@o1396617.ingest.sentry.io/4504220208267264"
 
 func main() {
 	serve.Source(plugin.Plugin(), serve.WithSourceSentryDSN(sentryDSN))
