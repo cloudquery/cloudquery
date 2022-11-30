16e1a1736ee74b53fe3cdf25b13951403579c24d
erezrokah <erezrokah@users.noreply.github.com>
2022-11-30 12:07:57 +0200

    fix(website): Don't error when new plugins are added

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -24,6 +24,29 @@ function parseName(name) {
   return parts[0];
 }
 
+function isUnreleasedPlugin(key, name) {
+  if (key === "cli") {
+    return false;
+  }
+  const pluginDir = path.resolve("..", "plugins", key.slice(0, -1), name);
+  try {
+    // check that is this a plugin
+    fs.accessSync(path.resolve(pluginDir, "go.mod"), fs.constants.F_OK);
+  } catch (e) {
+    return false;
+  }
+
+  const changelogPath = path.resolve(pluginDir, "CHANGELOG.md");
+  try {
+    const changelogContent = fs.readFileSync(changelogPath, "utf8");
+    const nonEmptyChangelog = changelogContent.includes("1.0.0");
+    return !nonEmptyChangelog;
+  } catch (err) {
+    // no ChangeLog, this is new plugin
+    return true;
+  }
+}
+
 function getVersionsForPrefix(prefix, files) {
   return Object.fromEntries(
     files
@@ -56,11 +79,13 @@ const replaceMdxCodeVersions = (node) => {
     Object.entries(patterns).forEach(([key, pattern]) => {
       const match = node.value.match(pattern);
       if (match && match.length >= 1) {
-        const version = versions[key][match[1].toLowerCase()];
-        if (version === undefined) {
-          throw new Error(`Could not find version for ${key} ${match[1]}`);
+        const name = match[1].toLowerCase();
+        const version = versions[key][name];
+        if (version !== undefined || isUnreleasedPlugin(key, name)) {
+          node.value = node.value.replace(pattern, version || "v1.0.0");
+        } else {
+          throw new Error(`Could not find version for ${key} ${name}`);
         }
-        node.value = node.value.replace(pattern, version);
       }
     });
   }
