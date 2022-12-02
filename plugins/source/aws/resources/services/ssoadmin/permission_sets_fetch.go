0e27b4e042789c7c4ff092d6389b5f66adce26ed
jhudson10x <106281542+jhudson10x@users.noreply.github.com>
2022-12-02 13:15:23 -0500

    Apply suggestions from code review
    Co-authored-by: bbernays <ben@bernays.me>

--- plugins/source/aws/resources/services/ssoadmin/permission_sets_fetch.go
+++ plugins/source/aws/resources/services/ssoadmin/permission_sets_fetch.go
@@ -25,11 +25,6 @@ func getSsoadminPermissionSetInlinePolicy(ctx context.Context, meta schema.Clien
 		return err
 	}
 
-	var inlinePolicy map[string]interface{}
-	err = json.Unmarshal([]byte(*response.InlinePolicy), &inlinePolicy)
-	if err != nil {
-		return err
-	}
 
 	return resource.Set(c.Name, inlinePolicy)
 }
