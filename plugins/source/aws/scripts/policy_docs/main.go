dd5cb720509a77502e6ab56adaa7dec0e784e268
Herman Schaaf <hermanschaaf@gmail.com>
2022-12-02 17:04:21 +0000

    Populate file from template

--- plugins/source/aws/scripts/policy_docs/main.go
+++ plugins/source/aws/scripts/policy_docs/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"embed"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -8,7 +9,13 @@ import (
 	"path"
 	"path/filepath"
 	"regexp"
+	"sort"
+	"strings"
+	"text/template"
 )
+
+//go:embed templates/*.go.tpl
+var templatesFS embed.FS
 
 var reInline = regexp.MustCompile(`(?i)\\ir (.+)`)
 var reTable = regexp.MustCompile(`(?i)(?:from|join)\s+(\w+)`)
@@ -43,14 +50,30 @@ type PolicyInfo struct {
 	Queries []Query
 }
 
+// Tables returns the unique set of tables needed to run all queries
+func (pi PolicyInfo) Tables() []string {
+	t := map[string]struct{}{}
+	for _, q := range pi.Queries {
+		for _, table := range q.Tables {
+			t[table] = struct{}{}
+		}
+	}
+	var final []string
+	for k := range t {
+		final = append(final, k)
+	}
+	sort.Strings(final)
+	return final
+}
+
 type Query struct {
 	Title  string
 	Path   string
 	Tables []string
 }
 
-func getPolicyInfo(dir string, policy Policy) (*PolicyInfo, error) {
-	queries, err := extractQueries(path.Join(dir, policy.Path))
+func getPolicyInfo(prefix, dir string, policy Policy) (*PolicyInfo, error) {
+	queries, err := extractQueries(prefix, path.Join(dir, policy.Path))
 	if err != nil {
 		return nil, fmt.Errorf("failed to extract queries: %w", err)
 	}
@@ -60,7 +83,7 @@ func getPolicyInfo(dir string, policy Policy) (*PolicyInfo, error) {
 	}, nil
 }
 
-func extractQueries(sqlPath string) ([]Query, error) {
+func extractQueries(prefix, sqlPath string) ([]Query, error) {
 	b, err := os.ReadFile(sqlPath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open file %v: %w", sqlPath, err)
@@ -72,6 +95,9 @@ func extractQueries(sqlPath string) ([]Query, error) {
 	if isPolicyQuery && len(tableMatches) > 0 {
 		q := Query{}
 		for _, m := range tableMatches {
+			if !strings.HasPrefix(m[1], prefix) {
+				continue
+			}
 			q.Tables = append(q.Tables, m[1])
 			q.Path = sqlPath
 		}
@@ -89,7 +115,7 @@ func extractQueries(sqlPath string) ([]Query, error) {
 	dir := filepath.Dir(sqlPath)
 	matches := reInline.FindAllStringSubmatch(content, -1)
 	for _, m := range matches {
-		q, err := extractQueries(path.Join(dir, m[1]))
+		q, err := extractQueries(prefix, path.Join(dir, m[1]))
 		if err != nil {
 			return nil, err
 		}
@@ -98,24 +124,44 @@ func extractQueries(sqlPath string) ([]Query, error) {
 	return queries, nil
 }
 
-func main() {
-	if len(os.Args) <= 2 {
-		log.Fatalf("Usage: %s <path to policies directory> <output dir>", os.Args[0])
+func writePolicyDocs(info []*PolicyInfo, outputPath string) error {
+	t, err := template.New("policies.md.go.tpl").ParseFS(templatesFS, "templates/policies.md.go.tpl")
+	if err != nil {
+		return fmt.Errorf("failed to parse template for README.md: %v", err)
 	}
-	dir := os.Args[1]
+	f, err := os.Create(outputPath)
+	if err != nil {
+		return fmt.Errorf("failed to create file %v: %v", outputPath, err)
+	}
+	defer f.Close()
+	if err := t.Execute(f, info); err != nil {
+		return fmt.Errorf("failed to execute template: %v", err)
+	}
+	return nil
+}
+
+func main() {
+	if len(os.Args) <= 3 {
+		log.Fatalf("Usage: %s <table prefix> <path to policies directory> <output filename>", os.Args[0])
+	}
+	prefix := os.Args[1]
+	dir := os.Args[2]
+	out := os.Args[3]
 	index, err := readIndex(dir)
 	if err != nil {
 		log.Fatalf("error reading index: %v", err)
 	}
 
+	var info []*PolicyInfo
 	for _, p := range index.Policies {
-		pi, err := getPolicyInfo(dir, p)
+		pi, err := getPolicyInfo(prefix, dir, p)
 		if err != nil {
-			log.Fatalf("error reading SQL for policy: %v", err)
+			log.Fatalf("error reading index: %v", err)
 		}
-		fmt.Println(pi.Name, len(pi.Queries))
-		for _, q := range pi.Queries {
-			fmt.Println(q.Title, q.Tables, q.Path)
-		}
+		info = append(info, pi)
+	}
+	err = writePolicyDocs(info, out)
+	if err != nil {
+		log.Fatalf("failed to write policy documentation to %v: %v", out, err)
 	}
 }
---
+++ plugins/source/aws/scripts/policy_docs/templates/policies.md.go.tpl
@@ -0,0 +1,24 @@
+# Policies and Compliance Frameworks
+
+CloudQuery SQL Policies for AWS. See the [Readme on Github](https://github.com/cloudquery/cloudquery/tree/main/plugins/source/aws/policies) for installation instructions.
+
+{{- range .}}
+## {{ .Name }}
+
+### Requirements
+{{ .Name }} requires the following tables to be synced before the policy is executed:
+
+```
+tables:
+{{- range $table := .Tables }}
+  - {{ $table }}
+{{- end }}
+```
+
+### Queries
+{{ .Name }} performs the following checks:
+{{- range $query := .Queries }}
+  - {{ $query.Title }}
+{{- end }}
+
+{{- end }}
