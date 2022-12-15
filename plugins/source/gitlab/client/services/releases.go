727b822d2ef696b9136a47de49bd9e75085f25df
bbernays <ben@bernays.me>
2022-12-15 08:32:16 -0600

    Update releases.go

--- plugins/source/gitlab/client/services/releases.go
+++ plugins/source/gitlab/client/services/releases.go
@@ -2,7 +2,6 @@ package services
 
 import "github.com/xanzy/go-gitlab"
 
-//go:generate mockgen -package=mocks -destination=../mocks/releases.go -source=releases.go ReleasesClient
 type ReleasesClient interface {
 	ListReleases(pid interface{}, opt *gitlab.ListReleasesOptions, options ...gitlab.RequestOptionFunc) ([]*gitlab.Release, *gitlab.Response, error)
 }
