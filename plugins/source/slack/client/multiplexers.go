79f84df7089b1d6d0f35cbc23c33d99017a040fd
Herman Schaaf <hermanschaaf@gmail.com>
2022-12-19 11:53:23 +0000

    fix alloc

--- plugins/source/slack/client/multiplexers.go
+++ plugins/source/slack/client/multiplexers.go
@@ -4,7 +4,7 @@ import "github.com/cloudquery/plugin-sdk/schema"
 
 func TeamMultiplex(meta schema.ClientMeta) []schema.ClientMeta {
 	client := meta.(*Client)
-	clients := make([]schema.ClientMeta, 0, len(client.Teams))
+	clients := make([]schema.ClientMeta, len(client.Teams))
 	for i, team := range client.Teams {
 		clients[i] = client.withTeamID(team.ID)
 	}
