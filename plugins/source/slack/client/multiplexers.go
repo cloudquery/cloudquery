96c5833be6b1063e5124d7d6ec675e57bc883000
Herman Schaaf <hermanschaaf@gmail.com>
2022-12-19 10:42:00 +0000

    Address some comments

--- plugins/source/slack/client/multiplexers.go
+++ plugins/source/slack/client/multiplexers.go
@@ -3,10 +3,10 @@ package client
 import "github.com/cloudquery/plugin-sdk/schema"
 
 func TeamMultiplex(meta schema.ClientMeta) []schema.ClientMeta {
-	var l = make([]schema.ClientMeta, 0)
 	client := meta.(*Client)
-	for _, team := range client.Teams {
-		l = append(l, client.withTeamID(team.ID))
+	clients := make([]schema.ClientMeta, 0, len(client.Teams))
+	for i, team := range client.Teams {
+		clients[i] = client.withTeamID(team.ID)
 	}
-	return l
+	return clients
 }
