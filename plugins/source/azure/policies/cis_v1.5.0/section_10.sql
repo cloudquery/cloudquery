e8c6e2e3a2eead565691bebcbd42f27d7669b268
Jason Kao <jason@cloudquery.io>
2022-12-20 20:31:04 -0500

    add in section 10, miscellaneous to CIS 1.5

---
+++ plugins/source/azure/policies/cis_v1.5.0/section_10.sql
@@ -0,0 +1,5 @@
+\echo "Executing CIS V1.5.0 Section 10: Miscellaneous"
+
+\set check_id '10.1'
+\echo "Executing check 10.1"
+\echo "Ensure that Resource Locks are set for Mission-Critical Azure Resources must be done manually"
