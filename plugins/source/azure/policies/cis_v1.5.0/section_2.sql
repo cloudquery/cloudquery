efe89c5a0e7af810bffe08c5e269aac17c89d65a
Jason Kao <jason@cloudquery.io>
2022-12-20 20:35:56 -0500

    Add section 2.1 Defender Plans of Microsoft Defender for Cloud

---
+++ plugins/source/azure/policies/cis_v1.5.0/section_2.sql
@@ -0,0 +1,58 @@
+\echo "Executing CIS V1.5.0 Section 2: Microsoft Defender for Cloud"
+
+-- Section 2.1 Defender Plans
+
+
+\set check_id '2.1.1'
+\echo "Executing check 2.1.1"
+\echo "Ensure That Microsoft Defender for Servers Is Set to 'On' must be done manually"
+
+\set check_id '2.1.2'
+\echo "Executing check 2.1.2"
+\echo "Ensure That Microsoft Defender for App Services Is Set To 'On' must be done manually"
+
+\set check_id '2.1.3'
+\echo "Executing check 2.1.3"
+\echo "Ensure That Microsoft Defender for Databases Is Set To 'On' must be done manually"
+
+\set check_id '2.1.4'
+\echo "Executing check 2.1.4"
+\echo "Ensure That Microsoft Defender for Azure SQL Databases Is Set To 'On' must be done manually"
+
+\set check_id '2.1.5'
+\echo "Executing check 2.1.5"
+\echo "Ensure That Microsoft Defender for SQL Servers on Machines Is Set To 'On' must be done manually"
+
+\set check_id '2.1.6'
+\echo "Executing check 2.1.6"
+\echo "Ensure That Microsoft Defender for Open-Source Relational Databases Is Set To 'On' must be done manually"
+
+\set check_id '2.1.7'
+\echo "Executing check 2.1.7"
+\echo "Ensure That Microsoft Defender for Storage Is Set To 'On' must be done manually"
+
+\set check_id '2.1.8'
+\echo "Executing check 2.1.8"
+\echo "Ensure That Microsoft Defender for Containers Is Set To 'On' must be done manually"
+
+\set check_id '2.1.9'
+\echo "Executing check 2.1.9"
+\echo "Ensure That Microsoft Defender for Cosmos DB Is Set To 'On' must be done manually"
+
+\set check_id '2.1.10'
+\echo "Executing check 2.1.10"
+\echo "Ensure That Microsoft Defender for Key Vault Is Set To 'On' must be done manually"
+
+\set check_id '2.1.11'
+\echo "Executing check 2.1.11"
+\echo "Ensure That Microsoft Defender for DNS Is Set To 'On' must be done manually"
+
+\set check_id '2.1.12'
+\echo "Executing check 2.1.12"
+\echo "Ensure That Microsoft Defender for IoT Is Set To 'On' must be done manually"
+
+\set check_id '2.1.13'
+\echo "Executing check 2.1.13"
+\echo "Ensure That Microsoft Defender for Resource Manager Is Set To 'On' must be done manually"
+
+-- Section 2.2 Auto provisioning
