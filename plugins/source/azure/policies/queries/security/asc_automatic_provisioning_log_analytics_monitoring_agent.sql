da4d8641b21759110e1d9d3c47395db12ef29aca
Jason Kao <jason@cloudquery.io>
2022-12-21 00:50:41 -0500

    update log analytics monitoring agent

--- plugins/source/azure/policies/queries/security/asc_automatic_provisioning_log_analytics_monitoring_agent.sql
+++ plugins/source/azure/policies/queries/security/asc_automatic_provisioning_log_analytics_monitoring_agent.sql
@@ -4,12 +4,14 @@ SELECT
   :'framework' as framework,
   :'check_id' as check_id,
   'Auto provisioning of the Log Analytics agent should be enabled on your subscription' as title,
-  azure_subscriptions.id AS subscription_id,
+  azure_subscription_subscriptions.id AS subscription_id,
 	azure_security_auto_provisioning_settings._cq_id,
   case
-    when auto_provision IS DISTINCT FROM 'AutoProvisionOn'
+    when properties->>'autoProvision' IS DISTINCT FROM 'On'
     then 'fail' else 'pass'
   end
 FROM
 	azure_security_auto_provisioning_settings
-	RIGHT JOIN azure_subscriptions ON azure_security_auto_provisioning_settings.subscription_id = azure_subscriptions.id
+	RIGHT JOIN azure_subscription_subscriptions ON azure_security_auto_provisioning_settings.subscription_id = azure_subscription_subscriptions.id
+
+--TODO: Seems similar to auto_provisioning_monitoring_agent_enabled.sql where that setting is for Monitoring Agent.  This query should be checked for accuracy.
