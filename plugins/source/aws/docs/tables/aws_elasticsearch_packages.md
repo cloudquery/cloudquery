0762a59bf2a2f9a1842461c72df25f06aef66726
candiduslynx <candiduslynx@users.noreply.github.com>
2022-12-21 13:04:32 +0200

    feat(aws): Add more Elastic Search resources (#5867)
    #### Summary
    
    Implements part of #5788.
    
    <!--

---
+++ plugins/source/aws/docs/tables/aws_elasticsearch_packages.md
@@ -0,0 +1,25 @@
+# Table: aws_elasticsearch_packages
+
+https://docs.aws.amazon.com/opensearch-service/latest/APIReference/API_PackageDetails.html
+
+The composite primary key for this table is (**account_id**, **region**, **id**).
+
+## Columns
+
+| Name          | Type          |
+| ------------- | ------------- |
+|_cq_source_name|String|
+|_cq_sync_time|Timestamp|
+|_cq_id|UUID|
+|_cq_parent_id|UUID|
+|account_id (PK)|String|
+|region (PK)|String|
+|available_package_version|String|
+|created_at|Timestamp|
+|error_details|JSON|
+|last_updated_at|Timestamp|
+|package_description|String|
+|id (PK)|String|
+|package_name|String|
+|package_status|String|
+|package_type|String|
--- plugins/source/aws/resources/services/elasticsearch/domains_fetch.go
+++ plugins/source/aws/resources/services/elasticsearch/domains_fetch.go
@@ -10,28 +10,72 @@ import (
 )
 
 func fetchElasticsearchDomains(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
-	c := meta.(*client.Client)
-	svc := c.Services().Elasticsearchservice
-	out, err := svc.ListDomainNames(ctx, &elasticsearchservice.ListDomainNamesInput{})
+	svc := meta.(*client.Client).Services().Elasticsearchservice
+
+	out, err := svc.ListDomainNames(ctx, nil)
 	if err != nil {
 		return err
 	}
 
 	res <- out.DomainNames
+
 	return nil
 }
 
 func getDomain(ctx context.Context, meta schema.ClientMeta, resource *schema.Resource) error {
-	c := meta.(*client.Client)
-	svc := c.Services().Elasticsearchservice
+	svc := meta.(*client.Client).Services().Elasticsearchservice
 
-	info := resource.Item.(types.DomainInfo)
-
-	domainOutput, err := svc.DescribeElasticsearchDomain(ctx, &elasticsearchservice.DescribeElasticsearchDomainInput{DomainName: info.DomainName})
+	out, err := svc.DescribeElasticsearchDomain(ctx,
+		&elasticsearchservice.DescribeElasticsearchDomainInput{
+			DomainName: resource.Item.(types.DomainInfo).DomainName,
+		},
+	)
 	if err != nil {
-		return nil
+		return err
 	}
 
-	resource.Item = domainOutput.DomainStatus
+	resource.SetItem(out.DomainStatus)
+
 	return nil
+}
+
+func resolveDomainTags(ctx context.Context, meta schema.ClientMeta, resource *schema.Resource, c schema.Column) error {
+	svc := meta.(*client.Client).Services().Elasticsearchservice
+
+	tagsOutput, err := svc.ListTags(ctx,
+		&elasticsearchservice.ListTagsInput{
+			ARN: resource.Item.(*types.ElasticsearchDomainStatus).ARN,
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	return resource.Set(c.Name, client.TagsToMap(tagsOutput.TagList))
+}
+
+func resolveAuthorizedPrincipals(ctx context.Context, meta schema.ClientMeta, resource *schema.Resource, c schema.Column) error {
+	svc := meta.(*client.Client).Services().Elasticsearchservice
+
+	input := &elasticsearchservice.ListVpcEndpointAccessInput{
+		DomainName: resource.Item.(*types.ElasticsearchDomainStatus).DomainName,
+	}
+
+	var principals []types.AuthorizedPrincipal
+	for {
+		out, err := svc.ListVpcEndpointAccess(ctx, input)
+		if err != nil {
+			return err
+		}
+
+		principals = append(principals, out.AuthorizedPrincipalList...)
+
+		if out.NextToken == nil {
+			break
+		}
+
+		input.NextToken = out.NextToken
+	}
+
+	return resource.Set(c.Name, principals)
 }
