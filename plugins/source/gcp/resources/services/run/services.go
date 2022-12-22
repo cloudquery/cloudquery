75814667cbe4f4b70690af5a5598c9ff3d52bd2b
Erez Rokah <erezrokah@users.noreply.github.com>
2022-12-22 19:39:41 +0200

    fix(gcp): List locations before getting run services (#5919)
    #### Summary
    
    Fixes https://github.com/cloudquery/cloudquery/issues/5385.
    
    In order to list run services we need to pass a location (can't use the wildcard). The v2 API of cloud run doesn't support listing locations, so we use the v1 API to list the locations, and v2 to list the services.
    Thank you @bernays for pointing me to the right API.
    
    This is implemented as a relation since it's a list->list.
    
    ### Notes:
    1. This is not a breaking change as if someone had `gcp_run_services` in the config it will now work + get the locations top level table
    2. Tried writing tests for it, but it's requires a bigger change to how we test. For testing locations we need to use HTTP server, but for testing services we need to use the gRPC server. Those have different endpoints, so we need to support replacing the client endpoint mid sync during the test. My suggestion is to wait for the v2 API to support listing locations, so we can use the gRPC approach
    
    <!--

--- plugins/source/gcp/resources/services/run/services.go
+++ plugins/source/gcp/resources/services/run/services.go
@@ -3,15 +3,8 @@
 package run
 
 import (
-	"context"
-	"google.golang.org/api/iterator"
-
-	pb "cloud.google.com/go/run/apiv2/runpb"
-
 	"github.com/cloudquery/plugin-sdk/schema"
 	"github.com/cloudquery/plugins/source/gcp/client"
-
-	"cloud.google.com/go/run/apiv2"
 )
 
 func Services() *schema.Table {
@@ -168,29 +161,4 @@ func Services() *schema.Table {
 			},
 		},
 	}
-}
-
-func fetchServices(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
-	c := meta.(*client.Client)
-	req := &pb.ListServicesRequest{
-		Parent: "projects/" + c.ProjectId + "locations/-",
-	}
-	gcpClient, err := run.NewServicesClient(ctx, c.ClientOptions...)
-	if err != nil {
-		return err
-	}
-	it := gcpClient.ListServices(ctx, req, c.CallOptions...)
-	for {
-		resp, err := it.Next()
-		if err == iterator.Done {
-			break
-		}
-		if err != nil {
-			return err
-		}
-
-		res <- resp
-
-	}
-	return nil
 }
