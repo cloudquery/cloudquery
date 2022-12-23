7908fd4b0e766f9e549d274d45e21ffd192976b0
Herman Schaaf <hermanschaaf@gmail.com>
2022-12-23 16:12:38 +0000

    feat(aws)!: Support recursive listing of AWS orgs and skipping of OUs and accounts (#5870)
    This changes the behavior of OUs to also traverse nested (child) OUs, if any. It also adds two new `org`-level config options: `skip_organization_units` and `skip_accounts`.
    
    - Closes https://github.com/cloudquery/cloudquery/issues/5029
    - Replaces https://github.com/cloudquery/cloudquery/pull/5864
    
    BEGIN_COMMIT_OVERRIDE
    feat(aws): Support recursive listing of AWS orgs and skipping of OUs and accounts
    
    BREAKING CHANGE: Child OUs will now also be recursively traversed when specifying OUs in AWS org mode
    BREAKING CHANGE: Account IDs are now validated to be exactly 12 digits
    BREAKING CHANGE: Organizational Unit (OU) IDs are now validated to match either `ou-` or `r-` formats
    
    END_COMMIT_OVERRIDE

--- plugins/source/aws/client/spec.go
+++ plugins/source/aws/client/spec.go
@@ -1,5 +1,13 @@
 package client
 
+import (
+	"errors"
+	"fmt"
+	"regexp"
+
+	"github.com/thoas/go-funk"
+)
+
 type Account struct {
 	ID              string   `json:"id"`
 	AccountName     string   `json:"account_name,omitempty"`
@@ -14,6 +22,8 @@ type Account struct {
 
 type AwsOrg struct {
 	OrganizationUnits           []string `json:"organization_units,omitempty"`
+	SkipMemberAccounts          []string `json:"skip_member_accounts,omitempty"`
+	SkipOrganizationalUnits     []string `json:"skip_organization_units,omitempty"`
 	AdminAccount                *Account `json:"admin_account"`
 	MemberCredentials           *Account `json:"member_trusted_principal"`
 	ChildAccountRoleName        string   `json:"member_role_name,omitempty"`
@@ -29,4 +39,50 @@ type Spec struct {
 	AWSDebug     bool      `json:"aws_debug,omitempty"`
 	MaxRetries   *int      `json:"max_retries,omitempty"`
 	MaxBackoff   *int      `json:"max_backoff,omitempty"`
+}
+
+func (s *Spec) Validate() error {
+	if s.Organization != nil && len(s.Accounts) > 0 {
+		return errors.New("specifying accounts via both the Accounts and Org properties is not supported. To achieve both, use multiple source configurations")
+	}
+	if s.Organization != nil {
+		if s.Organization.ChildAccountRoleName == "" {
+			return fmt.Errorf("member_role_name is required when using org configuration")
+		}
+		if err := validateAccounts(s.Organization.SkipMemberAccounts); err != nil {
+			return fmt.Errorf("invalid skip_member_accounts: %w", err)
+		}
+		if err := validateOUs(s.Organization.OrganizationUnits); err != nil {
+			return fmt.Errorf("invalid organization_units: %w", err)
+		}
+		if err := validateOUs(s.Organization.SkipOrganizationalUnits); err != nil {
+			return fmt.Errorf("invalid skip_organization_units: %w", err)
+		}
+	}
+	if s.Accounts != nil {
+		if err := validateAccounts(funk.Get(s.Accounts, "ID").([]string)); err != nil {
+			return fmt.Errorf("invalid accounts: %w", err)
+		}
+	}
+	return nil
+}
+
+func validateAccounts(accounts []string) error {
+	r := regexp.MustCompile(`^(\d{12})$`)
+	for _, account := range accounts {
+		if !r.MatchString(account) {
+			return fmt.Errorf("invalid account id: %s (should be 12 digits)", account)
+		}
+	}
+	return nil
+}
+
+func validateOUs(ous []string) error {
+	r := regexp.MustCompile(`^((ou\-[0-9a-z]{4,32}\-[a-z0-9]{8,32})|(r\-[0-9a-z]{4,32}))$`)
+	for _, ou := range ous {
+		if !r.MatchString(ou) {
+			return fmt.Errorf(`invalid OU: %s (should match "ou-*-*" or "r-*" with lowercase letters or digits)`, ou)
+		}
+	}
+	return nil
 }
