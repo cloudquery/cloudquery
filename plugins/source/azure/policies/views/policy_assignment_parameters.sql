3e837cf0d95e8b79b5ef8d0a225879793df9d90a
amanenk <andrejjrom@gmail.com>
2022-12-27 14:54:14 +0200

    wip

--- plugins/source/azure/policies/views/policy_assignment_parameters.sql
+++ plugins/source/azure/policies/views/policy_assignment_parameters.sql
@@ -1,11 +1,11 @@
 create or replace view view_azure_security_policy_parameters as
 SELECT
     id,
-    azure_resources_policy_assignments.subscription_id,
-    azure_resources_policy_assignments."name",
+    azure_policy_assignments.subscription_id,
+    azure_policy_assignments."name",
     parameters.*,
-    azure_resources_policy_assignments.parameters -> parameters.param ->> 'value' AS value
+    azure_policy_assignments.properties -> parameters.param ->> 'value' AS value
 FROM
-    azure_resources_policy_assignments,
-    jsonb_object_keys(azure_resources_policy_assignments.parameters) AS parameters ("param")
-WHERE azure_resources_policy_assignments."name" = 'SecurityCenterBuiltIn';
+    azure_policy_assignments,
+    jsonb_object_keys(azure_policy_assignments.properties) AS parameters ("param")
+WHERE azure_policy_assignments."name" = 'SecurityCenterBuiltIn';
