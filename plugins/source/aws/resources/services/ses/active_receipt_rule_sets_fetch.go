8a400b37c4768bc4452e1734a8ecf221f434629f
erezrokah <erezrokah@users.noreply.github.com>
2022-12-27 11:23:14 +0200

    fix(aws_ses_active_receipt_rule_sets): Don't call DescribeActiveReceiptRuleSet on unsupported regions

--- plugins/source/aws/resources/services/ses/active_receipt_rule_sets_fetch.go
+++ plugins/source/aws/resources/services/ses/active_receipt_rule_sets_fetch.go
@@ -7,9 +7,24 @@ import (
 	"github.com/cloudquery/plugin-sdk/schema"
 )
 
+var supportedRegions = []string{"us-east-1", "us-west-2", "eu-west-1"}
+
+func isRegionSupported(region string) bool {
+	for _, r := range supportedRegions {
+		if r == region {
+			return true
+		}
+	}
+	return false
+}
+
 func fetchSesActiveReceiptRuleSets(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
 	c := meta.(*client.Client)
 	svc := c.Services().Ses
+
+	if !isRegionSupported(c.Region) {
+		return nil
+	}
 
 	set, err := svc.DescribeActiveReceiptRuleSet(ctx, nil)
 	if err != nil {
