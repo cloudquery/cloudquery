dce8cae630636de5b59c24d368dae8ac18cdd9ad
erezrokah <erezrokah@users.noreply.github.com>
2022-12-28 15:13:23 +0200

    fix(bigquery-spec): Move `batch_size` from the plugin spec to the top level spec

--- plugins/destination/bigquery/client/spec.go
+++ plugins/destination/bigquery/client/spec.go
@@ -34,7 +34,6 @@ type Spec struct {
 	DatasetLocation       string                 `json:"dataset_location"`
 	TimePartitioning      TimePartitioningOption `json:"time_partitioning"`
 	ServiceAccountKeyJSON string                 `json:"service_account_key_json"`
-	BatchSize             int                    `json:"batch_size,omitempty"`
 }
 
 const defaultBatchSize = 1000
@@ -42,9 +41,6 @@ const defaultBatchSize = 1000
 func (s *Spec) SetDefaults() {
 	if s.TimePartitioning == "" {
 		s.TimePartitioning = TimePartitioningOptionNone
-	}
-	if s.BatchSize <= 0 {
-		s.BatchSize = defaultBatchSize
 	}
 }
 
