eaee194dcac13a48521d1236c3c9e9ddff813b70
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2022-12-28 23:22:15 +0200

    feat: Add destination GCS

---
+++ plugins/destination/gcs/client/spec.go
@@ -0,0 +1,32 @@
+package client
+
+import (
+	"fmt"
+)
+
+type FormatType string
+
+const (
+	FormatTypeCSV  = "csv"
+	FormatTypeJSON = "json"
+)
+
+type Spec struct {
+	Bucket   string     `json:"bucket,omitempty"`
+	Path     string     `json:"key,omitempty"`
+	Format   FormatType `json:"format,omitempty"`
+	NoRotate bool       `json:"no_rotate,omitempty"`
+}
+
+func (*Spec) SetDefaults() {}
+
+func (s *Spec) Validate() error {
+	if s.Bucket == "" {
+		return fmt.Errorf("bucket is required")
+	}
+	if s.Format == "" {
+		return fmt.Errorf("format is required")
+	}
+
+	return nil
+}
