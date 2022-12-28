89088fd278e99cc611bd157d6d3e1441225fd981
Kemal Hadimli <disq@users.noreply.github.com>
2022-12-28 23:07:49 +0000

    add DSN

--- plugins/source/shopify/main.go
+++ plugins/source/shopify/main.go
@@ -5,7 +5,7 @@ import (
 	"github.com/cloudquery/plugin-sdk/serve"
 )
 
-const sentryDSN = "" // TODO
+const sentryDSN = "https://9a27c2df309b49fa9520937069b370c6@o1396617.ingest.sentry.io/4504409131515904"
 
 func main() {
 	serve.Source(plugin.Plugin(), serve.WithSourceSentryDSN(sentryDSN))
