d616bf854582a334c2109a5eebc91cda55e5a1e1
Herman Schaaf <hermanschaaf@gmail.com>
2022-12-30 16:46:53 +0000

    Update progress bar on secondly interval

--- cli/cmd/sync2.go
+++ cli/cmd/sync2.go
@@ -119,46 +119,41 @@ func syncConnectionV2(ctx context.Context, cqDir string, sourceClient *clients.S
 		})
 	}
 
-	var done chan struct{}
-	var tickerDone chan struct{}
-	go func() {
-		t := time.NewTicker(500 * time.Millisecond).C
+	g.Go(func() error {
+		t := time.NewTicker(1 * time.Second)
+		defer func() {
+			for i := range destSubscriptions {
+				close(destSubscriptions[i])
+			}
+			t.Stop()
+		}()
 		for {
 			select {
-			case <-t:
-				bar.Add(0)
-			case <-done:
-				tickerDone <- struct{}{}
-				return
-			}
-		}
-	}()
-	g.Go(func() error {
-		for resource := range resources {
-			totalResources++
-			_ = bar.Add(1)
-			for i := range destSubscriptions {
-				select {
-				case <-gctx.Done():
-					return gctx.Err()
-				case destSubscriptions[i] <- resource:
+			case resource, ok := <-resources:
+				if !ok {
+					return nil
 				}
+				totalResources++
+				_ = bar.Add(1)
+				for i := range destSubscriptions {
+					select {
+					case <-gctx.Done():
+						return gctx.Err()
+					case destSubscriptions[i] <- resource:
+					}
+				}
+			case <-t.C:
+				_ = bar.Add(0)
+			case <-gctx.Done():
+				return nil
 			}
 		}
-		for i := range destSubscriptions {
-			close(destSubscriptions[i])
-		}
-		return nil
 	})
 
 	if err := g.Wait(); err != nil {
-		done <- struct{}{}
 		_ = bar.Finish()
 		return err
 	}
-	done <- struct{}{}
-	<-tickerDone
-
 	_ = bar.Finish()
 	syncTimeTook := time.Since(syncTime)
 
