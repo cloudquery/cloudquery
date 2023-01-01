5c7b80dddfd9c87ddb540cb273a01f1506937a3f
Erez Rokah <erezrokah@users.noreply.github.com>
2023-01-01 18:41:52 +0200

    test(s3): Use pre-existing bucket for tests (#6225)
    <!-- 🎉 Thank you for making CloudQuery awesome by submitting a PR 🎉 -->
    
    #### Summary
    
    Reverts
    https://github.com/cloudquery/cloudquery/pull/6179/commits/356137db4191f94b500db093c7365a8dbf88c234
    as we decide to exclude this bucket from the nuke process, see
    https://github.com/cloudquery/deployments/pull/49/files
    
    <!--
    Use the following steps to ensure your PR is ready to be reviewed
    
    - [ ] Read the [contribution guidelines](../blob/main/CONTRIBUTING.md)
    🧑‍🎓
    - [ ] Test locally on your own infrastructure
    - [ ] Run `go fmt` to format your code 🖊
    - [ ] Lint your changes via `golangci-lint run` 🚨 (install golangci-lint
    [here](https://golangci-lint.run/usage/install/#local-installation))
    - [ ] Update or add tests 🧪
    - [ ] Ensure the status checks below are successful ✅
    --->

--- plugins/destination/s3/client/client_test.go
+++ plugins/destination/s3/client/client_test.go
@@ -1,41 +1,14 @@
 package client
 
 import (
-	"context"
 	"testing"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/cloudquery/plugin-sdk/plugins/destination"
 )
 
-func getClient(t *testing.T) *s3.Client {
-	t.Helper()
-
-	cfg, err := config.LoadDefaultConfig(context.Background())
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	return s3.NewFromConfig(cfg)
-}
-
-func ensureBucketExists(t *testing.T, bucket string) {
-	t.Helper()
-	s3Client := getClient(t)
-	_, err := s3Client.CreateBucket(context.TODO(), &s3.CreateBucketInput{
-		Bucket: aws.String(bucket),
-	})
-	if err != nil {
-		t.Fatalf("failed to create bucket %s: %v", bucket, err)
-	}
-}
+const bucket = "cq-playground-test"
 
 func TestPluginCSV(t *testing.T) {
-	const bucket = "cq-playground-test-csv"
-	ensureBucketExists(t, bucket)
-
 	p := destination.NewPlugin("s3", "development", New, destination.WithManagedWriter())
 
 	destination.PluginTestSuiteRunner(t, p,
@@ -54,9 +27,6 @@ func TestPluginCSV(t *testing.T) {
 }
 
 func TestPluginJSON(t *testing.T) {
-	const bucket = "cq-playground-test-json"
-	ensureBucketExists(t, bucket)
-
 	p := destination.NewPlugin("s3", "development", New, destination.WithManagedWriter())
 
 	destination.PluginTestSuiteRunner(t, p,
