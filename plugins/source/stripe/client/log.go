3318a97be8588e17ceca321ddaf4a7dcfe1bbde4
Kemal Hadimli <disq@users.noreply.github.com>
2023-01-03 15:41:09 +0000

    log: use any

--- plugins/source/stripe/client/log.go
+++ plugins/source/stripe/client/log.go
@@ -6,18 +6,18 @@ type LeveledLogger struct {
 	zerolog.Logger
 }
 
-func (l *LeveledLogger) Debugf(format string, args ...interface{}) {
+func (l *LeveledLogger) Debugf(format string, args ...any) {
 	l.Logger.Debug().Msgf(format, args...)
 }
 
-func (l *LeveledLogger) Infof(format string, args ...interface{}) {
+func (l *LeveledLogger) Infof(format string, args ...any) {
 	l.Logger.Info().Msgf(format, args...)
 }
 
-func (l *LeveledLogger) Warnf(format string, args ...interface{}) {
+func (l *LeveledLogger) Warnf(format string, args ...any) {
 	l.Logger.Warn().Msgf(format, args...)
 }
 
-func (l *LeveledLogger) Errorf(format string, args ...interface{}) {
+func (l *LeveledLogger) Errorf(format string, args ...any) {
 	l.Logger.Error().Msgf(format, args...)
 }
