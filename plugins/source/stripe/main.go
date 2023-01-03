53699926bf96e25393fe3a7830df6e7be5efd7e3
Kemal Hadimli <disq@users.noreply.github.com>
2023-01-03 16:02:40 +0000

    Add Sentry DSN

--- plugins/source/stripe/main.go
+++ plugins/source/stripe/main.go
@@ -5,7 +5,7 @@ import (
 	"github.com/cloudquery/plugin-sdk/serve"
 )
 
-const sentryDSN = "" // TODO
+const sentryDSN = "https://4606a1601b52488889313375ba91df89@o1396617.ingest.sentry.io/4504441433751552"
 
 func main() {
 	serve.Source(plugin.Plugin(), serve.WithSourceSentryDSN(sentryDSN))
