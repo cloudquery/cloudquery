619e83a5ac39761c684184dbd4cfb04eacbd7674
Herman Schaaf <hermanschaaf@gmail.com>
2023-01-04 17:24:50 +0000

    Lint

--- plugins/source/hackernews/client/testing.go
+++ plugins/source/hackernews/client/testing.go
@@ -27,13 +27,10 @@ func MockTestHelper(t *testing.T, table *schema.Table, builder func(*testing.T, 
 	l := zerolog.New(zerolog.NewTestWriter(t)).Output(
 		zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.StampMicro},
 	).Level(zerolog.DebugLevel).With().Timestamp().Logger()
-	newTestExecutionClient := func(ctx context.Context, logger zerolog.Logger, spec specs.Source, be backend.Backend) (schema.ClientMeta, error) {
-		if opts.Backend != nil {
-			be = opts.Backend
-		}
+	newTestExecutionClient := func(ctx context.Context, logger zerolog.Logger, spec specs.Source, _ backend.Backend) (schema.ClientMeta, error) {
 		return &Client{
 			logger:     l,
-			Backend:    be,
+			Backend:    opts.Backend,
 			HackerNews: builder(t, ctrl),
 			Spec:       Spec{ItemConcurrency: 10},
 		}, nil
