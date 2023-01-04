8b36a935b2c0187b4165cfab6a51289b6742808c
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2023-01-04 14:34:50 +0200

    done till cloudwatchlogs

--- plugins/source/aws/resources/services/backup/plan_selections.go
+++ plugins/source/aws/resources/services/backup/plan_selections.go
@@ -5,6 +5,8 @@ package backup
 import (
 	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
 	"github.com/cloudquery/plugin-sdk/schema"
+	"github.com/cloudquery/plugin-sdk/transformers"
+	"github.com/aws/aws-sdk-go-v2/service/backup"
 )
 
 func PlanSelections() *schema.Table {
@@ -13,6 +15,7 @@ func PlanSelections() *schema.Table {
 		Description: `https://docs.aws.amazon.com/aws-backup/latest/devguide/API_GetBackupSelection.html`,
 		Resolver:    fetchBackupPlanSelections,
 		Multiplex:   client.ServiceAccountRegionMultiplexer("backup"),
+		Transform: transformers.TransformWithStruct(&backup.GetBackupSelectionOutput{}),
 		Columns: []schema.Column{
 			{
 				Name:     "account_id",
@@ -28,36 +31,6 @@ func PlanSelections() *schema.Table {
 				Name:     "plan_arn",
 				Type:     schema.TypeString,
 				Resolver: schema.ParentColumnResolver("arn"),
-			},
-			{
-				Name:     "backup_plan_id",
-				Type:     schema.TypeString,
-				Resolver: schema.PathResolver("BackupPlanId"),
-			},
-			{
-				Name:     "backup_selection",
-				Type:     schema.TypeJSON,
-				Resolver: schema.PathResolver("BackupSelection"),
-			},
-			{
-				Name:     "creation_date",
-				Type:     schema.TypeTimestamp,
-				Resolver: schema.PathResolver("CreationDate"),
-			},
-			{
-				Name:     "creator_request_id",
-				Type:     schema.TypeString,
-				Resolver: schema.PathResolver("CreatorRequestId"),
-			},
-			{
-				Name:     "selection_id",
-				Type:     schema.TypeString,
-				Resolver: schema.PathResolver("SelectionId"),
-			},
-			{
-				Name:     "result_metadata",
-				Type:     schema.TypeJSON,
-				Resolver: schema.PathResolver("ResultMetadata"),
 			},
 		},
 	}
