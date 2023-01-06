a7ded025033c603a2480113cc22a478dbd6480b5
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2023-01-06 22:14:10 +0200

    feat(pagerduty): Migrate codegen to transformations

--- plugins/source/pagerduty/resources/services/incidents/incident_log_entries.go
+++ plugins/source/pagerduty/resources/services/incidents/incident_log_entries.go
@@ -1,7 +1,9 @@
 package incidents
 
 import (
+	"github.com/PagerDuty/go-pagerduty"
 	"github.com/cloudquery/plugin-sdk/schema"
+	"github.com/cloudquery/plugin-sdk/transformers"
 )
 
 func IncidentLogEntries() *schema.Table {
@@ -9,6 +11,7 @@ func IncidentLogEntries() *schema.Table {
 		Name:        "pagerduty_incident_log_entries",
 		Description: `https://developer.pagerduty.com/api-reference/367602cbc1c28-list-log-entries-for-an-incident`,
 		Resolver:    fetchIncidentLogEntries,
+		Transform:   transformers.TransformWithStruct(&pagerduty.LogEntry{}, transformers.WithSkipFields("HTMLURL")),
 		Columns: []schema.Column{
 			{
 				Name:     "id",
@@ -19,79 +22,9 @@ func IncidentLogEntries() *schema.Table {
 				},
 			},
 			{
-				Name:     "type",
-				Type:     schema.TypeString,
-				Resolver: schema.PathResolver("Type"),
-			},
-			{
-				Name:     "summary",
-				Type:     schema.TypeString,
-				Resolver: schema.PathResolver("Summary"),
-			},
-			{
-				Name:     "self",
-				Type:     schema.TypeString,
-				Resolver: schema.PathResolver("Self"),
-			},
-			{
 				Name:     "html_url",
 				Type:     schema.TypeString,
 				Resolver: schema.PathResolver("HTMLURL"),
-			},
-			{
-				Name:     "created_at",
-				Type:     schema.TypeTimestamp,
-				Resolver: schema.PathResolver("CreatedAt"),
-			},
-			{
-				Name:     "agent",
-				Type:     schema.TypeJSON,
-				Resolver: schema.PathResolver("Agent"),
-			},
-			{
-				Name:     "channel",
-				Type:     schema.TypeJSON,
-				Resolver: schema.PathResolver("Channel"),
-			},
-			{
-				Name:     "teams",
-				Type:     schema.TypeJSON,
-				Resolver: schema.PathResolver("Teams"),
-			},
-			{
-				Name:     "contexts",
-				Type:     schema.TypeJSON,
-				Resolver: schema.PathResolver("Contexts"),
-			},
-			{
-				Name:     "acknowledgement_timeout",
-				Type:     schema.TypeInt,
-				Resolver: schema.PathResolver("AcknowledgementTimeout"),
-			},
-			{
-				Name:     "event_details",
-				Type:     schema.TypeJSON,
-				Resolver: schema.PathResolver("EventDetails"),
-			},
-			{
-				Name:     "assignees",
-				Type:     schema.TypeJSON,
-				Resolver: schema.PathResolver("Assignees"),
-			},
-			{
-				Name:     "incident",
-				Type:     schema.TypeJSON,
-				Resolver: schema.PathResolver("Incident"),
-			},
-			{
-				Name:     "service",
-				Type:     schema.TypeJSON,
-				Resolver: schema.PathResolver("Service"),
-			},
-			{
-				Name:     "user",
-				Type:     schema.TypeJSON,
-				Resolver: schema.PathResolver("User"),
 			},
 		},
 	}
