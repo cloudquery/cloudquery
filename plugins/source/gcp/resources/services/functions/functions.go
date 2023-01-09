855250380b5610c9292c4e1c900bbf086e26fd35
Erez Rokah <erezrokah@users.noreply.github.com>
2023-01-09 22:01:57 +0200

    fix(gcp-resources): Add `project_id` and `name` as PKs of `gcp_functions_functions` (#6538)
    #### Summary
    
    `gcp_functions_functions` was missing primary keys.
    
    Non breaking as the data exists
    
    <!--

--- plugins/source/gcp/resources/services/functions/functions.go
+++ plugins/source/gcp/resources/services/functions/functions.go
@@ -25,6 +25,17 @@ func Functions() *schema.Table {
 				Name:     "project_id",
 				Type:     schema.TypeString,
 				Resolver: client.ResolveProject,
+				CreationOptions: schema.ColumnCreationOptions{
+					PrimaryKey: true,
+				},
+			},
+			{
+				Name:     "name",
+				Type:     schema.TypeString,
+				Resolver: schema.PathResolver("Name"),
+				CreationOptions: schema.ColumnCreationOptions{
+					PrimaryKey: true,
+				},
 			},
 		},
 	}
