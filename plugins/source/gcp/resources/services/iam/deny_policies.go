11c437e5cc1f74d392b65e40075a56fd0f2d8c8d
erezrokah <erezrokah@users.noreply.github.com>
2023-01-09 17:54:31 +0200

    fix(gcp-resources): Add `project_id` and `name` as PKs of `gcp_iam_deny_policies`

--- plugins/source/gcp/resources/services/iam/deny_policies.go
+++ plugins/source/gcp/resources/services/iam/deny_policies.go
@@ -19,6 +19,17 @@ func DenyPolicies() *schema.Table {
 				Name:     "project_id",
 				Type:     schema.TypeString,
 				Resolver: client.ResolveProject,
+				CreationOptions: schema.ColumnCreationOptions{
+					PrimaryKey: true,
+				},
+			},
+			{
+				Name:     "name",
+				Type:     schema.TypeString,
+				Resolver: schema.PathResolver("Name"),
+				CreationOptions: schema.ColumnCreationOptions{
+					PrimaryKey: true,
+				},
 			},
 		},
 	}
