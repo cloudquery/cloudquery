0fdeb5476b974183bc299b31d8429fe5bdf24e8d
erezrokah <erezrokah@users.noreply.github.com>
2023-01-09 12:21:33 +0200

    refactor: Use type transformer

--- plugins/source/azuredevops/resources/services/core/projects.go
+++ plugins/source/azuredevops/resources/services/core/projects.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"github.com/cloudquery/cloudquery/plugins/source/azuredevops/client"
 	"github.com/cloudquery/plugin-sdk/schema"
 	"github.com/cloudquery/plugin-sdk/transformers"
 	"github.com/microsoft/azure-devops-go-api/azuredevops/v6/core"
@@ -10,13 +11,6 @@ func Projects() *schema.Table {
 	return &schema.Table{
 		Name:      "azuredevops_core_projects",
 		Resolver:  fetchProjects,
-		Transform: transformers.TransformWithStruct(&core.TeamProjectReference{}, transformers.WithSkipFields("Id")),
-		Columns: []schema.Column{
-			{
-				Name:     "id",
-				Type:     schema.TypeUUID,
-				Resolver: schema.PathResolver("Id"),
-			},
-		},
+		Transform: transformers.TransformWithStruct(&core.TeamProjectReference{}, client.Options()...),
 	}
 }
