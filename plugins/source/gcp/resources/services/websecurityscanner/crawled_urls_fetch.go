a7c9ba69b984ecadc711f75168da20400d3d0540
shimonp21 <shimonp21@gmail.com>
2023-01-11 13:02:27 +0200

    feat: Gcp websecurityscanner

---
+++ plugins/source/gcp/resources/services/websecurityscanner/crawled_urls_fetch.go
@@ -0,0 +1,37 @@
+package websecurityscanner
+
+import (
+	"context"
+
+	websecurityscanner "cloud.google.com/go/websecurityscanner/apiv1"
+	pb "cloud.google.com/go/websecurityscanner/apiv1/websecurityscannerpb"
+	"github.com/cloudquery/plugin-sdk/schema"
+	"github.com/cloudquery/plugins/source/gcp/client"
+	"google.golang.org/api/iterator"
+)
+
+func fetchCrawledUrls(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
+	c := meta.(*client.Client)
+	parentItem := parent.Item.(*pb.ScanRun)
+
+	gcpClient, err := websecurityscanner.NewClient(ctx, c.ClientOptions...)
+	if err != nil {
+		return err
+	}
+
+	it := gcpClient.ListCrawledUrls(ctx, &pb.ListCrawledUrlsRequest{
+		Parent: parentItem.Name,
+	}, c.CallOptions...)
+	for {
+		resp, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		res <- resp
+	}
+	return nil
+}
