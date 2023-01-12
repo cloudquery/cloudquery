4503e411829cfa34197f7e3e4602414c1d9e3e20
amanenk <andrejjrom@gmail.com>
2023-01-12 17:51:16 +0200

    fixed storage_account_id

--- plugins/source/azure/policies/queries/servicebus/resource_logs_in_service_bus_should_be_enabled.sql
+++ plugins/source/azure/policies/queries/servicebus/resource_logs_in_service_bus_should_be_enabled.sql
@@ -6,7 +6,7 @@ WITH logging_enabled AS (
                      ON s.cq_id = l.diagnostic_setting_cq_id
   WHERE l.enabled = TRUE
     AND l.category = 'AuditEvent'
-    AND (s.storage_account_id IS NOT NULL OR s.storage_account_id IS DISTINCT FROM '')
+    AND (s.diagnostic_settings_resource -> 'properties' -> 'storageAccountId' as diagnostic_settings_resource -> 'properties' ->> 'storageAccountId' as storage_account_id IS NOT NULL OR s.storage_account_id IS DISTINCT FROM '')
     AND retention_policy_enabled = TRUE
 )
 SELECT 
