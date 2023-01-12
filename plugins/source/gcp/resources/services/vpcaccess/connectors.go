59c32dc65c9df6870c4d973893a7e722c45188fd
shimonp21 <37939765+shimonp21@users.noreply.github.com>
2023-01-12 14:27:23 +0200

    feat: ADD GCP VPC Access (#6755)
    #### Summary
    
    <!--
    Explain what problem this PR addresses
    -->
    
    <!--

---
+++ plugins/source/gcp/resources/services/vpcaccess/connectors.go
@@ -0,0 +1,37 @@
+package vpcaccess
+
+import (
+	pb "cloud.google.com/go/vpcaccess/apiv1/vpcaccesspb"
+
+	"github.com/cloudquery/plugin-sdk/schema"
+	"github.com/cloudquery/plugin-sdk/transformers"
+	"github.com/cloudquery/plugins/source/gcp/client"
+)
+
+func Connectors() *schema.Table {
+	return &schema.Table{
+		Name:        "gcp_vpcaccess_connectors",
+		Description: `https://cloud.google.com/vpc/docs/reference/vpcaccess/rest/v1/projects.locations.connectors`,
+		Resolver:    fetchConnectors,
+		Multiplex:   client.ProjectMultiplexEnabledServices("vpcaccess.googleapis.com"),
+		Transform:   transformers.TransformWithStruct(&pb.Connector{}, client.Options()...),
+		Columns: []schema.Column{
+			{
+				Name:     "project_id",
+				Type:     schema.TypeString,
+				Resolver: client.ResolveProject,
+				CreationOptions: schema.ColumnCreationOptions{
+					PrimaryKey: true,
+				},
+			},
+			{
+				Name:     "name",
+				Type:     schema.TypeString,
+				Resolver: schema.PathResolver("Name"),
+				CreationOptions: schema.ColumnCreationOptions{
+					PrimaryKey: true,
+				},
+			},
+		},
+	}
+}
