9cb57978deb7ed5e2380f5e0934e5140a4ab7b7f
shimonp21 <shimonp21@gmail.com>
2023-01-16 12:51:39 +0200

    feat: Hubspot plugin

---
+++ plugins/source/hubspot/resources/services/crm/quotes_fetch.go
@@ -0,0 +1,45 @@
+package crm
+
+import (
+	"context"
+
+	"github.com/clarkmcc/go-hubspot"
+	"github.com/clarkmcc/go-hubspot/generated/v3/quotes"
+	"github.com/cloudquery/cloudquery/plugins/source/hubspot/client"
+	"github.com/cloudquery/plugin-sdk/schema"
+)
+
+func fetchQuotes(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
+	hubspotClient := quotes.NewAPIClient(quotes.NewConfiguration())
+	cqClient := meta.(*client.Client)
+
+	var after string
+	for {
+		req := hubspotClient.BasicApi.GetPage(hubspot.WithAuthorizer(ctx, cqClient.Authorizer)).Limit(client.DefaultPageSize)
+
+		if len(after) > 0 {
+			req = req.After(after)
+		}
+		out, _, err := req.Execute()
+		if err != nil {
+			return err
+		}
+
+		res <- out.Results
+
+		if !out.HasPaging() {
+			break
+		}
+		paging := out.GetPaging()
+		if !paging.HasNext() {
+			break
+		}
+		next := paging.GetNext()
+		after = next.After
+		if after == "" {
+			break
+		}
+	}
+
+	return nil
+}
