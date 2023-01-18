9edc1e5001ddb9d8818002c80a98633fd7aa320a
bbernays <ben@bernays.me>
2023-01-18 13:57:05 -0600

    Update client_test.go

--- plugins/destination/kinesisfirehose/client/client_test.go
+++ plugins/destination/kinesisfirehose/client/client_test.go
@@ -9,8 +9,7 @@ import (
 const streamARN = "cq-playground-test"
 
 func TestPluginJSON(t *testing.T) {
-	p := destination.NewPlugin("kinesisfirehose", "development", New, destination.WithManagedWriter())
-
+	p := destination.NewPlugin("kinesisfirehose", "development", New, destination.WithDefaultBatchSize(500))
 	destination.PluginTestSuiteRunner(t, p,
 		Spec{
 			NoRotate:  true,
