652db1a244b533202ee3b627087756a58e72e67e
bbernays <ben@bernays.me>
2023-01-18 12:09:49 -0600

    Update write.go

--- plugins/destination/kinesisfirehose/client/write.go
+++ plugins/destination/kinesisfirehose/client/write.go
@@ -3,16 +3,29 @@ package client
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/arn"
 	"github.com/aws/aws-sdk-go-v2/service/firehose"
 	"github.com/aws/aws-sdk-go-v2/service/firehose/types"
 	"github.com/cloudquery/plugin-sdk/schema"
 )
 
 func (c *Client) WriteTableBatch(ctx context.Context, table *schema.Table, data [][]any) error {
+	parsedARN, err := arn.Parse(c.pluginSpec.StreamARN)
+	if err != nil {
+		c.logger.Error().Err(err).Msg("invalid firehose stream ARN")
+		return err
+	}
+	resource := strings.Split(parsedARN.Resource, "/")
+	if len(resource) != 2 {
+		c.logger.Error().Err(err).Msg("invalid firehose stream ARN")
+		return fmt.Errorf("invalid firehose stream ARN")
+	}
 	recordsBatchInput := &firehose.PutRecordBatchInput{
-		DeliveryStreamName: aws.String(c.pluginSpec.StreamARN),
+		DeliveryStreamName: aws.String(resource[1]),
 	}
 
 	for _, resource := range data {
@@ -30,10 +43,13 @@ func (c *Client) WriteTableBatch(ctx context.Context, table *schema.Table, data 
 	}
 
 	resp, err := c.firehoseClient.PutRecordBatch(ctx, recordsBatchInput)
-
+	if err != nil {
+		c.logger.Error().Err(err).Msg("failed to write to firehose")
+		return err
+	}
 	if aws.ToInt32(resp.FailedPutCount) > int32(0) {
 		// Handle partial success
 		c.logger.Warn().Msg("partial success in writing to firehose")
 	}
-	return err
+	return nil
 }
