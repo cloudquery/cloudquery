6ba30112ef855a1d4678cbb5e409cd34da90c20e
erezrokah <erezrokah@users.noreply.github.com>
2023-01-23 19:56:25 +0200

    chore: Add code comment

--- plugins/source/gcp/resources/services/resourcemanager/project_policies_fetch.go
+++ plugins/source/gcp/resources/services/resourcemanager/project_policies_fetch.go
@@ -14,6 +14,7 @@ func fetchProjectPolicies(ctx context.Context, meta schema.ClientMeta, r *schema
 	if err != nil {
 		return err
 	}
+	// We need to use the protobut client to get the current version of the policy struct (v3)
 	output, err := projectsClient.Projects.GetIamPolicy("projects/"+c.ProjectId, &pb.GetIamPolicyRequest{}).Context(ctx).Do()
 	if err != nil {
 		return err
