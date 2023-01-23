69f61f5553afe412d32d9c68a54214fb762b8ae4
Kemal Hadimli <disq@users.noreply.github.com>
2023-01-23 11:19:56 +0000

    clean up spec

--- plugins/source/mixpanel/client/client.go
+++ plugins/source/mixpanel/client/client.go
@@ -2,7 +2,6 @@ package client
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"net/http"
 	"time"
@@ -58,47 +57,9 @@ func Configure(ctx context.Context, logger zerolog.Logger, s specs.Source, o sou
 }
 
 func getServiceClient(logger zerolog.Logger, spec *Spec) (*mixpanel.Client, error) {
-	if spec.Secret == "" {
-		// allow empty username for the deprecated project secret method: https://developer.mixpanel.com/reference/project-secret
-		return nil, errors.New("no credentials provided")
-	}
-	if spec.ProjectID < 1 {
-		return nil, errors.New("no project id provided")
-	}
-
-	const dateFormat = "2006-01-02"
-
-	if spec.StartDate == "" {
-		dt := time.Now().UTC().AddDate(0, 0, -30).Format(dateFormat)
-		logger.Info().Str("start_date", dt).Msg("no start date provided, defaulting to 30 days ago")
-		spec.StartDate = dt
-	}
-	if spec.EndDate == "" {
-		dt := time.Now().UTC().AddDate(0, 0, -1).Format(dateFormat)
-		logger.Info().Str("end_date", dt).Msg("no end date provided, defaulting to yesterday")
-		spec.EndDate = dt
-	}
-
-	if _, err := time.Parse(dateFormat, spec.StartDate); err != nil {
-		return nil, fmt.Errorf("invalid start date format: %w", err)
-	}
-	if _, err := time.Parse(dateFormat, spec.EndDate); err != nil {
-		return nil, fmt.Errorf("invalid end date format: %w", err)
-	}
-
-	if spec.Timeout < 1 {
-		spec.Timeout = 30
-	}
-	if spec.MaxRetries < 1 {
-		spec.MaxRetries = 5
-	}
-
-	rg, err := mixpanel.ParseRegion(spec.Region)
-	if err != nil {
+	spec.SetDefaults(logger)
+	if err := spec.Validate(); err != nil {
 		return nil, err
-	}
-	if rg == mixpanel.RegionNone {
-		rg = mixpanel.RegionUS
 	}
 
 	return mixpanel.New(
@@ -107,7 +68,7 @@ func getServiceClient(logger zerolog.Logger, spec *Spec) (*mixpanel.Client, erro
 			HC: &http.Client{
 				Timeout: time.Duration(spec.Timeout) * time.Second,
 			},
-			Region:     rg,
+			Region:     spec.region,
 			APIUser:    spec.Username,
 			APISecret:  spec.Secret,
 			ProjectID:  spec.ProjectID,
