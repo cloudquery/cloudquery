bc03159e28bb9304a80e54f08ccf69e6bd379e63
bbernays <ben@cloudquery.io>
2023-01-24 14:36:22 +0000

    Fix gcs

--- plugins/destination/gcs/client/spec.go
+++ plugins/destination/gcs/client/spec.go
@@ -13,12 +13,7 @@ type Spec struct {
 	*filetypes.FileSpec
 }
 
-func (s *Spec) SetDefaults() {
-	if s.FileSpec == nil {
-		s.FileSpec = &filetypes.FileSpec{}
-	}
-	s.FileSpec.SetDefaults()
-}
+func (s *Spec) SetDefaults() {}
 
 func (s *Spec) Validate() error {
 	if s.Bucket == "" {
