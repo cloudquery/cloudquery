ea21d5e8f12d83354085971ffed3c745d4163ffa
bbernays <ben@cloudquery.io>
2023-01-24 16:51:12 +0000

    Clean inputss

--- plugins/destination/s3/client/client.go
+++ plugins/destination/s3/client/client.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"path"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -71,7 +72,7 @@ func New(ctx context.Context, logger zerolog.Logger, spec specs.Destination) (de
 	// we want to run this test because we want it to fail early if the bucket is not accessible
 	if _, err := c.uploader.Upload(ctx, &s3.PutObjectInput{
 		Bucket: aws.String(c.pluginSpec.Bucket),
-		Key:    aws.String("/tmp/.cq-test-file"),
+		Key:    aws.String(path.Join(spec.Path, "tmp", ".cq-test-file")),
 		Body:   bytes.NewReader([]byte("")),
 	}); err != nil {
 		return nil, fmt.Errorf("failed to write test file to S3: %w", err)
