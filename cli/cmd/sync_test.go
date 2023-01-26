290e7d87fe3266551726d345b99aaa6492bd18ca
erezrokah <erezrokah@users.noreply.github.com>
2023-01-26 12:24:19 +0200

    test(cli): Add multiple sources test

--- cli/cmd/sync_test.go
+++ cli/cmd/sync_test.go
@@ -25,9 +25,10 @@ func getSyncCommand(t *testing.T, config string) (*cobra.Command, string) {
 
 func TestSync(t *testing.T) {
 	configs := []struct {
-		name   string
-		config string
-		err    string
+		name        string
+		config      string
+		err         string
+		logMessages []string
 	}{
 		{
 			name:   "should finish successfully for valid config",
@@ -37,6 +38,13 @@ func TestSync(t *testing.T) {
 			name:   "should fail with missing path error when path is missing",
 			config: "sync-missing-path-error.yml",
 			err:    "Error: failed to validate destination test: path is required",
+		},
+		{
+			name:   "should sync multiple sources and destinations",
+			config: "multiple-sources-destinations.yml",
+			logMessages: []string{
+				`Start sync destinations=\["test\-1.*?","test\-2.*?"\] module=cli source="test\-1.*?"`,
+				`Start sync destinations=\["test\-2.*?","test\-1.*?"\] module=cli source="test\-2.*?"`},
 		},
 	}
 
@@ -48,8 +56,13 @@ func TestSync(t *testing.T) {
 
 			// check that log was written and contains some lines from the plugin
 			b, logFileError := os.ReadFile(path.Join(cqDir, "cloudquery.log"))
+			logContent := string(b)
 			require.NoError(t, logFileError, "failed to read cloudquery.log")
-			require.NotEmpty(t, string(b), "cloudquery.log empty; expected some logs")
+			require.NotEmpty(t, logContent, "cloudquery.log empty; expected some logs")
+
+			for _, logMessage := range tc.logMessages {
+				require.Regexp(t, logMessage, logContent, "log message not found in cloudquery.log")
+			}
 
 			if tc.err == "" {
 				require.NoError(t, commandError)
