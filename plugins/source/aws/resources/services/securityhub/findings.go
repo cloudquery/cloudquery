371d504d7729ea7c1d074b16277d28aa895d9707
bbernays <ben@bernays.me>
2023-01-27 12:39:24 -0600

    Update findings.go

--- plugins/source/aws/resources/services/securityhub/findings.go
+++ plugins/source/aws/resources/services/securityhub/findings.go
@@ -9,11 +9,12 @@ import (
 
 func Findings() *schema.Table {
 	return &schema.Table{
-		Name:        "aws_securityhub_findings",
-		Description: `https://docs.aws.amazon.com/securityhub/1.0/APIReference/API_GetFindings.html`,
-		Resolver:    fetchFindings,
-		Transform:   transformers.TransformWithStruct(&types.AwsSecurityFinding{}, transformers.WithPrimaryKeys("AwsAccountId", "Region", "CreatedAt", "Description", "GeneratorId", "Id", "ProductArn", "SchemaVersion", "Title")),
-		Multiplex:   client.ServiceAccountRegionMultiplexer("securityhub"),
+		Name: "aws_securityhub_findings",
+		Description: `https://docs.aws.amazon.com/securityhub/1.0/APIReference/API_GetFindings.html. 
+The 'request_account_id' and 'request_region' columns are added to show the account and region of where the request was made from. This is useful when multi region and account aggregation is enabled.`,
+		Resolver:  fetchFindings,
+		Transform: transformers.TransformWithStruct(&types.AwsSecurityFinding{}, transformers.WithPrimaryKeys("AwsAccountId", "Region", "CreatedAt", "Description", "GeneratorId", "Id", "ProductArn", "SchemaVersion", "Title")),
+		Multiplex: client.ServiceAccountRegionMultiplexer("securityhub"),
 		Columns: []schema.Column{
 			{
 				Name:     "request_account_id",
