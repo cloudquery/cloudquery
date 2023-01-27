4f801a39f397d7cf4b72fb3d6bbcf4269c23fcba
candiduslynx <candiduslynx@users.noreply.github.com>
2023-01-27 18:09:03 +0200

    fix(azure)!: Add `subscription_id` to `azure_authorization_provider_operations_metadata` PK (#7269)
    Fixes #7241

--- plugins/source/azure/resources/services/authorization/provider_operations_metadata.go
+++ plugins/source/azure/resources/services/authorization/provider_operations_metadata.go
@@ -16,7 +16,7 @@ func ProviderOperationsMetadata() *schema.Table {
 		Description: "https://learn.microsoft.com/en-us/rest/api/authorization/provider-operations-metadata/list?tabs=HTTP#provideroperationsmetadata",
 		Multiplex:   client.SubscriptionMultiplexRegisteredNamespace("azure_authorization_provider_operations_metadata", client.Namespacemicrosoft_authorization),
 		Transform:   transformers.TransformWithStruct(&armauthorization.ProviderOperationsMetadata{}, transformers.WithPrimaryKeys("ID")),
-		Columns:     schema.ColumnList{client.SubscriptionID},
+		Columns:     schema.ColumnList{client.SubscriptionIDPK},
 	}
 }
 
