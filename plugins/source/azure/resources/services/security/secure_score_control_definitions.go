1344bd3b917639aaf28eb170a28ed106f54af7d2
candiduslynx <candiduslynx@users.noreply.github.com>
2023-01-27 18:22:26 +0200

    fix(azure)!: Add `subscription_id` to `azure_security_secure_score_control_definitions` PK (#7285)
    Fixes #7256

--- plugins/source/azure/resources/services/security/secure_score_control_definitions.go
+++ plugins/source/azure/resources/services/security/secure_score_control_definitions.go
@@ -16,7 +16,7 @@ func SecureScoreControlDefinitions() *schema.Table {
 		Description: "https://learn.microsoft.com/en-us/rest/api/defenderforcloud/secure-score-control-definitions/list?tabs=HTTP#securescorecontroldefinitionitem",
 		Multiplex:   client.SubscriptionMultiplexRegisteredNamespace("azure_security_secure_score_control_definitions", client.Namespacemicrosoft_security),
 		Transform:   transformers.TransformWithStruct(&armsecurity.SecureScoreControlDefinitionItem{}, transformers.WithPrimaryKeys("ID")),
-		Columns:     schema.ColumnList{client.SubscriptionID},
+		Columns:     schema.ColumnList{client.SubscriptionIDPK},
 	}
 }
 
