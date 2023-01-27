d42f307eb802a4c7efd39bf57b787ad649746ebe
Herman Schaaf <hermanschaaf@gmail.com>
2023-01-27 17:22:13 +0000

    Pass in cq-dir to discovery client

--- cli/cmd/sync.go
+++ cli/cmd/sync.go
@@ -73,7 +73,8 @@ func sync(cmd *cobra.Command, args []string) error {
 			}
 			destinationsSpecs = append(destinationsSpecs, *spec)
 		}
-		discoveryClient, err := discovery.NewClient(ctx, sourceSpec.Registry, registry.PluginTypeSource, sourceSpec.Path, sourceSpec.Version)
+
+		discoveryClient, err := discovery.NewClient(ctx, sourceSpec.Registry, registry.PluginTypeSource, sourceSpec.Path, sourceSpec.Version, discovery.WithDirectory(cqDir))
 		if err != nil {
 			return fmt.Errorf("failed to create discovery client for source %s: %w", sourceSpec.Name, err)
 		}
