37905489c72cf75f7b439494845e2580aea71ec6
candiduslynx <candiduslynx@gmail.com>
2023-01-29 17:15:04 +0200

    Change `azure_compute_skus` PK to `(name, _sku_hash)`

--- plugins/source/azure/resources/services/compute/skus.go
+++ plugins/source/azure/resources/services/compute/skus.go
@@ -2,12 +2,14 @@ package compute
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v4"
 	"github.com/cloudquery/cloudquery/plugins/source/azure/client"
 	"github.com/cloudquery/plugin-sdk/schema"
 	"github.com/cloudquery/plugin-sdk/transformers"
+	"github.com/mitchellh/hashstructure/v2"
 )
 
 func SKUs() *schema.Table {
@@ -17,9 +19,17 @@ func SKUs() *schema.Table {
 		Description: "https://learn.microsoft.com/en-us/rest/api/compute/resource-skus/list?tabs=HTTP#resourceskusresult",
 		Multiplex:   client.SubscriptionMultiplexRegisteredNamespace("azure_compute_skus", client.Namespacemicrosoft_compute),
 		Transform: transformers.TransformWithStruct(&armcompute.ResourceSKU{},
-			transformers.WithPrimaryKeys("Family", "Kind", "Name"),
+			transformers.WithPrimaryKeys("Name"),
 		),
-		Columns: schema.ColumnList{client.SubscriptionIDPK},
+		Columns: schema.ColumnList{
+			client.SubscriptionIDPK,
+			schema.Column{
+				Name:            "_sku_hash",
+				Type:            schema.TypeString,
+				Resolver:        calcEntryHash,
+				CreationOptions: schema.ColumnCreationOptions{PrimaryKey: true},
+			},
+		},
 	}
 }
 
@@ -38,4 +48,12 @@ func fetchResourceSKUs(ctx context.Context, meta schema.ClientMeta, parent *sche
 		res <- p.Value
 	}
 	return nil
+}
+
+func calcEntryHash(_ context.Context, _ schema.ClientMeta, resource *schema.Resource, c schema.Column) error {
+	hash, err := hashstructure.Hash(resource.Item, hashstructure.FormatV2, nil)
+	if err != nil {
+		return err
+	}
+	return resource.Set(c.Name, fmt.Sprint(hash))
 }
