31a604062fff5637373337310fc872714f9856ea
candiduslynx <candiduslynx@users.noreply.github.com>
2023-01-30 17:36:39 +0200

    fix(azure)!: Drop `subscription_id` from `azure_policy_data_policy_manifests` (#7344)
    Fixes #7251.
    Related to #7263.

--- plugins/source/azure/resources/services/policy/data_policy_manifests.go
+++ plugins/source/azure/resources/services/policy/data_policy_manifests.go
@@ -14,9 +14,7 @@ func DataPolicyManifests() *schema.Table {
 		Name:        "azure_policy_data_policy_manifests",
 		Resolver:    fetchDataPolicyManifests,
 		Description: "https://pkg.go.dev/github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armpolicy@v0.6.0#DataPolicyManifest",
-		Multiplex:   client.SubscriptionMultiplexRegisteredNamespace("azure_policy_data_policy_manifests", client.Namespacemicrosoft_authorization),
 		Transform:   transformers.TransformWithStruct(&armpolicy.DataPolicyManifest{}, transformers.WithPrimaryKeys("ID")),
-		Columns:     schema.ColumnList{client.SubscriptionIDPK},
 	}
 }
 
