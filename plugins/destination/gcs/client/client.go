85450adcc6a73e230a70ef2f56aff3d93dada185
Kemal <223029+disq@users.noreply.github.com>
2023-01-31 13:48:01 +0000

    fix(destinations): Unmarshal spec error messages (#7463)
    Co-authored-by: Kemal Hadimli <disq@users.noreply.github.com>
    Co-authored-by: candiduslynx <candiduslynx@users.noreply.github.com>

--- plugins/destination/gcs/client/client.go
+++ plugins/destination/gcs/client/client.go
@@ -35,7 +35,7 @@ func New(ctx context.Context, logger zerolog.Logger, spec specs.Destination) (de
 	}
 
 	if err := spec.UnmarshalSpec(&c.pluginSpec); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal postgresql spec: %w", err)
+		return nil, fmt.Errorf("failed to unmarshal gcs spec: %w", err)
 	}
 	if err := c.pluginSpec.Validate(); err != nil {
 		return nil, err
