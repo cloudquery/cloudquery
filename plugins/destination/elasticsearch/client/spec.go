d113abdc0dc78dd7e7031bb80989bc027ba4dbed
Herman Schaaf <hermanschaaf@gmail.com>
2023-02-01 10:25:03 +0000

    Add documentation

--- plugins/destination/elasticsearch/client/spec.go
+++ plugins/destination/elasticsearch/client/spec.go
@@ -3,20 +3,20 @@ package client
 import "runtime"
 
 type Spec struct {
-	Addresses []string // A list of Elasticsearch nodes to use.
-	Username  string   // Username for HTTP Basic Authentication.
-	Password  string   // Password for HTTP Basic Authentication.
+	Addresses []string `json:"addresses"` // A list of Elasticsearch nodes to use.
+	Username  string   `json:"username"`  // Username for HTTP Basic Authentication.
+	Password  string   `json:"password"`  // Password for HTTP Basic Authentication.
 
-	CloudID                string // Endpoint for the Elastic Service (https://elastic.co/cloud).
-	APIKey                 string // Base64-encoded token for authorization; if set, overrides username/password and service token.
-	ServiceToken           string // Service token for authorization; if set, overrides username/password.
-	CertificateFingerprint string // SHA256 hex fingerprint given by Elasticsearch on first launch.
+	CloudID                string `json:"cloud_id"`                // Endpoint for the Elastic Service (https://elastic.co/cloud).
+	APIKey                 string `json:"api_key"`                 // Base64-encoded token for authorization; if set, overrides username/password and service token.
+	ServiceToken           string `json:"service_token"`           // Service token for authorization; if set, overrides username/password.
+	CertificateFingerprint string `json:"certificate_fingerprint"` // SHA256 hex fingerprint given by Elasticsearch on first launch.
 
 	// PEM-encoded certificate authorities.
 	// When set, an empty certificate pool will be created, and the certificates will be appended to it.
-	CACert string
+	CACert string `json:"ca_cert"`
 
-	Concurrency int // Number of concurrent worker goroutines to use for indexing. (Default: number of CPUs)
+	Concurrency int `json:"concurrency"` // Number of concurrent worker goroutines to use for indexing. (Default: number of CPUs)
 }
 
 func (s *Spec) SetDefaults() {
