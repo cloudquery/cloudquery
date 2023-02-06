837f70b3ef0fff285e6405f3c5218b71b5ec23ff
bbernays <ben@bernays.me>
2023-02-06 14:48:15 -0500

    table

---
+++ plugins/source/aws/resources/services/s3/access_points.go
@@ -0,0 +1,37 @@
+package s3
+
+import (
+	"github.com/aws/aws-sdk-go-v2/service/s3control/types"
+	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
+	"github.com/cloudquery/plugin-sdk/schema"
+	"github.com/cloudquery/plugin-sdk/transformers"
+)
+
+func AccessPoints() *schema.Table {
+	return &schema.Table{
+		Name:      "aws_s3_access_points",
+		Resolver:  fetchAccessPoints,
+		Transform: transformers.TransformWithStruct(&types.AccessPoint{}),
+		Multiplex: client.AccountMultiplex,
+		Columns: []schema.Column{
+			{
+				Name:     "account_id",
+				Type:     schema.TypeString,
+				Resolver: client.ResolveAWSAccount,
+			},
+			{
+				Name:     "region",
+				Type:     schema.TypeString,
+				Resolver: client.ResolveAWSRegion,
+			},
+			{
+				Name:     "arn",
+				Type:     schema.TypeString,
+				Resolver: schema.PathResolver("AccessPointArn"),
+				CreationOptions: schema.ColumnCreationOptions{
+					PrimaryKey: true,
+				},
+			},
+		},
+	}
+}
