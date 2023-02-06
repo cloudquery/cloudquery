4a5650e3136030438265dd8d11e3149a0652d81c
bbernays <ben@cloudquery.io>
2023-02-06 11:12:50 -0500

    feat(AWS): Add support for EKS Node Groups (#7692)
    #### Summary
    
    <!--
    Explain what problem this PR addresses
    -->
    
    <!--

--- plugins/source/aws/resources/services/eks/clusters_mock_test.go
+++ plugins/source/aws/resources/services/eks/clusters_mock_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/service/eks"
+	"github.com/aws/aws-sdk-go-v2/service/eks/types"
 	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
 	"github.com/cloudquery/cloudquery/plugins/source/aws/client/mocks"
 	"github.com/cloudquery/plugin-sdk/faker"
@@ -24,6 +25,21 @@ func buildEksClusters(t *testing.T, ctrl *gomock.Controller) client.Services {
 		}, nil)
 	m.EXPECT().DescribeCluster(gomock.Any(), gomock.Any(), gomock.Any()).Return(
 		&l, nil)
+
+	ng := types.Nodegroup{}
+	err = faker.FakeObject(&ng)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	m.EXPECT().ListNodegroups(gomock.Any(), gomock.Any(), gomock.Any()).Return(
+		&eks.ListNodegroupsOutput{
+			Nodegroups: []string{"test-nodegroup"},
+		}, nil)
+	m.EXPECT().DescribeNodegroup(gomock.Any(), gomock.Any(), gomock.Any()).Return(
+		&eks.DescribeNodegroupOutput{
+			Nodegroup: &ng,
+		}, nil)
 	return client.Services{
 		Eks: m,
 	}
