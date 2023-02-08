b91e2d01a2394f04fb0c3a930d6204c756cd4f5f
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2023-02-08 21:05:35 +0200

    fix tests

--- plugins/destination/postgresql/client/client_test.go
+++ plugins/destination/postgresql/client/client_test.go
@@ -10,7 +10,7 @@ import (
 func getTestConnection() string {
 	testConn := os.Getenv("CQ_DEST_PG_TEST_CONN")
 	if testConn == "" {
-		return "postgresql://postgres:pass@localhost:5433/postgres?sslmode=disable"
+		return "postgresql://postgres:pass@localhost:5432/postgres?sslmode=disable"
 	}
 	return testConn
 }
@@ -24,40 +24,3 @@ func TestPgPlugin(t *testing.T) {
 		},
 		destination.PluginTestSuiteTests{})
 }
-
-// func TestPgPluginPrimaryKeyRename(t *testing.T) {
-// 	tableName := fmt.Sprintf("cq_test_pk_rename_%03d", rand.Intn(100))
-// 	tableWithStalePk := testdata.TestTable(tableName)
-// 	// We simulate that a primary column was renamed
-// 	tableWithStalePk.Columns = append(tableWithStalePk.Columns, schema.Column{Name: "stale_pk_1", Type: schema.TypeUUID, CreationOptions: schema.ColumnCreationOptions{PrimaryKey: true}})
-// 	tableWithStalePk.Columns = append(tableWithStalePk.Columns, schema.Column{Name: "stale_pk_2", Type: schema.TypeUUID, CreationOptions: schema.ColumnCreationOptions{PrimaryKey: true}})
-// 	p := destination.NewPlugin("postgresql", "development", New)
-// 	ctx := context.Background()
-
-// 	spec := Spec{
-// 		ConnectionString: getTestConnection(),
-// 		PgxLogLevel:      LogLevelTrace,
-// 	}
-
-// 	// Init the plugin so we can call migrate
-// 	if err := p.Init(ctx, zerolog.Logger{}, specs.Destination{Name: "stale_pk", Spec: spec}); err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// 	// Call migrate so the `stale_pk_1` and `stale_pk_2` columns are created
-// 	if err := p.Migrate(ctx, []*schema.Table{tableWithStalePk}); err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// 	// Migrate the table again without the `stale_pk_1` and `stale_pk_2` columns
-// 	table := testdata.TestTable(tableName)
-// 	err := p.Migrate(ctx, []*schema.Table{table})
-// 	expected := `the following primary keys were removed from the schema ["stale_pk_1" "stale_pk_2"] for table "%s".
-// You can migrate the table manually by running:
-// -------------------------------------------------------------------------------------------
-// alter table "%s" drop constraint if exists "%s_cqpk";
-// alter table "%s" alter column "stale_pk_1" drop not null;
-// alter table "%s" alter column "stale_pk_2" drop not null;
-// -------------------------------------------------------------------------------------------`
-// 	require.ErrorContains(t, err, fmt.Sprintf(expected, tableName, tableName, tableName, tableName, tableName))
-// }
