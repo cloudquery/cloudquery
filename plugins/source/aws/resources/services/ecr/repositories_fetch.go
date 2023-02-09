3faf458c2941b9123316bdad53096c789ebb88a1
Herman Schaaf <hermanschaaf@gmail.com>
2023-02-09 11:16:20 +0000

    leave out sha256

--- plugins/source/aws/resources/services/ecr/repositories_fetch.go
+++ plugins/source/aws/resources/services/ecr/repositories_fetch.go
@@ -122,7 +122,7 @@ func resolveImageArn(ctx context.Context, meta schema.ClientMeta, resource *sche
 		Service:   "ecr",
 		Region:    cl.Region,
 		AccountID: cl.AccountID,
-		Resource:  "repository/" + *item.RepositoryName + "/image/sha256:" + *item.ImageDigest,
+		Resource:  "repository/" + *item.RepositoryName + "/image/" + *item.ImageDigest,
 	}
 	return resource.Set(c.Name, a.String())
 }
