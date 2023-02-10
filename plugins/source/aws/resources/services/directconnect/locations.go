94d03894b590c44cbebe1086fa1577686bc62f59
bbernays <ben@bernays.me>
2023-02-10 11:21:45 -0600

    Update locations.go

--- plugins/source/aws/resources/services/directconnect/locations.go
+++ plugins/source/aws/resources/services/directconnect/locations.go
@@ -15,16 +15,8 @@ func Locations() *schema.Table {
 		Multiplex:   client.ServiceAccountRegionMultiplexer("directconnect"),
 		Transform:   transformers.TransformWithStruct(&types.Location{}),
 		Columns: []schema.Column{
-			{
-				Name:     "account_id",
-				Type:     schema.TypeString,
-				Resolver: client.ResolveAWSAccount,
-			},
-			{
-				Name:     "region",
-				Type:     schema.TypeString,
-				Resolver: client.ResolveAWSRegion,
-			},
+			client.DefaultAccountIDColumn(false),
+			client.DefaultRegionColumn(false),
 		},
 	}
 }
