32dfef6ca8ee7bdcf515db3fbd45cca6bcd3d2b5
bbernays <ben@bernays.me>
2023-02-11 14:15:36 -0600

    events

---
+++ plugins/source/aws/resources/services/elasticache/events.go
@@ -0,0 +1,22 @@
+package elasticache
+
+import (
+	"github.com/aws/aws-sdk-go-v2/service/elasticache/types"
+	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
+	"github.com/cloudquery/plugin-sdk/schema"
+	"github.com/cloudquery/plugin-sdk/transformers"
+)
+
+func Events() *schema.Table {
+	return &schema.Table{
+		Name:        "aws_elasticache_events",
+		Description: `https://docs.aws.amazon.com/AmazonElastiCache/latest/APIReference/API_Event.html`,
+		Resolver:    fetchElasticacheEvents,
+		Multiplex:   client.ServiceAccountRegionMultiplexer("elasticache"),
+		Transform:   transformers.TransformWithStruct(&types.Event{}, transformers.WithPrimaryKeys("Date", "SourceIdentifier", "SourceType")),
+		Columns: []schema.Column{
+			client.DefaultAccountIDColumn(false),
+			client.DefaultRegionColumn(false),
+		},
+	}
+}
