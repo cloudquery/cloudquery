c9b5dd0091796f11c2b2f3a6007684c47525767f
erezrokah <erezrokah@users.noreply.github.com>
2023-02-20 16:24:29 +0200

    feat(deps): Update Elasticsearch plugin-sdk to v1.38.2

--- plugins/destination/elasticsearch/client/write.go
+++ plugins/destination/elasticsearch/client/write.go
@@ -62,6 +62,12 @@ func (c *Client) WriteTableBatch(ctx context.Context, table *schema.Table, resou
 
 func pkIndexes(table *schema.Table) []int {
 	pks := table.PrimaryKeys()
+	if len(pks) == 0 {
+		// if no PK is defined, use all columns for the ID which is based on the indices returned by this function
+		for _, col := range table.Columns {
+			pks = append(pks, col.Name)
+		}
+	}
 	inds := make([]int, 0, len(pks))
 	for _, col := range pks {
 		inds = append(inds, table.Columns.Index(col))
