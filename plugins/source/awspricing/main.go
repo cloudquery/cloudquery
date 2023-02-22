c1c4c8720325ac390c7e242451dd58f03719a424
bbernays <ben@bernays.me>
2023-02-22 11:38:11 -0600

    sentry

--- plugins/source/awspricing/main.go
+++ plugins/source/awspricing/main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/cloudquery/plugin-sdk/serve"
 )
 
+const sentryDSN = "https://a7dd3cdeae17424d8cd70f9c76c38fe1@o1396617.ingest.sentry.io/4504724922368000"
+
 func main() {
-	serve.Source(plugin.Plugin())
+	serve.Source(plugin.Plugin(), serve.WithSourceSentryDSN(sentryDSN))
 }
