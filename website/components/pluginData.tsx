60791be9cf5a39c0232bdfab32ec52a9938b68e4
Herman Schaaf <hermanschaaf@gmail.com>
2023-02-22 16:45:17 +0000

    Comment out some plugins

--- website/components/pluginData.tsx
+++ website/components/pluginData.tsx
@@ -25,11 +25,11 @@ export const SOURCE_PLUGINS: Plugin[] = [
     logo: "/images/logos/plugins/azure.svg",
     id: "azure",
   },
-  {
-    name: "Azure DevOps",
-    logo: "/images/logos/plugins/azuredevops.svg",
-    id: "azuredevops",
-  },
+  // {
+  //   name: "Azure DevOps",
+  //   logo: "/images/logos/plugins/azuredevops.svg",
+  //   id: "azuredevops",
+  // },
   {
     name: "Cloudflare",
     logo: "/images/logos/plugins/cloudflare.svg",
@@ -46,33 +46,33 @@ export const SOURCE_PLUGINS: Plugin[] = [
     logo: "/images/logos/plugins/digitalocean.svg",
     id: "digitalocean",
   },
-  {
-    name: "Fastly",
-    logo: "/images/logos/plugins/fastly.svg",
-    id: "fastly",
-  },
-  {
-    name: "Gandi",
-    logo: "/images/logos/plugins/gandi.svg",
-    logoDark: "/images/logos/plugins/gandi-dark.svg",
-    id: "gandi",
-  },
+  // {
+  //   name: "Fastly",
+  //   logo: "/images/logos/plugins/fastly.svg",
+  //   id: "fastly",
+  // },
+  // {
+  //   name: "Gandi",
+  //   logo: "/images/logos/plugins/gandi.svg",
+  //   logoDark: "/images/logos/plugins/gandi-dark.svg",
+  //   id: "gandi",
+  // },
   {
     name: "Google Cloud Platform",
     logo: "/images/logos/plugins/gcp.svg",
     id: "gcp",
   },
-  // {
-  //   name: "GitHub",
-  //   logo: "/images/logos/plugins/github.svg",
-  //   logoDark: "/images/logos/plugins/github-dark.svg",
-  //   id: "github",
-  // },
-  // {
-  //   name: "GitLab",
-  //   logo: "/images/logos/plugins/gitlab.svg",
-  //   id: "gitlab",
-  // },
+  {
+    name: "GitHub",
+    logo: "/images/logos/plugins/github.svg",
+    logoDark: "/images/logos/plugins/github-dark.svg",
+    id: "github",
+  },
+  {
+    name: "GitLab",
+    logo: "/images/logos/plugins/gitlab.svg",
+    id: "gitlab",
+  },
   // {
   //   name: "Hacker News",
   //   logo: "/images/logos/plugins/hackernews.svg",
@@ -83,91 +83,91 @@ export const SOURCE_PLUGINS: Plugin[] = [
   //   logo: "/images/logos/plugins/heroku.svg",
   //   id: "heroku",
   // },
-  // {
-  //   name: "HubSpot",
-  //   logo: "/images/logos/plugins/hubspot.svg",
-  //   id: "hubspot",
-  // },
-  // {
-  //   name: "Kubernetes",
-  //   logo: "/images/logos/plugins/kubernetes.svg",
-  //   id: "k8s",
-  // },
-  // {
-  //   name: "LaunchDarkly",
-  //   logo: "/images/logos/plugins/launchdarkly.svg",
-  //   logoDark: "/images/logos/plugins/launchdarkly-dark.svg",
-  //   id: "launchdarkly",
-  // },
-  // {
-  //   name: "Mixpanel",
-  //   logo: "/images/logos/plugins/mixpanel.svg",
-  //   logoDark: "/images/logos/plugins/mixpanel-dark.svg",
-  //   id: "mixpanel",
-  // },
-  // {
-  //   name: "Okta",
-  //   logo: "/images/logos/plugins/okta.svg",
-  //   id: "okta",
-  // },
-  // {
-  //   name: "Oracle",
-  //   logo: "/images/logos/plugins/oracle.svg",
-  //   id: "oracle",
-  // },
-  // {
-  //   name: "Pagerduty",
-  //   logo: "/images/logos/plugins/pagerduty.svg",
-  //   id: "pagerduty",
-  // },
-  // {
-  //   name: "Plausible Analytics",
-  //   logo: "/images/logos/plugins/plausibleanalytics.svg",
-  //   id: "plausible",
-  // },
-  // {
-  //   name: "Salesforce",
-  //   logo: "/images/logos/plugins/salesforce.svg",
-  //   id: "salesforce",
-  // },
-  // {
-  //   name: "Swetrix",
-  //   logo: "/images/logos/plugins/swetrix.svg",
-  //   href: "https://github.com/swetrix/cq-source-swetrix",
-  //   id: "swetrix",
-  // },
-  // {
-  //   name: "Scaleway",
-  //   logo: "/images/logos/plugins/scaleway.svg",
-  //   href: "https://github.com/scaleway/cq-source-scaleway",
-  //   id: "scaleway",
-  // },
-  // {
-  //   name: "Shopify",
-  //   logo: "/images/logos/plugins/shopify.svg",
-  //   id: "shopify",
-  // },
-  // {
-  //   name: "Simple Analytics",
-  //   logo: "/images/logos/plugins/simpleanalytics.svg",
-  //   href: "https://github.com/simpleanalytics/cq-source-simpleanalytics",
-  //   id: "simpleanalytics",
-  // },
-  // {
-  //   name: "Slack",
-  //   logo: "/images/logos/plugins/slack.svg",
-  //   id: "slack",
-  // },
+  {
+    name: "HubSpot",
+    logo: "/images/logos/plugins/hubspot.svg",
+    id: "hubspot",
+  },
+  {
+    name: "Kubernetes",
+    logo: "/images/logos/plugins/kubernetes.svg",
+    id: "k8s",
+  },
+  {
+    name: "LaunchDarkly",
+    logo: "/images/logos/plugins/launchdarkly.svg",
+    logoDark: "/images/logos/plugins/launchdarkly-dark.svg",
+    id: "launchdarkly",
+  },
+  {
+    name: "Mixpanel",
+    logo: "/images/logos/plugins/mixpanel.svg",
+    logoDark: "/images/logos/plugins/mixpanel-dark.svg",
+    id: "mixpanel",
+  },
+  {
+    name: "Okta",
+    logo: "/images/logos/plugins/okta.svg",
+    id: "okta",
+  },
+  {
+    name: "Oracle",
+    logo: "/images/logos/plugins/oracle.svg",
+    id: "oracle",
+  },
+  {
+    name: "Pagerduty",
+    logo: "/images/logos/plugins/pagerduty.svg",
+    id: "pagerduty",
+  },
+  {
+    name: "Plausible Analytics",
+    logo: "/images/logos/plugins/plausibleanalytics.svg",
+    id: "plausible",
+  },
+  {
+    name: "Salesforce",
+    logo: "/images/logos/plugins/salesforce.svg",
+    id: "salesforce",
+  },
+  {
+    name: "Swetrix",
+    logo: "/images/logos/plugins/swetrix.svg",
+    href: "https://github.com/swetrix/cq-source-swetrix",
+    id: "swetrix",
+  },
+  {
+    name: "Scaleway",
+    logo: "/images/logos/plugins/scaleway.svg",
+    href: "https://github.com/scaleway/cq-source-scaleway",
+    id: "scaleway",
+  },
+  {
+    name: "Shopify",
+    logo: "/images/logos/plugins/shopify.svg",
+    id: "shopify",
+  },
+  {
+    name: "Simple Analytics",
+    logo: "/images/logos/plugins/simpleanalytics.svg",
+    href: "https://github.com/simpleanalytics/cq-source-simpleanalytics",
+    id: "simpleanalytics",
+  },
+  {
+    name: "Slack",
+    logo: "/images/logos/plugins/slack.svg",
+    id: "slack",
+  },
   // {
   //   name: "Snyk",
   //   logo: "/images/logos/plugins/snyk.svg",
   //   id: "snyk",
   // },
-  // {
-  //   name: "Stripe",
-  //   logo: "/images/logos/plugins/stripe.svg",
-  //   id: "stripe",
-  // },
+  {
+    name: "Stripe",
+    logo: "/images/logos/plugins/stripe.svg",
+    id: "stripe",
+  },
   // {
   //   name: "Tailscale",
   //   logo: "/images/logos/plugins/tailscale.svg",
@@ -185,19 +185,19 @@ export const SOURCE_PLUGINS: Plugin[] = [
   //   logoDark: "/images/logos/plugins/vercel-dark.svg",
   //   id: "vercel",
   // },
-  // {
-  //   name: "Yandex Cloud",
-  //   logo: "/images/logos/plugins/yandex.svg",
-  //   id: "yandexcloud",
-  //   href: "https://github.com/yandex-cloud/cq-source-yandex",
-  // },
-  // {
-  //   name: "All Plugins",
-  //   logo: "/images/logos/plugins/more.svg",
-  //   logoDark: "/images/logos/plugins/more-dark.svg",
-  //   id: "more",
-  //   href: "/docs/plugins/overview",
-  // },
+  {
+    name: "Yandex Cloud",
+    logo: "/images/logos/plugins/yandex.svg",
+    id: "yandexcloud",
+    href: "https://github.com/yandex-cloud/cq-source-yandex",
+  },
+  {
+    name: "All Plugins",
+    logo: "/images/logos/plugins/more.svg",
+    logoDark: "/images/logos/plugins/more-dark.svg",
+    id: "more",
+    href: "/docs/plugins/overview",
+  },
 ];
 
 export const DESTINATION_PLUGINS: Plugin[] = [
