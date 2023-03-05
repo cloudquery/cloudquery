3e6a788b8e2621ff477af0a4762913dfe1a624fd
erezrokah <erezrokah@users.noreply.github.com>
2023-03-05 18:36:52 +0200

    feat(website): Add tables to sitemap

--- website/next-sitemap.config.js
+++ website/next-sitemap.config.js
@@ -1,5 +1,14 @@
+const { getTablesData } = require("./utils/tables-data");
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://www.cloudquery.io",
   generateRobotsTxt: true,
+  additionalPaths: () => {
+    const tablesData = getTablesData();
+    const paths = tablesData.map(({ plugin, table }) => {
+      return { loc: `/docs/tables/${plugin}/${table}`};
+    });
+    return paths;
+  },
 };
