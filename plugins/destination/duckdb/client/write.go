0e8f291fef53dd73ec4acf3fc6c3c9c7cdaa72ad
Herman Schaaf <hermanschaaf@gmail.com>
2023-03-07 14:43:21 +0000

    Fix a couple of bugs

--- plugins/destination/duckdb/client/write.go
+++ plugins/destination/duckdb/client/write.go
@@ -20,6 +20,7 @@ func (c *Client) Write(ctx context.Context, tables schema.Tables, res <-chan *de
 			sql = c.upsert(table, r.Data)
 		}
 		expanded := expandData(table, r.Data)
+		c.logger.Info().Msg(fmt.Sprintf("Executing '%s' with values %v", sql, expanded))
 		if _, err := c.db.Exec(sql, expanded...); err != nil {
 			return fmt.Errorf("failed to execute '%s': %w", sql, err)
 		}
@@ -31,9 +32,8 @@ func expandData(table *schema.Table, data []any) []any {
 	var expanded []any
 	for i, d := range data {
 		if isArray(table.Columns[i]) {
-			for _, s := range d.([]string) {
-				expanded = append(expanded, s)
-			}
+			v := toArray(table.Columns[i], d)
+			expanded = append(expanded, v...)
 		} else {
 			expanded = append(expanded, d)
 		}
@@ -52,7 +52,6 @@ func (c *Client) upsert(table *schema.Table, data []any) string {
 	var sb strings.Builder
 	sb.WriteString("insert or replace into ")
 	c.insertQuery(&sb, table, data)
-	sb.WriteString(" on conflict do nothing")
 	return sb.String()
 }
 
@@ -72,7 +71,7 @@ func (*Client) insertQuery(sb *strings.Builder, table *schema.Table, data []any)
 	counter := 0
 	for i := range columns {
 		if isArray(table.Columns[i]) {
-			n := arrayLength(table.Columns[i], data[i])
+			n := len(toArray(table.Columns[i], data[i]))
 			sb.WriteString("[")
 			for j := 0; j < n; j++ {
 				sb.WriteString(fmt.Sprintf("$%d", counter+1))
@@ -96,16 +95,32 @@ func (*Client) insertQuery(sb *strings.Builder, table *schema.Table, data []any)
 
 func isArray(col schema.Column) bool {
 	switch col.Type {
-	case schema.TypeStringArray, schema.TypeCIDRArray, schema.TypeIntArray, schema.TypeInetArray, schema.TypeUUIDArray, schema.TypeMacAddrArray:
+	case schema.TypeIntArray, schema.TypeStringArray, schema.TypeCIDRArray, schema.TypeInetArray, schema.TypeUUIDArray, schema.TypeMacAddrArray:
 		return true
 	}
 	return false
 }
 
-func arrayLength(col schema.Column, data any) int {
+func toArray(col schema.Column, data any) []any {
 	switch col.Type {
-	case schema.TypeStringArray, schema.TypeCIDRArray, schema.TypeIntArray, schema.TypeInetArray, schema.TypeUUIDArray, schema.TypeMacAddrArray:
-		return len(data.([]string))
+	case schema.TypeIntArray:
+		if v, ok := data.([]int); ok {
+			a := make([]any, len(v))
+			for i := range v {
+				a[i] = v[i]
+			}
+			return a
+		}
+		return nil
+	case schema.TypeStringArray, schema.TypeCIDRArray, schema.TypeInetArray, schema.TypeUUIDArray, schema.TypeMacAddrArray:
+		if v, ok := data.([]string); ok {
+			a := make([]any, len(v))
+			for i := range v {
+				a[i] = v[i]
+			}
+			return a
+		}
+		return nil
 	}
 	panic("not a supported array type: " + col.Type.String() + " for column: " + col.Name)
 }
