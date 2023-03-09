716aba36cad94cb6839bb94633b25043daca0d45
Yevgeny Pats <yev.pats@gmail.com>
2023-03-09 03:14:03 -0500

    fix(dest-s3)!: Make region required and don't make getbucketlocation (#8843)
    This causes consistent confusion to our users and also don't follow least privilege. This now should require only PutObject. The user anyway always knows where the bucket is located.
    
    https://discord.com/channels/872925471417962546/873606591335759872/1083080462936838245

--- plugins/destination/s3/client/client.go
+++ plugins/destination/s3/client/client.go
@@ -57,11 +57,7 @@ func New(ctx context.Context, logger zerolog.Logger, spec specs.Destination) (de
 		return nil, fmt.Errorf("unable to load AWS SDK config: %w", err)
 	}
 
-	location, err := getBucketLocation(ctx, s3.NewFromConfig(cfg), c.pluginSpec.Bucket)
-	if err != nil {
-		return nil, fmt.Errorf("unable to determine region of S3 bucket: %w", err)
-	}
-	cfg.Region = location
+	cfg.Region = c.pluginSpec.Region
 	c.s3Client = s3.NewFromConfig(cfg)
 	c.uploader = manager.NewUploader(c.s3Client)
 	c.downloader = manager.NewDownloader(c.s3Client)
@@ -80,17 +76,4 @@ func New(ctx context.Context, logger zerolog.Logger, spec specs.Destination) (de
 
 func (*Client) Close(ctx context.Context) error {
 	return nil
-}
-
-func getBucketLocation(ctx context.Context, s3Client *s3.Client, bucket string) (string, error) {
-	output, err := s3Client.GetBucketLocation(ctx, &s3.GetBucketLocationInput{
-		Bucket: aws.String(bucket),
-	})
-	if err != nil {
-		return "", fmt.Errorf("failed to get bucket location: %w", err)
-	}
-	if output.LocationConstraint == "" {
-		return "us-east-1", nil
-	}
-	return string(output.LocationConstraint), nil
 }
