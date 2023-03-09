fdab4824716f3ff9e8af939b16fecd62a90d67ba
Kemal Hadimli <disq@users.noreply.github.com>
2023-03-09 22:26:45 +0000

    Use aws-sdk-go-v2 and auto-detect region

--- plugins/destination/gremlin/client/client.go
+++ plugins/destination/gremlin/client/client.go
@@ -9,8 +9,8 @@ import (
 	"time"
 
 	gremlingo "github.com/apache/tinkerpop/gremlin-go/v3/driver"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	v4 "github.com/aws/aws-sdk-go/aws/signer/v4"
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/cloudquery/plugin-sdk/plugins/destination"
 	"github.com/cloudquery/plugin-sdk/specs"
 	"github.com/rs/zerolog"
@@ -82,16 +82,27 @@ func (c *Client) getAuthInfo(ctx context.Context, baseURL string) (*gremlingo.Au
 		return gremlingo.BasicAuthInfo(c.pluginSpec.Username, c.pluginSpec.Password), nil
 
 	case authModeIAM:
+		// emptyStringSHA256 is a SHA256 of an empty string
+		const emptyStringSHA256 = `e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855`
+
 		req, err := http.NewRequest(http.MethodGet, baseURL, strings.NewReader(""))
 		if err != nil {
 			return nil, err
 		}
-		signer := v4.NewSigner(credentials.NewSharedCredentials("", ""))
-		if _, err := signer.Sign(req, strings.NewReader(""), "neptune-db", "us-east-1", time.Now()); err != nil {
+
+		cfg, err := config.LoadDefaultConfig(ctx, config.WithDefaultRegion("us-east-1"))
+		if err != nil {
+			return nil, fmt.Errorf("unable to load AWS SDK config: %w", err)
+		}
+		cr, err := cfg.Credentials.Retrieve(ctx)
+		if err != nil {
+			return nil, fmt.Errorf("unable to retrieve AWS credentials: %w", err)
+		}
+		signer := v4.NewSigner()
+		if err := signer.SignHTTP(ctx, cr, req, emptyStringSHA256, "neptune-db", cfg.Region, time.Now()); err != nil {
 			return nil, err
 		}
-
-		c.logger.Trace().Any("iam_headers", req.Header).Msg("IAM headers")
+		c.logger.Trace().Any("iam_headers", req.Header).Str("aws_region", cfg.Region).Msg("IAM headers")
 		return gremlingo.HeaderAuthInfo(req.Header), nil
 
 	default:
--- plugins/destination/gremlin/go.mod
+++ plugins/destination/gremlin/go.mod
@@ -4,15 +4,25 @@ go 1.19
 
 require (
 	github.com/apache/tinkerpop/gremlin-go/v3 v3.6.2
-	github.com/aws/aws-sdk-go v1.44.218
+	github.com/aws/aws-sdk-go-v2 v1.17.5
+	github.com/aws/aws-sdk-go-v2/config v1.18.15
 	github.com/cloudquery/plugin-sdk v1.42.0
 	github.com/rs/zerolog v1.29.0
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.15 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.23 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.29 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.23 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.30 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.23 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.12.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.18.5 // indirect
+	github.com/aws/smithy-go v1.13.5 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/nicksnyder/go-i18n/v2 v2.2.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.8.2 // indirect
