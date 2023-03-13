051cfc4bbb4e4dc3c879090d272772bed87988f3
erezrokah <erezrokah@users.noreply.github.com>
2023-03-13 14:01:45 +0200

    fix: Clean up types, handle nil byte arrays

--- plugins/source/mysql/client/sync.go
+++ plugins/source/mysql/client/sync.go
@@ -26,6 +26,9 @@ func (*Client) createResultsArray(table *schema.Table) []any {
 	results := make([]any, 0, len(table.Columns))
 	for _, col := range table.Columns {
 		switch col.Type {
+		case schema.TypeUUID, schema.TypeByteArray:
+			var r *[]byte
+			results = append(results, &r)
 		case schema.TypeBool:
 			var r *bool
 			results = append(results, &r)
@@ -35,47 +38,11 @@ func (*Client) createResultsArray(table *schema.Table) []any {
 		case schema.TypeFloat:
 			var r *float64
 			results = append(results, &r)
-		case schema.TypeUUID:
-			var r *[]byte
-			results = append(results, &r)
-		case schema.TypeString:
-			var r *string
-			results = append(results, &r)
-		case schema.TypeByteArray:
-			var r *[]byte
-			results = append(results, &r)
-		case schema.TypeStringArray:
-			var r string
-			results = append(results, &r)
 		case schema.TypeTimestamp:
 			var r *time.Time
 			results = append(results, &r)
-		case schema.TypeJSON:
-			var r string
-			results = append(results, &r)
-		case schema.TypeUUIDArray:
-			var r string
-			results = append(results, &r)
-		case schema.TypeCIDR:
+		default:
 			var r *string
-			results = append(results, &r)
-		case schema.TypeCIDRArray:
-			var r string
-			results = append(results, &r)
-		case schema.TypeMacAddr:
-			var r *string
-			results = append(results, &r)
-		case schema.TypeMacAddrArray:
-			var r string
-			results = append(results, &r)
-		case schema.TypeInet:
-			var r *string
-			results = append(results, &r)
-		case schema.TypeInetArray:
-			var r string
-			results = append(results, &r)
-		case schema.TypeIntArray:
-			var r string
 			results = append(results, &r)
 		}
 	}
@@ -127,6 +94,10 @@ func (c *Client) resourceFromValues(tableName string, values []any) (*schema.Res
 	table := c.Tables.Get(tableName)
 	resource := schema.NewResourceData(table, nil, values)
 	for i, col := range table.Columns {
+		// If the value points to an empty byte array array we set it to nil
+		if v, ok := values[i].(**[]byte); ok && *v == nil {
+			values[i] = nil
+		}
 		if err := resource.Set(col.Name, values[i]); err != nil {
 			return nil, err
 		}
