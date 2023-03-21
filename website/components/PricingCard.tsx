b8ee35225efd62b4886fe39edad84d9050bd3867
Herman Schaaf <hermanschaaf@gmail.com>
2023-03-21 14:29:00 +0000

    Some tweaks

--- website/components/PricingCard.tsx
+++ website/components/PricingCard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 export interface PricingTier {
     name: string;
+    description?: string;
     attributes: PricingTierAttributes;
 }
 
@@ -9,38 +10,74 @@ export interface PricingTierAttributes {
     Price: string;
     PriceMonthly: boolean;
     PriceDetails?: string;
-    Communication: string;
-    SLA: string;
-    TAM: string;
-    Features: string;
-    Contract: string;
+    Communication?: string;
+    SLA?: string;
+    TAM?: string;
+    Features?: string;
+    Contract?: string;
 }
 
 export default function PricingCard({tier} : {tier: PricingTier}) {
+    const check = <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor"
+                       viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd"
+              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+              clip-rule="evenodd"></path>
+    </svg>;
+
     return <div
-        className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-900 dark:text-white">
+        className="flex flex-col px-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-900 dark:text-white">
         <h3 className="mb-4 text-2xl font-semibold">{tier.name}</h3>
+        <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">{tier.description}</p>
         <div className="flex justify-center items-baseline my-8">
             <span className="mr-2 text-4xl font-extrabold">{tier.attributes.Price}</span>
             {tier.attributes.PriceMonthly ? <span className="text-gray-500 dark:text-gray-400">/month</span> : null}
         </div>
 
-        <ul role="list" className="mb-8 space-y-4 text-left">
+        <ul role="list" className="space-y-4 text-left mb-20">
+            {tier.attributes.Communication ?
             <li className="flex items-center space-x-3">
+                {check}
                 <span><strong>Communication</strong>: {tier.attributes.Communication}</span>
-            </li>
+            </li> : null }
+            {tier.attributes.SLA ?
             <li className="flex items-center space-x-3">
+                {check}
                 <span><strong>SLA</strong>: {tier.attributes.SLA}</span>
-            </li>
+            </li> : null }
+            {tier.attributes.TAM ?
             <li className="flex items-center space-x-3">
+                {check}
                 <span><strong>Technical Account Manager</strong>: {tier.attributes.TAM}</span>
-            </li>
+            </li> : null }
+            {tier.attributes.Features ?
             <li className="flex items-center space-x-3">
+                {check}
                 <span><strong>Priority feature development</strong>: {tier.attributes.Features}</span>
-            </li>
+            </li> : null }
+            {tier.attributes.Contract ?
             <li className="flex items-center space-x-3">
+                {check}
                 <span><strong>Contract</strong>: {tier.attributes.Contract}</span>
-            </li>
+            </li> : null }
         </ul>
+        <div className="relative h-full">
+            <div className="absolute inset-x-0 bottom-0">
+                {tier.name == 'Community' ?
+                    <a href="/docs/quickstart"
+                       className="btn btn-purple w-full block"
+                    >
+                        Get Started
+                    </a>
+                    :
+                    <a href="/contact"
+                        className="btn btn-purple w-full block"
+                    >
+                        Contact Us
+                    </a>
+                }
+            </div>
+        </div>
+
     </div>
 }
--- website/components/pages/pricing.tsx
+++ website/components/pages/pricing.tsx
@@ -5,51 +5,51 @@ import {PricingTier} from "../PricingCard";
 const tiers: PricingTier[] = [
     {
         name: "Community",
+        description: "Open source community support",
         attributes: {
             Price: "Free",
             PriceMonthly: false,
-            Communication: "Discord public channels",
-            SLA: "No",
-            TAM: "No",
-            Features: "No",
-            Contract: "Not needed",
+            Communication: "Discord public channels, GitHub issues",
+            Contract: "None needed",
         }
     },
     {
         name: "Business",
+        description: "For businesses that need prioritized support",
         attributes: {
             Price: "$4k",
             PriceMonthly: true,
             PriceDetails: "Annual contract. 10% discount if billed annually",
             Communication: "Private channel in any of Discord, Slack, or Teams",
             SLA: "24 hour response time on support tickets. 48 hours for a P0 bug fix",
-            TAM: "Yes. Up to 2 hours / month",
-            Features: "No",
+            TAM: "Up to 2 hours / month",
             Contract: "Standard Support Contract (minor modifications allowed)",
         }
     },
     {
         name: "Enterprise",
+        description: "Prioritized support and custom development",
         attributes: {
             Price: "$25k",
             PriceMonthly: true,
             PriceDetails: "Annual contract. 10% discount if billed annually",
             Communication: "Private channel in any of Discord, Slack, or Teams",
             SLA: "24 hour response time on support tickets. 24 hours for a P0 bug fix",
-            TAM: "Yes. Up to 4 hours / month",
+            TAM: "Up to 4 hours / month",
             Features: "Up to 5 resources / month; more on a best-effort basis.",
             Contract: "Standard Support Contract (minor modifications allowed)",
         }
     },
     {
         name: "Enterprise Custom",
+        description: "Customized contract to fit unique requirements",
         attributes: {
-            Price: "Contact us",
+            Price: "Custom",
             PriceMonthly: false,
             Communication: "Private channel in any of Discord, Slack, or Teams",
             SLA: "24 hour response time on support tickets. 24 hours for a P0 bug fix",
-            TAM: "Yes. Customizable hours / month",
-            Features: "Custom",
+            TAM: "Customizable hours / month",
+            Features: "Custom feature, resource and plugin development",
             Contract: "Custom Support Contract",
         }
     },
@@ -61,7 +61,7 @@ export default function Home() {
           <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
               <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
                   <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Support Plans</h2>
-                  <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">We offer flexible support plans that range from open source community support to custom enterprise-level support.</p>
+                  <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">We offer flexible support plans that range from free open source community support to custom enterprise-level support.</p>
               </div>
               <div className="space-y-8 lg:grid lg:grid-cols-4 sm:gap-6 xl:gap-10 lg:space-y-0">
                   {tiers.map((tier) => (
