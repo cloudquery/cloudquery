ae4059be3359c7b7ea815a5cfcec5ef9718f50f6
Herman Schaaf <hermanschaaf@gmail.com>
2023-03-21 14:52:42 +0000

    Small mobile fixes

--- website/components/pages/pricing.tsx
+++ website/components/pages/pricing.tsx
@@ -19,7 +19,7 @@ const tiers: PricingTier[] = [
         attributes: {
             Price: "$4k",
             PriceMonthly: true,
-            PriceDetails: "Annual contract. 10% discount if billed annually",
+            PriceDetails: "Annual contract. 10% discount if billed annually.",
             Communication: "Private channel in any of Discord, Slack, or Teams",
             SLA: "24 hour response time on support tickets. 48 hours for a P0 bug fix",
             TAM: "Up to 2 hours / month",
@@ -32,7 +32,7 @@ const tiers: PricingTier[] = [
         attributes: {
             Price: "$25k",
             PriceMonthly: true,
-            PriceDetails: "Annual contract. 10% discount if billed annually",
+            PriceDetails: "Annual contract. 10% discount if billed annually.",
             Communication: "Private channel in any of Discord, Slack, or Teams",
             SLA: "24 hour response time on support tickets. 24 hours for a P0 bug fix",
             TAM: "Up to 4 hours / month",
@@ -60,7 +60,7 @@ export default function Home() {
       <section>
           <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
               <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
-                  <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Support Plans</h2>
+                  <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Open Source Support Plans</h2>
                   <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">We offer flexible support plans that range from free open source community support to custom enterprise-level support.</p>
               </div>
               <div className="space-y-8 lg:grid lg:grid-cols-4 sm:gap-6 xl:gap-10 lg:space-y-0">
