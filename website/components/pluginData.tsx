a7351758bee5f27f640e86d44ce7d00b00063c09
Herman Schaaf <hermanschaaf@gmail.com>
2023-03-22 15:08:29 +0000

    chore(website): Fix some logos for dark mode (#9362)

--- website/components/pluginData.tsx
+++ website/components/pluginData.tsx
@@ -586,6 +586,7 @@ export const UNPUBLISHED_SOURCE_PLUGINS: Plugin[] = [
     kind: "unpublished",
     category: "cloud-infrastructure",
     logo: "/images/logos/plugins/hashicorp-vault.svg",
+    logoDark: "/images/logos/plugins/hashicorp-vault-dark.svg",
   },
   {
     name: "Infoblox",
@@ -594,6 +595,7 @@ export const UNPUBLISHED_SOURCE_PLUGINS: Plugin[] = [
     kind: "unpublished",
     category: "cloud-infrastructure",
     logo: "/images/logos/plugins/infoblox.svg",
+    logoDark: "/images/logos/plugins/infoblox-dark.svg",
   },
   {
     name: "Intercom",
@@ -710,6 +712,7 @@ export const UNPUBLISHED_SOURCE_PLUGINS: Plugin[] = [
     kind: "unpublished",
     category: "cloud-infrastructure",
     logo: "/images/logos/plugins/prisma.svg",
+    logoDark: "/images/logos/plugins/prisma-dark.svg",
   },
   {
     name: "Render",
@@ -793,6 +796,7 @@ export const UNPUBLISHED_SOURCE_PLUGINS: Plugin[] = [
     kind: "unpublished",
     category: "cloud-finops",
     logo: "/images/logos/plugins/square.svg",
+    logoDark: "/images/logos/plugins/square-dark.svg",
   },
   {
     name: "Tenable",
@@ -824,6 +828,7 @@ export const UNPUBLISHED_SOURCE_PLUGINS: Plugin[] = [
     kind: "unpublished",
     category: "marketing-analytics",
     logo: "/images/logos/plugins/typeform.svg",
+    logoDark: "/images/logos/plugins/typeform-dark.svg",
   },
   {
     name: "Zoho Campaign",
