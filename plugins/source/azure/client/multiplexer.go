aad2d84af45d9cacce5fc4f65c30d4aac2f7b4b0
erezrokah <erezrokah@users.noreply.github.com>
2023-03-22 18:54:32 +0200

    fix(azure): Remove redundant SingleSubscriptionMultiplex

--- plugins/source/azure/client/multiplexer.go
+++ plugins/source/azure/client/multiplexer.go
@@ -4,18 +4,6 @@ import (
 	"github.com/cloudquery/plugin-sdk/schema"
 )
 
-// SingleSubscriptionMultiplex can be used in cases where any single subscription would do. This can reduce the number
-// of API calls.
-func SingleSubscriptionMultiplex(meta schema.ClientMeta) []schema.ClientMeta {
-	client := meta.(*Client)
-	if len(client.subscriptions) == 0 {
-		return []schema.ClientMeta{}
-	}
-	return []schema.ClientMeta{
-		client.withSubscription(client.subscriptions[0]),
-	}
-}
-
 func SubscriptionMultiplexRegisteredNamespace(table, namespace string) func(schema.ClientMeta) []schema.ClientMeta {
 	return func(meta schema.ClientMeta) []schema.ClientMeta {
 		client := meta.(*Client)
