6362bd622101552532e692a236b38e0243c87e51
candiduslynx <candiduslynx@gmail.com>
2023-03-22 10:52:11 +0200

    add title transformer (currently not used)

--- plugins/source/googleanalytics/resources/plugin/plugin.go
+++ plugins/source/googleanalytics/resources/plugin/plugin.go
@@ -1,11 +1,33 @@
 package plugin
 
 import (
+	"strings"
+
 	"github.com/cloudquery/cloudquery/plugins/source/googleanalytics/client"
+	"github.com/cloudquery/plugin-sdk/caser"
 	"github.com/cloudquery/plugin-sdk/plugins/source"
+	"github.com/cloudquery/plugin-sdk/schema"
+	"golang.org/x/exp/maps"
 )
 
 var Version = "Development"
+
+var googleAnalyticsExceptions = map[string]string{
+	"googleanalytics": "Google Analytics",
+}
+
+func titleTransformer(table *schema.Table) string {
+	if table.Title != "" {
+		return table.Title
+	}
+	exceptions := maps.Clone(source.DefaultTitleExceptions)
+	for k, v := range googleAnalyticsExceptions {
+		exceptions[k] = v
+	}
+	csr := caser.New(caser.WithCustomExceptions(exceptions))
+	t := csr.ToTitle(table.Name)
+	return strings.Trim(strings.ReplaceAll(t, "  ", " "), " ")
+}
 
 func Plugin() *source.Plugin {
 	return source.NewPlugin(
@@ -14,5 +36,6 @@ func Plugin() *source.Plugin {
 		nil,
 		client.Configure,
 		source.WithDynamicTableOption(client.GetTables),
+		source.WithTitleTransformer(titleTransformer),
 	)
 }
