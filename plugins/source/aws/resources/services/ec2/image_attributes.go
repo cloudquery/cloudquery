ac34587561b386c778b0639a736d287cbf4abba5
bbernays <ben@bernays.me>
2023-03-24 10:19:15 -0500

    Update image_attributes.go

--- plugins/source/aws/resources/services/ec2/image_attributes.go
+++ plugins/source/aws/resources/services/ec2/image_attributes.go
@@ -3,6 +3,7 @@ package ec2
 import (
 	"context"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
@@ -29,7 +30,9 @@ func imageAttributesLaunchPermissions() *schema.Table {
 func fetchEc2ImageAttributeLaunchPermissions(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
 	c := meta.(*client.Client)
 	p := parent.Item.(types.Image)
-
+	if aws.ToString(p.OwnerId) != c.AccountID {
+		return nil
+	}
 	svc := c.Services().Ec2
 	output, err := svc.DescribeImageAttribute(ctx, &ec2.DescribeImageAttributeInput{
 		Attribute: types.ImageAttributeNameLaunchPermission,
