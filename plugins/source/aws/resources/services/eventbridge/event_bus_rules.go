9c80d63afe25eba7c9045787c7e6b1fd6459c002
Kemal Hadimli <disq@users.noreply.github.com>
2023-03-24 17:04:26 +0000

    feat(aws): More EventBridge resources

--- plugins/source/aws/resources/services/eventbridge/event_bus_rules.go
+++ plugins/source/aws/resources/services/eventbridge/event_bus_rules.go
@@ -1,18 +1,22 @@
 package eventbridge
 
 import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/eventbridge"
 	"github.com/aws/aws-sdk-go-v2/service/eventbridge/types"
 	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
 	"github.com/cloudquery/plugin-sdk/schema"
 	"github.com/cloudquery/plugin-sdk/transformers"
 )
 
-func EventBusRules() *schema.Table {
+func eventBusRules() *schema.Table {
 	tableName := "aws_eventbridge_event_bus_rules"
 	return &schema.Table{
 		Name:        tableName,
 		Description: `https://docs.aws.amazon.com/eventbridge/latest/APIReference/API_Rule.html`,
-		Resolver:    fetchEventbridgeEventBusRules,
+		Resolver:    fetchEventBusRules,
 		Multiplex:   client.ServiceAccountRegionMultiplexer(tableName, "events"),
 		Transform:   transformers.TransformWithStruct(&types.Rule{}, transformers.WithPrimaryKeys("Arn")),
 		Columns: []schema.Column{
@@ -26,8 +30,34 @@ func EventBusRules() *schema.Table {
 			{
 				Name:     "tags",
 				Type:     schema.TypeJSON,
-				Resolver: resolveEventbridgeEventBusRuleTags,
+				Resolver: resolveEventBusRuleTags,
 			},
 		},
 	}
+}
+
+func fetchEventBusRules(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
+	p := parent.Item.(types.EventBus)
+	input := eventbridge.ListRulesInput{
+		EventBusName: p.Arn,
+	}
+	c := meta.(*client.Client)
+	svc := c.Services().Eventbridge
+	for {
+		response, err := svc.ListRules(ctx, &input)
+		if err != nil {
+			return err
+		}
+		res <- response.Rules
+		if aws.ToString(response.NextToken) == "" {
+			break
+		}
+		input.NextToken = response.NextToken
+	}
+	return nil
+}
+
+func resolveEventBusRuleTags(ctx context.Context, meta schema.ClientMeta, resource *schema.Resource, c schema.Column) error {
+	eventBusArn := resource.Item.(types.Rule).Arn
+	return resolveTags(ctx, meta, resource, c, *eventBusArn)
 }
--- plugins/source/aws/resources/services/eventbridge/replays.go
+++ plugins/source/aws/resources/services/eventbridge/replays.go
@@ -1,6 +1,11 @@
 package eventbridge
 
 import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/arn"
+	"github.com/aws/aws-sdk-go-v2/service/eventbridge"
 	"github.com/aws/aws-sdk-go-v2/service/eventbridge/types"
 	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
 	"github.com/cloudquery/plugin-sdk/schema"
@@ -12,7 +17,7 @@ func Replays() *schema.Table {
 	return &schema.Table{
 		Name:        tableName,
 		Description: `https://docs.aws.amazon.com/eventbridge/latest/APIReference/API_Replay.html`,
-		Resolver:    fetchEventbridgeReplays,
+		Resolver:    fetchReplays,
 		Multiplex:   client.ServiceAccountRegionMultiplexer(tableName, "events"),
 		Transform:   transformers.TransformWithStruct(&types.Replay{}),
 		Columns: []schema.Column{
@@ -28,4 +33,36 @@ func Replays() *schema.Table {
 			},
 		},
 	}
+}
+
+func fetchReplays(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
+	var input eventbridge.ListReplaysInput
+	c := meta.(*client.Client)
+	svc := c.Services().Eventbridge
+	for {
+		response, err := svc.ListReplays(ctx, &input)
+		if err != nil {
+			return err
+		}
+		res <- response.Replays
+		if aws.ToString(response.NextToken) == "" {
+			break
+		}
+		input.NextToken = response.NextToken
+	}
+	return nil
+}
+
+func resolveReplayArn(ctx context.Context, meta schema.ClientMeta, resource *schema.Resource, c schema.Column) error {
+	cl := meta.(*client.Client)
+
+	a := arn.ARN{
+		Partition: cl.Partition,
+		Service:   "events",
+		Region:    cl.Region,
+		AccountID: cl.AccountID,
+		Resource:  "replay/" + aws.ToString(resource.Item.(types.Replay).ReplayName),
+	}
+
+	return resource.Set(c.Name, a.String())
 }
