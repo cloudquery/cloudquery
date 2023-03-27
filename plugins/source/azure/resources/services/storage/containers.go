e452dd3a53efb52b095af3f389a093c0177544f4
Erez Rokah <erezrokah@users.noreply.github.com>
2023-03-27 20:05:06 +0300

    feat(azure-resources): Add Missing Storage Account tables and columns (#9462)
    #### Summary
    
    Related to https://github.com/cloudquery/cloudquery/issues/8339 which is blocked due to https://github.com/Azure/azure-sdk-for-go/issues/20476.
    
    I also tried the old SDK but couldn't get it to work (looks like an access control/permissions issue).
    
    Also added a bunch of missing subscription ID columns
    
    <!--

--- plugins/source/azure/resources/services/storage/containers.go
+++ plugins/source/azure/resources/services/storage/containers.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/storage/armstorage"
+	"github.com/cloudquery/cloudquery/plugins/source/azure/client"
 	"github.com/cloudquery/plugin-sdk/schema"
 	"github.com/cloudquery/plugin-sdk/transformers"
 )
@@ -12,6 +13,6 @@ func containers() *schema.Table {
 		Resolver:    fetchContainers,
 		Description: "https://learn.microsoft.com/en-us/rest/api/storagerp/blob-containers/list?tabs=HTTP#listcontaineritem",
 		Transform:   transformers.TransformWithStruct(&armstorage.ListContainerItem{}, transformers.WithPrimaryKeys("ID")),
-		Columns:     schema.ColumnList{},
+		Columns:     schema.ColumnList{client.SubscriptionID},
 	}
 }
--- plugins/source/azure/resources/services/storage/tables.go
+++ plugins/source/azure/resources/services/storage/tables.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/storage/armstorage"
+	"github.com/cloudquery/cloudquery/plugins/source/azure/client"
 	"github.com/cloudquery/plugin-sdk/schema"
 	"github.com/cloudquery/plugin-sdk/transformers"
 )
@@ -12,6 +13,6 @@ func tables() *schema.Table {
 		Resolver:    fetchTables,
 		Description: "https://learn.microsoft.com/en-us/rest/api/storagerp/table/list?tabs=HTTP#table",
 		Transform:   transformers.TransformWithStruct(&armstorage.Table{}, transformers.WithPrimaryKeys("ID")),
-		Columns:     schema.ColumnList{},
+		Columns:     schema.ColumnList{client.SubscriptionID},
 	}
 }
