0501120068b0edf935cb31f605a0e34ada56b7b4
candiduslynx <candiduslynx@gmail.com>
2023-03-29 12:03:18 +0300

    extract common definitions

--- plugins/source/k8s/resources/services/core/nodes.go
+++ plugins/source/k8s/resources/services/core/nodes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/cloudquery/cloudquery/plugins/source/k8s/client"
 	"github.com/cloudquery/plugin-sdk/schema"
+	"github.com/cloudquery/plugin-sdk/transformers"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -14,21 +15,12 @@ func Nodes() *schema.Table {
 		Name:      "k8s_core_nodes",
 		Resolver:  fetchNodes,
 		Multiplex: client.ContextMultiplex,
-		Transform: client.TransformWithStruct(&v1.Node{}, client.WithMoreSkipFields("DoNotUseExternalID")),
-		Columns: []schema.Column{
-			{
-				Name:     "context",
-				Type:     schema.TypeString,
-				Resolver: client.ResolveContext,
-			},
-			{
-				Name:     "uid",
-				Type:     schema.TypeString,
-				Resolver: schema.PathResolver("UID"),
-				CreationOptions: schema.ColumnCreationOptions{
-					PrimaryKey: true,
-				},
-			},
+		Transform: client.TransformWithStruct(&v1.Node{},
+			client.WithMoreSkipFields("DoNotUseExternalID"),
+			transformers.WithPrimaryKeys("UID"),
+		),
+		Columns: schema.ColumnList{
+			client.ContextColumn,
 			{
 				Name:     "spec_pod_cidr",
 				Type:     schema.TypeCIDR,
