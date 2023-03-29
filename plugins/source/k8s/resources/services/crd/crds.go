d4b03a43d42e20119b0ced371dd9988c0fc3ccf3
candiduslynx <candiduslynx@users.noreply.github.com>
2023-03-29 22:58:36 +0300

    feat(k8s): Add CRDs support (#9504)
    Implements #7800

---
+++ plugins/source/k8s/resources/services/crd/crds.go
@@ -0,0 +1,38 @@
+package crd
+
+import (
+	"context"
+
+	"github.com/cloudquery/cloudquery/plugins/source/k8s/client"
+	"github.com/cloudquery/plugin-sdk/schema"
+	"github.com/cloudquery/plugin-sdk/transformers"
+	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func CRDs() *schema.Table {
+	return &schema.Table{
+		Name:      "k8s_crds",
+		Resolver:  fetchCRDs,
+		Multiplex: client.ContextMultiplex,
+		Transform: client.TransformWithStruct(new(v1.CustomResourceDefinition), transformers.WithPrimaryKeys("UID")),
+		Columns:   schema.ColumnList{client.ContextColumn},
+	}
+}
+
+func fetchCRDs(ctx context.Context, meta schema.ClientMeta, _ *schema.Resource, res chan<- any) error {
+	cl := meta.(*client.Client).APIExtensions().ApiextensionsV1().CustomResourceDefinitions()
+
+	opts := metav1.ListOptions{}
+	for {
+		result, err := cl.List(ctx, opts)
+		if err != nil {
+			return err
+		}
+		res <- result.Items
+		if result.GetContinue() == "" {
+			return nil
+		}
+		opts.Continue = result.GetContinue()
+	}
+}
