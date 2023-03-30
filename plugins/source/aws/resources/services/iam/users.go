4d9a39c058b907ffd1e767edac5da6bb710731cf
bbernays <ben@bernays.me>
2023-03-30 15:15:17 -0500

    Update users.go

--- plugins/source/aws/resources/services/iam/users.go
+++ plugins/source/aws/resources/services/iam/users.go
@@ -21,11 +21,7 @@ func Users() *schema.Table {
 		Transform:           transformers.TransformWithStruct(&types.User{}),
 		Multiplex:           client.ServiceAccountRegionMultiplexer(tableName, "iam"),
 		Columns: []schema.Column{
-			{
-				Name:     "arn",
-				Type:     schema.TypeString,
-				Resolver: schema.PathResolver("Arn"),
-			},
+			client.DefaultAccountIDColumn(true),
 			{
 				Name:     "id",
 				Type:     schema.TypeString,
@@ -34,7 +30,6 @@ func Users() *schema.Table {
 					PrimaryKey: true,
 				},
 			},
-			client.DefaultAccountIDColumn(true),
 			{
 				Name:     "tags",
 				Type:     schema.TypeJSON,
