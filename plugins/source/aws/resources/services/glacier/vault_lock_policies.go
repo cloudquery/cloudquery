b51deb60ac5ab53e8f23d6a3284608ff8db3edc6
bbernays <ben@bernays.me>
2023-04-01 11:23:22 -0400

    more

--- plugins/source/aws/resources/services/glacier/vault_lock_policies.go
+++ plugins/source/aws/resources/services/glacier/vault_lock_policies.go
@@ -1,7 +1,10 @@
 package glacier
 
 import (
+	"context"
+
 	"github.com/aws/aws-sdk-go-v2/service/glacier"
+	"github.com/aws/aws-sdk-go-v2/service/glacier/types"
 	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
 	"github.com/cloudquery/plugin-sdk/schema"
 	"github.com/cloudquery/plugin-sdk/transformers"
@@ -33,4 +36,19 @@ func vaultLockPolicies() *schema.Table {
 			},
 		},
 	}
+}
+
+func fetchGlacierVaultLockPolicies(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
+	c := meta.(*client.Client)
+	svc := c.Services().Glacier
+	p := parent.Item.(types.DescribeVaultOutput)
+
+	response, err := svc.GetVaultLock(ctx, &glacier.GetVaultLockInput{
+		VaultName: p.VaultName,
+	})
+	if err != nil {
+		return err
+	}
+	res <- response
+	return nil
 }
--- plugins/source/aws/resources/services/glacier/vault_notifications.go
+++ plugins/source/aws/resources/services/glacier/vault_notifications.go
@@ -1,6 +1,9 @@
 package glacier
 
 import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/service/glacier"
 	"github.com/aws/aws-sdk-go-v2/service/glacier/types"
 	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
 	"github.com/cloudquery/plugin-sdk/schema"
@@ -28,4 +31,19 @@ func vaultNotifications() *schema.Table {
 			},
 		},
 	}
+}
+
+func fetchGlacierVaultNotifications(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
+	c := meta.(*client.Client)
+	svc := c.Services().Glacier
+	p := parent.Item.(types.DescribeVaultOutput)
+
+	response, err := svc.GetVaultNotifications(ctx, &glacier.GetVaultNotificationsInput{
+		VaultName: p.VaultName,
+	})
+	if err != nil {
+		return err
+	}
+	res <- response.VaultNotificationConfig
+	return nil
 }
