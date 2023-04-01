24f1538091606385d7653271c6fd45823d685d8e
bbernays <ben@bernays.me>
2023-04-01 09:42:34 -0400

    elastic transcoder

--- plugins/source/aws/resources/services/elastictranscoder/pipelines.go
+++ plugins/source/aws/resources/services/elastictranscoder/pipelines.go
@@ -1,6 +1,9 @@
 package elastictranscoder
 
 import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/service/elastictranscoder"
 	"github.com/aws/aws-sdk-go-v2/service/elastictranscoder/types"
 	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
 	"github.com/cloudquery/plugin-sdk/schema"
@@ -32,4 +35,21 @@ func Pipelines() *schema.Table {
 			pipelineJobs(),
 		},
 	}
+}
+
+func fetchElastictranscoderPipelines(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
+	c := meta.(*client.Client)
+	svc := c.Services().Elastictranscoder
+
+	p := elastictranscoder.NewListPipelinesPaginator(svc, nil)
+	for p.HasMorePages() {
+		response, err := p.NextPage(ctx)
+		if err != nil {
+			return err
+		}
+
+		res <- response.Pipelines
+	}
+
+	return nil
 }
