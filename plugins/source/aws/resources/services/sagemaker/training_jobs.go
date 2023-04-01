1af31aad2afbe9e0dfb92e08fd3998122cb5a34b
bbernays <ben@cloudquery.io>
2023-04-01 08:01:05 -0500

    fix(aws): Tag Pagination Properly Handled (#9611)
    #### Summary
    
    Prior to this fix, cq would only resolve the first page of tags for sagemaker resources

--- plugins/source/aws/resources/services/sagemaker/training_jobs.go
+++ plugins/source/aws/resources/services/sagemaker/training_jobs.go
@@ -1,7 +1,10 @@
 package sagemaker
 
 import (
+	"context"
+
 	"github.com/aws/aws-sdk-go-v2/service/sagemaker"
+	"github.com/aws/aws-sdk-go-v2/service/sagemaker/types"
 	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
 	"github.com/cloudquery/plugin-sdk/schema"
 	"github.com/cloudquery/plugin-sdk/transformers"
@@ -35,4 +38,54 @@ func TrainingJobs() *schema.Table {
 			},
 		},
 	}
+}
+
+func fetchSagemakerTrainingJobs(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
+	c := meta.(*client.Client)
+	svc := c.Services().Sagemaker
+	config := sagemaker.ListTrainingJobsInput{}
+	paginator := sagemaker.NewListTrainingJobsPaginator(svc, &config)
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(ctx)
+		if err != nil {
+			return err
+		}
+		res <- page.TrainingJobSummaries
+	}
+	return nil
+}
+
+func getTrainingJob(ctx context.Context, meta schema.ClientMeta, resource *schema.Resource) error {
+	c := meta.(*client.Client)
+	svc := c.Services().Sagemaker
+	n := resource.Item.(types.TrainingJobSummary)
+	config := sagemaker.DescribeTrainingJobInput{
+		TrainingJobName: n.TrainingJobName,
+	}
+	response, err := svc.DescribeTrainingJob(ctx, &config)
+	if err != nil {
+		return err
+	}
+	resource.Item = response
+	return nil
+}
+
+func resolveSagemakerTrainingJobTags(ctx context.Context, meta schema.ClientMeta, resource *schema.Resource, col schema.Column) error {
+	r := resource.Item.(*sagemaker.DescribeTrainingJobOutput)
+	if r == nil {
+		return nil
+	}
+	svc := meta.(*client.Client).Services().Sagemaker
+	config := sagemaker.ListTagsInput{ResourceArn: r.TrainingJobArn}
+	paginator := sagemaker.NewListTagsPaginator(svc, &config)
+	var tags []types.Tag
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(ctx)
+		if err != nil {
+			return err
+		}
+		tags = append(tags, page.Tags...)
+	}
+
+	return resource.Set(col.Name, client.TagsToMap(tags))
 }
