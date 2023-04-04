802a002ffe00ec4c50a4cc63dc87e6918a0b74ae
candiduslynx <candiduslynx@gmail.com>
2023-04-04 13:41:28 +0300

    move fetch to table definitions

--- plugins/source/snyk/resources/services/organization/organizations.go
+++ plugins/source/snyk/resources/services/organization/organizations.go
@@ -1,6 +1,8 @@
 package organization
 
 import (
+	"context"
+
 	"github.com/cloudquery/cloudquery/plugins/source/snyk/client"
 	"github.com/cloudquery/plugin-sdk/schema"
 	"github.com/cloudquery/plugin-sdk/transformers"
@@ -15,4 +17,22 @@ func Organizations() *schema.Table {
 		Multiplex:   client.SingleOrganization,
 		Transform:   transformers.TransformWithStruct(&snyk.Organization{}, transformers.WithPrimaryKeys("ID")),
 	}
+}
+
+func fetchOrganizations(ctx context.Context, meta schema.ClientMeta, _ *schema.Resource, res chan<- any) error {
+	c := meta.(*client.Client)
+
+	result, _, err := c.Orgs.List(ctx)
+	if err != nil {
+		return err
+	}
+
+	// limit organizations
+	for _, org := range result {
+		if c.WantOrganization(org.ID) {
+			res <- org
+		}
+	}
+
+	return nil
 }
