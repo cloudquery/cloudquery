ab286bce8208d2a8585be5962ba36cf8094f7d6f
bbernays <ben@bernays.me>
2023-04-04 08:33:03 -0400

    Update inline_policy_blocked_kms_actions.sql

--- plugins/source/aws/policies/queries/kms/inline_policy_blocked_kms_actions.sql
+++ plugins/source/aws/policies/queries/kms/inline_policy_blocked_kms_actions.sql
@@ -65,7 +65,7 @@ from
                         )
                     when 'array' then policy_document -> 'Statement' end
         ) as statement
-        inner join on
+        inner join aws_iam_groups on
             aws_iam_groups.account_id = aws_iam_group_policies.account_id and
             aws_iam_groups on aws_iam_groups.arn = aws_iam_group_policies.group_arn
         ) as t
