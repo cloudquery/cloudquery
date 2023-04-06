6d78541a47769edc1a7caa05f18f9cf483a5d026
bbernays <ben@bernays.me>
2023-04-06 09:57:19 -0400

    Update byoip_cidrs.go

--- plugins/source/aws/resources/services/ec2/byoip_cidrs.go
+++ plugins/source/aws/resources/services/ec2/byoip_cidrs.go
@@ -34,9 +34,6 @@ func ByoipCidrs() *schema.Table {
 }
 
 func fetchEc2ByoipCidrs(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
-	config := ec2.DescribeByoipCidrsInput{
-		MaxResults: aws.Int32(100),
-	}
 
 	c := meta.(*client.Client)
 	// DescribeByoipCidrs does not work in next regions, so we ignore them.
@@ -47,16 +44,16 @@ func fetchEc2ByoipCidrs(ctx context.Context, meta schema.ClientMeta, parent *sch
 		return nil
 	}
 	svc := c.Services().Ec2
-	for {
-		response, err := svc.DescribeByoipCidrs(ctx, &config)
+	config := ec2.DescribeByoipCidrsInput{
+		MaxResults: aws.Int32(100),
+	}
+	paginator := ec2.NewDescribeByoipCidrsPaginator(svc, &config)
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(ctx)
 		if err != nil {
 			return err
 		}
-		res <- response.ByoipCidrs
-		if aws.ToString(response.NextToken) == "" {
-			break
-		}
-		config.NextToken = response.NextToken
+		res <- page.ByoipCidrs
 	}
 	return nil
 }
