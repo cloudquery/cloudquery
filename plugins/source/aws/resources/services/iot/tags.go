93961df3c09974327395197a85a3f754b49699e9
bbernays <ben@cloudquery.io>
2023-04-10 09:41:41 -0400

    refactor(aws): Consolidate IoT Tag Resolvers (#9802)
    #### Summary
    
    use single function to make API calls rather than reimplementing for each resource

---
+++ plugins/source/aws/resources/services/iot/tags.go
@@ -0,0 +1,26 @@
+package iot
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/service/iot"
+	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
+	"github.com/cloudquery/cloudquery/plugins/source/aws/client/services"
+	"github.com/cloudquery/plugin-sdk/schema"
+)
+
+func resolveIotTags(ctx context.Context, svc services.IotClient, resource *schema.Resource, c schema.Column, resourceArn *string) error {
+	input := iot.ListTagsForResourceInput{
+		ResourceArn: resourceArn,
+	}
+	tags := make(map[string]string)
+	paginator := iot.NewListTagsForResourcePaginator(svc, &input)
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(ctx)
+		if err != nil {
+			return err
+		}
+		client.TagsIntoMap(page.Tags, tags)
+	}
+	return resource.Set(c.Name, tags)
+}
