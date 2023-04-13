addf934a4da27d69764320d39c21282e29143211
candiduslynx <candiduslynx@gmail.com>
2023-04-13 16:02:31 +0300

    update k8s

--- plugins/source/k8s/client/helpers.go
+++ plugins/source/k8s/client/helpers.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"reflect"
 
-	"github.com/cloudquery/plugin-sdk/schema"
+	"github.com/cloudquery/plugin-sdk/v2/schema"
 	"github.com/thoas/go-funk"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
