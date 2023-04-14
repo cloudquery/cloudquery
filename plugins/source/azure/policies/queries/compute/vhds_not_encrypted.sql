73d7bfe63235becec20bc687503fe6e7bbb68e8b
FUJITA Yuki <yuki.fujita@bizreach.co.jp>
2023-04-14 09:57:54 +0900

    fix query

--- plugins/source/azure/policies/queries/compute/vhds_not_encrypted.sql
+++ plugins/source/azure/policies/queries/compute/vhds_not_encrypted.sql
@@ -1,4 +1,4 @@
-INTO azure_policy_results (execution_time, framework, check_id, title, subscription_id, resource_id, status)
+INSERT INTO azure_policy_results (execution_time, framework, check_id, title, subscription_id, resource_id, status)
 SELECT :'execution_time'                           AS execution_time,
        :'framework'                                AS framework,
        :'check_id'                                 AS check_id,
