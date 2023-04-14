0c03fb31e9ed46f571e24a27fdf898c2b112ed70
candiduslynx <candiduslynx@gmail.com>
2023-04-14 10:45:07 +0300

    update digitalocean

--- plugins/source/digitalocean/resources/services/spaces/cors.go
+++ plugins/source/digitalocean/resources/services/spaces/cors.go
@@ -2,8 +2,8 @@ package spaces
 
 import (
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
-	"github.com/cloudquery/plugin-sdk/schema"
-	"github.com/cloudquery/plugin-sdk/transformers"
+	"github.com/cloudquery/plugin-sdk/v2/schema"
+	"github.com/cloudquery/plugin-sdk/v2/transformers"
 )
 
 func cors() *schema.Table {
