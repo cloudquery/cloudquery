8f7cba42dacec41cc10f551013ed86a2ec4f903f
candiduslynx <candiduslynx@gmail.com>
2023-04-14 11:23:27 +0300

    update hackernews

--- plugins/source/hackernews/client/services/backend.go
+++ plugins/source/hackernews/client/services/backend.go
@@ -1,6 +1,6 @@
 package services
 
-import "github.com/cloudquery/plugin-sdk/backend"
+import "github.com/cloudquery/plugin-sdk/v2/backend"
 
 //go:generate mockgen -package=mocks -destination=../mocks/backend.go -source=backend.go BackendClient
 type Backend interface {
