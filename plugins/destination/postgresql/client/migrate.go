c37b198bcb919871c81730643362d63a87851ed6
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2023-04-17 19:35:58 +0300

    fix review

--- plugins/destination/postgresql/client/migrate.go
+++ plugins/destination/postgresql/client/migrate.go
@@ -326,10 +326,6 @@ func (c *Client) createTableIfNotExist(ctx context.Context, table *arrow.Schema)
 	primaryKeys := []string{}
 	for i, col := range table.Fields() {
 		pgType := c.SchemaTypeToPg(col.Type)
-		if pgType == "" {
-			c.logger.Warn().Str("table", tName).Str("column", col.Name).Msg("Column type is not supported, skipping")
-			continue
-		}
 		columnName := pgx.Identifier{col.Name}.Sanitize()
 		fieldDef := columnName + " " + pgType
 		if schema.IsUnique(col) {
