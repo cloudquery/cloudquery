652387179ff342cfc2e807e01e2f0c5a7fe3fd99
candiduslynx <candiduslynx@gmail.com>
2023-04-19 13:36:29 +0300

    reverse transform

--- plugins/destination/meilisearch/client/read.go
+++ plugins/destination/meilisearch/client/read.go
@@ -2,17 +2,12 @@ package client
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"net"
-	"strconv"
 
 	"github.com/apache/arrow/go/v12/arrow"
 	"github.com/apache/arrow/go/v12/arrow/array"
 	"github.com/apache/arrow/go/v12/arrow/memory"
 	"github.com/cloudquery/plugin-sdk/v2/schema"
-	"github.com/cloudquery/plugin-sdk/v2/types"
-	"github.com/google/uuid"
 	"github.com/meilisearch/meilisearch-go"
 )
 
@@ -60,119 +55,9 @@ func docToRecord(sc *arrow.Schema, doc map[string]any) (arrow.Record, error) {
 	builder := array.NewRecordBuilder(memory.DefaultAllocator, sc)
 	defer builder.Release()
 
-	for i, fld := range builder.Fields() {
-		val, ok := doc[sc.Field(i).Name]
-		if !ok || val == nil {
-			fld.AppendNull()
-			continue
-		}
-
-		switch tBuilder := fld.(type) {
-		case *array.BooleanBuilder:
-			tBuilder.Append(val.(bool))
-		case *array.Int8Builder:
-			tBuilder.Append(int8(val.(float64)))
-		case *array.Int16Builder:
-			tBuilder.Append(int16(val.(float64)))
-		case *array.Int32Builder:
-			tBuilder.Append(int32(val.(float64)))
-		case *array.Int64Builder:
-			tBuilder.Append(int64(val.(float64)))
-		case *array.Uint8Builder:
-			tBuilder.Append(uint8(val.(float64)))
-		case *array.Uint16Builder:
-			tBuilder.Append(uint16(val.(float64)))
-		case *array.Uint32Builder:
-			tBuilder.Append(uint32(val.(float64)))
-		case *array.Uint64Builder:
-			tBuilder.Append(uint64(val.(float64)))
-		case *array.Float32Builder:
-			tBuilder.Append(float32(val.(float64)))
-		case *array.Float64Builder:
-			tBuilder.Append(val.(float64))
-		case *array.StringBuilder:
-			tBuilder.Append(val.(string))
-		case *array.BinaryBuilder:
-			var data []byte
-			val := val.(string)
-			if val != "null" {
-				err := json.Unmarshal([]byte(strconv.Quote(val)), &data)
-				if err != nil {
-					return nil, err
-				}
-			}
-			tBuilder.Append(data)
-		case *array.TimestampBuilder:
-			ts, err := arrow.TimestampFromString(val.(string), arrow.Microsecond)
-			if err != nil {
-				return nil, err
-			}
-			tBuilder.Append(ts)
-		case *types.InetBuilder:
-			_, ipNet, err := net.ParseCIDR(val.(string))
-			if err != nil {
-				return nil, err
-			}
-			tBuilder.Append(*ipNet)
-		case *types.JSONBuilder:
-			tBuilder.Append(val)
-		case *types.MacBuilder:
-			mac, err := net.ParseMAC(val.(string))
-			if err != nil {
-				return nil, err
-			}
-			tBuilder.Append(mac)
-		case *types.UUIDBuilder:
-			uid, err := uuid.Parse(val.(string))
-			if err != nil {
-				return nil, err
-			}
-			tBuilder.Append(uid)
-		case *array.ListBuilder:
-			tBuilder.Append(true)
-			switch listBuilder := tBuilder.ValueBuilder().(type) {
-			case *array.StringBuilder:
-				// string array
-				for _, v := range val.([]any) {
-					listBuilder.Append(v.(string))
-				}
-			case *array.Int64Builder:
-				// int array
-				for _, v := range val.([]any) {
-					listBuilder.Append(int64(v.(float64)))
-				}
-			case *types.UUIDBuilder:
-				// uuid array
-				for _, v := range val.([]any) {
-					uid, err := uuid.Parse(v.(string))
-					if err != nil {
-						return nil, err
-					}
-					listBuilder.Append(uid)
-				}
-			case *types.InetBuilder:
-				// inet/cidr array
-				for _, v := range val.([]any) {
-					_, ipNet, err := net.ParseCIDR(v.(string))
-					if err != nil {
-						return nil, err
-					}
-					listBuilder.Append(*ipNet)
-				}
-			case *types.MacBuilder:
-				// macaddr array
-				for _, v := range val.([]any) {
-					mac, err := net.ParseMAC(v.(string))
-					if err != nil {
-						return nil, err
-					}
-					listBuilder.Append(mac)
-				}
-			default:
-				return nil, fmt.Errorf("unsupported list builder: %T", listBuilder)
-			}
-		default:
-			return nil, fmt.Errorf("unsupported builder: %T", tBuilder)
+	for i, builder := range builder.Fields() {
+		if err := reverseTransform(builder, doc[sc.Field(i).Name]); err != nil {
+			return nil, err
 		}
 	}
 	return builder.NewRecord(), nil
--- plugins/destination/meilisearch/client/transform.go
+++ plugins/destination/meilisearch/client/transform.go
@@ -1,8 +1,15 @@
 package client
 
 import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"strconv"
+
 	"github.com/apache/arrow/go/v12/arrow"
 	"github.com/apache/arrow/go/v12/arrow/array"
+	"github.com/cloudquery/plugin-sdk/v2/types"
+	"github.com/google/uuid"
 )
 
 func timestampValues(arr *array.Timestamp) []any {
@@ -43,4 +50,93 @@ func transpose(m map[string][]any, l int) []map[string]any {
 	}
 
 	return rows
+}
+
+func reverseTransform(builder array.Builder, val any) error {
+	if val == nil {
+		builder.AppendNull()
+		return nil
+	}
+
+	switch builder := builder.(type) {
+	case *array.BooleanBuilder:
+		builder.Append(val.(bool))
+	case *array.Int8Builder:
+		builder.Append(int8(val.(float64)))
+	case *array.Int16Builder:
+		builder.Append(int16(val.(float64)))
+	case *array.Int32Builder:
+		builder.Append(int32(val.(float64)))
+	case *array.Int64Builder:
+		builder.Append(int64(val.(float64)))
+	case *array.Uint8Builder:
+		builder.Append(uint8(val.(float64)))
+	case *array.Uint16Builder:
+		builder.Append(uint16(val.(float64)))
+	case *array.Uint32Builder:
+		builder.Append(uint32(val.(float64)))
+	case *array.Uint64Builder:
+		builder.Append(uint64(val.(float64)))
+	case *array.Float32Builder:
+		builder.Append(float32(val.(float64)))
+	case *array.Float64Builder:
+		builder.Append(val.(float64))
+	case *array.StringBuilder:
+		builder.Append(val.(string))
+	case *array.LargeStringBuilder:
+		builder.Append(val.(string))
+	case *array.BinaryBuilder:
+		var data []byte
+		val := val.(string)
+		if val != "null" {
+			err := json.Unmarshal([]byte(strconv.Quote(val)), &data)
+			if err != nil {
+				return err
+			}
+		}
+		builder.Append(data)
+	case *array.TimestampBuilder:
+		ts, err := arrow.TimestampFromString(val.(string), arrow.Microsecond)
+		if err != nil {
+			return err
+		}
+		builder.Append(ts)
+	case *types.InetBuilder:
+		_, ipNet, err := net.ParseCIDR(val.(string))
+		if err != nil {
+			return err
+		}
+		builder.Append(*ipNet)
+	case *types.JSONBuilder:
+		builder.Append(val)
+	case *types.MacBuilder:
+		mac, err := net.ParseMAC(val.(string))
+		if err != nil {
+			return err
+		}
+		builder.Append(mac)
+	case *types.UUIDBuilder:
+		uid, err := uuid.Parse(val.(string))
+		if err != nil {
+			return err
+		}
+		builder.Append(uid)
+	case array.ListLikeBuilder:
+		builder.Append(true)
+		valueBuilder := builder.ValueBuilder()
+		for _, v := range val.([]any) {
+			if err := reverseTransform(valueBuilder, v); err != nil {
+				return err
+			}
+		}
+	default:
+		v, ok := val.(string)
+		if !ok {
+			return fmt.Errorf("unsupported type %T with builder %T", val, builder)
+		}
+		if err := builder.AppendValueFromString(v); err != nil {
+			return err
+		}
+	}
+	return nil
 }
