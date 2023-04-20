fc9094f57f2b2e67a040c68725f2c6271f3da2f9
bbernays <ben@cloudquery.io>
2023-04-20 11:38:39 -0500

    fix(aws)!: Fix Primary Key for `aws_directconnect_gateways` (#10233)
    #### Summary
    
    `aws_directconnect_gateways` is a global resource. The region should not be in the primary key

--- plugins/source/aws/resources/services/directconnect/gateway_associations.go
+++ plugins/source/aws/resources/services/directconnect/gateway_associations.go
@@ -1,6 +1,10 @@
 package directconnect
 
 import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/directconnect"
 	"github.com/aws/aws-sdk-go-v2/service/directconnect/types"
 	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
 	"github.com/cloudquery/plugin-sdk/v2/schema"
@@ -30,4 +34,24 @@ func gatewayAssociations() *schema.Table {
 			},
 		},
 	}
+}
+
+func fetchDirectconnectGatewayAssociations(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
+	gateway := parent.Item.(types.DirectConnectGateway)
+	c := meta.(*client.Client)
+	svc := c.Services().Directconnect
+	config := directconnect.DescribeDirectConnectGatewayAssociationsInput{DirectConnectGatewayId: gateway.DirectConnectGatewayId}
+	// No paginator available
+	for {
+		output, err := svc.DescribeDirectConnectGatewayAssociations(ctx, &config)
+		if err != nil {
+			return err
+		}
+		res <- output.DirectConnectGatewayAssociations
+		if aws.ToString(output.NextToken) == "" {
+			break
+		}
+		config.NextToken = output.NextToken
+	}
+	return nil
 }
