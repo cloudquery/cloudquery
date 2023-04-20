62d428054c406155ec3c278d54c841b5b67e1eaf
Herman Schaaf <hermanschaaf@gmail.com>
2023-04-20 09:52:03 +0100

    uuid -> UUID

--- plugins/destination/file/client/read.go
+++ plugins/destination/file/client/read.go
@@ -18,7 +18,7 @@ func (c *Client) Read(ctx context.Context, arrowSchema *arrow.Schema, sourceName
 		return fmt.Errorf("reading is not supported when `no_rotate` is false. Table: %q; Source: %q", tableName, sourceName)
 	}
 	if strings.Contains(c.pluginSpec.Path, PathVarUUID) {
-		return fmt.Errorf("reading is not supported when `path` contains uuid variable. Table: %q; Source: %q", tableName, sourceName)
+		return fmt.Errorf("reading is not supported when `path` contains UUID variable. Table: %q; Source: %q", tableName, sourceName)
 	}
 	name := replacePathVariables(c.pluginSpec.Path, tableName, c.pluginSpec.Format, uuid.NewString(), time.Time{})
 	f, err := os.Open(name)
