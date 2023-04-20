3d3aa8f3a4a215fee96f45c9a6550b1e6b503b7d
bbernays <ben@bernays.me>
2023-04-20 12:58:48 -0500

    Update ebs_snapshots_mock_test.go

--- plugins/source/aws/resources/services/ec2/ebs_snapshots_mock_test.go
+++ plugins/source/aws/resources/services/ec2/ebs_snapshots_mock_test.go
@@ -3,6 +3,7 @@ package ec2
 import (
 	"testing"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
@@ -23,10 +24,12 @@ func buildEc2EbsSnapshots(t *testing.T, ctrl *gomock.Controller) client.Services
 	if err != nil {
 		t.Fatal(err)
 	}
+	s.OwnerId = aws.String("testAccount")
 	m.EXPECT().DescribeSnapshots(gomock.Any(), gomock.Any(), gomock.Any()).Return(
 		&ec2.DescribeSnapshotsOutput{
 			Snapshots: []types.Snapshot{s},
 		}, nil)
+
 	m.EXPECT().DescribeSnapshotAttribute(gomock.Any(), gomock.Any(), gomock.Any()).Return(
 		&ec2.DescribeSnapshotAttributeOutput{
 			CreateVolumePermissions: []types.CreateVolumePermission{sa},
