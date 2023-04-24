a6b050d48318df60e8916d67114b966d32b5b451
candiduslynx <candiduslynx@gmail.com>
2023-04-24 13:19:30 +0300

    struct test

--- plugins/destination/clickhouse/typeconv/ch/value/extension.go
+++ plugins/destination/clickhouse/typeconv/ch/value/extension.go
@@ -1,6 +1,7 @@
 package value
 
 import (
+	"github.com/apache/arrow/go/v12/arrow"
 	"github.com/apache/arrow/go/v12/arrow/array"
 	"github.com/cloudquery/plugin-sdk/v2/types"
 	"github.com/google/uuid"
@@ -16,4 +17,26 @@ func extensionValue(arr array.ExtensionArray) (any, error) {
 		// we fallback here to string representation
 		return valueStrData(arr), nil
 	}
+}
+
+func marshalValue[A any](arr arrow.Array) []*A {
+	res := make([]*A, arr.Len())
+	for i := 0; i < arr.Len(); i++ {
+		if arr.IsValid(i) && !arr.IsNull(i) {
+			val := arr.GetOneForMarshal(i).(A)
+			res[i] = &val
+		}
+	}
+	return res
+}
+
+func valueStrData(arr arrow.Array) []*string {
+	res := make([]*string, arr.Len())
+	for i := 0; i < arr.Len(); i++ {
+		if arr.IsValid(i) && !arr.IsNull(i) {
+			val := arr.ValueStr(i)
+			res[i] = &val
+		}
+	}
+	return res
 }
