8baafb83668dc1199a11a771f8740eb71bfdd095
bbernays <ben@bernays.me>
2023-04-24 14:57:02 -0500

    Create cost_current_month_mock_test.go

---
+++ plugins/source/aws/resources/services/costexplorer/cost_current_month_mock_test.go
@@ -0,0 +1,32 @@
+package costexplorer
+
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/service/costexplorer"
+	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
+	"github.com/cloudquery/cloudquery/plugins/source/aws/client/mocks"
+	"github.com/cloudquery/plugin-sdk/v2/faker"
+	"github.com/golang/mock/gomock"
+)
+
+func buildCurrentMonthCost(t *testing.T, ctrl *gomock.Controller) client.Services {
+	m := mocks.NewMockCostexplorerClient(ctrl)
+
+	gcuo := costexplorer.GetCostAndUsageOutput{}
+	err := faker.FakeObject(&gcuo)
+	if err != nil {
+		t.Fatal(err)
+	}
+	gcuo.NextPageToken = nil
+	m.EXPECT().GetCostAndUsage(gomock.Any(), gomock.Any(), gomock.Any()).MinTimes(1).Return(
+		&gcuo, nil)
+
+	return client.Services{
+		Costexplorer: m,
+	}
+}
+
+func TestCostExplorerCurrentMonth(t *testing.T) {
+	client.AwsMockTestHelper(t, CurrentMonthCost(), buildCurrentMonthCost, client.TestOptions{})
+}
