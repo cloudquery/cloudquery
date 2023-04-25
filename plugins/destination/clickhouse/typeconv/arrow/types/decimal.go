87958db4d90d5cce1ac22f0d690d2159398cbf6f
candiduslynx <candiduslynx@gmail.com>
2023-04-25 09:12:07 +0300

    simplify signature

--- plugins/destination/clickhouse/typeconv/arrow/types/decimal.go
+++ plugins/destination/clickhouse/typeconv/arrow/types/decimal.go
@@ -5,12 +5,12 @@ import (
 	"github.com/apache/arrow/go/v12/arrow"
 )
 
-func decimalType(name string, col *column.Decimal) (*arrow.Field, error) {
+func decimalType(name string, col *column.Decimal) *arrow.Field {
 	var decimal arrow.DecimalType
 	if precision := col.Precision(); precision <= 38 {
 		decimal = &arrow.Decimal128Type{Precision: int32(precision), Scale: int32(col.Scale())}
 	} else {
 		decimal = &arrow.Decimal256Type{Precision: int32(precision), Scale: int32(col.Scale())}
 	}
-	return &arrow.Field{Name: name, Type: decimal}, nil
+	return &arrow.Field{Name: name, Type: decimal}
 }
