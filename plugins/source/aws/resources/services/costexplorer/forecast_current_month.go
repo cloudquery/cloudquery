c2f84e8db1c0c444e51712845203797a60f23166
bbernays <ben@bernays.me>
2023-04-25 09:47:50 -0500

    opt in flag

--- plugins/source/aws/resources/services/costexplorer/forecast_current_month.go
+++ plugins/source/aws/resources/services/costexplorer/forecast_current_month.go
@@ -15,21 +15,40 @@ import (
 func CurrentMonthForecast() *schema.Table {
 	tableName := "awscost_costexplorer_forecast_current_month"
 	return &schema.Table{
-		Name:        tableName,
-		Description: `https://docs.aws.amazon.com/xray/latest/api/API_Group.html`,
-		Resolver:    fetchForecast,
-		Transform:   transformers.TransformWithStruct(&types.ForecastResult{}),
-		Multiplex:   client.AccountMultiplex(tableName),
+		Name: tableName,
+		Description: `https://docs.aws.amazon.com/aws-cost-management/latest/APIReference/API_GetCostForecast.html
+To sync this table you must set the 'include_paid_apis' option to 'true' in the AWS provider configuration. `,
+		Resolver:  fetchForecast,
+		Transform: transformers.TransformWithStruct(&types.ForecastResult{}),
+		Multiplex: client.AccountMultiplex(tableName),
 		Columns: []schema.Column{
-			client.DefaultAccountIDColumn(false),
+			client.DefaultAccountIDColumn(true),
+			{
+				Name:        "start_date",
+				Description: `The start date covered by the forecast.`,
+				Type:        schema.TypeString,
+				Resolver:    schema.PathResolver("TimePeriod.Start"),
+				CreationOptions: schema.ColumnCreationOptions{
+					PrimaryKey: true,
+				},
+			},
+			{
+				Name:        "end_date",
+				Description: `The end date covered by the forecast.`,
+				Type:        schema.TypeString,
+				Resolver:    schema.PathResolver("TimePeriod.End"),
+				CreationOptions: schema.ColumnCreationOptions{
+					PrimaryKey: true,
+				},
+			},
 		},
 	}
 }
 
 func fetchForecast(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
 	cl := meta.(*client.Client)
-	if !cl.Spec.SkipNonFreeAPIs {
-		cl.Logger().Info().Msg("skipping `awscost_costexplorer_forecast_current_month` because `use_non_free_apis` is set to false")
+	if !cl.Spec.IncludePaidAPIs {
+		cl.Logger().Info().Msg("skipping `awscost_costexplorer_forecast_current_month` because `include_paid_apis` is set to false")
 		return nil
 	}
 
