4cf78ae5bc3b4d2567bfa76e539d12ac5d86fa86
bbernays <ben@bernays.me>
2023-04-25 14:36:06 -0500

    Update deletestale.go

--- plugins/destination/neo4j/client/deletestale.go
+++ plugins/destination/neo4j/client/deletestale.go
@@ -17,9 +17,10 @@ func (c *Client) DeleteStale(ctx context.Context, tables schema.Schemas, source 
 	for _, table := range tables {
 		tableName := schema.TableName(table)
 		stmt := fmt.Sprintf(deleteCypher, tableName)
-		if _, err := session.Run(ctx, stmt, map[string]any{"cq_source_name": source, "cq_sync_time": syncTime}); err != nil {
+		if _, err := session.Run(ctx, stmt, map[string]any{"cq_source_name": source, "cq_sync_time": syncTime.Truncate(time.Millisecond)}); err != nil {
 			return err
 		}
+
 	}
 	return session.Close(ctx)
 }
