4b718f90a1c528f89ba5875dcb6a6d4e6ed761a8
bbernays <ben@bernays.me>
2023-05-01 13:10:34 -0400

    Update utility.go

--- plugins/source/aws/resources/services/costexplorer/utility.go
+++ plugins/source/aws/resources/services/costexplorer/utility.go
@@ -9,3 +9,10 @@ func beginningOfMonth(date time.Time) time.Time {
 func endOfMonth(date time.Time) time.Time {
 	return date.AddDate(0, 1, -date.Day()+1)
 }
+
+func forecastEndOfMonth(now time.Time) time.Time {
+	if now.Format("2006-01-02") == endOfMonth(now).Format("2006-01-02") {
+		return now.AddDate(0, 0, 1)
+	}
+	return endOfMonth(now).AddDate(0, 0, 1)
+}
