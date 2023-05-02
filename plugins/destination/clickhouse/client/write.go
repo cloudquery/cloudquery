c799625858720b43bb26567b6231332d321989d7
candiduslynx <candiduslynx@gmail.com>
2023-05-02 15:56:05 +0300

    use reader

--- plugins/destination/clickhouse/client/write.go
+++ plugins/destination/clickhouse/client/write.go
@@ -4,17 +4,23 @@ import (
 	"context"
 
 	"github.com/apache/arrow/go/v12/arrow"
+	"github.com/apache/arrow/go/v12/arrow/array"
 	"github.com/cloudquery/cloudquery/plugins/destination/clickhouse/queries"
 	"github.com/cloudquery/cloudquery/plugins/destination/clickhouse/typeconv/ch/values"
 )
 
 func (c *Client) WriteTableBatch(ctx context.Context, sc *arrow.Schema, records []arrow.Record) error {
+	reader, err := array.NewRecordReader(sc, records)
+	if err != nil {
+		return err
+	}
+
 	batch, err := c.conn.PrepareBatch(ctx, queries.Insert(sc))
 	if err != nil {
 		return err
 	}
 
-	if err := values.BatchAddRecords(ctx, sc, batch, records); err != nil {
+	if err := values.BatchAddRecords(ctx, batch, reader); err != nil {
 		_ = batch.Abort()
 		return err
 	}
