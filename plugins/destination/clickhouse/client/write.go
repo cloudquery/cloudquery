b2385ff37a8be795019c43a7cc7ac7189eca35d4
candiduslynx <candiduslynx@gmail.com>
2023-05-04 02:19:57 +0300

    no array.RecordReader for now (x2 speedup)

--- plugins/destination/clickhouse/client/write.go
+++ plugins/destination/clickhouse/client/write.go
@@ -4,23 +4,17 @@ import (
 	"context"
 
 	"github.com/apache/arrow/go/v12/arrow"
-	"github.com/apache/arrow/go/v12/arrow/array"
 	"github.com/cloudquery/cloudquery/plugins/destination/clickhouse/queries"
 	"github.com/cloudquery/cloudquery/plugins/destination/clickhouse/typeconv/ch/values"
 )
 
 func (c *Client) WriteTableBatch(ctx context.Context, sc *arrow.Schema, records []arrow.Record) error {
-	reader, err := array.NewRecordReader(sc, records)
-	if err != nil {
-		return err
-	}
-
 	batch, err := c.conn.PrepareBatch(ctx, queries.Insert(sc))
 	if err != nil {
 		return err
 	}
 
-	if err := values.BatchAddRecords(ctx, batch, reader); err != nil {
+	if err := values.BatchAddRecords(ctx, batch, sc, records); err != nil {
 		_ = batch.Abort()
 		return err
 	}
