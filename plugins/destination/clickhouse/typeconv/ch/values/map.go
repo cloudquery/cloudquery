e72e25191ffa1252a2e887bda493335b00331085
candiduslynx <candiduslynx@gmail.com>
2023-05-03 22:21:51 +0300

    test + both options

--- plugins/destination/clickhouse/typeconv/ch/values/map.go
+++ plugins/destination/clickhouse/typeconv/ch/values/map.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/ClickHouse/clickhouse-go/v2/lib/column"
+	"github.com/apache/arrow/go/v12/arrow"
 	"github.com/apache/arrow/go/v12/arrow/array"
 	"github.com/cloudquery/cloudquery/plugins/destination/clickhouse/typeconv/ch/types"
 )
@@ -32,29 +33,7 @@ func mapValue(arr *array.Map) (any, error) {
 		return nil, fmt.Errorf("unexpected reflect type for map: %q", valueType.String())
 	}
 
-	keysArr, itemsArr := arr.Keys(), arr.Items()
-	keys, err := FromArray(keysArr) // []*[]*...
-	if err != nil {
-		return nil, err
-	}
-	items, err := FromArray(itemsArr) // []*[]*...
-	if err != nil {
-		return nil, err
-	}
-	keysValue, itemsValue := reflect.ValueOf(keys), reflect.ValueOf(items)
-
-	res := reflect.MakeSlice(reflect.SliceOf(reflect.PointerTo(valueType)), arr.Len(), arr.Len()) // we do []*(type) for nullable assignment
-	for i := 0; i < arr.Len(); i++ {
-		var val reflect.Value
-		// we need to fill in for the in-depth recursive parsing by ClickHouse SDK
-		if arr.IsNull(i) {
-			val = reflect.MakeMap(valueType) // zero-sized map
-		} else {
-			val = makeMap(valueType, keysValue.Index(i).Elem(), itemsValue.Index(i).Elem())
-		}
-		res.Index(i).Set(val)
-	}
-	return res.Interface(), nil
+	return makeMapSlice(valueType, arr)
 }
 
 func marshalValuesToStrings(arr *array.Map) []*string {
@@ -70,11 +49,94 @@ func marshalValuesToStrings(arr *array.Map) []*string {
 	return res
 }
 
-func makeMap(mapType reflect.Type, keys, items reflect.Value) reflect.Value {
+func makeMapSlice(mapType reflect.Type, arr *array.Map) (any, error) {
+	res := reflect.MakeSlice(reflect.SliceOf(reflect.PointerTo(mapType)), arr.Len(), arr.Len()) // we do []*(type) for nullable assignment
+	for i := 0; i < arr.Len(); i++ {
+		val := reflect.New(mapType)
+		if arr.IsNull(i) {
+			// we need to fill in for the in-depth recursive parsing by ClickHouse SDK
+			res.Index(i).Set(val)
+			continue
+		}
+		start, end := arr.ValueOffsets(i)
+		mapVal, err := makeMapWithList(mapType, array.NewSlice(arr.ListValues(), start, end))
+		//keys := array.NewSlice(arr.Keys(), start, end)
+		//items := array.NewSlice(arr.Items(), start, end)
+		//mapVal, err := makeMapKV(mapType, keys, items)
+		if err != nil {
+			return nil, err
+		}
+		val.Elem().Set(*mapVal)
+		res.Index(i).Set(val)
+	}
+	return res.Interface(), nil
+}
+
+func makeMapKV(mapType reflect.Type, keyArr, itemArr arrow.Array) (*reflect.Value, error) {
+	keysVal, err := FromArray(keyArr)
+	if err != nil {
+		return nil, err
+	}
+	keys := reflect.ValueOf(keysVal)
+
+	itemsVal, err := FromArray(itemArr)
+	if err != nil {
+		return nil, err
+	}
+	items := reflect.ValueOf(itemsVal)
+
+	if keys.Len() != items.Len() {
+		return nil, fmt.Errorf("keys len (%d) != items len (%d)", keys.Len(), items.Len())
+	}
+
 	val := reflect.MakeMapWithSize(mapType, keys.Len())
 	for i := 0; i < keys.Len(); i++ {
+		key, item := keys.Index(i).Elem(), items.Index(i)
+		// we unwrap the item only if it's nested type: map or slice/array
+		if item.Kind() == reflect.Pointer {
+			switch item.Type().Elem().Kind() {
+			case reflect.Map, reflect.Slice, reflect.Array:
+				item = item.Elem()
+			case reflect.Invalid: // we encountered the nil value, still we need to unwrap
+
+			}
+		}
 		// Arrow maps don't support nullable keys, so no need to check
-		val.SetMapIndex(keys.Index(i).Elem(), items.Index(i))
+		// BEWARE: reflect.Value.SetMapIndex deletes the value from map if it's the zero value (nil)
+		val.SetMapIndex(key, item)
 	}
-	return val
+	return &val, nil
+}
+
+func makeMapWithList(mapType reflect.Type, arr arrow.Array) (*reflect.Value, error) {
+	data, err := FromArray(arr)
+	if err != nil {
+		return nil, err
+	}
+	// we do know that this is []*[]*map[string]any (map is implemented as list of structs(key, item))
+	actualData := data.([]*map[string]any)
+
+	const (
+		keyField  = "key"
+		itemField = "value"
+	)
+
+	value := reflect.MakeMapWithSize(mapType, len(actualData))
+	for _, elem := range actualData {
+		// elem should NEVER be nil (at least key has to be filled in)
+		key := reflect.ValueOf((*elem)[keyField]).Elem()
+		item := reflect.ValueOf((*elem)[itemField])
+		if item.Kind() == reflect.Pointer {
+			switch item.Type().Elem().Kind() {
+			case reflect.Map, reflect.Slice, reflect.Array:
+				if item.IsNil() {
+					item = reflect.New(item.Type().Elem()).Elem()
+				} else {
+					item = item.Elem()
+				}
+			}
+		}
+		value.SetMapIndex(key, item)
+	}
+	return &value, nil
 }
--- plugins/destination/clickhouse/typeconv/ch/values/map_test.go
+++ plugins/destination/clickhouse/typeconv/ch/values/map_test.go
@@ -6,6 +6,8 @@ import (
 	"github.com/apache/arrow/go/v12/arrow"
 	"github.com/apache/arrow/go/v12/arrow/array"
 	"github.com/apache/arrow/go/v12/arrow/memory"
+	"github.com/cloudquery/plugin-sdk/v2/types"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 )
 
@@ -13,22 +15,49 @@ func Test_mapValue(t *testing.T) {
 	mapType := arrow.MapOf(
 		new(arrow.StringType),
 		arrow.StructOf(
-			arrow.Field{Name: "nullable_bool", Type: new(arrow.BooleanType), Nullable: true},
-			arrow.Field{Name: "non_nullable_bool", Type: new(arrow.BooleanType)},
+			arrow.Field{Name: "bool_n", Type: new(arrow.BooleanType), Nullable: true},
+			arrow.Field{Name: "bool", Type: new(arrow.BooleanType)},
+			arrow.Field{Name: "list", Type: arrow.ListOf(types.NewUUIDType())},
+			arrow.Field{
+				Name: "map",
+				Type: arrow.MapOf(new(arrow.Int32Type), new(arrow.Float64Type)),
+			},
+			arrow.Field{
+				Name:     "map_n",
+				Type:     arrow.MapOf(new(arrow.Int32Type), new(arrow.Float64Type)),
+				Nullable: true,
+			},
 		),
 	)
 
 	builder := array.NewMapBuilderWithType(memory.DefaultAllocator, mapType)
-	keyBuilder, itemBuilder := builder.KeyBuilder().(*array.StringBuilder), builder.ValueBuilder().(*array.StructBuilder)
-	nullableBld := itemBuilder.FieldBuilder(0).(*array.BooleanBuilder)
-	nonNullableBld := itemBuilder.FieldBuilder(1).(*array.BooleanBuilder)
+	keyBuilder, itemBuilder := builder.KeyBuilder().(*array.StringBuilder), builder.ItemBuilder().(*array.StructBuilder)
+
+	boolBld := itemBuilder.FieldBuilder(0).(*array.BooleanBuilder)
+	boolNBld := itemBuilder.FieldBuilder(1).(*array.BooleanBuilder)
+	listBld := itemBuilder.FieldBuilder(2).(*array.ListBuilder)
+	listValBld := listBld.ValueBuilder().(*types.UUIDBuilder)
+
+	mapBld := itemBuilder.FieldBuilder(3).(*array.MapBuilder)
+	mapKeyBld, mapItemBld := mapBld.KeyBuilder().(*array.Int32Builder), mapBld.ItemBuilder().(*array.Float64Builder)
+
+	mapNBld := itemBuilder.FieldBuilder(4).(*array.MapBuilder)
+	mapNKeyBld, mapNItemBld := mapNBld.KeyBuilder().(*array.Int32Builder), mapNBld.ItemBuilder().(*array.Float64Builder)
 
 	// single proper value
 	builder.Append(true)
 	keyBuilder.Append("proper")
 	itemBuilder.Append(true)
-	nullableBld.Append(true)
-	nonNullableBld.Append(true)
+	boolBld.Append(false)
+	boolNBld.Append(true)
+	listBld.Append(true)
+	listValBld.Append(uuid.NameSpaceDNS)
+	mapBld.Append(true)
+	mapKeyBld.Append(123)
+	mapItemBld.Append(123.456)
+	mapNBld.Append(true)
+	mapNKeyBld.Append(321)
+	mapNItemBld.Append(654.321)
 
 	// single empty value
 	builder.Append(true)
@@ -39,8 +68,15 @@ func Test_mapValue(t *testing.T) {
 	builder.Append(true)
 	keyBuilder.Append("proper")
 	itemBuilder.Append(true)
-	nullableBld.Append(true)
-	nonNullableBld.Append(true)
+	boolBld.Append(false)
+	boolNBld.Append(true)
+	listBld.Append(true)
+	listValBld.Append(uuid.NameSpaceDNS)
+	mapBld.Append(true)
+	mapKeyBld.Append(123)
+	mapItemBld.Append(123.456)
+	mapNBld.AppendNull() // nil map
+
 	keyBuilder.Append("empty")
 	itemBuilder.AppendNull()
 
@@ -50,7 +86,7 @@ func Test_mapValue(t *testing.T) {
 	data, err := mapValue(builder.NewMapArray())
 	require.NoError(t, err)
 
-	elems := data.([]*map[string]*map[string]any)
+	elems := data.([]*map[string]map[string]any)
 	require.Equal(t, 4, len(elems))
 
 	// single proper value
@@ -59,7 +95,13 @@ func Test_mapValue(t *testing.T) {
 	val, ok := (*elem)["proper"]
 	require.True(t, ok)
 	require.NotNil(t, val)
-	require.Equal(t, map[string]any{"non_nullable_bool": true, "nullable_bool": true}, *val)
+	require.EqualValues(t, map[string]any{
+		"bool":   ptr(true),
+		"bool_n": ptr(false),
+		"list":   ptr([]*uuid.UUID{&uuid.NameSpaceDNS}),
+		"map":    ptr(map[int32]*float64{123: ptr(float64(123.456))}),
+		"map_n":  ptr(map[int32]*float64{321: ptr(float64(654.321))}),
+	}, val)
 
 	// single empty value
 	elem = elems[1]
@@ -74,12 +116,21 @@ func Test_mapValue(t *testing.T) {
 	val, ok = (*elem)["proper"]
 	require.True(t, ok)
 	require.NotNil(t, val)
-	require.Equal(t, map[string]any{"non_nullable_bool": true, "nullable_bool": true}, *val)
+	require.EqualValues(t, map[string]any{
+		"bool":   ptr(true),
+		"bool_n": ptr(false),
+		"list":   ptr([]*uuid.UUID{&uuid.NameSpaceDNS}),
+		"map":    ptr(map[int32]*float64{123: ptr(float64(123.456))}),
+		"map_n":  ptr(map[int32]*float64(nil)),
+	}, val)
 	val, ok = (*elem)["empty"]
 	require.True(t, ok)
 	require.Nil(t, val)
 
 	// null
 	elem = elems[3]
-	require.Nil(t, elem)
+	require.NotNil(t, elem)
+	require.Nil(t, *elem)
 }
+
+func ptr[A any](a A) *A { return &a }
