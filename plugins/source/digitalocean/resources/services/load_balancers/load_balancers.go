d3f308d51d7ae25cda170d976da51e5150b69307
Kemal <223029+disq@users.noreply.github.com>
2023-05-05 10:17:11 +0100

    fix(digitalocean): Re-add missing resources: Load Balancers and Regions (#10563)
    Fixes #10561

---
+++ plugins/source/digitalocean/resources/services/load_balancers/load_balancers.go
@@ -0,0 +1,51 @@
+package load_balancers
+
+import (
+	"context"
+
+	"github.com/cloudquery/cloudquery/plugins/source/digitalocean/client"
+	"github.com/cloudquery/plugin-sdk/v2/schema"
+	"github.com/cloudquery/plugin-sdk/v2/transformers"
+	"github.com/digitalocean/godo"
+)
+
+func LoadBalancers() *schema.Table {
+	return &schema.Table{
+		Name:        "digitalocean_load_balancers",
+		Description: "https://pkg.go.dev/github.com/digitalocean/godo#LoadBalancer",
+		Resolver:    fetchLoadBalancers,
+		Transform:   transformers.TransformWithStruct(&godo.LoadBalancer{}, transformers.WithPrimaryKeys("ID")),
+	}
+}
+
+func fetchLoadBalancers(ctx context.Context, meta schema.ClientMeta, _ *schema.Resource, res chan<- any) error {
+	svc := meta.(*client.Client)
+	opt := &godo.ListOptions{
+		PerPage: client.MaxItemsPerPage,
+	}
+	for {
+		var (
+			data []godo.LoadBalancer
+			resp *godo.Response
+		)
+		listFunc := func() error {
+			var err error
+			data, resp, err = svc.Services.LoadBalancers.List(ctx, opt)
+			return err
+		}
+		err := client.ThrottleWrapper(ctx, svc, listFunc)
+		if err != nil {
+			return err
+		}
+		res <- data
+		if resp.Links == nil || resp.Links.IsLastPage() {
+			break
+		}
+		page, err := resp.Links.CurrentPage()
+		if err != nil {
+			return err
+		}
+		opt.Page = page + 1
+	}
+	return nil
+}
---
+++ website/tables/digitalocean/digitalocean_load_balancers.md
@@ -0,0 +1,40 @@
+# Table: digitalocean_load_balancers
+
+This table shows data for DigitalOcean Load Balancers.
+
+https://pkg.go.dev/github.com/digitalocean/godo#LoadBalancer
+
+The primary key for this table is **id**.
+
+## Columns
+
+| Name          | Type          |
+| ------------- | ------------- |
+|_cq_source_name|String|
+|_cq_sync_time|Timestamp|
+|_cq_id|UUID|
+|_cq_parent_id|UUID|
+|id (PK)|String|
+|name|String|
+|ip|String|
+|size|String|
+|size_unit|Int|
+|algorithm|String|
+|status|String|
+|created_at|String|
+|forwarding_rules|JSON|
+|health_check|JSON|
+|sticky_sessions|JSON|
+|region|JSON|
+|droplet_ids|IntArray|
+|tag|String|
+|tags|StringArray|
+|redirect_http_to_https|Bool|
+|enable_proxy_protocol|Bool|
+|enable_backend_keepalive|Bool|
+|vpc_uuid|String|
+|disable_lets_encrypt_dns_records|Bool|
+|validate_only|Bool|
+|project_id|String|
+|http_idle_timeout_seconds|Int|
+|firewall|JSON|
