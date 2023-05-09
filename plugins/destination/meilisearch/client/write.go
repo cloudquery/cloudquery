af54a45a131293af93eacff1a02695c1d5823161
candiduslynx <candiduslynx@gmail.com>
2023-05-09 11:04:11 +0300

    destination/meilisearch

--- plugins/destination/meilisearch/client/write.go
+++ plugins/destination/meilisearch/client/write.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/apache/arrow/go/v12/arrow"
+	"github.com/apache/arrow/go/v13/arrow"
+	"github.com/cloudquery/plugin-pb-go/specs"
 	"github.com/cloudquery/plugin-sdk/v2/schema"
-	"github.com/cloudquery/plugin-sdk/v2/specs"
 )
 
 func (c *Client) WriteTableBatch(ctx context.Context, sc *arrow.Schema, records []arrow.Record) error {
