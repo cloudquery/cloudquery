c17e12c8e4c6f66c80b3255514e5df7891910553
candiduslynx <candiduslynx@gmail.com>
2023-05-09 11:22:48 +0300

    destination/sqlite

--- plugins/destination/sqlite/client/transformer.go
+++ plugins/destination/sqlite/client/transformer.go
@@ -1,8 +1,8 @@
 package client
 
 import (
-	"github.com/apache/arrow/go/v12/arrow"
-	"github.com/apache/arrow/go/v12/arrow/array"
+	"github.com/apache/arrow/go/v13/arrow"
+	"github.com/apache/arrow/go/v13/arrow/array"
 )
 
 func getValue(arr arrow.Array, i int) any {
