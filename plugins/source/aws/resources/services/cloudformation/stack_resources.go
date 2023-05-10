b0962ce4377c680dcc7849484f0bcdbb31df2c6f
bbernays <ben@bernays.me>
2023-05-10 14:20:45 -0500

    Update stack_resources.go

--- plugins/source/aws/resources/services/cloudformation/stack_resources.go
+++ plugins/source/aws/resources/services/cloudformation/stack_resources.go
@@ -1,6 +1,9 @@
 package cloudformation
 
 import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
 	"github.com/aws/aws-sdk-go-v2/service/cloudformation/types"
 	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
 	"github.com/cloudquery/plugin-sdk/v2/schema"
@@ -25,4 +28,24 @@ func stackResources() *schema.Table {
 			},
 		},
 	}
+}
+
+func fetchCloudformationStackResources(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
+	stack := parent.Item.(types.Stack)
+	config := cloudformation.ListStackResourcesInput{
+		StackName: stack.StackName,
+	}
+	c := meta.(*client.Client)
+	svc := c.Services().Cloudformation
+	paginator := cloudformation.NewListStackResourcesPaginator(svc, &config)
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(ctx, func(options *cloudformation.Options) {
+			options.Region = c.Region
+		})
+		if err != nil {
+			return err
+		}
+		res <- page.StackResourceSummaries
+	}
+	return nil
 }
