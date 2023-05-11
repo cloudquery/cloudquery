1852a21add780e732e4692353a7e82b8699e5ea4
Kemal Hadimli <disq@users.noreply.github.com>
2023-05-11 10:32:01 +0100

    chore(file): Upgrade to SDK v3

--- plugins/destination/file/client/client.go
+++ plugins/destination/file/client/client.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/cloudquery/filetypes/v2"
 	"github.com/cloudquery/plugin-pb-go/specs"
-	"github.com/cloudquery/plugin-sdk/v2/plugins/destination"
+	"github.com/cloudquery/plugin-sdk/v3/plugins/destination"
 	"github.com/rs/zerolog"
 )
 
--- plugins/destination/file/client/read.go
+++ plugins/destination/file/client/read.go
@@ -8,24 +8,23 @@ import (
 	"time"
 
 	"github.com/apache/arrow/go/v13/arrow"
-	"github.com/cloudquery/plugin-sdk/v2/schema"
+	"github.com/cloudquery/plugin-sdk/v3/schema"
 	"github.com/google/uuid"
 )
 
-func (c *Client) Read(ctx context.Context, arrowSchema *arrow.Schema, sourceName string, res chan<- arrow.Record) error {
-	tableName := schema.TableName(arrowSchema)
+func (c *Client) Read(ctx context.Context, table *schema.Table, sourceName string, res chan<- arrow.Record) error {
 	if !c.pluginSpec.NoRotate {
-		return fmt.Errorf("reading is not supported when `no_rotate` is false. Table: %q; Source: %q", tableName, sourceName)
+		return fmt.Errorf("reading is not supported when `no_rotate` is false. Table: %q; Source: %q", table.Name, sourceName)
 	}
 	if strings.Contains(c.pluginSpec.Path, PathVarUUID) {
-		return fmt.Errorf("reading is not supported when `path` contains UUID variable. Table: %q; Source: %q", tableName, sourceName)
+		return fmt.Errorf("reading is not supported when `path` contains UUID variable. Table: %q; Source: %q", table.Name, sourceName)
 	}
-	name := replacePathVariables(c.pluginSpec.Path, tableName, c.pluginSpec.Format, uuid.NewString(), time.Time{})
+	name := replacePathVariables(c.pluginSpec.Path, table.Name, c.pluginSpec.Format, uuid.NewString(), time.Time{})
 	f, err := os.Open(name)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
 
-	return c.Client.Read(f, arrowSchema, sourceName, res)
+	return c.Client.Read(f, table.ToArrowSchema(), sourceName, res)
 }
