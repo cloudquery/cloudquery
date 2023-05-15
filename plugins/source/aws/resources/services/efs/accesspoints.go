5078acbe2f1b5c7409c3ef58cb63e1517b8851c4
bbernays <ben@bernays.me>
2023-05-15 14:03:54 -0500

    Create accesspoints.go

---
+++ plugins/source/aws/resources/services/efs/accesspoints.go
@@ -0,0 +1,55 @@
+package efs
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/service/efs"
+	"github.com/aws/aws-sdk-go-v2/service/efs/types"
+	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
+	"github.com/cloudquery/plugin-sdk/v2/schema"
+	"github.com/cloudquery/plugin-sdk/v2/transformers"
+)
+
+func AccessPoints() *schema.Table {
+	tableName := "aws_efs_accesspoints"
+	return &schema.Table{
+		Name:        tableName,
+		Description: `https://docs.aws.amazon.com/efs/latest/ug/API_AccessPointDescription.html`,
+		Resolver:    fetchAccessPoints,
+		Multiplex:   client.ServiceAccountRegionMultiplexer(tableName, "elasticfilesystem"),
+		Transform:   transformers.TransformWithStruct(&types.AccessPointDescription{}),
+		Columns: []schema.Column{
+			client.DefaultAccountIDColumn(false),
+			client.DefaultRegionColumn(false),
+			{
+				Name:     "arn",
+				Type:     schema.TypeString,
+				Resolver: schema.PathResolver("AccessPointArn"),
+				CreationOptions: schema.ColumnCreationOptions{
+					PrimaryKey: true,
+				},
+			},
+			{
+				Name:     "tags",
+				Type:     schema.TypeJSON,
+				Resolver: client.ResolveTags,
+			},
+		},
+	}
+}
+
+func fetchAccessPoints(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
+	c := meta.(*client.Client)
+	svc := c.Services().Efs
+	paginator := efs.NewDescribeAccessPointsPaginator(svc, nil)
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(ctx, func(options *efs.Options) {
+			options.Region = c.Region
+		})
+		if err != nil {
+			return err
+		}
+		res <- page.AccessPoints
+	}
+	return nil
+}
