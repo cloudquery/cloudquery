925f0b535a299f0c794866ba95a35698f9d0204b
FUJITA Yuki <yuki.fujita@bizreach.co.jp>
2023-05-23 16:07:09 +0900

    fix condition

--- plugins/source/azure/policies/queries/monitor/log_alert_for_create_or_update_security_solution.sql
+++ plugins/source/azure/policies/queries/monitor/log_alert_for_create_or_update_security_solution.sql
@@ -13,23 +13,29 @@ scopes AS (
     SELECT
         subscription_id,
         id,
-        scope::text
-    FROM azure_monitor_activity_log_alerts, jsonb_array_elements(properties->'scopes') AS scope
+        scope
+    FROM azure_monitor_activity_log_alerts, jsonb_array_elements_text(properties->'scopes') AS scope
+),
+conditions AS (
+    SELECT
+        fields.subscription_id AS subscription_id,
+        fields.id AS id,
+        scopes.scope AS scope,
+        location = 'global'
+            AND enabled
+            AND equals = 'Microsoft.Security/securitySolutions/write'
+            AND scopes.scope ~ '^\/subscriptions\/[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}$'
+        AS condition
+    FROM fields JOIN scopes ON fields.id = scopes.id
+    WHERE field = 'operationName'
 )
 SELECT
     :'execution_time'                                                              AS execution_time,
     :'framework'                                                                   AS framework,
     :'check_id'                                                                    AS check_id,
     'Ensure that Activity Log Alert exists for Create or Update Security Solution' AS title,
-    fields.subscription_id                                                         AS subscription_id,
-    fields.id                                                                      AS resource_id,
-    CASE
-        WHEN location = 'global'
-         AND enabled
-         AND equals = 'Microsoft.Security/securitySolutions/write'
-         AND scopes.scope ~ '^"\/subscriptions\/[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}"$'
-        THEN 'pass'
-        ELSE 'fail'
-    END                                                                            AS status
-FROM fields JOIN scopes ON fields.id = scopes.id
-WHERE field = 'operationName'
+    subscription_id                                                                AS subscription_id,
+    scope                                                                          AS resrouce_id,
+    bool_or(condition)                                                             AS status
+FROM conditions
+GROUP BY subscription_id, scope
