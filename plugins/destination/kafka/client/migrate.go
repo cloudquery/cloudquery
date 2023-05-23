ba962084263f6cf77fe008e2798921c51b0bb18a
bbernays <ben@cloudquery.io>
2023-05-23 16:24:32 -0500

    feat(kafka): Update to github.com/cloudquery/plugin-sdk/v3 (#10902)
    #### Summary
    
    closes #10721

--- plugins/destination/kafka/client/migrate.go
+++ plugins/destination/kafka/client/migrate.go
@@ -3,10 +3,10 @@ package client
 import (
 	"context"
 
-	"github.com/cloudquery/plugin-sdk/v2/schema"
+	"github.com/cloudquery/plugin-sdk/v3/schema"
 )
 
 // For Kafka migrate is a no-op
-func (*Client) Migrate(context.Context, schema.Schemas) error {
+func (*Client) Migrate(context.Context, schema.Tables) error {
 	return nil
 }
