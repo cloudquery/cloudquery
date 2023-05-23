a79d3593b8275882ee546823d6b9e601b2c47e69
FUJITA Yuki <yuki.fujita@bizreach.co.jp>
2023-05-23 14:17:50 +0900

    implement 5.2.x conditions

---
+++ plugins/source/azure/policies/queries/monitor/log_alert_for_delete_security_solution.sql
@@ -0,0 +1,35 @@
+INSERT INTO azure_policy_results
+WITH fields AS (
+    SELECT
+        subscription_id,
+        id,
+        location,
+        (properties->'enabled')::boolean AS enabled,
+        conditions->>'field' AS field,
+        conditions->>'equals' AS equals
+    FROM azure_monitor_activity_log_alerts, jsonb_array_elements(properties->'condition'->'allOf') AS conditions
+),
+scopes AS (
+    SELECT
+        subscription_id,
+        id,
+        scope::text
+    FROM azure_monitor_activity_log_alerts, jsonb_array_elements(properties->'scopes') AS scope
+)
+SELECT
+    :'execution_time'                                                    AS execution_time,
+    :'framework'                                                         AS framework,
+    :'check_id'                                                          AS check_id,
+    'Ensure that Activity Log Alert exists for Delete Security Solution' AS title,
+    fields.subscription_id                                               AS subscription_id,
+    fields.id                                                            AS resource_id,
+    CASE
+        WHEN location = 'global'
+         AND enabled
+         AND equals = 'Microsoft.Security/securitySolutions/delete'
+         AND scopes.scope ~ '^"\/subscriptions\/[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}"$'
+        THEN 'pass'
+        ELSE 'fail'
+    END                                                                            AS status
+FROM fields JOIN scopes ON fields.id = scopes.id
+WHERE field = 'operationName'
