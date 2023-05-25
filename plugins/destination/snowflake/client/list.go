a30bda8fbe0c1c9a9e2c7a02bbea84882d6ce71a
candiduslynx <candiduslynx@gmail.com>
2023-05-25 17:13:49 +0300

    rm unused

--- plugins/destination/snowflake/client/list.go
+++ plugins/destination/snowflake/client/list.go
@@ -17,15 +17,3 @@ func snowflakeStrToArray(val string) []string {
 	}
 	return strs
 }
-
-// https://github.com/snowflakedb/gosnowflake/issues/674
-func snowflakeStrToIntArray(val string) []string {
-	val = strings.TrimPrefix(val, "[\n  ")
-	val = strings.TrimSuffix(val, "\n]")
-	strs := strings.Split(val, ",\n  ")
-	for i := range strs {
-		strs[i] = strings.ReplaceAll(strs[i], "\\\"", "\"")
-		strs[i] = strings.ReplaceAll(strs[i], "\\n", "\n")
-	}
-	return strs
-}
