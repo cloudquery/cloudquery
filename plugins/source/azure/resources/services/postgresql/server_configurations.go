ad3c772dbf9c73548b25a3c4edb82ae1ae99978f
Yevgeny Pats <yev.pats@gmail.com>
2023-05-26 13:19:20 +0300

    feat(azure)!: Update to SDK V3 Arrow native (#10974)
    Closes #10735 
    
    BEGIN_COMMIT_OVERRIDE
    feat: Update to use [Apache Arrow](https://arrow.apache.org/) type
    system (#10974)
    
    BREAKING-CHANGE: This release introduces an internal change to our type
    system to use [Apache Arrow](https://arrow.apache.org/). This should not
    have any visible breaking changes, however due to the size of the change
    we are introducing it under a major version bump to communicate that it
    might have some bugs that we weren't able to catch during our internal
    tests. If you encounter an issue during the upgrade, please submit a
    [bug
    report](https://github.com/cloudquery/cloudquery/issues/new/choose). You
    will also need to update destinations depending on which one you use:
    - Azure Blob Storage >= v3.2.0
    - BigQuery >= v3.0.0
    - ClickHouse >= v3.1.1
    - DuckDB >= v1.1.6
    - Elasticsearch >= v2.0.0
    - File >= v3.2.0
    - Firehose >= v2.0.2
    - GCS >= v3.2.0
    - Gremlin >= v2.1.10
    - Kafka >= v3.0.1
    - Meilisearch >= v2.0.1
    - Microsoft SQL Server >= v4.2.0
    - MongoDB >= v2.0.1
    - MySQL >= v2.0.2
    - Neo4j >= v3.0.0
    - PostgreSQL >= v4.2.0
    - S3 >= v4.4.0
    - Snowflake >= v2.1.1
    - SQLite >= v2.2.0
    
    END_COMMIT_OVERRIDE

--- plugins/source/azure/resources/services/postgresql/server_configurations.go
+++ plugins/source/azure/resources/services/postgresql/server_configurations.go
@@ -2,8 +2,8 @@ package postgresql
 
 import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/postgresql/armpostgresql"
-	"github.com/cloudquery/plugin-sdk/v2/schema"
-	"github.com/cloudquery/plugin-sdk/v2/transformers"
+	"github.com/cloudquery/plugin-sdk/v3/schema"
+	"github.com/cloudquery/plugin-sdk/v3/transformers"
 )
 
 func serverConfigurations() *schema.Table {
