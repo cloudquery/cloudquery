71bdd4d516abf2e024f639ab2d0d12807f412975
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2023-05-26 18:35:31 +0300

    feat(oracle)!: Update to SDK V3 Arrow native

--- plugins/source/oracle/resources/services/identity/users.go
+++ plugins/source/oracle/resources/services/identity/users.go
@@ -1,9 +1,10 @@
 package identity
 
 import (
+	"github.com/apache/arrow/go/v13/arrow"
 	"github.com/cloudquery/cloudquery/plugins/source/oracle/client"
-	"github.com/cloudquery/plugin-sdk/v2/schema"
-	"github.com/cloudquery/plugin-sdk/v2/transformers"
+	"github.com/cloudquery/plugin-sdk/v3/schema"
+	"github.com/cloudquery/plugin-sdk/v3/transformers"
 	"github.com/oracle/oci-go-sdk/v65/identity"
 )
 
@@ -16,12 +17,10 @@ func Users() *schema.Table {
 			transformers.WithTypeTransformer(client.OracleTypeTransformer)),
 		Columns: []schema.Column{
 			{
-				Name:     "id",
-				Type:     schema.TypeString,
-				Resolver: schema.PathResolver("Id"),
-				CreationOptions: schema.ColumnCreationOptions{
-					PrimaryKey: true,
-				},
+				Name:       "id",
+				Type:       arrow.BinaryTypes.String,
+				Resolver:   schema.PathResolver("Id"),
+				PrimaryKey: true,
 			},
 		},
 	}
