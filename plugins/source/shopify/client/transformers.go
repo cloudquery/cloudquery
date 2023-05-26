c55c566332c88c7fb85cd7d2a5b6ef0f38184f87
Kemal Hadimli <disq@users.noreply.github.com>
2023-05-26 14:56:37 +0100

    chore(shopify): Update SDK to 3.6.5, remove unnecessary transformer

--- plugins/source/shopify/client/transformers.go
+++ plugins/source/shopify/client/transformers.go
@@ -1,27 +1,10 @@
 package client
 
 import (
-	"reflect"
-
-	"github.com/apache/arrow/go/v13/arrow"
 	"github.com/cloudquery/plugin-sdk/v3/schema"
 	"github.com/cloudquery/plugin-sdk/v3/transformers"
-	"github.com/cloudquery/plugin-sdk/v3/types"
 )
 
-func typeTransformer(field reflect.StructField) (arrow.DataType, error) {
-	switch reflect.New(field.Type).Elem().Interface().(type) {
-	case []any:
-		return types.ExtensionTypes.JSON, nil
-	default:
-		return nil, nil
-	}
-}
-
-var options = []transformers.StructTransformerOption{
-	transformers.WithTypeTransformer(typeTransformer),
-}
-
 func TransformWithStruct(t any, opts ...transformers.StructTransformerOption) schema.Transform {
-	return transformers.TransformWithStruct(t, append(options, opts...)...)
+	return transformers.TransformWithStruct(t, opts...)
 }
