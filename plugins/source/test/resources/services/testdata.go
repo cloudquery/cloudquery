bf03fb8025d6d1f02c2c5ed5f419077019e719eb
Kemal Hadimli <disq@users.noreply.github.com>
2023-05-26 11:07:20 +0100

    feat(test)!: Migrate to Arrow native SDK

--- plugins/source/test/resources/services/testdata.go
+++ plugins/source/test/resources/services/testdata.go
@@ -4,26 +4,31 @@ import (
 	"context"
 	"strings"
 
-	"github.com/cloudquery/plugin-sdk/v2/schema"
-	"github.com/cloudquery/plugin-sdk/v2/testdata"
+	"github.com/cloudquery/plugin-sdk/v3/schema"
 )
 
 func TestDataTable() *schema.Table {
-	table := testdata.TestSourceTable("test_testdata_table")
+	table := schema.TestTable("test_testdata_table", schema.TestSourceOptions{})
 	for i, c := range table.Columns {
 		if strings.HasPrefix(c.Name, "_cq_") {
 			table.Columns[i].Name = "test" + c.Name
 		}
-		table.Columns[i].CreationOptions = schema.ColumnCreationOptions{}
+		table.Columns[i].PrimaryKey = false
+		table.Columns[i].IncrementalKey = false
+		table.Columns[i].NotNull = false
 		table.Columns[i].Resolver = schema.PathResolver(table.Columns[i].Name)
 	}
 
-	data := testdata.GenTestDataV1(table)
+	data := schema.GenTestData(table, schema.GenTestDataOptions{
+		MaxRows: 1,
+	})
+	if len(data) != 1 {
+		panic("Expected 1 row of data")
+	}
+
 	dataAsMap := make(map[string]any)
-	for i, c := range table.Columns {
-		if data[i].GetStatus() == schema.Present {
-			dataAsMap[c.Name] = data[i].String()
-		}
+	for i, col := range data[0].Columns() {
+		dataAsMap[data[0].ColumnName(i)] = col.ValueStr(0)
 	}
 
 	table.Description = "Testdata table"
