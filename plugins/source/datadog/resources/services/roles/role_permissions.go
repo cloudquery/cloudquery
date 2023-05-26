74370e3480404e6adc66029006571c17bca914f2
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2023-05-26 13:57:01 +0300

    fix left overs sdk v2

--- plugins/source/datadog/resources/services/roles/role_permissions.go
+++ plugins/source/datadog/resources/services/roles/role_permissions.go
@@ -6,7 +6,7 @@ import (
 	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
 	"github.com/apache/arrow/go/v13/arrow"
 	"github.com/cloudquery/cloudquery/plugins/source/datadog/client"
-	"github.com/cloudquery/plugin-sdk/v2/types"
+	"github.com/cloudquery/plugin-sdk/v3/types"
 	"github.com/cloudquery/plugin-sdk/v3/schema"
 	"github.com/cloudquery/plugin-sdk/v3/transformers"
 )
