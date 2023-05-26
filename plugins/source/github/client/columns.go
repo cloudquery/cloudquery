d790ec7c140267998b030c3374a99436471938f8
Kemal Hadimli <disq@users.noreply.github.com>
2023-05-26 13:37:03 +0100

    feat(github)!: Migrate to Arrow native SDK

--- plugins/source/github/client/columns.go
+++ plugins/source/github/client/columns.go
@@ -1,23 +1,22 @@
 package client
 
 import (
-	"github.com/cloudquery/plugin-sdk/v2/schema"
+	"github.com/apache/arrow/go/v13/arrow"
+	"github.com/cloudquery/plugin-sdk/v3/schema"
 )
 
 var (
 	OrgColumn = schema.Column{
-		Name:            "org",
-		Type:            schema.TypeString,
-		Resolver:        ResolveOrg,
-		Description:     `The Github Organization of the resource.`,
-		CreationOptions: schema.ColumnCreationOptions{PrimaryKey: true},
+		Name:        "org",
+		Type:        arrow.BinaryTypes.String,
+		Resolver:    ResolveOrg,
+		Description: `The Github Organization of the resource.`,
+		PrimaryKey:  true,
 	}
 	RepositoryIDColumn = schema.Column{
-		Name:     "repository_id",
-		Type:     schema.TypeInt,
-		Resolver: ResolveRepositoryID,
-		CreationOptions: schema.ColumnCreationOptions{
-			PrimaryKey: true,
-		},
+		Name:       "repository_id",
+		Type:       arrow.PrimitiveTypes.Int64,
+		Resolver:   ResolveRepositoryID,
+		PrimaryKey: true,
 	}
 )
--- plugins/source/github/resources/services/teams/members.go
+++ plugins/source/github/resources/services/teams/members.go
@@ -6,8 +6,9 @@ import (
 	"strings"
 
 	"github.com/cloudquery/cloudquery/plugins/source/github/client"
-	"github.com/cloudquery/plugin-sdk/v2/schema"
-	"github.com/cloudquery/plugin-sdk/v2/transformers"
+	"github.com/cloudquery/plugin-sdk/v2/types"
+	"github.com/cloudquery/plugin-sdk/v3/schema"
+	"github.com/cloudquery/plugin-sdk/v3/transformers"
 	"github.com/google/go-github/v49/github"
 )
 
@@ -21,7 +22,7 @@ func members() *schema.Table {
 			teamIDColumn,
 			{
 				Name:     "membership",
-				Type:     schema.TypeJSON,
+				Type:     types.ExtensionTypes.JSON,
 				Resolver: resolveMembership,
 			},
 		},
