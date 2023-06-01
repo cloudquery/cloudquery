9874f77735bd692e154b1cd7c68c884fac01604a
FUJITA Yuki <yuki.fujita@bizreach.co.jp>
2023-06-01 18:37:22 +0900

    fix check_id

--- plugins/source/azure/policies/cis_v1.3.0/section_1.sql
+++ plugins/source/azure/policies/cis_v1.3.0/section_1.sql
@@ -1,7 +1,6 @@
 \echo "Executing CIS V1.3.0 Section 1"
+\set check_id "1.21"
+\echo "Executing check 1.21"
+\ir ../queries/iam/custom_subscription_owner_roles.sql
 \set check_id "1.22"
 \echo "Executing check 1.22"
-
-\set check_id "1.23"
-\echo "Executing check 1.23"
-\ir ../queries/iam/custom_subscription_owner_roles.sql
