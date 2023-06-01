ba6a8597052b8a257a917bdd24c20bd2d12cd3c8
FUJITA Yuki <yuki.fujita@bizreach.co.jp>
2023-06-01 17:59:42 +0900

    implement CIS Azure 1.23

--- plugins/source/azure/policies/cis_v1.3.0/section_1.sql
+++ plugins/source/azure/policies/cis_v1.3.0/section_1.sql
@@ -1,1 +1,7 @@
-\echo "Executing CIS V1.3.0 Section 1 (Manual)"
+\echo "Executing CIS V1.3.0 Section 1"
+\set check_id "1.22"
+\echo "Executing check 1.22"
+
+\set check_id "1.23"
+\echo "Executing check 1.23"
+\ir ../queries/iam/custom_subscription_owner_roles.sql
