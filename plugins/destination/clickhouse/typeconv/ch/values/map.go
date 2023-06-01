0a1764ff0cceb650d914ad5cb0a34e66d33baf3d
Alex Shcherbakov <candiduslynx@users.noreply.github.com>
2023-06-01 16:47:18 +0300

    fix(deps): Update `github.com/cloudquery/plugin-sdk/v3` to `v3.8.1` (#11078)
    Extracted from #11043
    Depends on:
    * https://github.com/cloudquery/plugin-sdk/pull/913
    * https://github.com/apache/arrow/pull/35823
    * https://github.com/cloudquery/plugin-sdk/pull/921

--- plugins/destination/clickhouse/typeconv/ch/values/map.go
+++ plugins/destination/clickhouse/typeconv/ch/values/map.go
@@ -32,23 +32,24 @@ func mapValue(arr *array.Map) (any, error) {
 		return nil, fmt.Errorf("unexpected reflect type for map: %q", valueType.String())
 	}
 
-	return makeMapSlice(valueType, arr)
+	sanitized, err := sanitizeNested(arr)
+	if err != nil {
+		return nil, err
+	}
+
+	return makeMapSlice(valueType, sanitized.(*array.Map))
 }
 
 func makeMapSlice(mapType reflect.Type, arr *array.Map) (any, error) {
 	res := reflect.MakeSlice(reflect.SliceOf(reflect.PointerTo(mapType)), arr.Len(), arr.Len()) // we do []*(type) for nullable assignment
 	for i := 0; i < arr.Len(); i++ {
-		val := reflect.New(mapType)
-		if arr.IsNull(i) {
-			// we need to fill in for the in-depth recursive parsing by ClickHouse SDK
-			res.Index(i).Set(val)
-			continue
-		}
 		start, end := arr.ValueOffsets(i)
 		mapVal, err := makeMap(mapType, array.NewSlice(arr.ListValues(), start, end))
 		if err != nil {
 			return nil, err
 		}
+
+		val := reflect.New(mapType)
 		val.Elem().Set(*mapVal)
 		res.Index(i).Set(val)
 	}
