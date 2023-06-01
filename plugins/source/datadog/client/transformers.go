a3b24e5afbbbc88b0547eea5bd2171a2bdd7c5b0
Kemal Hadimli <disq@users.noreply.github.com>
2023-06-01 12:57:57 +0100

    fix nullable resolvers

--- plugins/source/datadog/client/transformers.go
+++ plugins/source/datadog/client/transformers.go
@@ -1,12 +1,14 @@
 package client
 
 import (
+	"context"
 	"reflect"
 
 	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
 	"github.com/apache/arrow/go/v13/arrow"
 	"github.com/cloudquery/plugin-sdk/v3/schema"
 	"github.com/cloudquery/plugin-sdk/v3/transformers"
+	"github.com/thoas/go-funk"
 )
 
 func typeTransformer(field reflect.StructField) (arrow.DataType, error) {
@@ -28,8 +30,45 @@ func typeTransformer(field reflect.StructField) (arrow.DataType, error) {
 	}
 }
 
+type nullable interface {
+	IsSet() bool
+}
+
+func resolverTransformer(field reflect.StructField, path string) schema.ColumnResolver {
+	switch reflect.New(field.Type).Elem().Interface().(type) {
+	case datadog.NullableInt, datadog.NullableInt32, datadog.NullableInt64,
+		datadog.NullableBool,
+		datadog.NullableFloat32,
+		datadog.NullableFloat64,
+		datadog.NullableString,
+		datadog.NullableTime:
+
+		return func(_ context.Context, meta schema.ClientMeta, r *schema.Resource, c schema.Column) error {
+			val := funk.Get(r.Item, path, funk.WithAllowZero())
+			if !val.(nullable).IsSet() {
+				return r.Set(c.Name, nil)
+			}
+			getFunc, ok := reflect.TypeOf(val).MethodByName("Get")
+			if !ok {
+				panic("no Get() on nullable type " + field.Type.String())
+			}
+
+			ret := getFunc.Func.Call([]reflect.Value{reflect.ValueOf(val)})
+			if len(ret) != 1 {
+				panic("expected 1 return value from Get()")
+			}
+
+			return r.Set(c.Name, ret[0].Interface())
+		}
+
+	default:
+		return nil
+	}
+}
+
 var options = []transformers.StructTransformerOption{
 	transformers.WithTypeTransformer(typeTransformer),
+	transformers.WithResolverTransformer(resolverTransformer),
 }
 
 func TransformWithStruct(t any, opts ...transformers.StructTransformerOption) schema.Transform {
--- plugins/source/datadog/resources/services/monitors/monitors_mock_test.go
+++ plugins/source/datadog/resources/services/monitors/monitors_mock_test.go
@@ -38,6 +38,7 @@ func buildMonitorsMock(t *testing.T, ctrl *gomock.Controller) client.DatadogServ
 	}
 	i64val := int64(123)
 	i32val := int32(123)
+	textVal := "test string"
 	dt[0].ActiveChild.Set(datadogV1.NewDowntimeChild())
 	dt[0].Canceled.Set(&i64val)
 	dt[0].End.Set(&i64val)
@@ -45,6 +46,7 @@ func buildMonitorsMock(t *testing.T, ctrl *gomock.Controller) client.DatadogServ
 	dt[0].ParentId.Set(&i64val)
 	dt[0].Recurrence.Set(datadogV1.NewDowntimeRecurrence())
 	dt[0].UpdaterId.Set(&i32val)
+	dt[0].Message.Set(&textVal)
 
 	d.EXPECT().ListMonitorDowntimes(gomock.Any(), gomock.Any()).Return(dt, nil, nil)
 
