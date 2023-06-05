b95fd8b8e210320e7761ecfb205fc4c26b09a119
bbernays <ben@bernays.me>
2023-06-05 14:34:30 -0700

    Update image_last_launched.go

--- plugins/source/aws/resources/services/ec2/image_last_launched.go
+++ plugins/source/aws/resources/services/ec2/image_last_launched.go
@@ -13,9 +13,10 @@ import (
 
 func imageAttributesLastLaunchTime() *schema.Table {
 	return &schema.Table{
-		Name:        "aws_ec2_image_last_launched_times",
-		Description: `https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_LaunchPermission.html`,
-		Resolver:    fetchEc2ImageAttributeLastLaunchTime,
+		Name: "aws_ec2_image_last_launched_times",
+		Description: `https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_DescribeImageAttribute.html. 
+The date and time, in ISO 8601 date-time format, when the AMI was last used to launch an EC2 instance. When the AMI is used to launch an instance, there is a 24-hour delay before that usage is reported.`,
+		Resolver: fetchEc2ImageAttributeLastLaunchTime,
 		Columns: []schema.Column{
 			{
 				Name:       "image_arn",
