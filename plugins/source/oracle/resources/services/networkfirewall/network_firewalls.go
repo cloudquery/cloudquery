c6745c71e3e4f43cec9b540a6ac8a1265fa8dd33
candiduslynx <candiduslynx@gmail.com>
2023-06-08 13:12:56 +0300

    rest

--- plugins/source/oracle/resources/services/networkfirewall/network_firewalls.go
+++ plugins/source/oracle/resources/services/networkfirewall/network_firewalls.go
@@ -1,8 +1,11 @@
 package networkfirewall
 
 import (
+	"context"
+
 	"github.com/cloudquery/cloudquery/plugins/source/oracle/client"
 	"github.com/cloudquery/plugin-sdk/v3/schema"
+	"github.com/oracle/oci-go-sdk/v65/common"
 	"github.com/oracle/oci-go-sdk/v65/networkfirewall"
 )
 
@@ -14,4 +17,32 @@ func NetworkFirewalls() *schema.Table {
 		Transform: client.TransformWithStruct(&networkfirewall.NetworkFirewallSummary{}),
 		Columns:   schema.ColumnList{client.RegionColumn, client.CompartmentIDColumn},
 	}
+}
+
+func fetchNetworkFirewalls(ctx context.Context, meta schema.ClientMeta, _ *schema.Resource, res chan<- any) error {
+	cqClient := meta.(*client.Client)
+
+	var page *string
+	for {
+		request := networkfirewall.ListNetworkFirewallsRequest{
+			CompartmentId: common.String(cqClient.CompartmentOcid),
+			Page:          page,
+		}
+
+		response, err := cqClient.OracleClients[cqClient.Region].NetworkfirewallNetworkfirewallClient.ListNetworkFirewalls(ctx, request)
+
+		if err != nil {
+			return err
+		}
+
+		res <- response.Items
+
+		if response.OpcNextPage == nil {
+			break
+		}
+
+		page = response.OpcNextPage
+	}
+
+	return nil
 }
