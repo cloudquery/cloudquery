c4ea217ba52524dbc4999e3079dbf20e6e5d3ca5
candiduslynx <candiduslynx@gmail.com>
2023-06-08 12:56:05 +0300

    filestorage

--- plugins/source/oracle/resources/services/filestorage/export_sets.go
+++ plugins/source/oracle/resources/services/filestorage/export_sets.go
@@ -1,8 +1,11 @@
 package filestorage
 
 import (
+	"context"
+
 	"github.com/cloudquery/cloudquery/plugins/source/oracle/client"
 	"github.com/cloudquery/plugin-sdk/v3/schema"
+	"github.com/oracle/oci-go-sdk/v65/common"
 	"github.com/oracle/oci-go-sdk/v65/filestorage"
 )
 
@@ -14,4 +17,33 @@ func ExportSets() *schema.Table {
 		Transform: client.TransformWithStruct(&filestorage.ExportSetSummary{}),
 		Columns:   schema.ColumnList{client.RegionColumn, client.CompartmentIDColumn, client.AvailabilityDomainColumn},
 	}
+}
+
+func fetchExportSets(ctx context.Context, meta schema.ClientMeta, _ *schema.Resource, res chan<- any) error {
+	cqClient := meta.(*client.Client)
+
+	var page *string
+	for {
+		request := filestorage.ListExportSetsRequest{
+			CompartmentId:      common.String(cqClient.CompartmentOcid),
+			AvailabilityDomain: common.String(cqClient.AvailabilityDomain),
+			Page:               page,
+		}
+
+		response, err := cqClient.OracleClients[cqClient.Region].FilestorageFilestorageClient.ListExportSets(ctx, request)
+
+		if err != nil {
+			return err
+		}
+
+		res <- response.Items
+
+		if response.OpcNextPage == nil {
+			break
+		}
+
+		page = response.OpcNextPage
+	}
+
+	return nil
 }
