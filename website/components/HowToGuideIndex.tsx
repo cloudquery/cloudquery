63b0f5b7feeee128c6bdafcc6d9cad63cfa3dcff
Herman Schaaf <hermanschaaf@gmail.com>
2023-06-12 13:24:46 +0100

    Add deployment docs to how-to guides

--- website/components/HowToGuideIndex.tsx
+++ website/components/HowToGuideIndex.tsx
@@ -1,4 +1,4 @@
-import { getPagesUnderRoute } from "nextra/context";
+import {getPagesUnderRoute} from "nextra/context";
 import Link from "next/link";
 
 interface Page {
@@ -10,8 +10,20 @@ interface Page {
 }
 
 export function HowToGuideIndex({ more = "Read the guide" }) {
-  return getPagesUnderRoute("/how-to-guides")
+  const howTo = getPagesUnderRoute("/how-to-guides");
+  const deploy = getPagesUnderRoute("/docs/deployment");
+  // combine howTo and deploy
+  const allGuides = [...howTo, ...deploy];
+  return allGuides
     .slice()
+    .sort((a, b) => {
+        const aTitle = a.frontMatter?.title || a.meta?.title || a.name;
+        const bTitle = b.frontMatter?.title || b.meta?.title || b.name;
+        return aTitle.localeCompare(bTitle);
+    })
+    .filter((page) => {
+        return page.name !== "overview";
+    })
     .map((page) => {
       return (
         <div key={page.route} className="mb-10">
@@ -20,7 +32,7 @@ export function HowToGuideIndex({ more = "Read the guide" }) {
             style={{ color: "inherit", textDecoration: "none" }}
             className="block font-semibold mt-8 nx-text-2xl"
             >
-            {page.meta?.title || page.meta?.title || page.name}
+            {page.frontMatter?.title || page.meta?.title || page.name}
           </Link>
           <p className="opacity-80" style={{ marginTop: ".5rem" }}>
             {page.meta?.description}{" "}
