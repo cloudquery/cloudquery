f46a9b6eb307277be6e3c7de88076e8f9b569d5c
Herman Schaaf <hermanschaaf@gmail.com>
2023-06-12 15:28:48 +0100

    Add og:title and og:video meta tags

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -2,6 +2,8 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 import { Footer } from "./components/Footer";
 import { EditLink } from "./components/EditLink";
 import CloudQueryLogo from "./components/logos/CloudQuery";
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 
 const theme: DocsThemeConfig = {
   project: {
@@ -39,8 +41,9 @@ const theme: DocsThemeConfig = {
       </>
     );
   },
-  head: function Head() {
-    return (
+  head: () => {
+      const { frontMatter } = useConfig()
+      return (
       <>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
@@ -68,6 +71,20 @@ const theme: DocsThemeConfig = {
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="en_IE" />
         <meta property="og:site_name" content="CloudQuery" />
+        {(frontMatter.title) ?
+            <meta property="og:title" content={frontMatter.title} />
+            : null
+        }
+        {(frontMatter.video) ?
+          <>
+              <meta property="og:video:url" content={frontMatter.video} />
+              <meta property="og:video:secure_url" content={frontMatter.video} />
+              <meta property="og:video:type" content="application/x-shockwave-flash"/>
+              <meta property="og:video:width" content="1280" />
+              <meta property="og:video:height" content="720" />
+          </>
+          : null
+        }
       </>
     );
   },
