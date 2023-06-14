ea858fc26f13c564bcae3a409d36c2c6ebb8f009
bbernays <ben@bernays.me>
2023-06-14 12:41:07 -0500

    feat(aws): Table Options Validation (#11548)
    #### Summary
    
    This pr automatically adds the validation by iterating through all struct members and calling validate. As we scale out table options, it is one fewer thing we will have to generate/ remember to add

--- plugins/source/aws/client/tableoptions/table_options.go
+++ plugins/source/aws/client/tableoptions/table_options.go
@@ -1,40 +1,29 @@
 package tableoptions
 
-type TableOptions struct {
-	CloudwatchMetrics []CloudwatchMetrics `json:"aws_alpha_cloudwatch_metrics,omitempty"`
+import "reflect"
 
+type customInputValidation interface {
+	Validate() error
+}
+
+type TableOptions struct {
+	CloudwatchMetrics      CloudwatchMetrics       `json:"aws_alpha_cloudwatch_metrics,omitempty"`
 	CloudTrailEvents       *CloudtrailAPIs         `json:"aws_cloudtrail_events,omitempty"`
 	AccessAnalyzerFindings *AccessanalyzerFindings `json:"aws_accessanalyzer_analyzer_findings,omitempty"`
 	Inspector2Findings     *Inspector2APIs         `json:"aws_inspector2_findings,omitempty"`
 }
 
 func (t TableOptions) Validate() error {
-	if t.CloudTrailEvents != nil {
-		err := t.CloudTrailEvents.Validate()
-		if err != nil {
-			return err
+	v := reflect.ValueOf(t)
+	for i := 0; i < v.NumField(); i++ {
+		table := v.Field(i).Interface()
+		if !reflect.ValueOf(table).IsNil() {
+			tableInput := table.(customInputValidation)
+			err := tableInput.Validate()
+			if err != nil {
+				return err
+			}
 		}
 	}
-
-	if t.AccessAnalyzerFindings != nil {
-		err := t.AccessAnalyzerFindings.Validate()
-		if err != nil {
-			return err
-		}
-	}
-
-	if t.Inspector2Findings != nil {
-		err := t.Inspector2Findings.Validate()
-		if err != nil {
-			return err
-		}
-	}
-
-	for _, m := range t.CloudwatchMetrics {
-		if err := m.Validate(); err != nil {
-			return err
-		}
-	}
-
 	return nil
 }
