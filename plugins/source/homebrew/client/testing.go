ece5ba5a7e93e0a0000054044ebbefd8f61a75c4
Herman Schaaf <hermanschaaf@gmail.com>
2023-06-19 14:16:39 +0100

    Migrate

---
+++ plugins/source/homebrew/client/testing.go
@@ -0,0 +1,46 @@
+package client
+
+import (
+	"context"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/cloudquery/cloudquery/plugins/source/homebrew/internal/homebrew"
+	"github.com/cloudquery/plugin-sdk/v4/scheduler"
+	"github.com/cloudquery/plugin-sdk/v4/schema"
+	"github.com/cloudquery/plugin-sdk/v4/transformers"
+	"github.com/rs/zerolog"
+)
+
+type TestOptions struct{}
+
+func MockTestHelper(t *testing.T, table *schema.Table, builder func(*testing.T) *homebrew.Client, opts TestOptions) {
+	table.IgnoreInTests = false
+	t.Helper()
+	l := zerolog.New(zerolog.NewTestWriter(t)).Output(
+		zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.StampMicro},
+	).Level(zerolog.DebugLevel).With().Timestamp().Logger()
+	schedulerClient := &Client{
+		Homebrew:   builder(t),
+		Logger:     l,
+		Spec:       nil,
+		MaxRetries: defaultMaxRetries,
+		Backoff:    defaultBackoff,
+	}
+	tables := schema.Tables{table}
+	if err := transformers.TransformTables(tables); err != nil {
+		t.Fatal(err)
+	}
+	sc := scheduler.NewScheduler(schedulerClient, scheduler.WithLogger(l))
+	messages, err := sc.SyncAll(context.Background(), tables)
+	if err != nil {
+		t.Fatal(err)
+	}
+	inserts := messages.GetInserts()
+	records := inserts.GetRecordsForTable(table)
+	emptyColumns := schema.FindEmptyColumns(table, records)
+	if len(emptyColumns) > 0 {
+		t.Fatalf("empty columns: %v", emptyColumns)
+	}
+}
--- plugins/source/homebrew/resources/plugin/client.go
+++ plugins/source/homebrew/resources/plugin/client.go
@@ -21,6 +21,18 @@ const (
 	defaultBackoff    = 10 * time.Second
 )
 
+var tables = []*schema.Table{
+	analytics.Installs(homebrew.Days30),
+	analytics.Installs(homebrew.Days90),
+	analytics.Installs(homebrew.Days365),
+	analytics.CaskInstalls(homebrew.Days30),
+	analytics.CaskInstalls(homebrew.Days90),
+	analytics.CaskInstalls(homebrew.Days365),
+	analytics.BuildErrors(homebrew.Days30),
+	analytics.BuildErrors(homebrew.Days90),
+	analytics.BuildErrors(homebrew.Days365),
+}
+
 type Client struct {
 	SchedulerClient *client.Client
 	logger          zerolog.Logger
@@ -37,13 +49,12 @@ func (c *Client) Logger() *zerolog.Logger {
 }
 
 func (c *Client) Sync(ctx context.Context, options plugin.SyncOptions, res chan<- message.Message) error {
-	tables, _ := tables(ctx)
-	scheduler := scheduler.NewScheduler(tables, c.SchedulerClient, scheduler.WithSchedulerStrategy(scheduler.StrategyDFS))
-	return scheduler.Sync(ctx, res)
+	scheduler := scheduler.NewScheduler(c.SchedulerClient, scheduler.WithSchedulerStrategy(scheduler.StrategyDFS))
+	return scheduler.Sync(ctx, tables, res)
 }
 
 func (c *Client) Tables(ctx context.Context) (schema.Tables, error) {
-	return tables(ctx)
+	return tables, nil
 }
 
 func (c *Client) Close(ctx context.Context) error {
@@ -51,7 +62,6 @@ func (c *Client) Close(ctx context.Context) error {
 }
 
 func Configure(ctx context.Context, logger zerolog.Logger, spec []byte) (plugin.Client, error) {
-
 	config := &client.Spec{}
 	if err := json.Unmarshal(spec, config); err != nil {
 		return nil, fmt.Errorf("failed to unmarshal spec: %w", err)
@@ -62,13 +72,13 @@ func Configure(ctx context.Context, logger zerolog.Logger, spec []byte) (plugin.
 		return nil, fmt.Errorf("failed to validate spec: %w", err)
 	}
 
-	homberClient := homebrew.NewClient()
+	homebrewClient := homebrew.NewClient()
 	if err != nil {
 		return nil, fmt.Errorf("failed to create homebrew client: %w", err)
 	}
 
 	schedulerClient := &client.Client{
-		Homebrew:   homberClient,
+		Homebrew:   homebrewClient,
 		Logger:     logger,
 		Spec:       config,
 		MaxRetries: defaultMaxRetries,
@@ -78,19 +88,5 @@ func Configure(ctx context.Context, logger zerolog.Logger, spec []byte) (plugin.
 	return &Client{
 		logger:          logger,
 		SchedulerClient: schedulerClient,
-	}, nil
-}
-
-func tables(ctx context.Context) (schema.Tables, error) {
-	return []*schema.Table{
-		analytics.Installs(homebrew.Days30),
-		analytics.Installs(homebrew.Days90),
-		analytics.Installs(homebrew.Days365),
-		analytics.CaskInstalls(homebrew.Days30),
-		analytics.CaskInstalls(homebrew.Days90),
-		analytics.CaskInstalls(homebrew.Days365),
-		analytics.BuildErrors(homebrew.Days30),
-		analytics.BuildErrors(homebrew.Days90),
-		analytics.BuildErrors(homebrew.Days365),
 	}, nil
 }
