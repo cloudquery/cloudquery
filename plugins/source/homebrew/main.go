5ae00635dae9598e6eb2beae9064944b750c914c
Herman Schaaf <hermanschaaf@gmail.com>
2023-06-19 14:47:51 +0100

    Call Serve()

--- plugins/source/homebrew/main.go
+++ plugins/source/homebrew/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+
 	"github.com/cloudquery/cloudquery/plugins/source/homebrew/resources/plugin"
 	"github.com/cloudquery/plugin-sdk/v4/serve"
 )
@@ -8,5 +10,6 @@ import (
 const sentryDSN = "https://e944e0a50f1c43eaa6ffd89e7b39f4da@o1396617.ingest.sentry.io/4504734217142272"
 
 func main() {
-	serve.Plugin(plugin.Plugin(), serve.WithPluginSentryDSN(sentryDSN))
+	p := serve.Plugin(plugin.Plugin(), serve.WithPluginSentryDSN(sentryDSN))
+	p.Serve(context.Background())
 }
