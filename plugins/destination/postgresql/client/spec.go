24df3682281359df61f693068984dda0d4e2d342
Herman Schaaf <hermanschaaf@gmail.com>
2023-06-20 15:51:39 +0100

    Add migrate mode to spec

--- plugins/destination/postgresql/client/spec.go
+++ plugins/destination/postgresql/client/spec.go
@@ -1,9 +1,15 @@
 package client
 
+import "github.com/cloudquery/plugin-pb-go/specs/v0"
+
 type Spec struct {
 	ConnectionString string   `json:"connection_string,omitempty"`
 	PgxLogLevel      LogLevel `json:"pgx_log_level,omitempty"`
+	MigrateMode      string   `json:"migrate_mode,omitempty"`
 }
 
-func (*Spec) SetDefaults() {
+func (s *Spec) SetDefaults() {
+	if s.MigrateMode == "" {
+		s.MigrateMode = specs.MigrateModeSafe.String()
+	}
 }
