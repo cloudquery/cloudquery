4708619cb750755e9c437d3f8237641d3f04e2dc
erezrokah <erezrokah@users.noreply.github.com>
2023-06-26 16:18:36 +0200

    fix: Add missing serve

--- plugins/source/firestore/main.go
+++ plugins/source/firestore/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"log"
+
 	"github.com/cloudquery/cloudquery/plugins/source/firestore/resources/plugin"
 	"github.com/cloudquery/plugin-sdk/v4/serve"
 )
@@ -8,5 +11,8 @@ import (
 const sentryDSN = "https://0f23371080a341f1984c9f6d7e928e4a@o1396617.ingest.sentry.io/4505007730917376"
 
 func main() {
-	serve.Plugin(plugin.Plugin(), serve.WithPluginSentryDSN(sentryDSN))
+	pluginServe := serve.Plugin(plugin.Plugin(), serve.WithPluginSentryDSN(sentryDSN))
+	if err := pluginServe.Serve(context.Background()); err != nil {
+		log.Fatalf("failed to serve plugin: %v", err)
+	}
 }
