64953cddb17b44d375e7f451362e8f22fde18e67
erezrokah <erezrokah@users.noreply.github.com>
2023-06-26 16:09:07 +0200

    fix: Add missing serve

--- plugins/source/mysql/main.go
+++ plugins/source/mysql/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"log"
+
 	"github.com/cloudquery/cloudquery/plugins/source/mysql/resources/plugin"
 	"github.com/cloudquery/plugin-sdk/v4/serve"
 )
@@ -8,5 +11,8 @@ import (
 const sentryDSN = "https://128084695e12456cb6b9be9b32f097da@o1396617.ingest.sentry.io/4504830878416896"
 
 func main() {
-	serve.Plugin(plugin.Plugin(), serve.WithPluginSentryDSN(sentryDSN))
+	pluginServe := serve.Plugin(plugin.Plugin(), serve.WithPluginSentryDSN(sentryDSN))
+	if err := pluginServe.Serve(context.Background()); err != nil {
+		log.Fatalf("failed to serve plugin: %v", err)
+	}
 }
