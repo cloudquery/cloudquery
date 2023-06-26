51c57e5df73fdac1496f4be4ace532ca06f559dd
Jason Kao <100613312+jsonpr@users.noreply.github.com>
2023-06-26 17:18:44 -0400

    Add in check for SQS SSE

--- plugins/source/aws/policies/queries/sqs/sqs_queues_should_be_encrypted_at_rest_using_aws_kms.sql
+++ plugins/source/aws/policies/queries/sqs/sqs_queues_should_be_encrypted_at_rest_using_aws_kms.sql
@@ -7,6 +7,6 @@ select
     account_id,
     arn as resource_id,
     case when
-        kms_master_key_id is null or kms_master_key_id = ''
+        (kms_master_key_id is null or kms_master_key_id = '') and sqs_managed_sse_enabled = false
     then 'fail' else 'pass' end as status
 from aws_sqs_queues
