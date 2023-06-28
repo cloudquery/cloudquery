0e0a78cf6ef521c54941536faa958dda72550830
Kemal Hadimli <disq@users.noreply.github.com>
2023-06-28 20:02:39 +0100

    undo gremlin

--- plugins/destination/gremlin/client/deletestale.go
+++ plugins/destination/gremlin/client/deletestale.go
@@ -2,30 +2,29 @@ package client
 
 import (
 	"context"
+	"time"
 
 	gremlingo "github.com/apache/tinkerpop/gremlin-go/v3/driver"
-	"github.com/cloudquery/plugin-sdk/v4/message"
-	"github.com/cloudquery/plugin-sdk/v4/schema"
+	"github.com/cloudquery/plugin-sdk/v3/schema"
 )
 
-func (c *Client) DeleteStale(_ context.Context, msgs []*message.WriteDeleteStale) error {
+func (c *Client) DeleteStale(ctx context.Context, tables schema.Tables, source string, syncTime time.Time) error {
 	session, closer, err := c.newSession()
 	if err != nil {
 		return err
 	}
 	defer closer()
 
-	for _, msg := range msgs {
+	for _, table := range tables {
 		g := gremlingo.Traversal_().WithRemote(session).
 			V().
-			HasLabel(msg.GetTable().Name).
-			Has(schema.CqSourceNameColumn.Name, msg.SourceName).
-			Has(schema.CqSyncTimeColumn.Name, gremlingo.P.Lt(msg.SyncTime)).
+			HasLabel(table.Name).
+			Has(schema.CqSourceNameColumn.Name, source).
+			Has(schema.CqSyncTimeColumn.Name, gremlingo.P.Lt(syncTime)).
 			SideEffect(AnonT.Drop())
 		if err := <-g.Iterate(); err != nil {
 			return err
 		}
 	}
-
 	return nil
 }
