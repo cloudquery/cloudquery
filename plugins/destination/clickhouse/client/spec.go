24b893647a6c67f6de6389639f04e29797212d18
candiduslynx <candiduslynx@gmail.com>
2023-06-29 23:26:20 +0300

    5 MiB instead of 50

--- plugins/destination/clickhouse/client/spec.go
+++ plugins/destination/clickhouse/client/spec.go
@@ -59,7 +59,7 @@ func (s *Spec) SetDefaults() {
 	}
 
 	if s.BatchSizeBytes == 0 {
-		const defaultBatchSizeBytes = 50 << 20 // 50 MiB
+		const defaultBatchSizeBytes = 5 << 20 // 50 MiB
 		s.BatchSizeBytes = defaultBatchSizeBytes
 	}
 
