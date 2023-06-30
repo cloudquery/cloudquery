0a95ffd2eac0fbfdf89e07d5facb3a8f70fa77dd
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2023-06-30 16:51:12 +0300

    add not implemented

--- plugins/destination/bigquery/client/deletestale.go
+++ plugins/destination/bigquery/client/deletestale.go
@@ -2,12 +2,12 @@ package client
 
 import (
 	"context"
-	"errors"
 
 	"github.com/cloudquery/plugin-sdk/v4/message"
+	"github.com/cloudquery/plugin-sdk/v4/plugin"
 )
 
 // DeleteStale is not currently implemented for BigQuery, as it only supports "append" write mode.
 func (*Client) DeleteStale(context.Context, message.WriteDeleteStales) error {
-	return errors.New("DeleteStale not implemented")
+	return plugin.ErrNotImplemented
 }
