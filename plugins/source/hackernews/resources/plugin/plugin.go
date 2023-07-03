3c686be3de603cf0a29393b755275c275db44a2c
Herman Schaaf <hermanschaaf@gmail.com>
2023-07-03 18:23:26 +0100

    feat!: Upgrade Hacker News plugin to SDK v4 (#11855)
    Upgrades Hacker News plugin to SDK v4, which allows any destination plugin to be used as a backend. 
    
    This will only work with recent CLI versions and destinations that are also upgraded to V4. 
    
    The backend should now be configured like this:
    
    ```
    kind: source
    spec:
      name: "hackernews"
      version: "vX.Y.Z"
      path: "cloudquery/hackernews"
      tables: 
        ["*"]
      destinations: 
        - "postgresql"
      backend_options:
        table_name: "cq_state_hackernews"
        connection: "@@plugins.postgresql.connection"
      spec:
        item_concurrency: 100
        start_time: "2023-07-01T00:00:00Z"
    ---
    kind: destination
    spec:
      name: "postgresql"
      version: "vX.Y.Z"
      registry: "github"
      path: "cloudquery/postgresql"
      write_mode: "overwrite-delete-stale"
      spec:
        connection_string: "postgresql://postgres:pass@localhost:5432/cloudquery?sslmode=disable"
    ```

--- plugins/source/hackernews/resources/plugin/plugin.go
+++ plugins/source/hackernews/resources/plugin/plugin.go
@@ -1,24 +1,17 @@
 package plugin
 
 import (
-	"github.com/cloudquery/cloudquery/plugins/source/hackernews/client"
-	"github.com/cloudquery/cloudquery/plugins/source/hackernews/resources/services/items"
-	"github.com/cloudquery/plugin-sdk/v3/plugins/source"
-	"github.com/cloudquery/plugin-sdk/v3/schema"
+	"github.com/cloudquery/plugin-sdk/v4/plugin"
 )
 
 var (
 	Version = "development"
 )
 
-func Plugin() *source.Plugin {
-	tables := []*schema.Table{
-		items.Items(),
-	}
-	return source.NewPlugin(
+func Plugin() *plugin.Plugin {
+	return plugin.NewPlugin(
 		"hackernews",
 		Version,
-		tables,
-		client.Configure,
+		Configure,
 	)
 }
