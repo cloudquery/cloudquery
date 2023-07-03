e24e4bb77289d13f412a56b5ff456549ecd48ee8
Kemal <223029+disq@users.noreply.github.com>
2023-07-03 16:49:22 +0100

    feat(file): Upgrade to SDK v4 (#11659)
    Seems to work, Parquet test had to be done in a different way due to flushing and footer.

--- plugins/destination/file/client/read.go
+++ plugins/destination/file/client/read.go
@@ -8,23 +8,23 @@ import (
 	"time"
 
 	"github.com/apache/arrow/go/v13/arrow"
-	"github.com/cloudquery/plugin-sdk/v3/schema"
+	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/google/uuid"
 )
 
-func (c *Client) Read(ctx context.Context, table *schema.Table, sourceName string, res chan<- arrow.Record) error {
-	if !c.pluginSpec.NoRotate {
-		return fmt.Errorf("reading is not supported when `no_rotate` is false. Table: %q; Source: %q", table.Name, sourceName)
+func (c *Client) Read(_ context.Context, table *schema.Table, res chan<- arrow.Record) error {
+	if !c.spec.NoRotate {
+		return fmt.Errorf("reading is not supported when `no_rotate` is false. Table: %q", table.Name)
 	}
-	if strings.Contains(c.pluginSpec.Path, PathVarUUID) {
-		return fmt.Errorf("reading is not supported when `path` contains UUID variable. Table: %q; Source: %q", table.Name, sourceName)
+	if strings.Contains(c.spec.Path, PathVarUUID) {
+		return fmt.Errorf("reading is not supported when `path` contains UUID variable. Table: %q", table.Name)
 	}
-	name := replacePathVariables(c.pluginSpec.Path, table.Name, c.pluginSpec.Format, uuid.NewString(), time.Time{})
+	name := replacePathVariables(c.spec.Path, table.Name, c.spec.Format, uuid.NewString(), time.Time{})
 	f, err := os.Open(name)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
 
-	return c.Client.Read(f, table, sourceName, res)
+	return c.Client.Read(f, table, res)
 }
