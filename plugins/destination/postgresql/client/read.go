5e98caf3b67151a4eff6deea45fe391addb5c954
Herman Schaaf <hermanschaaf@gmail.com>
2023-07-03 17:28:16 +0100

    Remove sync

--- plugins/destination/postgresql/client/read.go
+++ plugins/destination/postgresql/client/read.go
@@ -3,6 +3,7 @@ package client
 import (
 	"bytes"
 	"context"
+	"database/sql/driver"
 	"fmt"
 	"net"
 	"net/netip"
@@ -198,4 +199,116 @@ func (c *Client) reverseTransformer(table *schema.Table, values []any) (arrow.Re
 	}
 	rec := bldr.NewRecord()
 	return rec, nil
+}
+
+func prepareValueForResourceSet(dataType arrow.DataType, v any) (any, error) {
+	if v == nil {
+		return nil, nil
+	}
+	switch tp := dataType.(type) {
+	case *arrow.Decimal128Type:
+		switch vt := v.(type) {
+		case pgtype.Numeric:
+			if vt.Valid {
+				v = decimal128.FromBigInt(vt.Int)
+			} else {
+				v = nil
+			}
+		}
+	case *arrow.ListType:
+		vl := v.([]any)
+		for i := range vl {
+			var err error
+			vl[i], err = prepareValueForResourceSet(tp.Elem(), vl[i])
+			if err != nil {
+				return nil, err
+			}
+		}
+		return vl, nil
+	case *arrow.LargeListType:
+		vl := v.([]any)
+		for i := range vl {
+			var err error
+			vl[i], err = prepareValueForResourceSet(tp.Elem(), vl[i])
+			if err != nil {
+				return nil, err
+			}
+		}
+		return vl, nil
+	case *arrow.StringType:
+		if value, ok := v.(driver.Valuer); ok {
+			if value == driver.Valuer(nil) {
+				v = nil
+			} else {
+				val, err := value.Value()
+				if err != nil {
+					return nil, err
+				}
+				if s, ok := val.(string); ok {
+					v = s
+				}
+			}
+		}
+		return v, nil
+	case *arrow.Time32Type:
+		switch vt := v.(type) {
+		case pgtype.Time:
+			t, err := vt.TimeValue()
+			if err != nil {
+				return nil, err
+			}
+			v = stringForTime(t, tp.Unit)
+			return v, nil
+		case string:
+			v = vt
+		}
+	case *arrow.Time64Type:
+		switch vt := v.(type) {
+		case pgtype.Time:
+			t, err := vt.TimeValue()
+			if err != nil {
+				return nil, err
+			}
+			v = stringForTime(t, tp.Unit)
+			return v, nil
+		case string:
+			v = vt
+		}
+	case *arrow.Uint64Type:
+		switch vt := v.(type) {
+		case pgtype.Numeric:
+			if !vt.Valid {
+				v = nil
+			} else {
+				v = vt.Int.Uint64()
+			}
+		}
+		return v, nil
+	case *arrow.TimestampType:
+		t, ok := v.(time.Time)
+		if ok {
+			v = t.Format(time.RFC3339Nano)
+		}
+		return v, nil
+	}
+	return v, nil
+}
+
+func stringForTime(t pgtype.Time, unit arrow.TimeUnit) string {
+	extra := ""
+	hour := t.Microseconds / 1e6 / 60 / 60
+	minute := t.Microseconds / 1e6 / 60 % 60
+	second := t.Microseconds / 1e6 % 60
+	micros := t.Microseconds % 1e6
+	switch unit {
+	case arrow.Millisecond:
+		extra = fmt.Sprintf(".%03d", (micros)/1e3)
+	case arrow.Microsecond:
+		extra = fmt.Sprintf(".%06d", micros)
+	case arrow.Nanosecond:
+		// postgres doesn't support nanosecond precision
+		extra = fmt.Sprintf(".%06d", micros)
+	}
+
+	return fmt.Sprintf("%02d:%02d:%02d"+extra, hour, minute, second)
 }
--- plugins/destination/postgresql/go.mod
+++ plugins/destination/postgresql/go.mod
@@ -12,7 +12,6 @@ require (
 	github.com/jackc/pgx/v5 v5.3.1
 	github.com/rs/zerolog v1.29.1
 	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df
-	golang.org/x/sync v0.1.0
 )
 
 // TODO: remove once all updates are merged
@@ -45,6 +44,7 @@ require (
 	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/mod v0.11.0 // indirect
 	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	golang.org/x/tools v0.6.0 // indirect
