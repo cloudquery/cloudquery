f4f740ae7da19bda70ada59dcbd594aa9b502fc5
Kemal Hadimli <disq@users.noreply.github.com>
2023-07-04 18:10:34 +0100

    feat(vercel): Upgrade to SDK v4

--- plugins/source/vercel/client/spec.go
+++ plugins/source/vercel/client/spec.go
@@ -1,5 +1,7 @@
 package client
 
+import "errors"
+
 type Spec struct {
 	AccessToken string   `json:"access_token,omitempty"`
 	TeamIDs     []string `json:"team_ids,omitempty"`
@@ -9,4 +11,30 @@ type Spec struct {
 	PageSize    int64  `json:"page_size,omitempty"`
 	MaxRetries  int64  `json:"max_retries,omitempty"`
 	MaxWait     int64  `json:"max_wait_secs,omitempty"`
+}
+
+func (s *Spec) SetDefaults() {
+	if s.EndpointURL == "" {
+		s.EndpointURL = "https://api.vercel.com"
+	}
+	if s.Timeout < 1 {
+		s.Timeout = 5
+	}
+	if s.PageSize < 1 {
+		s.PageSize = 100
+	}
+	if s.MaxRetries < 1 {
+		s.MaxRetries = 10
+	}
+	if s.MaxWait < 1 {
+		s.MaxWait = 300
+	}
+}
+
+func (s Spec) Validate() error {
+	if s.AccessToken == "" {
+		return errors.New("no access token provided")
+	}
+
+	return nil
 }
--- plugins/source/vercel/resources/plugin/plugin.go
+++ plugins/source/vercel/resources/plugin/plugin.go
@@ -1,29 +1,17 @@
 package plugin
 
 import (
-	"github.com/cloudquery/cloudquery/plugins/source/vercel/client"
-	"github.com/cloudquery/cloudquery/plugins/source/vercel/resources/services/deployment"
-	"github.com/cloudquery/cloudquery/plugins/source/vercel/resources/services/domain"
-	"github.com/cloudquery/cloudquery/plugins/source/vercel/resources/services/project"
-	"github.com/cloudquery/cloudquery/plugins/source/vercel/resources/services/team"
-	"github.com/cloudquery/plugin-sdk/v3/plugins/source"
-	"github.com/cloudquery/plugin-sdk/v3/schema"
+	"github.com/cloudquery/plugin-sdk/v4/plugin"
 )
 
 var (
 	Version = "development"
 )
 
-func Plugin() *source.Plugin {
-	return source.NewPlugin(
+func Plugin() *plugin.Plugin {
+	return plugin.NewPlugin(
 		"vercel",
 		Version,
-		[]*schema.Table{
-			domain.Domains(),
-			team.Teams(),
-			project.Projects(),
-			deployment.Deployments(),
-		},
-		client.Configure,
+		Configure,
 	)
 }
