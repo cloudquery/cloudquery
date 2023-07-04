bbbae13ee519073751d15323f7dd5fc0354c39a0
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2023-07-04 18:31:48 +0300

    feat(cloudflare): Update to SDK V4

--- plugins/source/cloudflare/main.go
+++ plugins/source/cloudflare/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"github.com/cloudquery/cloudquery/plugins/source/cloudflare/resources/plugin"
-	"github.com/cloudquery/plugin-sdk/v3/serve"
+	internalPlugin "github.com/cloudquery/cloudquery/plugins/source/cloudflare/resources/plugin"
+	"github.com/cloudquery/plugin-sdk/v4/serve"
 )
 
 const sentryDSN = "https://6beffd303fbd4661a7b7c6c2d546b580@o1396617.ingest.sentry.io/6747634"
 
 func main() {
-	serve.Source(plugin.Plugin(), serve.WithSourceSentryDSN(sentryDSN))
+	serve.Plugin(internalPlugin.Plugin(), serve.WithPluginSentryDSN(sentryDSN))
 }
