3b215e65b6150f43e6036e1b573ed5fad3726820
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2023-07-05 21:35:40 +0300

    fix main

--- plugins/source/googleanalytics/main.go
+++ plugins/source/googleanalytics/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"log"
+
 	"github.com/cloudquery/cloudquery/plugins/source/googleanalytics/resources/plugin"
 	"github.com/cloudquery/plugin-sdk/v4/serve"
 )
@@ -8,5 +11,7 @@ import (
 const sentryDSN = "https://1389ab4b050f4862879db19a783ecb78@o1396617.ingest.sentry.io/4504792343379968"
 
 func main() {
-	serve.Plugin(plugin.Plugin(), serve.WithPluginSentryDSN(sentryDSN))
+	if err := serve.Plugin(plugin.Plugin(), serve.WithPluginSentryDSN(sentryDSN)).Serve(context.Background()); err != nil {
+		log.Fatal(err)
+	}
 }
