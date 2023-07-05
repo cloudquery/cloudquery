70ee4fdd422bd06398683eb9237858b0f94b24c9
Kemal Hadimli <disq@users.noreply.github.com>
2023-07-05 15:12:09 +0100

    lint

--- plugins/source/digitalocean/resources/plugin/tables.go
+++ plugins/source/digitalocean/resources/plugin/tables.go
@@ -84,14 +84,7 @@ func titleTransformer(table *schema.Table) string {
 	if table.Title != "" {
 		return table.Title
 	}
-	exceptions := make(map[string]string)
-	//for k, v := range source.DefaultTitleExceptions {
-	//	exceptions[k] = v
-	//}
-	for k, v := range customExceptions {
-		exceptions[k] = v
-	}
-	csr := caser.New(caser.WithCustomExceptions(exceptions))
+	csr := caser.New(caser.WithCustomExceptions(customExceptions))
 	t := csr.ToTitle(table.Name)
 	return strings.Trim(strings.ReplaceAll(t, "  ", " "), " ")
 }
