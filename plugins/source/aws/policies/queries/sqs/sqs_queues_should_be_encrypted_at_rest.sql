30d415ca186a136da420ad75e6bf3d5457c12ab2
Jason Kao <100613312+jsonpr@users.noreply.github.com>
2023-07-05 11:27:45 -0400

    feat(aws-policies): Update sqs encryption for aws foundational security policies (#11777)
    This updates the SQS Encryption at Rest Control to be in line with AWS Foundational Security Best Practices.  Prior to updating, I checked the other frameworks for usage and this control was only used by the AWS Foundational Security Best Practices.
    
    Our previous control only checked for AWS-KMS keys.  The Amazon control checks for encryption at rest which can include using a non-KMS managed key - the Amazon SQS managed key (SSE-SQS).
    
    I tested with the following test cases:
    
    - Queue with no encryption specified (Fail)
    - Queue with Amazon Managed SSE-KMS (Pass)
    - Queue with Customer Managed SSE-KMS (Pass)
    - Queue with SSE-SQS (Pass)
    
    https://docs.aws.amazon.com/securityhub/latest/userguide/sqs-controls.html#sqs-1
    
    Additionally, I renamed the file and description to be in line with FSBP.

--- plugins/source/aws/policies/queries/sqs/sqs_queues_should_be_encrypted_at_rest.sql
+++ plugins/source/aws/policies/queries/sqs/sqs_queues_should_be_encrypted_at_rest.sql
@@ -3,10 +3,10 @@ select
     :'execution_time' as execution_time,
     :'framework' as framework,
     :'check_id' as check_id,
-    'SQS queues should be encrypted at rest using AWS KMS' as title,
+    'Amazon SQS queues should be encrypted at rest' as title,
     account_id,
     arn as resource_id,
     case when
-        kms_master_key_id is null or kms_master_key_id = ''
+        (kms_master_key_id is null or kms_master_key_id = '') and sqs_managed_sse_enabled = false
     then 'fail' else 'pass' end as status
 from aws_sqs_queues
