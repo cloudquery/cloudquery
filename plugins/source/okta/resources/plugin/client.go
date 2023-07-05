e41827647577ed99b4770ee081ece985758ef6a2
Kemal Hadimli <disq@users.noreply.github.com>
2023-07-05 13:43:51 +0100

    feat(okta): Upgrade to SDK v4

---
+++ plugins/source/okta/resources/plugin/client.go
@@ -0,0 +1,126 @@
+package plugin
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/cloudquery/cloudquery/plugins/source/okta/client"
+	"github.com/cloudquery/cloudquery/plugins/source/okta/resources/services/applications"
+	"github.com/cloudquery/cloudquery/plugins/source/okta/resources/services/groups"
+	"github.com/cloudquery/cloudquery/plugins/source/okta/resources/services/users"
+	"github.com/cloudquery/plugin-sdk/v4/message"
+	"github.com/cloudquery/plugin-sdk/v4/plugin"
+	"github.com/cloudquery/plugin-sdk/v4/scheduler"
+	"github.com/cloudquery/plugin-sdk/v4/schema"
+	"github.com/okta/okta-sdk-golang/v3/okta"
+	"google.golang.org/grpc"
+
+	"github.com/rs/zerolog"
+)
+
+const (
+	maxMsgSize = 100 * 1024 * 1024 // 100 MiB
+)
+
+type Client struct {
+	logger      zerolog.Logger
+	config      client.Spec
+	tables      schema.Tables
+	scheduler   *scheduler.Scheduler
+	backendConn *grpc.ClientConn
+	services    *okta.APIClient
+
+	plugin.UnimplementedDestination
+}
+
+func (c *Client) Logger() *zerolog.Logger {
+	return &c.logger
+}
+
+func (c *Client) Sync(ctx context.Context, options plugin.SyncOptions, res chan<- message.SyncMessage) error {
+	tt, err := c.tables.FilterDfs(options.Tables, options.SkipTables, options.SkipDependentTables)
+	if err != nil {
+		return err
+	}
+
+	if options.BackendOptions != nil {
+		c.logger.Warn().Msg("State backend not supported in plugin, skipping")
+	}
+
+	schedulerClient := client.New(c.logger, c.config, c.services)
+	return c.scheduler.Sync(ctx, schedulerClient, tt, res, scheduler.WithSyncDeterministicCQID(options.DeterministicCQID))
+}
+
+func (c *Client) Tables(_ context.Context, options plugin.TableOptions) (schema.Tables, error) {
+	tt, err := c.tables.FilterDfs(options.Tables, options.SkipTables, options.SkipDependentTables)
+	if err != nil {
+		return nil, err
+	}
+	return tt, nil
+}
+
+func (c *Client) Close(_ context.Context) error {
+	return c.backendConn.Close()
+}
+
+func Configure(_ context.Context, logger zerolog.Logger, specBytes []byte, opts plugin.NewClientOptions) (plugin.Client, error) {
+	if opts.NoConnection {
+		return &Client{
+			logger: logger,
+			tables: getTables(),
+		}, nil
+	}
+
+	config := client.Spec{}
+	if err := json.Unmarshal(specBytes, &config); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal spec: %w", err)
+	}
+	config.SetDefaults(&logger)
+	if err := config.Validate(); err != nil {
+		return nil, fmt.Errorf("failed to validate spec: %w", err)
+	}
+
+	cf := okta.NewConfiguration(
+		okta.WithOrgUrl(config.Domain),
+		okta.WithToken(config.Token),
+		okta.WithCache(true),
+		okta.WithRateLimitMaxBackOff(int64(config.RateLimit.MaxBackoff/time.Second)), // this param takes int64 of seconds
+		okta.WithRateLimitMaxRetries(config.RateLimit.MaxRetries),
+	)
+	cf.Debug = config.Debug
+	services := okta.NewAPIClient(cf)
+
+	return &Client{
+		config: config,
+		logger: logger,
+		scheduler: scheduler.NewScheduler(
+			scheduler.WithLogger(logger),
+			scheduler.WithConcurrency(uint64(config.Concurrency)),
+		),
+		services: services,
+		tables:   getTables(),
+	}, nil
+}
+
+func getTables() schema.Tables {
+	tables := []*schema.Table{
+		users.Users(),
+		groups.Groups(),
+		applications.Applications(),
+	}
+	for _, t := range tables {
+		if err := t.Transform(t); err != nil {
+			panic(err)
+		}
+		for _, rel := range t.Relations {
+			if err := rel.Transform(rel); err != nil {
+				panic(err)
+			}
+		}
+
+		schema.AddCqIDs(t)
+	}
+	return tables
+}
