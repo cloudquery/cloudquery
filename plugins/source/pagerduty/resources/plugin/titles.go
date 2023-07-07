95b0f53a21957213ae9ced1a136295d2dcfbff67
Herman Schaaf <hermanschaaf@gmail.com>
2023-07-07 17:19:31 +0100

    Migrate Pagerduty to plugin-sdk v4

---
+++ plugins/source/pagerduty/resources/plugin/titles.go
@@ -0,0 +1,26 @@
+package plugin
+
+import (
+	"github.com/cloudquery/plugin-sdk/v4/caser"
+	"github.com/cloudquery/plugin-sdk/v4/docs"
+	"github.com/cloudquery/plugin-sdk/v4/schema"
+)
+
+var customExceptions = map[string]string{
+	"pagerduty": "PagerDuty",
+}
+
+func titleTransformer(table *schema.Table) string {
+	if table.Title != "" {
+		return table.Title
+	}
+	exceptions := make(map[string]string)
+	for k, v := range docs.DefaultTitleExceptions {
+		exceptions[k] = v
+	}
+	for k, v := range customExceptions {
+		exceptions[k] = v
+	}
+	csr := caser.New(caser.WithCustomExceptions(exceptions))
+	return csr.ToTitle(table.Name)
+}
