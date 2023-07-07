f453a6643cb97e76c8cb35897c274e2e413b5d13
Herman Schaaf <hermanschaaf@gmail.com>
2023-07-07 14:47:04 +0100

    Include types

--- cli/cmd/errors.go
+++ cli/cmd/errors.go
@@ -13,9 +13,9 @@ func handleSendError(err error, client plugin.Plugin_WriteClient, msgType string
 		// we need to get back the original error
 		if _, err := client.CloseAndRecv(); err != nil {
 			if e, ok := status.FromError(err); ok {
-				return fmt.Errorf("write client returned error: %v", e.Message())
+				return fmt.Errorf("write client returned error (%v): %v", msgType, e.Message())
 			}
-			return fmt.Errorf("failed to close and receive: %v", err)
+			return fmt.Errorf("failed to close and receive write client (%v): %v", msgType, err)
 		}
 	}
 	return fmt.Errorf("failed to send write request (%v): %w", msgType, err)
