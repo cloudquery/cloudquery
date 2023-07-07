0cc2cdad0cdccb148d6c09335f75aa6cb2c7c9c0
Jason Kao <jason@cloudquery.io>
2023-07-07 14:29:17 -0400

    update SSM documents public

--- plugins/source/aws/policies/queries/ssm/documents_should_not_be_public.sql
+++ plugins/source/aws/policies/queries/ssm/documents_should_not_be_public.sql
@@ -7,7 +7,7 @@ select
     account_id,
     arn as resource_id,
     case when
-        'all' = ANY(ARRAY(SELECT JSONB_ARRAY_ELEMENTS_TEXT(p->'AccountIds'))) -- TODO check
+        'all' = ANY(ARRAY(SELECT JSONB_ARRAY_ELEMENTS_TEXT(p->'AccountIds'))) 
     then 'fail' else 'pass' end as status
 from aws_ssm_documents, jsonb_array_elements(aws_ssm_documents.permissions) p
 where owner in (select account_id from aws_iam_accounts)
