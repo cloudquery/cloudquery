fd17f4333bf5bbdd19003d541933334ddb5dd339
bbernays <ben@bernays.me>
2023-07-07 09:01:52 -0400

    Update write.go

--- plugins/destination/mysql/client/write.go
+++ plugins/destination/mysql/client/write.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/cloudquery/plugin-sdk/v4/message"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
+	"github.com/rs/zerolog"
 )
 
 func getInsertQueryBuild(table *schema.Table) *strings.Builder {
@@ -26,31 +27,47 @@ func getInsertQueryBuild(table *schema.Table) *strings.Builder {
 	return &builder
 }
 
-func (c *Client) writeResources(ctx context.Context, query string, msgs message.WriteInserts) error {
-	table := msgs[0].GetTable()
-	pks := make([]int, 0)
-	for i, col := range table.Columns {
-		if !col.PrimaryKey {
-			continue
-		}
-		sqlType := arrowTypeToMySqlStr(col.Type)
-		if sqlType != "blob" && sqlType != "text" {
-			continue
-		}
-		// only if the PK is a blob or a text do we care about the length of the data
-		pks = append(pks, i)
+func logTablesWithTruncation(logger zerolog.Logger, tables map[string]bool) {
+	keys := make([]string, len(tables))
+	if len(tables) == 0 {
+		return
 	}
+	for k := range tables {
+		keys = append(keys, k)
+	}
+	logger.Warn().Strs("tables", keys).Msg("tables contain a value in a primary key that is longer than what is supported by MySQL. only the first 191 characters will be included in the index. To see the complete record enable debug logs using `--log-level debug`")
+}
+
+func (c *Client) writeResources(ctx context.Context, query string, msgs message.WriteInserts) error {
+	tablesWithTruncation := make(map[string]bool)
 	for _, msg := range msgs {
+		// This assumes that all the records in the message are from the same table
+		// If we can assume all msgs are from the same table, we can move this outside the loop
+		table := msg.GetTable()
+		pks := make([]int, 0)
+		for i, col := range table.Columns {
+			if !col.PrimaryKey {
+				continue
+			}
+			sqlType := arrowTypeToMySqlStr(col.Type)
+			if sqlType != "blob" && sqlType != "text" {
+				continue
+			}
+			// only if the PK is a blob or a text do we care about the length of the data
+			pks = append(pks, i)
+		}
 		rec := msg.Record
 		transformedRecords, err := transformRecord(rec)
 		if err != nil {
 			return err
 		}
+
 		// log a warning that a blob or text field that is a PK has more than 191 characters
 		for _, record := range transformedRecords {
 			for _, pki := range pks {
 				if len(record[pki].(string)) > maxPrefixLength {
-					c.logger.Warn().Any("record", record).Msgf("record contains a primary key that is longer than mysql can handle. only the first %d will be included in the index", maxPrefixLength)
+					c.logger.Debug().Any("record", record).Msgf("record contains a primary key that is longer than MySQL can handle. only the first %d will be included in the index", maxPrefixLength)
+					tablesWithTruncation[table.Name] = true
 				}
 			}
 		}
@@ -58,10 +75,12 @@ func (c *Client) writeResources(ctx context.Context, query string, msgs message.
 		for _, transformedRecord := range transformedRecords {
 			_, err := c.db.ExecContext(ctx, query, transformedRecord...)
 			if err != nil {
+				logTablesWithTruncation(c.logger, tablesWithTruncation)
 				return err
 			}
 		}
 	}
+	logTablesWithTruncation(c.logger, tablesWithTruncation)
 	return nil
 }
 
