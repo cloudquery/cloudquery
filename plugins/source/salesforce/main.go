9668d5ee324efc2eff66193b03ceb4d7f4833b78
Kemal <223029+disq@users.noreply.github.com>
2023-07-08 14:01:43 +0100

    feat(salesforce)!: Upgrade to SDK v4 (#12075)
    BEGIN_COMMIT_OVERRIDE
    feat!: Upgrades the salesforce source plugin to use plugin-sdk v4. This version does not contain any user-facing breaking changes, but because it is now using CloudQuery gRPC protocol v3, it does require use of a destination plugin that also supports protocol v3. All recent destination plugin versions support this.
    END_COMMIT_OVERRIDE
    
    Closes https://github.com/cloudquery/cloudquery/issues/12073

--- plugins/source/salesforce/main.go
+++ plugins/source/salesforce/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
-	"github.com/cloudquery/cloudquery/plugins/source/salesforce/resources/plugin"
+	"context"
+	"log"
 
-	"github.com/cloudquery/plugin-sdk/v3/serve"
+	"github.com/cloudquery/cloudquery/plugins/source/salesforce/resources/plugin"
+	"github.com/cloudquery/plugin-sdk/v4/serve"
 )
 
-const sentryDsn = "https://77af2b07ee5c45519f17f2f6314421c4@o1396617.ingest.sentry.io/4504390800310272"
+const sentryDSN = "https://77af2b07ee5c45519f17f2f6314421c4@o1396617.ingest.sentry.io/4504390800310272"
 
 func main() {
-	serve.Source(plugin.Plugin(), serve.WithSourceSentryDSN(sentryDsn))
+	p := serve.Plugin(plugin.Plugin(), serve.WithPluginSentryDSN(sentryDSN))
+	if err := p.Serve(context.Background()); err != nil {
+		log.Fatalf("failed to serve plugin: %v", err)
+	}
 }
