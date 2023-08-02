e35cc5c462a9b30038aff829d82382ed30b2608e
Martin Norbury <martin.norbury@gmail.com>
2023-08-02 14:56:45 +0100

    Add sentry DSN

--- plugins/source/vault/main.go
+++ plugins/source/vault/main.go
@@ -9,8 +9,10 @@ import (
 	"github.com/cloudquery/plugin-sdk/v4/serve"
 )
 
+const sentryDSN = "https://64ac2cdbdb9cb64ed7d78053412495b8@o1396617.ingest.sentry.io/4505635677863936"
+
 func main() {
-	if err := serve.Plugin(plugin.Plugin()).Serve(context.Background()); err != nil {
+	if err := serve.Plugin(plugin.Plugin(), serve.WithPluginSentryDSN(sentryDSN)).Serve(context.Background()); err != nil {
 		log.Fatalf("failed to serve plugin: %v", err)
 	}
 }
