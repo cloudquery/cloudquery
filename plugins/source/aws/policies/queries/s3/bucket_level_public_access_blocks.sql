b16147ba3b2d4556bc9d840aa69b99f0a9693a84
FUJITA Yuki <yuki.fujita@bizreach.co.jp>
2023-08-02 10:52:42 +0900

    consider s3_account condition

--- plugins/source/aws/policies/queries/s3/bucket_level_public_access_blocks.sql
+++ plugins/source/aws/policies/queries/s3/bucket_level_public_access_blocks.sql
@@ -4,13 +4,16 @@ select
     :'framework' as framework,
     :'check_id' as check_id,
     'S3 Block Public Access setting should be enabled at the bucket-level' as title,
-    account_id,
-    arn AS resource_id,
-    case when
-        block_public_acls is not TRUE
-        or block_public_policy is not TRUE
-        or ignore_public_acls is not TRUE
-        or restrict_public_buckets is not TRUE
-    then 'fail' else 'pass' end as status
+    s3.account_id,
+    s3.arn AS resource_id,
+    case 
+        when (s3.block_public_acls or as3a.block_public_acls)
+         and (s3.block_public_policy or as3a.block_public_policy)
+         and (s3.ignore_public_acls or as3a.ignore_public_acls)
+         and (s3.restrict_public_buckets or as3a.restrict_public_buckets)
+        then 'pass' 
+        else 'fail' 
+    end as status
 from
-    aws_s3_buckets
+    aws_s3_buckets s3
+    join aws_s3_accounts as3a on s3.account_id = as3a.account_id
