252a3e3e744d9e7dea2114f098faca7f2adcbe5e
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2023-08-02 18:08:32 +0300

    fix

--- plugins/source/jira/main.go
+++ plugins/source/jira/main.go
@@ -5,12 +5,13 @@ import (
 	"log"
 
 	"github.com/cloudquery/cloudquery/plugins/source/jira/client"
+	internalPlugin "github.com/cloudquery/cloudquery/plugins/source/jira/resources/plugin"
 	"github.com/cloudquery/plugin-sdk/v4/plugin"
 	"github.com/cloudquery/plugin-sdk/v4/serve"
 )
 
 func main() {
-	p := plugin.NewPlugin("jira", "0.0.1", client.New)
+	p := plugin.NewPlugin("jira", internalPlugin.Version, client.New)
 	if err := serve.Plugin(p).Serve(context.Background()); err != nil {
 		log.Fatal(err)
 	}
