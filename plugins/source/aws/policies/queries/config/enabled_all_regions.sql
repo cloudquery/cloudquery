ab1280207b33764fd941460e375c29ec26704084
FUJITA Yuki <yuki.fujita@bizreach.co.jp>
2023-08-03 15:50:59 +0900

    check all regions

--- plugins/source/aws/policies/queries/config/enabled_all_regions.sql
+++ plugins/source/aws/policies/queries/config/enabled_all_regions.sql
@@ -4,10 +4,11 @@ select
     :'framework' as framework,
     :'check_id' as check_id,
     'AWS Config should be enabled' as title,
-    account_id,
-    arn as resource_id,
+    aws_regions.account_id,
+    aws_regions.region as resource_id,
     case when
-      (recording_group->>'IncludeGlobalResourceTypes')::boolean IS NOT TRUE
+      aws_config_configuration_recorders.arn IS NULL
+      OR (recording_group->>'IncludeGlobalResourceTypes')::boolean IS NOT TRUE
       OR (recording_group->>'AllSupported')::boolean IS NOT TRUE
       OR status_recording IS NOT TRUE
       OR status_last_status IS DISTINCT FROM 'SUCCESS'
@@ -15,4 +16,7 @@ select
     else 'pass'
     end as status
 FROM
-    aws_config_configuration_recorders
+    aws_regions
+    left join aws_config_configuration_recorders
+      on aws_regions.region = aws_config_configuration_recorders.region
+      and aws_regions.account_id = aws_config_configuration_recorders.account_id
