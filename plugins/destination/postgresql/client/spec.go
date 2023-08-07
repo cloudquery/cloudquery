8525b52431b517c1d599b03f5467d8eee245c32b
candiduslynx <candiduslynx@gmail.com>
2023-08-07 11:39:34 +0300

    postgresql

--- plugins/destination/postgresql/client/spec.go
+++ plugins/destination/postgresql/client/spec.go
@@ -3,7 +3,6 @@ package client
 import (
 	"time"
 
-	"github.com/cloudquery/plugin-pb-go/specs"
 	"github.com/cloudquery/plugin-sdk/v4/configtype"
 )
 
@@ -16,16 +15,12 @@ const (
 type Spec struct {
 	ConnectionString string              `json:"connection_string,omitempty"`
 	PgxLogLevel      LogLevel            `json:"pgx_log_level,omitempty"`
-	MigrateMode      string              `json:"migrate_mode,omitempty"`
 	BatchSize        int                 `json:"batch_size,omitempty"`
 	BatchSizeBytes   int                 `json:"batch_size_bytes,omitempty"`
 	BatchTimeout     configtype.Duration `json:"batch_timeout,omitempty"`
 }
 
 func (s *Spec) SetDefaults() {
-	if s.MigrateMode == "" {
-		s.MigrateMode = specs.MigrateModeSafe.String()
-	}
 	if s.BatchSize <= 0 {
 		s.BatchSize = defaultBatchSize
 	}
