73a9d03f0ba3a8cb0ee328f403f8f0ba077036fe
FUJITA Yuki <yuki.fujita@bizreach.co.jp>
2023-08-15 11:28:10 +0900

    fix detecting condition to unify results by arn

--- plugins/source/aws/policies/queries/ec2/no_broad_public_ingress_acl_on_port_22_3389.sql
+++ plugins/source/aws/policies/queries/ec2/no_broad_public_ingress_acl_on_port_22_3389.sql
@@ -7,13 +7,18 @@ select
   'Ensure no Network ACLs allow ingress from 0.0.0.0/0 to remote server administration ports (Automated)',
   account_id,
   arn,
-  case when
-      (cidr_block = '0.0.0.0/0' or ipv6_cidr_block = '::/0')
+  case
+    when bool_or(
+      rule_action = 'allow'
+      and (cidr_block = '0.0.0.0/0' or ipv6_cidr_block = '::/0')
       and (
-          (port_range_from is null and port_range_to is null) -- all ports
-          or 22 between port_range_from and port_range_to
-          or 3389 between port_range_from and port_range_to)
-      then 'fail'
-      else 'pass'
+        (port_range_from is null and port_range_to is null) -- all ports
+        or 22 between port_range_from and port_range_to
+        or 3389 between port_range_from and port_range_to
+      )
+    )
+    then 'fail'
+    else 'pass'
   end
-from view_aws_nacl_allow_ingress_rules
+from view_aws_nacl_ingress_rules
+group by account_id, arn
