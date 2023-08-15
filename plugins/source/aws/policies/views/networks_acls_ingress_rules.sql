f1d8e343eddd0fa32db8a2b4c76cda36a4cd9124
FUJITA Yuki <yuki.fujita@bizreach.co.jp>
2023-08-15 11:27:36 +0900

    tale all ingress rules includes deny policy

--- plugins/source/aws/policies/views/networks_acls_ingress_rules.sql
+++ plugins/source/aws/policies/views/networks_acls_ingress_rules.sql
@@ -1,15 +1,15 @@
-CREATE OR REPLACE VIEW view_aws_nacl_allow_ingress_rules AS
-WITH rules AS (SELECT aena.arn,
-                      aena.account_id,
-                      (jsonb_array_elements(entries) -> 'PortRange' ->> 'From')::int AS port_range_from,
-                      (jsonb_array_elements(entries) -> 'PortRange' ->> 'To')::int   AS port_range_to,
-                      jsonb_array_elements(entries) ->> 'Protocol'                   AS protocol,
-                      jsonb_array_elements(entries) ->> 'CidrBlock'                  AS cidr_block,
-                      jsonb_array_elements(entries) ->> 'Ipv6CidrBlock'              AS ipv6_cidr_block,
-                      jsonb_array_elements(entries) ->> 'Egress'                     AS egress,
-                      jsonb_array_elements(entries) ->> 'RuleAction'                 AS rule_action
-               FROM aws_ec2_network_acls aena)
-SELECT arn, account_id, port_range_from, port_range_to, protocol, cidr_block, ipv6_cidr_block
-FROM rules
-WHERE egress IS DISTINCT FROM 'true'
-  AND rule_action = 'allow';
+CREATE OR REPLACE VIEW view_aws_nacl_ingress_rules AS
+SELECT
+  account_id,
+  region,
+  arn,
+  is_default,
+  vpc_id,
+  (entry -> 'PortRange' ->> 'From')::int AS port_range_from,
+  (entry -> 'PortRange' ->> 'To')::int   AS port_range_to,
+  entry ->> 'Protocol'                   AS protocol,
+  entry ->> 'CidrBlock'                  AS cidr_block,
+  entry ->> 'Ipv6CidrBlock'              AS ipv6_cidr_block,
+  entry ->> 'RuleAction'                 AS rule_action
+FROM aws_ec2_network_acls, jsonb_array_elements(entries) AS entry
+WHERE NOT (entry ->> 'Egress')::bool
