eda215c0d204becf18d406be686fde2585bc4e77
Erez Rokah <erezrokah@users.noreply.github.com>
2023-08-25 14:33:25 +0200

    fix: Call list tables once on insert if `--no-migrate` was used (#13327)
    #### Summary
    
    Follow up to https://github.com/cloudquery/cloudquery/pull/13323 to handle `--no-migrate`. Thanks @candiduslynx for pointing it out
    
    <!--

--- plugins/destination/postgresql/client/insert.go
+++ plugins/destination/postgresql/client/insert.go
@@ -20,6 +20,15 @@ func (c *Client) InsertBatch(ctx context.Context, messages message.WriteInserts)
 		return err
 	}
 
+	// This happens when the CLI was invoked with `sync --no-migrate`
+	if c.pgTablesToPKConstraints == nil {
+		// Passing nil for include and exclude lists all tables and populates c.pgTablesToPKConstraints
+		_, err := c.listTables(ctx, nil, nil)
+		if err != nil {
+			return err
+		}
+	}
+
 	tables = c.normalizeTables(tables)
 	if err != nil {
 		return err
