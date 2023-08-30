cloudquery/cloudquery

commit feb8f87d8d761eb9c49ce84329ad0397f730a918

fix(deps): Update `github.com/cloudquery/arrow/go/v13` to `github.com/apache/arrow/go/v14` (#13341)
This PR contains the following updates:

| Package | Type | Update | Change |
|---|---|---|---|
|
[github.com/cloudquery/arrow/go/v13](https://togithub.com/cloudquery/arrow)
| replace | digest | `5b83d4f` -> `a069d71` |

---

### ⚠ Dependency Lookup Warnings ⚠

Warnings were logged while processing this repo. Please check the
Dependency Dashboard for more information.

---

### Configuration

📅 **Schedule**: Branch creation - "before 3am on Monday" (UTC),
Automerge - At any time (no schedule defined).

🚦 **Automerge**: Disabled by config. Please merge this manually once you
are satisfied.

♻ **Rebasing**: Whenever PR becomes conflicted, or you tick the
rebase/retry checkbox.

🔕 **Ignore**: Close this PR and you won't be reminded about this update
again.

---

- [ ] <!-- rebase-check -->If you want to rebase/retry this PR, check
this box

---

This PR has been generated by [Renovate
Bot](https://togithub.com/renovatebot/renovate).

<!--renovate-debug:eyJjcmVhdGVkSW5WZXIiOiIzNi42Ny4xIiwidXBkYXRlZEluVmVyIjoiMzYuNjcuMSIsInRhcmdldEJyYW5jaCI6Im1haW4ifQ==-->

---------

Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>
Co-authored-by: candiduslynx <candiduslynx@gmail.com>
Co-authored-by: Alex Shcherbakov <candiduslynx@users.noreply.github.com>

plugins/destination/postgresql/client/spec.go
@@ -8,8 +8,8 @@ import (
 
 const (
 	defaultBatchSize      = 10000
-	defaultBatchSizeBytes = 100000000
-	defaultBatchTimeout   = 60 * time.Second
+	defaultBatchSizeBytes = 1000000
+	defaultBatchTimeout   = 10 * time.Second
 )
 
 type Spec struct {

plugins/source/pagerduty/resources/services/priorities/priorities.go
@@ -1,8 +1,7 @@
 package priorities
 
 import (
-	"github.com/PagerDuty/go-pagerduty"
-	"github.com/apache/arrow/go/v13/arrow"
+	"github.com/apache/arrow/go/v14/arrow"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/transformers"
 )

plugins/source/pagerduty/resources/services/schedules/schedules.go
@@ -1,8 +1,7 @@
 package schedules
 
 import (
-	"github.com/PagerDuty/go-pagerduty"
-	"github.com/apache/arrow/go/v13/arrow"
+	"github.com/apache/arrow/go/v14/arrow"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/transformers"
 )

plugins/source/stripe/resources/services/charges/charges.go
@@ -5,11 +5,10 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/apache/arrow/go/v13/arrow"
+	"github.com/apache/arrow/go/v14/arrow"
 	"github.com/cloudquery/cloudquery/plugins/source/stripe/client"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/transformers"
-	"github.com/stripe/stripe-go/v74"
 )
 
 func Charges() *schema.Table {

plugins/source/stripe/resources/services/checkout/checkout_sessions.go
@@ -3,11 +3,10 @@ package checkout
 import (
 	"context"
 
-	"github.com/apache/arrow/go/v13/arrow"
+	"github.com/apache/arrow/go/v14/arrow"
 	"github.com/cloudquery/cloudquery/plugins/source/stripe/client"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/transformers"
-	"github.com/stripe/stripe-go/v74"
 )
 
 func CheckoutSessions() *schema.Table {

plugins/source/stripe/resources/services/issuing/issuing_cardholders.go
@@ -5,11 +5,10 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/apache/arrow/go/v13/arrow"
+	"github.com/apache/arrow/go/v14/arrow"
 	"github.com/cloudquery/cloudquery/plugins/source/stripe/client"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/transformers"
-	"github.com/stripe/stripe-go/v74"
 )
 
 func IssuingCardholders() *schema.Table {

plugins/source/stripe/resources/services/issuing/issuing_disputes.go
@@ -5,11 +5,10 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/apache/arrow/go/v13/arrow"
+	"github.com/apache/arrow/go/v14/arrow"
 	"github.com/cloudquery/cloudquery/plugins/source/stripe/client"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/transformers"
-	"github.com/stripe/stripe-go/v74"
 )
 
 func IssuingDisputes() *schema.Table {

plugins/source/stripe/resources/services/reporting/reporting_report_runs.go
@@ -5,11 +5,10 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/apache/arrow/go/v13/arrow"
+	"github.com/apache/arrow/go/v14/arrow"
 	"github.com/cloudquery/cloudquery/plugins/source/stripe/client"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/transformers"
-	"github.com/stripe/stripe-go/v74"
 )
 
 func ReportingReportRuns() *schema.Table {

commit cc30fb09d1a039948754cbda09a1adf1b58d0c54

fix: Remove stale code (#13445)
Caused by https://github.com/cloudquery/cloudquery/pull/13341

plugins/destination/postgresql/client/spec.go
@@ -8,8 +8,8 @@ import (
 
 const (
 	defaultBatchSize      = 10000
-	defaultBatchSizeBytes = 1000000
-	defaultBatchTimeout   = 10 * time.Second
+	defaultBatchSizeBytes = 100000000
+	defaultBatchTimeout   = 60 * time.Second
 )
 
 type Spec struct {

plugins/source/pagerduty/resources/services/priorities/priorities.go
@@ -1,6 +1,7 @@
 package priorities
 
 import (
+	"github.com/PagerDuty/go-pagerduty"
 	"github.com/apache/arrow/go/v14/arrow"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/transformers"

plugins/source/pagerduty/resources/services/schedules/schedules.go
@@ -1,6 +1,7 @@
 package schedules
 
 import (
+	"github.com/PagerDuty/go-pagerduty"
 	"github.com/apache/arrow/go/v14/arrow"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/transformers"

plugins/source/stripe/resources/services/charges/charges.go
@@ -9,6 +9,7 @@ import (
 	"github.com/cloudquery/cloudquery/plugins/source/stripe/client"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/transformers"
+	"github.com/stripe/stripe-go/v74"
 )
 
 func Charges() *schema.Table {

plugins/source/stripe/resources/services/checkout/checkout_sessions.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cloudquery/cloudquery/plugins/source/stripe/client"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/transformers"
+	"github.com/stripe/stripe-go/v74"
 )
 
 func CheckoutSessions() *schema.Table {

plugins/source/stripe/resources/services/issuing/issuing_cardholders.go
@@ -9,6 +9,7 @@ import (
 	"github.com/cloudquery/cloudquery/plugins/source/stripe/client"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/transformers"
+	"github.com/stripe/stripe-go/v74"
 )
 
 func IssuingCardholders() *schema.Table {

plugins/source/stripe/resources/services/issuing/issuing_disputes.go
@@ -9,6 +9,7 @@ import (
 	"github.com/cloudquery/cloudquery/plugins/source/stripe/client"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/transformers"
+	"github.com/stripe/stripe-go/v74"
 )
 
 func IssuingDisputes() *schema.Table {

plugins/source/stripe/resources/services/reporting/reporting_report_runs.go
@@ -9,6 +9,7 @@ import (
 	"github.com/cloudquery/cloudquery/plugins/source/stripe/client"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/transformers"
+	"github.com/stripe/stripe-go/v74"
 )
 
 func ReportingReportRuns() *schema.Table {

plugins/source/stripe/resources/services/treasury/treasury_outbound_payments.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cloudquery/cloudquery/plugins/source/stripe/client"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/transformers"
+	"github.com/stripe/stripe-go/v74"
 )
 
 func TreasuryOutboundPayments() *schema.Table {