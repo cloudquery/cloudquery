90ba2459db86ec906d95e158c5c1560afd05f3a7
candiduslynx <candiduslynx@gmail.com>
2023-09-01 15:47:22 +0300

    fix

--- plugins/destination/elasticsearch/client/delete_stale.go
+++ plugins/destination/elasticsearch/client/delete_stale.go
@@ -52,13 +52,13 @@ func (c *Client) DeleteStale(ctx context.Context, msgs message.WriteDeleteStales
 	return g.Wait()
 }
 
-func (c *Client) deleteStaleIndex(ctx context.Context, index string, req *deletebyquery.Request) error {
+func (c *Client) deleteStaleIndex(ctx context.Context, tableName string, req *deletebyquery.Request) error {
 	// refresh index before delete, to ensure all written data is available
-	if err := c.refreshIndex(ctx, index); err != nil {
+	if err := c.refreshIndex(ctx, tableName+"*"); err != nil {
 		return fmt.Errorf("failed to refresh index before delete: %w", err)
 	}
 
-	resp, err := c.typedClient.DeleteByQuery(index).Request(req).WaitForCompletion(true).Do(ctx)
+	resp, err := c.typedClient.DeleteByQuery(tableName + "*").Request(req).WaitForCompletion(true).Do(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to delete stale entries: %w", err)
 	}
