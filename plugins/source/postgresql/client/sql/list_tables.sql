80d7b8d3f8d4d3299403524ab985b8f976c141fb
Martin Norbury <martin.norbury@gmail.com>
2023-09-04 13:33:34 +0100

    chore: Refactor SQL query into embed file (#13614)
    A no-op refactoring to move to using an `embed` SQL string ready for https://github.com/cloudquery/cloudquery/pull/13612
    
    relates: https://github.com/cloudquery/cloudquery/pull/13612

---
+++ plugins/source/postgresql/client/sql/list_tables.sql
@@ -0,0 +1,58 @@
+-- +----------------+-------------+-------------+------------+----------------+-----------+-----------+---------------------+
+-- | ordinal_position | table_name | column_name | data_type | is_primary_key | not_null  | is_unique | constraint_name     |
+-- +----------------+-------------+-------------+------------+----------------+-----------+-----------+---------------------+
+-- |              1 | users       | id          | bigint     | YES            | true      | true      | cq_users_pk         |
+-- |              2 | users       | name        | text       | NO             | false     | false     |                     |
+-- |              3 | users       | email       | text       | NO             | true      | false     | cq_users_pk         |
+-- |              1 | posts       | id          | bigint     | YES            | true      | true      | cq_posts_pk         |
+-- |              2 | posts       | title       | text       | NO             | false     | false     |                     |
+SELECT
+    columns.ordinal_position AS ordinal_position,
+    pg_class.relname AS table_name,
+    pg_attribute.attname AS column_name,
+    CASE
+        -- This is required per the differences in pg_catalog.format_type implementations
+        -- between PostgreSQL & CockroachDB.
+        -- namely, numeric(20,0)[] is returned as numeric[] unless we use the typelem format + []
+        WHEN pg_type.typcategory = 'A' AND pg_type.typelem != 0
+            THEN pg_catalog.format_type(pg_type.typelem, pg_attribute.atttypmod) || '[]'
+        ELSE pg_catalog.format_type(pg_attribute.atttypid, pg_attribute.atttypmod)
+        END AS data_type,
+    CASE
+        WHEN conkey IS NOT NULL AND contype = 'p' AND array_position(conkey, pg_attribute.attnum) > 0 THEN true
+        ELSE false
+        END AS is_primary_key,
+    CASE
+        WHEN pg_attribute.attnotnull THEN true
+        ELSE false
+        END AS not_null,
+    CASE
+        WHEN
+                conkey IS NOT NULL
+                AND (contype = 'p' OR contype = 'u')
+                AND array_length(conkey, 1) = 1  -- we don't handle composite unique keys
+                AND array_position(conkey, pg_attribute.attnum) > 0
+            THEN true
+        ELSE false
+        END AS is_unique,
+    COALESCE(pg_constraint.conname, '') AS constraint_name
+FROM
+    pg_catalog.pg_attribute
+        INNER JOIN
+    pg_catalog.pg_type ON pg_type.oid = pg_attribute.atttypid
+        INNER JOIN
+    pg_catalog.pg_class ON pg_class.oid = pg_attribute.attrelid
+        INNER JOIN
+    pg_catalog.pg_namespace ON pg_namespace.oid = pg_class.relnamespace
+        LEFT JOIN
+    pg_catalog.pg_constraint ON pg_constraint.conrelid = pg_attribute.attrelid
+        AND conkey IS NOT NULL AND array_position(conkey, pg_attribute.attnum) > 0
+        AND (contype = 'p' OR contype = 'u')
+        INNER JOIN
+    information_schema.columns ON columns.table_name = pg_class.relname AND columns.column_name = pg_attribute.attname AND columns.table_schema = pg_catalog.pg_namespace.nspname
+WHERE
+        pg_attribute.attnum > 0
+  AND NOT pg_attribute.attisdropped
+  AND pg_catalog.pg_namespace.nspname = '%s'
+ORDER BY
+    table_name ASC , ordinal_position ASC;
