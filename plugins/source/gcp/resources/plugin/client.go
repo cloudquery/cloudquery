bda21fc7d69412dc93cbf6f5e1c44989f602b191
Yevgeny Pats <yev.pats@gmail.com>
2023-09-22 14:31:06 +0300

    feat(main): Add JSON schema for spec (#13876)
    Blocked by: https://github.com/cloudquery/plugin-sdk/pull/1214

--- plugins/source/gcp/resources/plugin/client.go
+++ plugins/source/gcp/resources/plugin/client.go
@@ -11,6 +11,7 @@ import (
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/state"
 	"github.com/cloudquery/plugins/source/gcp/client"
+	"github.com/cloudquery/plugins/source/gcp/client/spec"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
@@ -35,17 +36,17 @@ func NewClient(ctx context.Context, logger zerolog.Logger, specBytes []byte, opt
 	if options.NoConnection {
 		return c, nil
 	}
-	spec := &client.Spec{}
-	if err := json.Unmarshal(specBytes, spec); err != nil {
+	s := &spec.Spec{}
+	if err := json.Unmarshal(specBytes, s); err != nil {
 		return nil, err
 	}
-	spec.SetDefaults()
-	syncClient, err := client.New(ctx, logger, spec)
+	s.SetDefaults()
+	syncClient, err := client.New(ctx, logger, s)
 	if err != nil {
 		return nil, err
 	}
 	c.syncClient = syncClient.(*client.Client)
-	c.scheduler = scheduler.NewScheduler(scheduler.WithLogger(logger), scheduler.WithConcurrency(spec.Concurrency), scheduler.WithStrategy(spec.Scheduler))
+	c.scheduler = scheduler.NewScheduler(scheduler.WithLogger(logger), scheduler.WithConcurrency(s.Concurrency), scheduler.WithStrategy(s.Scheduler))
 	return c, nil
 }
 
