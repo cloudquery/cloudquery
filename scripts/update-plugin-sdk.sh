60621de0d600f0afbae5918fcecaf97347fd50a0
erezrokah <erezrokah@users.noreply.github.com>
2023-09-27 12:56:46 +0200

    fix(deps): Update module github.com/cloudquery/plugin-sdk/v4 to v4.11.1

--- scripts/update-plugin-sdk.sh
+++ scripts/update-plugin-sdk.sh
@@ -4,5 +4,5 @@ PLUGIN_DIRS=$(ls plugins/source)
 for plugin in $PLUGIN_DIRS; do
 	echo "Updating plugin-sdk for $plugin"
 
-	(cd "plugins/source/$plugin" && go get -u github.com/cloudquery/plugin-sdk && go mod tidy)
+	(cd "plugins/source/$plugin" && go get -u github.com/cloudquery/plugin-sdk/v4 && go mod tidy)
 done
