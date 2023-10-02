ec3a607945bd87f788a7d60cdac1612c850d329b
Alex Shcherbakov <candiduslynx@users.noreply.github.com>
2023-10-02 18:07:52 +0300

    feat: Use autogenerated JSON schema (#14111)
    Follow-up for #13876
    Blocked by:
    - [x] https://github.com/invopop/jsonschema/pull/103
    - [x] https://github.com/invopop/jsonschema/pull/105
    - [x] https://github.com/cloudquery/codegen/pull/29
    - [x] https://github.com/cloudquery/plugin-sdk/pull/1254

--- plugins/source/gcp/client/spec/spec_test.go
+++ plugins/source/gcp/client/spec/spec_test.go
@@ -4,11 +4,12 @@ import (
 	"encoding/json"
 	"testing"
 
+	"github.com/cloudquery/codegen/jsonschema"
 	"github.com/cloudquery/plugin-sdk/v4/plugin"
 	"github.com/stretchr/testify/require"
 )
 
-func TestSpec(t *testing.T) {
+func TestJSONSchema(t *testing.T) {
 	validator, err := plugin.JSONSchemaValidator(JSONSchema)
 	require.NoError(t, err)
 
@@ -376,4 +377,10 @@ func TestSpec(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestEnsureJSONSchema(t *testing.T) {
+	data, err := jsonschema.Generate(new(Spec))
+	require.NoError(t, err)
+	require.JSONEqf(t, string(data), JSONSchema, "new schema should be:\n%s\n", string(data))
 }
