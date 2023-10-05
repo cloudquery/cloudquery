7dc8eaa107661f9e779fc722298a40bc53b44202
candiduslynx <candiduslynx@gmail.com>
2023-10-05 22:16:03 +0300

    rm account.default_region default

--- plugins/source/aws/client/spec/account.go
+++ plugins/source/aws/client/spec/account.go
@@ -7,7 +7,7 @@ type Account struct {
 	RoleARN         string   `json:"role_arn,omitempty" jsonschema:"pattern=^arn(:[^:\n]*){5}([:/].*)?$"`
 	RoleSessionName string   `json:"role_session_name,omitempty"`
 	ExternalID      string   `json:"external_id,omitempty"`
-	DefaultRegion   string   `json:"default_region,omitempty" jsonschema:"default=us-east-1"`
+	DefaultRegion   string   `json:"default_region,omitempty"`
 	Regions         []string `json:"regions,omitempty" jsonschema:"minLength=1"`
 
 	// explicitly ignore in JSON parsing, as this is filled in later
