18a60e4115e6b588bf7560e554a7c61695584680
candiduslynx <candiduslynx@gmail.com>
2023-10-09 10:12:07 +0300

    regroup imports

--- plugins/source/aws/resources/services/cloudwatch/metrics.go
+++ plugins/source/aws/resources/services/cloudwatch/metrics.go
@@ -6,15 +6,14 @@ import (
 	"strconv"
 
 	"github.com/apache/arrow/go/v14/arrow"
-	"github.com/aws/aws-sdk-go-v2/service/cloudwatch/types"
-	"github.com/cloudquery/cloudquery/plugins/source/aws/client/spec/tableoptions"
-	cqtypes "github.com/cloudquery/plugin-sdk/v4/types"
-	"github.com/mitchellh/hashstructure/v2"
-
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatch"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatch/types"
 	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
+	"github.com/cloudquery/cloudquery/plugins/source/aws/client/spec/tableoptions"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/transformers"
+	cqtypes "github.com/cloudquery/plugin-sdk/v4/types"
+	"github.com/mitchellh/hashstructure/v2"
 )
 
 type metricOutput struct {
--- plugins/source/aws/resources/services/elasticache/clusters.go
+++ plugins/source/aws/resources/services/elasticache/clusters.go
@@ -3,8 +3,6 @@ package elasticache
 import (
 	"context"
 
-	sdkTypes "github.com/cloudquery/plugin-sdk/v4/types"
-
 	"github.com/apache/arrow/go/v14/arrow"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/elasticache"
@@ -12,6 +10,7 @@ import (
 	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/transformers"
+	sdkTypes "github.com/cloudquery/plugin-sdk/v4/types"
 )
 
 func Clusters() *schema.Table {
