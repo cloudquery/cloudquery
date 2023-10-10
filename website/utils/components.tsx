c6c985d5416c653d87b7d7f2464cf2ca6fb7cf51
erezrokah <erezrokah@users.noreply.github.com>
2023-10-10 12:37:34 +0200

    feat: Move Alicloud plugin docs to Markdown

---
+++ website/utils/components.tsx
@@ -0,0 +1,7 @@
+import AlicloudConfiguration from "../pages/docs/plugins/sources/alicloud/_configuration.mdx";
+import AlicloudAuthentication from "../pages/docs/plugins/sources/alicloud/_authentication.mdx";
+
+export const components = {
+  "sources-alicloud-configuration": <AlicloudConfiguration />,
+  "sources-alicloud-authentication": <AlicloudAuthentication />,
+};
