9d183c7e3a2874fac02cecf1d1b152e48d9042df
bbernays <ben@bernays.me>
2023-10-10 14:30:28 -0500

    Update deletestale.go

--- plugins/destination/postgresql/client/deletestale.go
+++ plugins/destination/postgresql/client/deletestale.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/apache/arrow/go/v14/arrow"
 	"github.com/cloudquery/plugin-sdk/v4/message"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/jackc/pgx/v5"
@@ -30,3 +31,118 @@ func (c *Client) DeleteStaleBatch(ctx context.Context, messages message.WriteDel
 	}
 	return nil
 }
+
+func (c *Client) DeleteRecordsBatch(ctx context.Context, messages message.WriteDeleteRecords) error {
+	batch := &pgx.Batch{}
+	for _, msg := range messages {
+		sql := generateDeleteCTE(msg.DeleteRecord)
+		vals := extractDeleteKeyValues(msg.DeleteKeys)
+		batch.Queue(sql, vals...)
+	}
+	br := c.conn.SendBatch(ctx, batch)
+	if err := br.Close(); err != nil {
+		return fmt.Errorf("failed to execute batch: %w", err)
+	}
+	return nil
+}
+
+func generateInitialDelete(tableName string, deleteKeys map[string]arrow.Record) string {
+	var sb strings.Builder
+	sb.WriteString("DELETE from ")
+	sb.WriteString(pgx.Identifier{tableName}.Sanitize())
+	sb.WriteString(" where ")
+
+	counter := 1
+	for key := range deleteKeys {
+		if counter > 1 {
+			sb.WriteString(" AND ")
+		}
+		sb.WriteString(pgx.Identifier{key}.Sanitize())
+		sb.WriteString(fmt.Sprintf(" = $%d", counter))
+		counter++
+	}
+
+	sb.WriteString(" RETURNING ")
+	// TODO: This column is not guaranteed to exist
+	sb.WriteString(pgx.Identifier{schema.CqIDColumn.Name}.Sanitize())
+	return sb.String()
+}
+
+func generateRelationsDelete(tableRelation message.TableRelation) string {
+	var sb strings.Builder
+	sb.WriteString("DELETE from ")
+	sb.WriteString(pgx.Identifier{tableRelation.TableName}.Sanitize())
+	sb.WriteString(" where ")
+	sb.WriteString(pgx.Identifier{schema.CqParentIDColumn.Name}.Sanitize())
+	sb.WriteString(" in (select ")
+	sb.WriteString(pgx.Identifier{schema.CqIDColumn.Name}.Sanitize())
+	sb.WriteString(" from ")
+	sb.WriteString(pgx.Identifier{tableRelation.ParentTable + "_CTE"}.Sanitize())
+	sb.WriteString(")")
+	return sb.String()
+}
+
+func generateDeleteCTE(deleteRecord message.DeleteRecord) string {
+	tables := make([]string, len(deleteRecord.TableRelations))
+	initialDelete := generateInitialDelete(deleteRecord.TableName, deleteRecord.DeleteKeys)
+	var sb strings.Builder
+	sb.WriteString("WITH ")
+	sb.WriteString(pgx.Identifier{deleteRecord.TableName + "_CTE"}.Sanitize())
+	sb.WriteString(" AS (")
+	sb.WriteString(initialDelete)
+	sb.WriteString(") ")
+
+	for i, tableRelation := range deleteRecord.TableRelations {
+		sb.WriteString(", ")
+		sb.WriteString(pgx.Identifier{tableRelation.TableName + "_CTE"}.Sanitize())
+		sb.WriteString(" AS (")
+		sb.WriteString(generateRelationsDelete(tableRelation))
+		sb.WriteString(") RETURNING ")
+		sb.WriteString(pgx.Identifier{schema.CqIDColumn.Name}.Sanitize())
+		tables[i] = tableRelation.TableName
+	}
+	for _, table := range tables {
+		sb.WriteString("Select count(*) from ")
+		sb.WriteString(pgx.Identifier{table + "_CTE"}.Sanitize())
+		sb.WriteString(" UNION ALL ")
+	}
+	sb.WriteString("Select count(*) from ")
+	sb.WriteString(pgx.Identifier{deleteRecord.TableName + "_CTE"}.Sanitize())
+
+	return sb.String()
+}
+
+func extractDeleteKeyValues(deleteKeyMap map[string]arrow.Record) []any {
+
+	results := make([]any, len(deleteKeyMap))
+	keys := make([]string, len(deleteKeyMap))
+
+	i := 0
+	for k := range deleteKeyMap {
+		keys[i] = k
+		i++
+	}
+
+	for i, record := range keys {
+		col := deleteKeyMap[record].Column(0)
+		transformed := transformArr(col)
+		results[i] = transformed[0]
+
+	}
+	return results
+}
+
+// func createDeleteCTE(message message.WriteDeleteRecord) string {
+
+// 	var sb strings.Builder
+// 	sb.WriteString("with deleted as (")
+// 	sb.WriteString("delete from ")
+// 	sb.WriteString(pgx.Identifier{message.TableName}.Sanitize())
+// 	sb.WriteString(" where ")
+// 	sb.WriteString(schema.CqSourceNameColumn.Name)
+// 	sb.WriteString(" = $1 and ")
+// 	sb.WriteString(schema.CqSyncTimeColumn.Name)
+// 	sb.WriteString(" < $2")
+// 	sb.WriteString(" returning *)")
+// 	return sb.String()
+// }
