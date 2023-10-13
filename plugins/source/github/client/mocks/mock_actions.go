ff1202811ca39b1549f35dcdca27d5b1fc871cd1
Jon Jozwiak <jonjozwiak@users.noreply.github.com>
2023-10-13 10:28:53 -0500

    feat: (github) Adding Actions workflowRuns, RunUsage, and Jobs

--- plugins/source/github/client/mocks/mock_actions.go
+++ plugins/source/github/client/mocks/mock_actions.go
@@ -35,6 +35,54 @@ func (m *MockActionsService) EXPECT() *MockActionsServiceMockRecorder {
 	return m.recorder
 }
 
+// GetWorkflowRunUsageByID mocks base method.
+func (m *MockActionsService) GetWorkflowRunUsageByID(arg0 context.Context, arg1, arg2 string, arg3 int64) (*github.WorkflowRunUsage, *github.Response, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetWorkflowRunUsageByID", arg0, arg1, arg2, arg3)
+	ret0, _ := ret[0].(*github.WorkflowRunUsage)
+	ret1, _ := ret[1].(*github.Response)
+	ret2, _ := ret[2].(error)
+	return ret0, ret1, ret2
+}
+
+// GetWorkflowRunUsageByID indicates an expected call of GetWorkflowRunUsageByID.
+func (mr *MockActionsServiceMockRecorder) GetWorkflowRunUsageByID(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetWorkflowRunUsageByID", reflect.TypeOf((*MockActionsService)(nil).GetWorkflowRunUsageByID), arg0, arg1, arg2, arg3)
+}
+
+// ListRepositoryWorkflowRuns mocks base method.
+func (m *MockActionsService) ListRepositoryWorkflowRuns(arg0 context.Context, arg1, arg2 string, arg3 *github.ListWorkflowRunsOptions) (*github.WorkflowRuns, *github.Response, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "ListRepositoryWorkflowRuns", arg0, arg1, arg2, arg3)
+	ret0, _ := ret[0].(*github.WorkflowRuns)
+	ret1, _ := ret[1].(*github.Response)
+	ret2, _ := ret[2].(error)
+	return ret0, ret1, ret2
+}
+
+// ListRepositoryWorkflowRuns indicates an expected call of ListRepositoryWorkflowRuns.
+func (mr *MockActionsServiceMockRecorder) ListRepositoryWorkflowRuns(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ListRepositoryWorkflowRuns", reflect.TypeOf((*MockActionsService)(nil).ListRepositoryWorkflowRuns), arg0, arg1, arg2, arg3)
+}
+
+// ListWorkflowJobs mocks base method.
+func (m *MockActionsService) ListWorkflowJobs(arg0 context.Context, arg1, arg2 string, arg3 int64, arg4 *github.ListWorkflowJobsOptions) (*github.Jobs, *github.Response, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "ListWorkflowJobs", arg0, arg1, arg2, arg3, arg4)
+	ret0, _ := ret[0].(*github.Jobs)
+	ret1, _ := ret[1].(*github.Response)
+	ret2, _ := ret[2].(error)
+	return ret0, ret1, ret2
+}
+
+// ListWorkflowJobs indicates an expected call of ListWorkflowJobs.
+func (mr *MockActionsServiceMockRecorder) ListWorkflowJobs(arg0, arg1, arg2, arg3, arg4 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ListWorkflowJobs", reflect.TypeOf((*MockActionsService)(nil).ListWorkflowJobs), arg0, arg1, arg2, arg3, arg4)
+}
+
 // ListWorkflows mocks base method.
 func (m *MockActionsService) ListWorkflows(arg0 context.Context, arg1, arg2 string, arg3 *github.ListOptions) (*github.Workflows, *github.Response, error) {
 	m.ctrl.T.Helper()
---
+++ plugins/source/github/resources/services/actions/workflowRunUsage.go
@@ -0,0 +1,44 @@
+package actions
+
+import (
+	"context"
+
+	"github.com/apache/arrow/go/v14/arrow"
+	"github.com/cloudquery/cloudquery/plugins/source/github/client"
+	"github.com/cloudquery/plugin-sdk/v4/schema"
+	"github.com/google/go-github/v49/github"
+)
+
+func WorkflowRunUsage() *schema.Table {
+	return &schema.Table{
+		Name:      "github_workflow_run_usage",
+		Resolver:  fetchWorkflowRunUsage,
+		Transform: client.TransformWithStruct(&github.WorkflowRunUsage{}),
+		Columns: []schema.Column{
+			client.OrgColumn,
+			client.RepositoryIDColumn,
+			{
+				Name:        "run_id",
+				Type:        arrow.PrimitiveTypes.Int64,
+				Resolver:    schema.ParentColumnResolver("id"),
+				Description: `Run ID`,
+				PrimaryKey:  true,
+			},
+		},
+	}
+}
+
+func fetchWorkflowRunUsage(ctx context.Context, meta schema.ClientMeta, resource *schema.Resource, res chan<- any) error {
+	c := meta.(*client.Client)
+	repo := c.Repository
+	run := resource.Item.(*github.WorkflowRun)
+
+	workflowRunUsage, _, err := c.Github.Actions.GetWorkflowRunUsageByID(ctx, c.Org, *repo.Name, *run.ID)
+	if err != nil {
+		return err
+	}
+
+	res <- workflowRunUsage
+
+	return nil
+}
