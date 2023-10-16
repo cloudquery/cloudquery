a336072ddb53ddf806c8732c02c654e9804c176a
erezrokah <erezrokah@users.noreply.github.com>
2023-10-16 16:46:45 +0200

    chore: Add component

--- website/utils/components.tsx
+++ website/utils/components.tsx
@@ -3,9 +3,15 @@ import AlicloudAuthentication from "../pages/docs/plugins/sources/alicloud/_auth
 import AWSConfiguration from "../pages/docs/plugins/sources/aws/_configuration.md";
 import AWSAuthentication from "../pages/docs/plugins/sources/aws/_authentication.md";
 
+import AZBlobAuthentication from "../pages/docs/plugins/destinations/azblob/_authentication.md";
+import AZBlobConfiguration from "../pages/docs/plugins/destinations/azblob/_configuration.md";
+
 export const components = {
   "sources-alicloud-configuration": <AlicloudConfiguration />,
   "sources-alicloud-authentication": <AlicloudAuthentication />,
   "sources-aws-configuration": <AWSConfiguration />,
   "sources-aws-authentication": <AWSAuthentication />,
+
+  "destinations-azblob-authentication": <AZBlobAuthentication />,
+  "destinations-azblob-configuration": <AZBlobConfiguration />,
 };
