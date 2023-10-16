4550ed6438f070ee70ee89e2dd6be2916b2661f4
erezrokah <erezrokah@users.noreply.github.com>
2023-10-16 17:27:59 +0200

    chore: Migrate kafka destination docs to MD

--- website/utils/components.tsx
+++ website/utils/components.tsx
@@ -3,9 +3,13 @@ import AlicloudAuthentication from "../pages/docs/plugins/sources/alicloud/_auth
 import AWSConfiguration from "../pages/docs/plugins/sources/aws/_configuration.md";
 import AWSAuthentication from "../pages/docs/plugins/sources/aws/_authentication.md";
 
+import KafkaConfiguration from "../pages/docs/plugins/destinations/kafka/_configuration.md";
+
 export const components = {
   "sources-alicloud-configuration": <AlicloudConfiguration />,
   "sources-alicloud-authentication": <AlicloudAuthentication />,
   "sources-aws-configuration": <AWSConfiguration />,
   "sources-aws-authentication": <AWSAuthentication />,
+
+  "destinations-kafka-configuration": <KafkaConfiguration />,
 };
