3e1fdeb9913b32b6179e56d221e7513f9da837fe
erezrokah <erezrokah@users.noreply.github.com>
2023-10-16 17:00:09 +0200

    chore: Migrate elasticsearch destination docs to MD

--- website/utils/components.tsx
+++ website/utils/components.tsx
@@ -43,11 +43,11 @@ import BigQueryAuthentication from "../pages/docs/plugins/destinations/bigquery/
 import BigQueryConfiguration from "../pages/docs/plugins/destinations/bigquery/_configuration.md";
 import ClickHouseConfiguration from "../pages/docs/plugins/destinations/clickhouse/_configuration.md";
 import DuckDBConfiguration from "../pages/docs/plugins/destinations/duckdb/_configuration.md";
+import ElasticSearchConfiguration from "../pages/docs/plugins/destinations/elasticsearch/_configuration.md";
 import GremlinConfiguration from "../pages/docs/plugins/destinations/gremlin/_configuration.md";
 import PostgresDestConfiguration from "../pages/docs/plugins/destinations/postgresql/_configuration.md";
 import SnowflakeConfiguration from "../pages/docs/plugins/destinations/snowflake/_configuration.md";
 import SQLiteConfiguration from "../pages/docs/plugins/destinations/sqlite/_configuration.md";
-
 
 export const components = {
   "sources-airtable-authentication": <AirtableAuthentication />,
@@ -94,6 +94,7 @@ export const components = {
   "destinations-bigquery-configuration": <BigQueryConfiguration />,
   "destinations-clickhouse-configuration": <ClickHouseConfiguration />,
   "destinations-duckdb-configuration": <DuckDBConfiguration />,
+  "destinations-elasticsearch-configuration": <ElasticSearchConfiguration />,
   "destinations-gremlin-configuration": <GremlinConfiguration />,
   "destinations-postgresql-configuration": <PostgresDestConfiguration />,
   "destinations-snowflake-configuration": <SnowflakeConfiguration />,
