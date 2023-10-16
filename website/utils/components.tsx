b79940f7b4de03c09ae2b3d9e004a274206ed0d0
erezrokah <erezrokah@users.noreply.github.com>
2023-10-16 16:47:37 +0200

    chore: Add component

--- website/utils/components.tsx
+++ website/utils/components.tsx
@@ -3,9 +3,15 @@ import AlicloudAuthentication from "../pages/docs/plugins/sources/alicloud/_auth
 import AWSConfiguration from "../pages/docs/plugins/sources/aws/_configuration.md";
 import AWSAuthentication from "../pages/docs/plugins/sources/aws/_authentication.md";
 
+import BigQueryAuthentication from "../pages/docs/plugins/destinations/bigquery/_authentication.md";
+import BigQueryConfiguration from "../pages/docs/plugins/destinations/bigquery/_configuration.md";
+
 export const components = {
   "sources-alicloud-configuration": <AlicloudConfiguration />,
   "sources-alicloud-authentication": <AlicloudAuthentication />,
   "sources-aws-configuration": <AWSConfiguration />,
   "sources-aws-authentication": <AWSAuthentication />,
+
+  "destinations-bigquery-authentication": <BigQueryAuthentication />,
+  "destinations-bigquery-configuration": <BigQueryConfiguration />,
 };
