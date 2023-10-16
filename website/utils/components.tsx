fb2e2078d9f36d285b75e599ab98c03e4216cd6b
erezrokah <erezrokah@users.noreply.github.com>
2023-10-16 17:00:09 +0200

    chore: Migrate elasticsearch destination docs to MD

--- website/utils/components.tsx
+++ website/utils/components.tsx
@@ -3,9 +3,13 @@ import AlicloudAuthentication from "../pages/docs/plugins/sources/alicloud/_auth
 import AWSConfiguration from "../pages/docs/plugins/sources/aws/_configuration.md";
 import AWSAuthentication from "../pages/docs/plugins/sources/aws/_authentication.md";
 
+import ElasticSearchConfiguration from "../pages/docs/plugins/destinations/elasticsearch/_configuration.md";
+
 export const components = {
   "sources-alicloud-configuration": <AlicloudConfiguration />,
   "sources-alicloud-authentication": <AlicloudAuthentication />,
   "sources-aws-configuration": <AWSConfiguration />,
   "sources-aws-authentication": <AWSAuthentication />,
+
+  "destinations-elasticsearch-configuration": <ElasticSearchConfiguration />,
 };
