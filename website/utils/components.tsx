1cec118894e1d5982edb9becbde9aaa346550ed0
erezrokah <erezrokah@users.noreply.github.com>
2023-10-16 17:15:31 +0200

    chore: Migrate firehose destination docs to MD

--- website/utils/components.tsx
+++ website/utils/components.tsx
@@ -3,9 +3,15 @@ import AlicloudAuthentication from "../pages/docs/plugins/sources/alicloud/_auth
 import AWSConfiguration from "../pages/docs/plugins/sources/aws/_configuration.md";
 import AWSAuthentication from "../pages/docs/plugins/sources/aws/_authentication.md";
 
+import FirehoseConfiguration from "../pages/docs/plugins/destinations/firehose/_configuration.md";
+import FirehoseAuthentication from "../pages/docs/plugins/destinations/firehose/_authentication.md";
+
 export const components = {
   "sources-alicloud-configuration": <AlicloudConfiguration />,
   "sources-alicloud-authentication": <AlicloudAuthentication />,
   "sources-aws-configuration": <AWSConfiguration />,
   "sources-aws-authentication": <AWSAuthentication />,
+
+  "destinations-firehose-configuration": <FirehoseConfiguration />,
+  "destinations-firehose-authentication": <FirehoseAuthentication />,
 };
