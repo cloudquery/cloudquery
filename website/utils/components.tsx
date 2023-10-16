f7295f9fd587e55cf9863dd7209652f44260f360
erezrokah <erezrokah@users.noreply.github.com>
2023-10-16 16:51:31 +0200

    chore: Migrate duckdb destination docs to MD

--- website/utils/components.tsx
+++ website/utils/components.tsx
@@ -3,9 +3,12 @@ import AlicloudAuthentication from "../pages/docs/plugins/sources/alicloud/_auth
 import AWSConfiguration from "../pages/docs/plugins/sources/aws/_configuration.md";
 import AWSAuthentication from "../pages/docs/plugins/sources/aws/_authentication.md";
 
+import DuckDBConfiguration from "../pages/docs/plugins/destinations/duckdb/_configuration.md";
+
 export const components = {
   "sources-alicloud-configuration": <AlicloudConfiguration />,
   "sources-alicloud-authentication": <AlicloudAuthentication />,
   "sources-aws-configuration": <AWSConfiguration />,
   "sources-aws-authentication": <AWSAuthentication />,
+  "destinations-duckdb-configuration": <DuckDBConfiguration />,
 };
