e0f9d5938ee0d607d74690dad8f4d854f8874b97
Erez Rokah <erezrokah@users.noreply.github.com>
2023-10-16 17:23:13 +0200

    chore: Migrate postgres destination docs to MD (#14559)
    #### Summary
    
    Part of https://github.com/cloudquery/cloud/issues/457 (internal issue)
    
    <!--

--- website/utils/components.tsx
+++ website/utils/components.tsx
@@ -2,10 +2,13 @@ import AlicloudConfiguration from "../pages/docs/plugins/sources/alicloud/_confi
 import AlicloudAuthentication from "../pages/docs/plugins/sources/alicloud/_authentication.md";
 import AWSConfiguration from "../pages/docs/plugins/sources/aws/_configuration.md";
 import AWSAuthentication from "../pages/docs/plugins/sources/aws/_authentication.md";
+import PostgresDestConfiguration from "../pages/docs/plugins/destinations/postgresql/_configuration.md";
 
 export const components = {
   "sources-alicloud-configuration": <AlicloudConfiguration />,
   "sources-alicloud-authentication": <AlicloudAuthentication />,
   "sources-aws-configuration": <AWSConfiguration />,
   "sources-aws-authentication": <AWSAuthentication />,
+
+  "destinations-postgresql-configuration": <PostgresDestConfiguration />,
 };
