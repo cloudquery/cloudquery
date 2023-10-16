0f683e03cd08452d387db544e075b8f364e58140
candiduslynx <candiduslynx@gmail.com>
2023-10-16 16:33:49 +0300

    gen

--- plugins/source/azure/client/spec/spec.go
+++ plugins/source/azure/client/spec/spec.go
@@ -1,24 +1,21 @@
 package spec
 
-const (
-	defaultConcurrency = 50000
-)
-
 type Spec struct {
-	Subscriptions        []string `json:"subscriptions"`
+	Subscriptions        []string `json:"subscriptions" jsonschema:"minLength=1,uniqueItems=true"`
 	CloudName            string   `json:"cloud_name"`
-	DiscoveryConcurrency int      `json:"discovery_concurrency"`
-	SkipSubscriptions    []string `json:"skip_subscriptions"`
+	SkipSubscriptions    []string `json:"skip_subscriptions" jsonschema:"minLength=1,uniqueItems=true"`
 	NormalizeIDs         bool     `json:"normalize_ids"`
-	Concurrency          int      `json:"concurrency"`
 	OIDCToken            string   `json:"oidc_token"`
+	DiscoveryConcurrency int      `json:"discovery_concurrency" jsonschema:"minimum=1,default=400"`
+	Concurrency          int      `json:"concurrency" jsonschema:"minimum=1,default=50000"`
 }
 
 func (s *Spec) SetDefaults() {
 	if s.DiscoveryConcurrency <= 0 {
 		s.DiscoveryConcurrency = 400
 	}
-	if s.Concurrency == 0 {
+	if s.Concurrency <= 0 {
+		const defaultConcurrency = 50000
 		s.Concurrency = defaultConcurrency
 	}
 }
