62640855175e598e3bc2053ab7045af91d5ab666
erezrokah <erezrokah@users.noreply.github.com>
2023-10-16 17:19:06 +0200

    chore: Migrate gcs destination docs to MD

--- website/utils/components.tsx
+++ website/utils/components.tsx
@@ -3,9 +3,15 @@ import AlicloudAuthentication from "../pages/docs/plugins/sources/alicloud/_auth
 import AWSConfiguration from "../pages/docs/plugins/sources/aws/_configuration.md";
 import AWSAuthentication from "../pages/docs/plugins/sources/aws/_authentication.md";
 
+import GCSAuthentication from "../pages/docs/plugins/destinations/gcs/_authentication.md";
+import GCSConfiguration from "../pages/docs/plugins/destinations/gcs/_configuration.md";
+
 export const components = {
   "sources-alicloud-configuration": <AlicloudConfiguration />,
   "sources-alicloud-authentication": <AlicloudAuthentication />,
   "sources-aws-configuration": <AWSConfiguration />,
   "sources-aws-authentication": <AWSAuthentication />,
+
+  "destinations-gcs-authentication": <GCSAuthentication />,
+  "destinations-gcs-configuration": <GCSConfiguration />,
 };
