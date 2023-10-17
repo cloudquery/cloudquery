c02b1cd47fe60b8cfda383c7f8591664ce252f71
Kemal Hadimli <disq@users.noreply.github.com>
2023-10-17 18:09:46 +0100

    Remove newlines from addon testdata

--- cli/cmd/addon_publish_test.go
+++ cli/cmd/addon_publish_test.go
@@ -147,8 +147,8 @@ func checkCreateAddonVersionRequest(t *testing.T, r *http.Request) {
 	want := map[string]any{
 		"addon_deps":  nil,
 		"checksum":    "b537240431bb4868264e48a8c646ebd3a9e355140d27d7fe559b5cbfd3ce6f31",
-		"doc":         "# Test Addon README\n",
-		"message":     "# Test Addon Changelog\n",
+		"doc":         "# Test Addon README",
+		"message":     "# Test Addon Changelog",
 		"plugin_deps": []any{"cloudquery/source/test@v1.0.0"},
 	}
 	if diff := cmp.Diff(want, got); diff != "" {
