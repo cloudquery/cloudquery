d491d80e6f5e9e856572f0ea76834feabd6e7dd4
Erez Rokah <erezrokah@users.noreply.github.com>
2023-10-17 11:03:55 +0200

    chore: Migrate awspricing source docs to MD (#14614)
    <!-- 🎉 Thank you for making CloudQuery awesome by submitting a PR 🎉 -->
    
    #### Summary
    
    Part of https://github.com/cloudquery/cloudquery-issues/issues/620
    (internal issue)
    
    <!--
    Use the following steps to ensure your PR is ready to be reviewed
    
    - [ ] Read the [contribution guidelines](../blob/main/CONTRIBUTING.md)
    🧑‍🎓
    - [ ] Run `make lint` to ensure the proposed changes follow the coding
    style 🚨 (install golangci-lint
    [here](https://golangci-lint.run/usage/install/#local-installation))
    - [ ] Run `make test` to ensure the proposed changes pass the tests 🧪
    - [ ] If changing a source plugin run `make gen` to ensure docs are up
    to date 📝
    - [ ] Ensure the status checks below are successful ✅
    --->

--- website/utils/components.tsx
+++ website/utils/components.tsx
@@ -2,6 +2,7 @@ import AlicloudConfiguration from "../pages/docs/plugins/sources/alicloud/_confi
 import AlicloudAuthentication from "../pages/docs/plugins/sources/alicloud/_authentication.md";
 import AWSConfiguration from "../pages/docs/plugins/sources/aws/_configuration.md";
 import AWSAuthentication from "../pages/docs/plugins/sources/aws/_authentication.md";
+import AWSPricingConfiguration from "../pages/docs/plugins/sources/awspricing/_configuration.md";
 
 import AZBlobAuthentication from "../pages/docs/plugins/destinations/azblob/_authentication.md";
 import AZBlobConfiguration from "../pages/docs/plugins/destinations/azblob/_configuration.md";
@@ -16,6 +17,7 @@ export const components = {
   "sources-alicloud-authentication": <AlicloudAuthentication />,
   "sources-aws-configuration": <AWSConfiguration />,
   "sources-aws-authentication": <AWSAuthentication />,
+  "sources-awspricing-configuration": <AWSPricingConfiguration />,
   "destinations-azblob-authentication": <AZBlobAuthentication />,
   "destinations-azblob-configuration": <AZBlobConfiguration />,
   "destinations-duckdb-configuration": <DuckDBConfiguration />,
