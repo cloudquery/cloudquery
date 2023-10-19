680ae64b14221553720ea946bd197ae7cb386c80
Herman Schaaf <hermanschaaf@gmail.com>
2023-10-19 21:21:37 +0100

    Try some more

---
+++ scripts/workflows/get_last_successful_commit_sha.js
@@ -0,0 +1,20 @@
+module.exports = async ({github, context}) => {
+  const allCommits = context.github.event.pull_request.commits;
+  // loop from second-to-last commit to first
+  for (const commit of allCommits.reverse().slice(1)) {
+    const checkRuns = await github.paginate(github.rest.checks.listForRef, {
+      owner: 'cloudquery',
+      repo: context.repo.repo,
+      ref: commit.sha,
+      status: 'completed',
+      per_page: 100
+    })
+    for (const checkRun of checkRuns) {
+      if (checkRun.conclusion === 'success') {
+        return checkRun.head_sha
+      }
+    }
+  }
+  // by default return base sha
+  return github.event.pull_request.base.sha
+}
---
+++ scripts/workflows/have_files_matching_workflow_changed.js
@@ -0,0 +1,28 @@
+module.exports = async ({github, context}) => {
+  const files = process.env.FILES.split(' ')
+  console.log(files)
+  if (files.length >= 300) {
+    // This is a GitHub limitation https://github.com/cloudquery/cloudquery/issues/2688
+    throw new Error('Too many files changed. Skipping check. Please split your PR into multiple ones.')
+  }
+
+  const matchesWorkflow = (file, action) => {
+    if (!file.startsWith('.github/workflows/')) {
+      return false
+    }
+    try {
+      const contents = fs.readFileSync(file, 'utf8');
+      return contents.includes(`name: "${action}"`)
+    } catch {
+      return false
+    }
+  }
+  const matchesFile = (action) => {
+    return files.some(file => file.startsWith(`${action}/`) || matchesWorkflow(file, action))
+  }
+
+  if (matchesFile(github.action)) {
+    return true
+  }
+  return false
+}
