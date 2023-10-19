d553c04e69434e382a369d2958990613c40ca452
erezrokah <erezrokah@users.noreply.github.com>
2023-10-19 10:41:06 +0200

    chore: Update wait-for-all script

--- scripts/workflows/wait_for_required_workflows.js
+++ scripts/workflows/wait_for_required_workflows.js
@@ -41,7 +41,7 @@ module.exports = async ({github, context}) => {
     // We test the CLI on multiple OSes, so we need to wait for all of them
     if (actions.includes("cli")) {
         actions = actions.filter(action => action !== "cli")
-        actions = ["cli (large-ubuntu-monorepo)", "cli (large-windows-monorepo)", "cli (macos-latest-xl)", ...actions]
+        actions = ["cli (ubuntu-latest)", "cli (windows-latest)", "cli (macos-latest)", ...actions]
     }
 
     // Enforce policy tests for AWS, Azure, GCP and K8s plugins
