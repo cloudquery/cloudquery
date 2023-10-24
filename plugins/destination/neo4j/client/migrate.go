b71bd11dd363933f0df59cad1505406b3330f508
candiduslynx <candiduslynx@gmail.com>
2023-10-24 19:16:41 +0300

    Revert "try constraints"
    This reverts commit a45da8c7ed6d95653e237ba4350e55630cf50002.

--- plugins/destination/neo4j/client/migrate.go
+++ plugins/destination/neo4j/client/migrate.go
@@ -10,10 +10,10 @@ import (
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
-// MigrateTables tries to create constraints & indexes for the tables.
-// It will issue `CREATE CONSTRAINT ... IF NOT EXIST`.
+// MigrateTables tries to create indexes for the tables.
+// It will issue `CREATE RANGE INDEX ... IF NOT EXIST`.
 // If the error occurs & indicates that the issue is caused by conflicting schema, 2 scenarios can happen:
-// 1. Force mode is selected for migration: drop & recreate constraint (without checking for error this time)
+// 1. Force mode is selected for migration: drop & recreate index (without checking for error this time)
 // 2. No forced migration is requested - return error.
 func (c *Client) MigrateTables(ctx context.Context, messages message.WriteMigrateTables) error {
 	if len(messages) == 0 {
@@ -24,22 +24,22 @@ func (c *Client) MigrateTables(ctx context.Context, messages message.WriteMigrat
 	defer sess.Close(ctx)
 
 	for _, m := range messages {
-		if err := c.tryCreateConstraint(ctx, sess, m); err != nil {
+		if err := c.tryCreateIndex(ctx, sess, m); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func (c *Client) tryCreateConstraint(ctx context.Context, sess neo4j.SessionWithContext, migrate *message.WriteMigrateTable) error {
-	createQuery := createConstraintQuery(migrate.Table)
+func (c *Client) tryCreateIndex(ctx context.Context, sess neo4j.SessionWithContext, migrate *message.WriteMigrateTable) error {
+	createQuery := createIndexQuery(migrate.Table)
 	if len(createQuery) == 0 {
 		c.logger.Debug().Str("table", migrate.Table.Name).Msg("table has no primary keys, skipping")
 		// nothing to be done here, table has no primary keys
 		return nil
 	}
 
-	constraint := constraintName(migrate.Table)
+	index := indexName(migrate.Table)
 
 	_, err := sess.Run(ctx, createQuery, map[string]any{})
 	if err == nil {
@@ -49,31 +49,31 @@ func (c *Client) tryCreateConstraint(ctx context.Context, sess neo4j.SessionWith
 	if !migrate.MigrateForce {
 		c.logger.Err(err).
 			Str("table", migrate.Table.Name).
-			Str("constraint", constraint).
-			Msg("failed to create constraint")
-		return fmt.Errorf("failed to create constraint for %q: %w", migrate.Table.Name, err)
+			Str("index", index).
+			Msg("failed to create index")
+		return fmt.Errorf("failed to create index for %q: %w", migrate.Table.Name, err)
 	}
 
 	c.logger.Warn().
 		Str("table", migrate.Table.Name).
-		Str("constraint", constraint).
+		Str("index", index).
 		Err(err).
-		Msg("failed to create constraint, recreating")
+		Msg("failed to create index, recreating")
 
-	_, err = sess.Run(ctx, `DROP CONSTRAINT `+constraint+` IF EXISTS;`, map[string]any{})
+	_, err = sess.Run(ctx, `DROP INDEX `+index+` IF EXISTS;`, map[string]any{})
 	if err != nil {
 		c.logger.Err(err).
 			Str("table", migrate.Table.Name).
-			Str("constraint", constraint).
-			Msg("failed to drop constraint")
-		return fmt.Errorf("failed to drop constraint for %q: %w", migrate.Table.Name, err)
+			Str("index", index).
+			Msg("failed to drop index")
+		return fmt.Errorf("failed to drop index for %q: %w", migrate.Table.Name, err)
 	}
 
 	_, err = sess.Run(ctx, createQuery, map[string]any{})
 	return err
 }
 
-func createConstraintQuery(table *schema.Table) string {
+func createIndexQuery(table *schema.Table) string {
 	pks := table.PrimaryKeys()
 	if len(pks) == 0 {
 		// empty query = no need to do anything
@@ -81,11 +81,11 @@ func createConstraintQuery(table *schema.Table) string {
 	}
 
 	var sb strings.Builder
-	sb.WriteString(`CREATE CONSTRAINT `)
-	sb.WriteString(constraintName(table))
+	sb.WriteString(`CREATE INDEX `)
+	sb.WriteString(indexName(table))
 	sb.WriteString(` IF NOT EXISTS FOR (n:`)
 	sb.WriteString(table.Name)
-	sb.WriteString(`) REQUIRE (`)
+	sb.WriteString(`) ON (`)
 	for i, name := range pks {
 		if i != 0 {
 			sb.WriteString(`, `)
@@ -93,11 +93,11 @@ func createConstraintQuery(table *schema.Table) string {
 		sb.WriteString(`n.`)
 		sb.WriteString(name)
 	}
-	sb.WriteString(`) IS UNIQUE;`)
+	sb.WriteString(`);`)
 
 	return sb.String()
 }
 
-func constraintName(table *schema.Table) string {
-	return `_cq_constraint_` + table.Name
+func indexName(table *schema.Table) string {
+	return `_cq_index_` + table.Name
 }
