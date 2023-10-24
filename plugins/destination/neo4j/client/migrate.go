35b8a8492d6259657d3884dc2c97dc929e27b30d
candiduslynx <candiduslynx@gmail.com>
2023-10-24 14:40:23 +0300

    doc

--- plugins/destination/neo4j/client/migrate.go
+++ plugins/destination/neo4j/client/migrate.go
@@ -6,7 +6,11 @@ import (
 	"github.com/cloudquery/plugin-sdk/v4/message"
 )
 
-// Migrate tables. Like mongo, neo4j does not have a schema, so this is a no-op.
-func (*Client) MigrateTables(context.Context, message.WriteMigrateTables) error {
+// MigrateTables tries to create indexes for the tables.
+// It will issue `CREATE RANGE INDEX ... IF NOT EXIST`.
+// If the error occurs & indicates that the issue is caused by conflicting schema, 2 scenarios can happen:
+// 1. Force mode is selected for migration: drop & recreate index (without checking for error this time)
+// 2. No forced migration is requested - return error.
+func (c *Client) MigrateTables(ctx context.Context, migrate message.WriteMigrateTables) error {
 	return nil
 }
