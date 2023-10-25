e2eb37099f3ea2c9e2da39ec58102feadf111119
spathak <spathak@infoblox.com>
2023-10-25 17:06:11 +0530

    add uts

---
+++ plugins/source/gcp/resources/services/networkconnectivity/locations_test.go
@@ -0,0 +1,51 @@
+package networkconnectivity
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"google.golang.org/api/networkconnectivity/v1"
+
+	"github.com/cloudquery/cloudquery/plugins/source/gcp/client"
+	"github.com/cloudquery/plugin-sdk/v4/faker"
+)
+
+func createLocation(mux *httprouter.Router) error {
+	var item networkconnectivity.ListLocationsResponse
+	if err := faker.FakeObject(&item); err != nil {
+		return err
+	}
+	item.Locations = []*networkconnectivity.Location{
+		{
+			DisplayName: "test-display-name",
+			LocationId:  "global",
+			Metadata:    []byte(`{"@type":"type.googleapis.com/google.cloud.networkconnectivity.v1.LocationMetadata","locationFeatures":["SITE_TO_CLOUD_SPOKES"]}`),
+		},
+	}
+	item.NextPageToken = ""
+	mux.GET("/v1/projects/testProject/locations", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		b, err := json.Marshal(&item)
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, "unable to marshal request: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		if _, err = w.Write(b); err != nil {
+			http.Error(w, "failed to write", http.StatusBadRequest)
+			return
+		}
+	})
+
+	if err := createInternalRanges(mux); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func TestLocations(t *testing.T) {
+	client.MockTestRestHelper(t, Locations(), createLocation, client.TestOptions{})
+}
