68fbc8e0d1333de66bd4bcdb8d2975a4317dd67e
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2023-10-26 17:38:17 +0300

    feat: Updated query s3_cross_region_replication.sql

--- plugins/source/aws/policies/queries/s3/s3_cross_region_replication.sql
+++ plugins/source/aws/policies/queries/s3/s3_cross_region_replication.sql
@@ -1,18 +1,40 @@
+WITH basic_table as (
+select 
+	arn,
+	region,
+	jsonb_array_elements(replication_rules) -> 'Destination' ->> 'Bucket' as dest,
+	jsonb_array_elements(replication_rules) ->> 'Status' as stat
+	from
+	aws_s3_buckets
+),
+sec as (
+	select bt.arn,
+		bt.region,
+		bt.dest,
+		asb.region as other,
+		bt.stat
+		from basic_table as bt
+		left join aws_s3_buckets as asb on dest = asb.arn
+)
 insert into aws_policy_results
-select
-    :'execution_time' as execution_time,
+SELECT
+	:'execution_time' as execution_time,
     :'framework' as framework,
     :'check_id' as check_id,
-    'S3 buckets with replication rules should be enabled' as title,
+    'S3 buckets should have cross-Region replication enabled' as title,
     aws_s3_buckets.account_id,
-    aws_s3_buckets.arn as resource_id,
-    case when
-        r->>'Status' is distinct from 'Enabled'
-    then 'fail' else 'pass' end as status
-from
-     aws_s3_buckets, JSONB_ARRAY_ELEMENTS(
-         case jsonb_typeof(replication_rules)
-         when 'array' then replication_rules
-         else '[]' end
-     ) as r
--- Note: This query doesn't validate that the destination bucket is actually in a different region
+    aws_s3_buckets.arn AS resource_id,
+    CASE
+        WHEN EXISTS (
+            SELECT 1
+            FROM sec
+            WHERE 
+			sec.arn = aws_s3_buckets.arn
+			and
+			stat = 'Enabled' and region != other
+        )
+        THEN 'pass'
+        ELSE 'fail'
+    END AS status
+FROM
+    aws_s3_buckets;
