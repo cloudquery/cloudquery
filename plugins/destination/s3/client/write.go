993f20cc61a058aad631d8bc3daec8f154fbd63b
bbernays <ben@bernays.me>
2023-11-01 17:09:29 -0500

    Update write.go

--- plugins/destination/s3/client/write.go
+++ plugins/destination/s3/client/write.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+	"encoding/json"
 	"io"
 	"path"
 	"reflect"
@@ -31,7 +32,7 @@ const (
 	MinuteVar     = "{{MINUTE}}"
 )
 
-var reInvalidJSONKey = regexp.MustCompile(`\W`)
+var reInvalidJSONKey = regexp.MustCompile("[^a-zA-Z0-9_.]+")
 
 func (c *Client) WriteTable(ctx context.Context, msgs <-chan *message.WriteInsert) error {
 	var s *filetypes.Stream
@@ -57,7 +58,12 @@ func (c *Client) WriteTable(ctx context.Context, msgs <-chan *message.WriteInser
 		}
 
 		if c.spec.Athena {
-			msg.Record = sanitizeRecordJSONKeys(msg.Record)
+			var err error
+			msg.Record, err = sanitizeRecordJSONKeys(msg.Record)
+			if err != nil {
+				_ = s.FinishWithError(err)
+				return err
+			}
 		}
 
 		if err := s.Write([]arrow.Record{msg.Record}); err != nil {
@@ -75,7 +81,7 @@ func (c *Client) Write(ctx context.Context, msgs <-chan message.WriteMessage) er
 
 // sanitizeRecordJSONKeys replaces all invalid characters in JSON keys with underscores. This is required
 // for compatibility with Athena.
-func sanitizeRecordJSONKeys(record arrow.Record) arrow.Record {
+func sanitizeRecordJSONKeys(record arrow.Record) (arrow.Record, error) {
 	cols := make([]arrow.Array, record.NumCols())
 	for i, col := range record.Columns() {
 		if arrow.TypeEqual(col.DataType(), types.NewJSONType()) {
@@ -85,27 +91,48 @@ func sanitizeRecordJSONKeys(record arrow.Record) arrow.Record {
 					b.AppendNull()
 					continue
 				}
-				obj := col.GetOneForMarshal(r)
-				sanitizeJSONKeysForObject(obj)
-				b.Append(obj)
+				bArray, err := sanitizeRawJsonMessage(col.GetOneForMarshal(r).(json.RawMessage))
+				if err != nil {
+					return nil, err
+				}
+				b.Append(bArray)
+
 			}
 			cols[i] = b.NewArray()
 			continue
 		}
 		cols[i] = col
 	}
-	return array.NewRecord(record.Schema(), cols, record.NumRows())
+	return array.NewRecord(record.Schema(), cols, record.NumRows()), nil
+}
+
+func sanitizeRawJsonMessage(rawMessage json.RawMessage) ([]byte, error) {
+
+	var objInterface any
+	err := json.Unmarshal(rawMessage, &objInterface)
+	if err != nil {
+		return nil, err
+	}
+	sanitizeJSONKeysForObject(objInterface)
+	cleanedObject, err := json.Marshal(&objInterface)
+	if err != nil {
+		return nil, err
+	}
+
+	return cleanedObject, nil
 }
 
 func sanitizeJSONKeysForObject(obj any) {
 	value := reflect.ValueOf(obj)
-	switch value.Kind() {
+	k := value.Kind()
+	switch k {
 	case reflect.Map:
 		iter := value.MapRange()
 		for iter.Next() {
 			k := iter.Key()
 			if k.Kind() == reflect.String {
-				nk := reInvalidJSONKey.ReplaceAllString(k.String(), "_")
+				str := k.String()
+				nk := reInvalidJSONKey.ReplaceAllString(str, "_")
 				v := iter.Value()
 				sanitizeJSONKeysForObject(v.Interface())
 				value.SetMapIndex(k, reflect.Value{})
@@ -114,7 +141,8 @@ func sanitizeJSONKeysForObject(obj any) {
 		}
 	case reflect.Slice:
 		for i := 0; i < value.Len(); i++ {
-			sanitizeJSONKeysForObject(value.Index(i).Interface())
+			key := value.Index(i).Interface()
+			sanitizeJSONKeysForObject(key)
 		}
 	}
 }
