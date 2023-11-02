799a9ecdc286bd6e83061e254c7d50e74d7674a1
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2023-11-02 10:59:19 -0400

    feat: Deprecate preview plugins and move into premium
    We are moving preview plugins to premium due to huge costs
    in development & support so we can continue maintaining them
    and keeping the quality high.

--- plugins/source/airtable/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createServeCommand } from '@cloudquery/plugin-sdk-javascript/plugin/serve';
-
-import { newAirtablePlugin } from './plugin.js';
-
-const main = () => {
-  createServeCommand(newAirtablePlugin()).parse();
-};
-
-main();
--- plugins/source/bitbucket/app/src/main/java/bitbucket/client/configuration/ConfigurationException.java
+++
@@ -1,7 +0,0 @@
-package bitbucket.client.configuration;
-
-public class ConfigurationException extends Exception {
-  public ConfigurationException(String msg) {
-    super(msg);
-  }
-}
