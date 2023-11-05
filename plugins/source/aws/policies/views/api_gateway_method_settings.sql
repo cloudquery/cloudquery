c343eb8f3644f2afde1a5a62435170dc9e70425a
ronsh12 <101520407+ronsh12@users.noreply.github.com>
2023-11-05 16:23:43 +0200

    feat: Updated macro view api_gateway_method_settings

--- plugins/source/aws/policies/views/api_gateway_method_settings.sql
+++ plugins/source/aws/policies/views/api_gateway_method_settings.sql
@@ -1,22 +1,39 @@
 create or replace view view_aws_apigateway_method_settings as
+with apigateway_rest_api_stages as(
+  select
+    *,
+    method_settings::text = '{}'::text as is_empty_method_settings
+  from aws_apigateway_rest_api_stages
+)
 select
-    s.arn,
-    s.rest_api_arn,
-    s.stage_name,
-    s.tracing_enabled as stage_data_trace_enabled,
-    s.cache_cluster_enabled as stage_caching_enabled,
-    s.web_acl_arn as waf,
-    s.client_certificate_id as cert,
-    key as method,
-    (
-        value::JSON -> 'DataTraceEnabled'
-    )::TEXT::BOOLEAN as data_trace_enabled,
-    (value::JSON -> 'CachingEnabled')::TEXT::BOOLEAN as caching_enabled,
-    (
-        value::JSON -> 'CacheDataEncrypted'
-    )::TEXT::BOOLEAN as cache_data_encrypted,
-    (value::JSON -> 'LoggingLevel')::TEXT as logging_level,
-    r.account_id
-from aws_apigateway_rest_api_stages s, aws_apigateway_rest_apis r,
-    JSONB_EACH_TEXT(s.method_settings)
-where s.rest_api_arn=r.arn
+     s.arn,
+     s.rest_api_arn,
+	 s.stage_name,
+	 s.tracing_enabled as stage_data_trace_enabled,
+     s.cache_cluster_enabled as stage_caching_enabled,
+     s.web_acl_arn as waf,
+     s.client_certificate_id as cert,
+case when (s.is_empty_method_settings = false)
+        then (select key from JSONB_EACH_TEXT(s.method_settings))
+        else '/*/'
+    end as method,
+    case when (s.is_empty_method_settings = false)
+        then (select (value::JSON -> 'DataTraceEnabled')::TEXT::BOOLEAN from JSONB_EACH_TEXT(s.method_settings))
+        else false
+    end as data_trace_enabled,
+    case when (s.is_empty_method_settings = false)
+        then (select (value::JSON -> 'CachingEnabled')::TEXT::BOOLEAN from JSONB_EACH_TEXT(s.method_settings))
+        else false
+    end as caching_enabled,
+    case when (s.is_empty_method_settings = false)
+        then (select (value::JSON -> 'CacheDataEncrypted')::TEXT::BOOLEAN from JSONB_EACH_TEXT(s.method_settings))
+        else false
+    end as cache_data_encrypted,
+    case when (s.is_empty_method_settings = false)
+        then (select (value::JSON -> 'LoggingLevel')::TEXT from JSONB_EACH_TEXT(s.method_settings))
+        else '"OFF"'
+   end as logging_level,
+     r.account_id
+
+from apigateway_rest_api_stages s, aws_apigateway_rest_apis r
+ where s.rest_api_arn=r.arn
