606620c69ab8b5259648628bc8b4700f8b6c931d
candiduslynx <candiduslynx@gmail.com>
2023-11-20 09:48:09 +0200

    fix!: Add `image_arn` to `aws_ec2_image_launch_permissions` primary keys Instead of #14934 Part of #12648

--- plugins/source/aws/resources/services/ec2/images_mock_test.go
+++ plugins/source/aws/resources/services/ec2/images_mock_test.go
@@ -27,44 +27,21 @@ func buildEc2ImagesMock(t *testing.T, ctrl *gomock.Controller) client.Services {
 	deprecationTime := "2050-11-05T08:15:30-05:00"
 	g.DeprecationTime = &deprecationTime
 
-	m.EXPECT().DescribeImages(gomock.Any(), gomock.Any(), gomock.Any()).Return(
-		&ec2.DescribeImagesOutput{
-			Images: []types.Image{g},
-		}, nil).Times(2)
-
-	// Below functionality was removed during SDK v2 migration. It was previously used
-	// in resolveEc2ImageLastLaunchedTime to fetch the last launch time, but currently
-	// we do not support this functionality in v2.
-
-	// lastLaunched := "1994-11-05T08:15:30-05:00"
-	// m.EXPECT().DescribeImageAttribute(
-	// 	gomock.Any(),
-	// 	&ec2.DescribeImageAttributeInput{
-	// 		Attribute: types.ImageAttributeNameLastLaunchedTime,
-	// 		ImageId:   g.ImageId,
-	// 	},
-	// 	gomock.Any(),
-	// ).Return(
-	// 	&ec2.DescribeImageAttributeOutput{
-	// 		LastLaunchedTime: &types.AttributeValue{Value: &lastLaunched},
-	// 	},
-	// 	nil,
-	// ).Times(2)
+	m.EXPECT().
+		DescribeImages(gomock.Any(), gomock.Any(), gomock.Any()).
+		Return(&ec2.DescribeImagesOutput{Images: []types.Image{g}}, nil).
+		Times(2)
 
 	lp := types.LaunchPermission{}
 	require.NoError(t, faker.FakeObject(&lp))
 
-	m.EXPECT().DescribeImageAttribute(
-		gomock.Any(),
-		gomock.Any(),
-		gomock.Any(),
-	).Return(
-		&ec2.DescribeImageAttributeOutput{
-			LaunchPermissions: []types.LaunchPermission{lp},
-			LastLaunchedTime:  &types.AttributeValue{Value: &creationDate},
-		},
-		nil,
-	).Times(2)
+	m.EXPECT().
+		DescribeImageAttribute(gomock.Any(), &ec2.DescribeImageAttributeInput{Attribute: types.ImageAttributeNameLaunchPermission, ImageId: g.ImageId}, gomock.Any()).
+		Return(&ec2.DescribeImageAttributeOutput{LaunchPermissions: []types.LaunchPermission{lp}}, nil)
+
+	m.EXPECT().
+		DescribeImageAttribute(gomock.Any(), &ec2.DescribeImageAttributeInput{Attribute: types.ImageAttributeNameLastLaunchedTime, ImageId: g.ImageId}, gomock.Any()).
+		Return(&ec2.DescribeImageAttributeOutput{LastLaunchedTime: &types.AttributeValue{Value: &creationDate}}, nil)
 
 	return services
 }
