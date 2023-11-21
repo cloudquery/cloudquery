a206cee226834581743b43638b2080b046906fd8
candiduslynx <candiduslynx@gmail.com>
2023-11-21 16:10:12 +0200

    update second query

--- plugins/source/aws/policies_v1/views/log_metric_filter_and_alarm.sql
+++ plugins/source/aws/policies_v1/views/log_metric_filter_and_alarm.sql
@@ -3,18 +3,26 @@ with af as (
   select distinct a.arn, a.actions_enabled, a.alarm_actions, m->'MetricStat'->'Metric'->>'MetricName' as metric_name -- TODO check
   from aws_cloudwatch_alarms a, jsonb_array_elements(a.metrics) as m
 )
+with tes as (
+  select trail_arn from aws_cloudtrail_trail_event_selectors
+  where exists(
+    select * from jsonb_array_elements(event_selectors) as es
+    where es ->>'ReadWriteType' = 'All' and (es->>'IncludeManagementEvents')::boolean = TRUE
+  ) or exists(
+    select * from jsonb_array_elements(advanced_event_selectors) as aes
+    where not exists(select * from jsonb_array_elements(aes ->'FieldSelectors') as aes_fs where aes_fs ->>'Field' = 'readOnly'))
+  )
+)
 select
     t.account_id,
     t.region,
     t.cloud_watch_logs_log_group_arn,
     mf.filter_pattern as pattern
 from aws_cloudtrail_trails t
-inner join aws_cloudtrail_trail_event_selectors tes on t.arn = tes.trail_arn
+inner join tes on t.arn = tes.trail_arn
 inner join aws_cloudwatchlogs_metric_filters mf on mf.log_group_name = t.cloudwatch_logs_log_group_name
 inner join af on mf.filter_name = af.metric_name
 inner join aws_sns_subscriptions ss on ss.topic_arn = ANY(af.alarm_actions)
 where t.is_multi_region_trail = TRUE
     and (t.status->>'IsLogging')::boolean = TRUE
-    and tes.include_management_events = TRUE
-    and tes.read_write_type = 'All'
     and ss.arn like 'aws:arn:%'
