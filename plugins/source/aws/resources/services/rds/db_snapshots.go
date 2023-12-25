51f41641ee896c2ce5383abd01c7f0ecda39955e
bbernays <ben@bernays.me>
2023-12-25 11:18:50 -0600

    Update db_snapshots.go

--- plugins/source/aws/resources/services/rds/db_snapshots.go
+++ plugins/source/aws/resources/services/rds/db_snapshots.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/rds"
 	"github.com/aws/aws-sdk-go-v2/service/rds/types"
 	"github.com/cloudquery/cloudquery/plugins/source/aws/client"
+	"github.com/cloudquery/cloudquery/plugins/source/aws/resources/services/rds/models"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/transformers"
 	sdkTypes "github.com/cloudquery/plugin-sdk/v4/types"
@@ -18,7 +19,7 @@ func DbSnapshots() *schema.Table {
 		Name:        tableName,
 		Description: `https://docs.aws.amazon.com/AmazonRDS/latest/APIReference/API_DBSnapshot.html`,
 		Resolver:    fetchRdsDbSnapshots,
-		Transform:   transformers.TransformWithStruct(&types.DBSnapshot{}, transformers.WithSkipFields("TagList")),
+		Transform:   transformers.TransformWithStruct(&types.DBSnapshot{}, transformers.WithSkipFields("TagList", "Attributes"), transformers.WithUnwrapAllEmbeddedStructs()),
 		Multiplex:   client.ServiceAccountRegionMultiplexer(tableName, "rds"),
 		Columns: []schema.Column{
 			client.DefaultAccountIDColumn(false),
@@ -55,13 +56,49 @@ func fetchRdsDbSnapshots(ctx context.Context, meta schema.ClientMeta, parent *sc
 		if err != nil {
 			return nil
 		}
-		res <- page.DBSnapshots
+		for _, DBSnapshot := range page.DBSnapshots {
+			attributes, err := fetchSnapshotAttributes(ctx, meta, DBSnapshot.DBSnapshotIdentifier)
+			if err != nil {
+				// Log error and continue resolving
+				cl.Logger().Warn().Err(err).Msg("failed to fetch snapshot attributes")
+			}
+			newAttributes := make([]models.ExtendedAttributes, len(attributes))
+			for _, attribute := range attributes {
+				newAttributes = append(newAttributes, models.ExtendedAttributes{DBSnapshotAttribute: attribute})
+			}
+			res <- models.ExtendedSnapshots{
+				DBSnapshot: DBSnapshot,
+				Attributes: newAttributes,
+			}
+		}
 	}
 	return nil
 }
 
+func fetchSnapshotAttributes(ctx context.Context, meta schema.ClientMeta, dbSnapshotIdentifier *string) ([]types.DBSnapshotAttribute, error) {
+	cl := meta.(*client.Client)
+	svc := cl.Services(client.AWSServiceRds).Rds
+	out, err := svc.DescribeDBSnapshotAttributes(
+		ctx,
+		&rds.DescribeDBSnapshotAttributesInput{DBSnapshotIdentifier: dbSnapshotIdentifier},
+		func(o *rds.Options) {
+			o.Region = cl.Region
+		},
+	)
+	if err != nil {
+		if cl.IsNotFoundError(err) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	if out.DBSnapshotAttributesResult == nil {
+		return nil, nil
+	}
+	return out.DBSnapshotAttributesResult.DBSnapshotAttributes, nil
+}
+
 func resolveRDSDBSnapshotTags(ctx context.Context, meta schema.ClientMeta, resource *schema.Resource, c schema.Column) error {
-	s := resource.Item.(types.DBSnapshot)
+	s := resource.Item.(models.ExtendedSnapshots)
 	tags := map[string]*string{}
 	for _, t := range s.TagList {
 		tags[*t.Key] = t.Value
@@ -70,25 +107,6 @@ func resolveRDSDBSnapshotTags(ctx context.Context, meta schema.ClientMeta, resou
 }
 
 func resolveRDSDBSnapshotAttributes(ctx context.Context, meta schema.ClientMeta, resource *schema.Resource, column schema.Column) error {
-	s := resource.Item.(types.DBSnapshot)
-	cl := meta.(*client.Client)
-	svc := cl.Services(client.AWSServiceRds).Rds
-	out, err := svc.DescribeDBSnapshotAttributes(
-		ctx,
-		&rds.DescribeDBSnapshotAttributesInput{DBSnapshotIdentifier: s.DBSnapshotIdentifier},
-		func(o *rds.Options) {
-			o.Region = cl.Region
-		},
-	)
-	if err != nil {
-		if cl.IsNotFoundError(err) {
-			return nil
-		}
-		return err
-	}
-	if out.DBSnapshotAttributesResult == nil {
-		return nil
-	}
-
-	return resource.Set(column.Name, out.DBSnapshotAttributesResult.DBSnapshotAttributes)
+	s := resource.Item.(models.ExtendedSnapshots)
+	return resource.Set(column.Name, s.Attributes)
 }
