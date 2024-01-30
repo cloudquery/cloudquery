5532fb05cd0da37681f17667b796ed673826e5a9
Martin Norbury <martin.norbury@gmail.com>
2024-01-30 16:29:30 +0000

    Adding comments to Spec to match website

--- plugins/source/datadog/client/spec.go
+++ plugins/source/datadog/client/spec.go
@@ -2,15 +2,23 @@ package client
 
 import _ "embed"
 
+// Spec the (nested) spec used by the Datadog source plugin.
 type Spec struct {
-	Accounts    []Account `json:"accounts"`
-	Concurrency int       `json:"concurrency"`
-	Site        string    `json:"site"`
+	// Specify which accounts to sync data from.
+	Accounts []Account `json:"accounts"`
+	// A best effort maximum number of Go routines to use. Lower this number to reduce memory usage.
+	Concurrency int `json:"concurrency"`
+	// The Datadog site to connect to. This is usually one of datadoghq.com or datadoghq.eu - see site documentation for more information.
+	Site string `json:"site"`
 }
 
+// Account used to specify one or more accounts to extract information from.
 type Account struct {
-	Name   string `json:"name"`
+	// Account name.
+	Name string `json:"name"`
+	// Datadog API key.
 	APIKey string `json:"api_key"`
+	// Datadog App key.
 	AppKey string `json:"app_key"`
 }
 
