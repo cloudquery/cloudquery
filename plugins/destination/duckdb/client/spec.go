d1892d98c7362ae48fa8ae5d3ef1667c950911eb
Kemal Hadimli <disq@users.noreply.github.com>
2024-01-31 09:35:13 +0000

    No "the"

--- plugins/destination/duckdb/client/spec.go
+++ plugins/destination/duckdb/client/spec.go
@@ -11,10 +11,10 @@ type Spec struct {
 	// Absolute or relative path to a file, such as `./example.duckdb`
 	ConnectionString string `json:"connection_string,omitempty" jsonschema:"required,minLength=1"`
 
-	// Maximum amount of items may be grouped together to be written as a single write
+	// Maximum amount of items that may be grouped together to be written in a single write
 	BatchSize int `json:"batch_size,omitempty" jsonschema:"minimum=1,default=1000"`
 
-	// Maximum size of items that may be grouped together to be written as a single write
+	// Maximum size of items that may be grouped together to be written in a single write.
 	BatchSizeBytes int `json:"batch_size_bytes,omitempty" jsonschema:"minimum=1,default=4194304"`
 
 	// Enables debug logging
--- plugins/destination/duckdb/docs/overview.md
+++ plugins/destination/duckdb/docs/overview.md
@@ -24,11 +24,11 @@ This is the top level spec used by the DuckDB destination Plugin.
 
 - `batch_size` (`integer`) (optional) (default: `1000`)
 
-  The maximum amount of items that may be grouped together to be written in a single write.
+  Maximum amount of items that may be grouped together to be written in a single write.
 
 - `batch_size_bytes` (`integer`) (optional) (default: `4194304` (4 MiB))
 
-  The maximum size of items that may be grouped together to be written in a single write.
+  Maximum size of items that may be grouped together to be written in a single write.
 
 - `debug` (`boolean`) (optional) (default: `false`)
 
