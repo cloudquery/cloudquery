0cdf8a150e51231bfb79e5412a3fa7fc166abc0d
Martin Norbury <martin.norbury@gmail.com>
2024-01-31 11:09:08 +0000

    Adding JSON schema support to the firestore plugin
    fixes: https://github.com/cloudquery/cloudquery/issues/16359

--- plugins/source/firestore/client/spec.go
+++ plugins/source/firestore/client/spec.go
@@ -1,23 +1,34 @@
 package client
 
 import (
+	_ "embed"
 	"encoding/base64"
 	"fmt"
+	"github.com/invopop/jsonschema"
 	"strings"
 
 	"cloud.google.com/go/firestore"
 )
 
+// Spec is the (nested) spec used by Firestore Source Plugin
 type Spec struct {
-	ProjectID          string `json:"project_id"`
-	UseBase64          bool   `json:"use_base64"`
+	// the ID of the project to use for this client. If not specified, the project id will be auto-detected from the credentials
+	ProjectID string `json:"project_id"`
+	// if true the service account JSON content will be treated as base64 encoded
+	UseBase64 bool `json:"use_base64" jsonschema:"default=false"`
+	//  service account JSON content
 	ServiceAccountJSON string `json:"service_account_json"`
-	MaxBatchSize       int    `json:"max_batch_size"`
-	OrderBy            string `json:"order_by"`
-	OrderDirection     string `json:"order_direction"`
+	// maximum batch size for each request when reading Firestore data
+	MaxBatchSize int `json:"max_batch_size" jsonschema:"minimum=1"`
+	// field(s) to order the results by
+	OrderBy string `json:"order_by"`
+	// the direction to order the results by when order_by is specified - accepts either asc or desc
+	OrderDirection string `json:"order_direction" jsonschema:"enum=asc,enum=desc,default=asc"`
 }
 
 func (s *Spec) Validate() error {
+	// decode base64 if needed - note if the Validate function is removed from the spec, this will need to be done
+	// elsewhere in the application
 	if s.UseBase64 {
 		data, err := base64.StdEncoding.DecodeString(s.ServiceAccountJSON)
 		if err != nil {
@@ -35,6 +46,12 @@ func (s *Spec) Validate() error {
 	return nil
 }
 
+func (Spec) JSONSchemaExtend(sc *jsonschema.Schema) {
+	// Configure defaults
+	sc.Properties.Value("project_id").Default = firestore.DetectProjectID
+	sc.Properties.Value("max_batch_size").Default = 50_000
+}
+
 func (s *Spec) SetDefaults() {
 	if s.MaxBatchSize == 0 {
 		s.MaxBatchSize = 50_000
@@ -43,3 +60,6 @@ func (s *Spec) SetDefaults() {
 		s.ProjectID = firestore.DetectProjectID
 	}
 }
+
+//go:embed schema.json
+var JSONSchema string
