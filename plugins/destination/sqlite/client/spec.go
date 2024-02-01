4bdb544fbc9a0fbff27961cf52e7ff416affe074
Kemal <223029+disq@users.noreply.github.com>
2024-02-01 03:28:49 -0800

    feat: Add JSON schema to sqlite destination plugin (#16396)
    Implements https://github.com/cloudquery/cloudquery/issues/16391

--- plugins/destination/sqlite/client/spec.go
+++ plugins/destination/sqlite/client/spec.go
@@ -1,8 +1,14 @@
 package client
 
+import _ "embed"
+
 type Spec struct {
-	ConnectionString string `json:"connection_string,omitempty"`
+	// Path to a file, such as `./mydb.sql`
+	ConnectionString string `json:"connection_string,omitempty" jsonschema:"required,minLength=1"`
 }
+
+//go:embed schema.json
+var JSONSchema string
 
 func (*Spec) SetDefaults() {
 	// stub for any future defaults
