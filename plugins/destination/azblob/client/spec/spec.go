f781052926c25b5c4e377cb18a6a31d71066e2cc
candiduslynx <candiduslynx@gmail.com>
2024-02-01 15:21:26 +0200

    fix msg

--- plugins/destination/azblob/client/spec/spec.go
+++ plugins/destination/azblob/client/spec/spec.go
@@ -78,7 +78,7 @@ func (s *Spec) Validate() error {
 	}
 
 	if s.NoRotate && ((s.BatchSize != nil && *s.BatchSize > 0) || (s.BatchSizeBytes != nil && *s.BatchSizeBytes > 0) || (s.BatchTimeout != nil && s.BatchTimeout.Duration() > 0)) {
-		return fmt.Errorf("`no_rotate` cannot be used with non-zero `batch_size`, `batch_size_bytes` or `batch_timeout_ms`")
+		return fmt.Errorf("`no_rotate` cannot be used with non-zero `batch_size`, `batch_size_bytes` or `batch_timeout`")
 	}
 
 	// required for s.FileSpec.Validate call
