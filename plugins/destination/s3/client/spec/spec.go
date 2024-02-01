ef98623635255872eefd3c8e4552e729165e744b
candiduslynx <candiduslynx@gmail.com>
2024-02-01 21:17:00 +0200

    descs

--- plugins/destination/s3/client/spec/spec.go
+++ plugins/destination/s3/client/spec/spec.go
@@ -25,19 +25,65 @@ const (
 type Spec struct {
 	filetypes.FileSpec
 
-	NoRotate  bool   `json:"no_rotate,omitempty"`
-	Bucket    string `json:"bucket,omitempty"`
-	Region    string `json:"region,omitempty"`
-	Path      string `json:"path,omitempty"`
-	Athena    bool   `json:"athena,omitempty"`
-	TestWrite *bool  `json:"test_write,omitempty"`
+	// Bucket where to sync the files.
+	Bucket string `json:"bucket,omitempty" jsonschema:"required,minLength=1"`
 
-	Endpoint              string               `json:"endpoint,omitempty"`
-	UsePathStyle          bool                 `json:"use_path_style,omitempty"`
-	EndpointSkipTLSVerify bool                 `json:"endpoint_skip_tls_verify,omitempty"`
-	BatchSize             *int64               `json:"batch_size"`
-	BatchSizeBytes        *int64               `json:"batch_size_bytes"`
-	BatchTimeout          *configtype.Duration `json:"batch_timeout"`
+	// Region where bucket is located.
+	Region string `json:"region,omitempty" jsonschema:"required,minLength=1"`
+
+	//  Path to where the files will be uploaded in the above bucket. The path supports the following placeholder variables:
+	//
+	// - `{{TABLE}}` will be replaced with the table name
+	// - `{{FORMAT}}` will be replaced with the file format, such as `csv`, `json` or `parquet`. If compression is enabled, the format will be `csv.gz`, `json.gz` etc.
+	// - `{{UUID}}` will be replaced with a random UUID to uniquely identify each file
+	// - `{{YEAR}}` will be replaced with the current year in `YYYY` format
+	// - `{{MONTH}}` will be replaced with the current month in `MM` format
+	// - `{{DAY}}` will be replaced with the current day in `DD` format
+	// - `{{HOUR}}` will be replaced with the current hour in `HH` format
+	// - `{{MINUTE}}` will be replaced with the current minute in `mm` format
+	//
+	// **Note** that timestamps are in `UTC` and will be the current time at the time the file is written, not when the sync started.
+	Path string `json:"path,omitempty" jsonschema:"required,minLength=1"`
+
+	// If set to `true`, the plugin will write to one file per table.
+	// Otherwise, for every batch a new file will be created with a different `.<UUID>` suffix.
+	NoRotate bool `json:"no_rotate,omitempty" jsonschema:"default=false"`
+
+	// When `athena` is set to `true`, the S3 plugin will sanitize keys in JSON columns to be compatible with the Hive Metastore / Athena.
+	// This allows tables to be created with a Glue Crawler and then queried via Athena, without changes to the table schema.
+	Athena bool `json:"athena,omitempty" jsonschema:"default=false"`
+
+	// Ensure write access to the given bucket and path by writing a test object on each sync.
+	// If you are sure that the bucket and path are writable, you can set this to `false` to skip the test.
+	TestWrite *bool `json:"test_write,omitempty" jsonschema:"default=true"`
+
+	// Endpoint to use for S3 API calls. This is useful for S3-compatible storage services such as MinIO.
+	// **Note**: if you want to use path-style addressing, i.e., `https://s3.amazonaws.com/BUCKET/KEY`, `use_path_style` should be enabled, too.
+	Endpoint string `json:"endpoint,omitempty"  jsonschema:"default="`
+
+	// Allows to use path-style addressing in the `endpoint` option, i.e., `https://s3.amazonaws.com/BUCKET/KEY`.
+	// By default, the S3 client will use virtual hosted bucket addressing when possible (`https://BUCKET.s3.amazonaws.com/KEY`).
+	UsePathStyle bool `json:"use_path_style,omitempty" jsonschema:"default=false"`
+
+	// Disable TLS verification for requests to your S3 endpoint.
+	//
+	// This option is intended to be used when using a custom endpoint using the `endpoint` option.
+	EndpointSkipTLSVerify bool `json:"endpoint_skip_tls_verify,omitempty" jsonschema:"default=false"`
+
+	// Maximum number of items that may be grouped together to be written in a single write.
+	//
+	// Defaults to `10000` unless `no_rotate` is `true` (will be `0` then).
+	BatchSize *int64 `json:"batch_size" jsonschema:"minimum=1,default=10000"`
+
+	// Maximum size of items that may be grouped together to be written in a single write.
+	//
+	// Defaults to `52428800` (50 MiB) unless `no_rotate` is `true` (will be `0` then).
+	BatchSizeBytes *int64 `json:"batch_size_bytes" jsonschema:"minimum=1,default=52428800"`
+
+	// Maximum interval between batch writes.
+	//
+	// Defaults to `30s` unless `no_rotate` is `true` (will be `0s` then).
+	BatchTimeout *configtype.Duration `json:"batch_timeout" jsonschema:"default=30s"`
 }
 
 func (s *Spec) SetDefaults() {
