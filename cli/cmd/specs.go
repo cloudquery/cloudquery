38990858b56be67ef091af4df67f59d452825a8d
Alex Shcherbakov <candiduslynx@users.noreply.github.com>
2024-02-08 13:19:50 +0200

    Update cli/cmd/specs.go
    Co-authored-by: Kemal <223029+disq@users.noreply.github.com>

--- cli/cmd/specs.go
+++ cli/cmd/specs.go
@@ -169,7 +169,7 @@ func parseJSONSchema(jsonSchema string) (*jsonschema.Schema, error) {
 			// We add resource as `file`, but there's none, actually.
 			// So, we need to prettify message a bit.
 			return nil, fmt.Errorf("jsonschema compilation failed: %w",
-				errors.New(strings.Replace(se.Err.Error(), "jsonschema: '' ", "jsonschema: ", 1)))
+				errors.New(strings.Replace(se.Err.Error(), "jsonschema: '' ", "", 1)))
 		}
 		return nil, err
 	}
