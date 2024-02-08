e81a0a93329db1fd30d7de45321dbf50dd01b66f
Kemal <223029+disq@users.noreply.github.com>
2024-02-08 06:07:08 -0800

    feat: Add JSON Schema (#16547)

--- plugins/source/notion/client/spec.go
+++ plugins/source/notion/client/spec.go
@@ -1,9 +1,14 @@
 package client
 
+import _ "embed"
+
 type Spec struct {
-	BearerToken   string `json:"bearer_token,omitempty"`
-	NotionVersion string `json:"notion_version,omitempty"`
+	BearerToken   string `json:"bearer_token" jsonschema:"required,minLength=1"`
+	NotionVersion string `json:"notion_version,omitempty" jsonschema:"minLength=1,default=2022-02-22"`
 }
+
+//go:embed schema.json
+var JSONSchema string
 
 func (s *Spec) SetDefaults() {
 	if len(s.NotionVersion) < 1 {
