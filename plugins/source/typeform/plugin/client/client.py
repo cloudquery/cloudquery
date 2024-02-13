4ebcce9187db2bb95415d3fa619457f09d179446
candiduslynx <candiduslynx@gmail.com>
2024-02-13 09:56:53 +0200

    feat: Add JSON Schema to `typeform` source plugin

--- plugins/source/typeform/plugin/client/client.py
+++ plugins/source/typeform/plugin/client/client.py
@@ -17,6 +17,47 @@ class Spec:
         if self.access_token is None:
             raise Exception("access_token must be provided")
 
+    @staticmethod
+    def json_schema():
+        return '''{
+  "$schema": "https://json-schema.org/draft/2020-12/schema",
+  "$id": "https://github.com/cloudquery/cloudquery/plugins/source/typeform/spec",
+  "$ref": "#/$defs/Spec",
+  "$defs": {
+    "Spec": {
+      "properties": {
+        "access_token": {
+          "type": "string",
+          "minLength": 1,
+          "description": "Your [personal access token](https://www.typeform.com/developers/get-started/personal-access-token/) from the Typeform Dashboard."
+        },
+        "base_url": {
+          "type": "string",
+          "default": "https://api.typeform.com",
+          "description": "The base URL to fetch data from. Use `https://api.eu.typeform.com` if your account is stored in the EU."
+        },
+        "concurrency": {
+          "type": "integer",
+          "minimum": 1,
+          "default": 100,
+          "description": "Maximum number of requests to perform concurrently."
+        },
+        "queue_size": {
+          "type": "integer",
+          "minimum": 1,
+          "default": 10000,
+          "description": "Maximum number of items to have in the queue before waiting for an unfinished request to complete."
+        }
+      },
+      "additionalProperties": false,
+      "type": "object",
+      "required": [
+        "access_token"
+      ]
+    }
+  }
+}
+'''
 
 class Client:
     def __init__(self, spec: Spec) -> None:
