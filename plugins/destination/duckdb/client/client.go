cloudquery/cloudquery

commit 8192d6d99898692e9232d29d9ac71f5d2c6d8d4a

feat: Use Appender interface (#16668)
Avoid writing to parquet if we can (appends only)


~Using upstream driver has some shortcomings from the get go: The interface decides on the schema based on passed types, so having untyped `nil`s don't help. But typed nils are also not handled. The [fork/PR](https://github.com/marcboeker/go-duckdb/pull/150) fixes these issues but I still get 0 rows on the test db (or motherduck)~ The PR is now merged upstream and tagged as 1.6.1.

~I had to use my [fork](https://github.com/disq/go-duckdb/tree/feat/allow-nulls-in-appender-first-row) to get around nils, another way to this would be if the `appender.initColTypes` was exported, then we could just pass on concrete types for init and then use nullable data as normal.~

~Another issue: We create JSON types as `json` but appender doesn't know about it. Sending `string` instead (which is what the json alias points to internally in duckdb, varchar) doesn't work. Might need the newer appender implementation for this to go forward.~


Example config:
```yaml
kind: source
spec:
  name: test
  path: cloudquery/test
  registry: cloudquery
  version: v4.0.8
#  tables: ["*"]
  tables: [ "test_some_table" ]
  destinations: ["duckdb"]
  spec:
    num_clients: 1
    num_rows: 4
    num_sub_rows: 1
#    num_clients: 10
#    num_rows: 10000
    num_sub_rows: 0
#    num_sub_rows: 10
---
kind: destination
spec:
  name: duckdb
  version: v0.0.0
  registry: grpc
  path: localhost:7777
#  write_mode: "overwrite"
  write_mode: "append"
  spec:
    connection_string: tmp_duck.db
#    connection_string: md:my_db
    # Optional parameters
    batch_size: 10000
#    batch_size_bytes: 14194304 # 14 MiB
    # debug: false
```

plugins/destination/duckdb/client/client.go
@@ -12,6 +12,7 @@ import (
 
 	internalPlugin "github.com/cloudquery/cloudquery/plugins/destination/duckdb/resources/plugin"
 	"github.com/cloudquery/plugin-sdk/v4/plugin"
+	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/cloudquery/plugin-sdk/v4/writers/batchwriter"
 	"github.com/marcboeker/go-duckdb"
 	"github.com/rs/zerolog"
@@ -21,11 +22,14 @@ type Client struct {
 	plugin.UnimplementedSource
 	batchwriter.UnimplementedDeleteRecord
 
+	connector *duckdb.Connector
 	db        *sql.DB
-	connector driver.Connector
-	logger    zerolog.Logger
-	spec      Spec
-	writer    *batchwriter.BatchWriter
+	conn      driver.Conn // used in Appender
+
+	logger   zerolog.Logger
+	spec     Spec
+	writer   *batchwriter.BatchWriter
+	dbTables schema.Tables // current state of tables, used in Appender
 }
 
 var _ plugin.Client = (*Client)(nil)
@@ -74,20 +78,22 @@ func New(ctx context.Context, logger zerolog.Logger, spec []byte, _ plugin.NewCl
 }
 
 func (c *Client) Close(ctx context.Context) error {
-	var err error
-
 	if c.db == nil {
 		return fmt.Errorf("client already closed or not initialized")
 	}
 
-	if err := c.writer.Close(ctx); err != nil {
-		_ = c.db.Close()
-		c.db = nil
-		return fmt.Errorf("failed to close writer: %w", err)
+	err1 := c.writer.Close(ctx)
+	if err1 != nil {
+		err1 = fmt.Errorf("failed to close writer: %w", err1)
 	}
 
-	err = c.db.Close()
-	c.db = nil
+	err := errors.Join(err1, c.db.Close(), func() error {
+		if c.conn == nil {
+			return nil
+		}
+		return c.conn.Close()
+	}())
+	c.db, c.conn = nil, nil
 	return err
 }
 

plugins/destination/duckdb/client/migrate.go
@@ -135,6 +135,10 @@ func (c *Client) MigrateTables(ctx context.Context, msgs message.WriteMigrateTab
 	if err != nil {
 		return err
 	}
+	c.dbTables = duckdbTables
+	defer func() {
+		c.dbTables = c.dbTables.FlattenTables()
+	}()
 
 	normalizedTables := c.normalizeColumns(tables)
 	normalizedTablesSafeMode := make(schema.Tables, 0, len(normalizedTables))
@@ -165,10 +169,14 @@ func (c *Client) MigrateTables(ctx context.Context, msgs message.WriteMigrateTab
 			if err := c.createTableIfNotExist(ctx, table.Name, table); err != nil {
 				return err
 			}
+			c.dbTables = nil // force refresh
 			continue
 		}
 
 		changes := table.GetChanges(duckdb)
+		if len(changes) > 0 {
+			c.dbTables = nil // force refresh
+		}
 		if c.canAutoMigrate(changes) {
 			c.logger.Info().Str("table", table.Name).Msg("Table exists, auto-migrating")
 			if err := c.autoMigrateTable(ctx, table, changes); err != nil {
@@ -179,6 +187,13 @@ func (c *Client) MigrateTables(ctx context.Context, msgs message.WriteMigrateTab
 			if err := c.recreateTable(ctx, table); err != nil {
 				return err
 			}
+		}
+	}
+
+	if c.dbTables == nil {
+		c.dbTables, err = c.duckDBTables(ctx, tables)
+		if err != nil {
+			return fmt.Errorf("failed to get duckdb tables after migrations: %w", err)
 		}
 	}
 

plugins/destination/duckdb/client/write.go
@@ -15,7 +15,11 @@ import (
 	"github.com/cloudquery/plugin-sdk/v4/message"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/google/uuid"
+	"github.com/marcboeker/go-duckdb"
 )
+
+// Disable to use the old way of writing to duckdb for appends
+const useAppender = true
 
 func nonPkIndices(sc *schema.Table) []int {
 	var indices []int
@@ -125,11 +129,59 @@ func (c *Client) Write(ctx context.Context, msgs <-chan message.WriteMessage) er
 	return nil
 }
 
+func (c *Client) appendRows(table *schema.Table, msgs message.WriteInserts) (retErr error) {
+	if c.conn == nil {
+		// connecting before MigrateTable results in appender creation failure
+		var err error
+		c.conn, err = c.connector.Connect(context.Background())
+		if err != nil {
+			return fmt.Errorf("failed to connect: %w", err)
+		}
+	}
+
+	appender, err := duckdb.NewAppenderFromConn(c.conn, "", table.Name)
+	if err != nil {
+		return fmt.Errorf("failed to create appender for %s: %w", table.Name, err)
+	}
+	defer func() {
+		err := appender.Close()
+		if err != nil && retErr == nil {
+			retErr = fmt.Errorf("failed to close appender for %s: %w", table.Name, err)
+		}
+	}()
+
+	tableState := c.dbTables.Get(table.Name)
+	if tableState == nil {
+		return fmt.Errorf("table %s not found in duckdb", table.Name) // should never happen as appender would've failed
+	}
+
+	fields := msgs[0].Record.Schema().Fields()
+
+	for _, msg := range msgs {
+		arr, err := transformRecordToGoType(msg.Record, fields, tableState.Columns)
+		if err != nil {
+			return fmt.Errorf("failed to transform record to go type for %s: %w", table.Name, err)
+		}
+		for i := range arr {
+			if err := appender.AppendRow(arr[i]...); err != nil {
+				return fmt.Errorf("failed to append row to %s: %w", table.Name, err)
+			}
+		}
+	}
+
+	return nil
+}
+
 func (c *Client) WriteTableBatch(ctx context.Context, name string, msgs message.WriteInserts) error {
 	if len(msgs) == 0 {
 		return nil
 	}
+
 	table := msgs[0].GetTable()
+
+	if useAppender && len(table.PrimaryKeys()) == 0 {
+		return c.appendRows(table, msgs)
+	}
 
 	writeStart := time.Now()
 	tmpFile, err := writeTMPFile(table, msgs)
@@ -140,6 +192,7 @@ func (c *Client) WriteTableBatch(ctx context.Context, name string, msgs message.
 	defer os.Remove(tmpFile)
 
 	if len(table.PrimaryKeys()) == 0 {
+		// This is only used if useAppender is disabled, for testing or debugging reasons
 		copyStart := time.Now()
 		defer func() {
 			c.logger.Debug().Str("table", table.Name).Str("duration", time.Since(copyStart).String()).Msg("copy file to table")

plugins/destination/duckdb/client/read.go
@@ -117,6 +117,10 @@ func reverseTransformArray(dt arrow.DataType, arr arrow.Array) arrow.Array {
 		// We save date types as Timestamp
 		return reverseTransformDate64(arr.(*array.Timestamp))
 	case *arrow.StructType:
+		if sarr, ok := arr.(*array.String); ok {
+			return reverseTransformStruct(dt, sarr)
+		}
+
 		arr := arr.(*array.Struct)
 		children := make([]arrow.ArrayData, arr.NumField())
 		for i := range children {
@@ -132,8 +136,13 @@ func reverseTransformArray(dt arrow.DataType, arr arrow.Array) arrow.Array {
 			arr.NullN(),
 			0, // we use 0 as offset for struct arrays, as the child arrays would already be sliced properly
 		))
-
 	case arrow.ListLikeType: // also handles maps
+		if mapdt, ok := dt.(*arrow.MapType); ok {
+			if sarr, ok := arr.(*array.String); ok {
+				return reverseTransformMap(mapdt, sarr)
+			}
+		}
+
 		return array.MakeFromData(array.NewData(
 			dt, arr.Len(),
 			arr.Data().Buffers(),
@@ -180,6 +189,35 @@ func reverseTransformFromBinary(dt arrow.DataType, arr array.BinaryLike) arrow.A
 	return builder.NewArray()
 }
 
+func reverseTransformStruct(dt *arrow.StructType, arr *array.String) arrow.Array {
+	bldr := array.NewStructBuilder(memory.DefaultAllocator, dt)
+	defer bldr.Release()
+	for i := 0; i < arr.Len(); i++ {
+		if arr.IsNull(i) {
+			bldr.AppendNull()
+			continue
+		}
+		if err := bldr.AppendValueFromString(arr.Value(i)); err != nil {
+			panic(err)
+		}
+	}
+	return bldr.NewStructArray()
+}
+
+func reverseTransformMap(dt *arrow.MapType, arr *array.String) arrow.Array {
+	bldr := array.NewMapBuilder(memory.DefaultAllocator, dt.KeyType(), dt.ItemType(), dt.KeysSorted)
+	defer bldr.Release()
+	for i := 0; i < arr.Len(); i++ {
+		if arr.IsNull(i) {
+			bldr.AppendNull()
+			continue
+		}
+		if err := bldr.AppendValueFromString(arr.Value(i)); err != nil {
+			panic(err)
+		}
+	}
+	return bldr.NewMapArray()
+}
 func reverseTransformUint8(arr *array.Uint32) arrow.Array {
 	builder := array.NewUint8Builder(memory.DefaultAllocator)
 	for i := 0; i < arr.Len(); i++ {

plugins/destination/duckdb/client/transform.go
@@ -30,24 +30,10 @@ func transformArray(arr arrow.Array) arrow.Array {
 	case *array.Timestamp:
 		// mismatching unit or tz
 		return transformTimestamp(duckDBToArrow(arrowToDuckDB(arr.DataType())).(*arrow.TimestampType), arr)
-
 	case *array.Struct:
-		dt := arr.DataType().(*arrow.StructType)
-		children := make([]arrow.ArrayData, arr.NumField())
-		names := make([]string, arr.NumField())
-		for i := range children {
-			children[i] = transformArray(arr.Field(i)).Data()
-			names[i] = dt.Field(i).Name
-		}
-
-		return array.NewStructData(array.NewData(
-			transformTypeForWriting(dt), arr.Len(),
-			arr.Data().Buffers(),
-			children,
-			arr.NullN(),
-			0, // we use 0 as offset for struct arrays, as the child arrays would already be sliced properly
-		))
-
+		return transformStruct(arr)
+	case *array.Map:
+		return transformMap(arr)
 	case array.ListLike: // this includes maps, too
 		return array.MakeFromData(array.NewData(
 			transformTypeForWriting(arr.DataType()), arr.Len(),
@@ -112,6 +98,34 @@ func transformUint8ToUint32Array(arr *array.Uint8) arrow.Array {
 		bldr.Append(uint32(arr.Value(i)))
 	}
 	return bldr.NewArray()
+}
+
+func transformStruct(arr *array.Struct) arrow.Array {
+	bldr := array.NewStringBuilder(memory.DefaultAllocator)
+	defer bldr.Release()
+
+	for i := 0; i < arr.Len(); i++ {
+		if arr.IsNull(i) {
+			bldr.AppendNull()
+			continue
+		}
+		bldr.Append(arr.ValueStr(i))
+	}
+	return bldr.NewStringArray()
+}
+
+func transformMap(arr *array.Map) arrow.Array {
+	bldr := array.NewStringBuilder(memory.DefaultAllocator)
+	defer bldr.Release()
+
+	for i := 0; i < arr.Len(); i++ {
+		if arr.IsNull(i) {
+			bldr.AppendNull()
+			continue
+		}
+		bldr.Append(arr.ValueStr(i))
+	}
+	return bldr.NewStringArray()
 }
 
 func transformToStringArray(arr arrow.Array) arrow.Array {

plugins/destination/duckdb/client/transform_gotype.go
@@ -1,0 +1,161 @@
+package client
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/apache/arrow/go/v15/arrow"
+	"github.com/apache/arrow/go/v15/arrow/array"
+	"github.com/cloudquery/plugin-sdk/v4/schema"
+	"github.com/cloudquery/plugin-sdk/v4/types"
+	"github.com/marcboeker/go-duckdb"
+)
+
+func getTypedNilValue(arr arrow.Array) any {
+	switch arr := arr.(type) {
+	case *types.UUIDArray:
+		return nilPtrOf[duckdb.UUID]()
+	case *array.Timestamp, *array.Date32, *array.Date64:
+		return nilPtrOf[time.Time]()
+	case *array.Boolean:
+		return nilPtrOf[bool]()
+	case *array.Int8:
+		return nilPtrOf[int8]()
+	case *array.Int16:
+		return nilPtrOf[int16]()
+	case *array.Int32:
+		return nilPtrOf[int32]()
+	case *array.Int64:
+		return nilPtrOf[int64]()
+	case *array.Uint8, *array.Uint16:
+		return nilPtrOf[uint32]() // use uint32
+	case *array.Uint32:
+		return nilPtrOf[uint32]()
+	case *array.Uint64:
+		return nilPtrOf[uint64]()
+	case *array.Float32:
+		return nilPtrOf[float32]()
+	case *array.Float64:
+		return nilPtrOf[float64]()
+	case *array.String:
+		return nilPtrOf[string]()
+	case *array.Binary, *array.LargeBinary, *array.FixedSizeBinary:
+		return nilPtrOf[[]byte]()
+	case *array.Map:
+		// unsupported in appender: use string
+		return nilPtrOf[string]()
+	case *array.Struct:
+		// Can't create a Go struct dynamically and maps are unsupported: use string
+		return nilPtrOf[string]()
+	case array.ListLike: // should be after *array.Map
+		v := getTypedNilValue(arr.ListValues())
+		return reflect.New(reflect.SliceOf(reflect.TypeOf(v))).Interface()
+	default:
+		return nilPtrOf[string]()
+	}
+}
+
+// getValue assumes the value at i is valid
+func getValue(arr arrow.Array, i int) any {
+	switch arr := arr.(type) {
+	case *types.UUIDArray:
+		v, _ := arr.Value(i).MarshalBinary()
+		return duckdb.UUID(v)
+	case *array.Timestamp:
+		timeUnit := arr.DataType().(*arrow.TimestampType).Unit
+		return arr.Value(i).ToTime(timeUnit)
+	case *array.Date32:
+		return arr.Value(i).ToTime()
+	case *array.Date64:
+		return arr.Value(i).ToTime()
+	case *array.Boolean:
+		return arr.Value(i)
+	case *array.Int8:
+		return arr.Value(i)
+	case *array.Int16:
+		return arr.Value(i)
+	case *array.Int32:
+		return arr.Value(i)
+	case *array.Int64:
+		return arr.Value(i)
+	case *array.Uint8:
+		return uint32(arr.Value(i)) // use uint32
+	case *array.Uint16:
+		return uint32(arr.Value(i)) // use uint32
+	case *array.Uint32:
+		return arr.Value(i)
+	case *array.Uint64:
+		return arr.Value(i)
+	case *array.Float32:
+		return arr.Value(i)
+	case *array.Float64:
+		return arr.Value(i)
+	case *array.String:
+		return arr.Value(i)
+	case *array.Binary:
+		return arr.Value(i)
+	case *array.LargeBinary:
+		return arr.Value(i)
+	case *array.FixedSizeBinary:
+		return arr.Value(i)
+	case *array.Map:
+		// unsupported in appender: use string
+		return arr.ValueStr(i)
+	case array.ListLike: // should be after *array.Map
+		return arrowListToGoSlice(arr, i)
+	case *array.Struct:
+		// Can't create a Go struct dynamically and maps are unsupported: use string
+		return arr.ValueStr(i)
+	default:
+		return arr.ValueStr(i)
+	}
+}
+
+// Convert an Arrow list to a Go slice where each element is a pointer, to be able to represent nulls
+func arrowListToGoSlice(arr array.ListLike, i int) any {
+	from, to := arr.ValueOffsets(i)
+	slice := array.NewSlice(arr.ListValues(), from, to)
+	defer slice.Release()
+
+	lv := getTypedNilValue(arr.ListValues())
+	val := reflect.MakeSlice(reflect.SliceOf(reflect.TypeOf(lv)), slice.Len(), slice.Len())
+	for i := 0; i < slice.Len(); i++ {
+		if slice.IsNull(i) {
+			continue
+		}
+		// slice of pointers, make everything a pointer
+		sv := reflect.ValueOf(getValue(slice, i))
+		psv := reflect.New(sv.Type())
+		psv.Elem().Set(sv)
+		val.Index(i).Set(psv)
+	}
+	return val.Interface()
+}
+
+func transformRecordToGoType(record arrow.Record, arrowFields []arrow.Field, colList schema.ColumnList) ([][]driver.Value, error) {
+	res := make([][]driver.Value, record.NumRows())
+	tc := len(colList)
+	for i := range res {
+		res[i] = make([]driver.Value, tc)
+	}
+
+	for i, f := range arrowFields { // i: arrow column index
+		j := colList.Index(f.Name) // look up the column index in the destination table
+		if j == -1 {
+			return nil, fmt.Errorf("column not found: %s", f.String()) // should never happen
+		}
+		arr := record.Column(i)
+		for k := range res {
+			if arr.IsValid(k) {
+				res[k][j] = getValue(arr, k)
+			}
+		}
+	}
+	return res, nil
+}
+
+func nilPtrOf[T any]() *T {
+	return nil // typed nil
+}

plugins/destination/duckdb/client/types.go
@@ -22,10 +22,8 @@ func transformFieldsForWriting(fields []arrow.Field) []arrow.Field {
 
 func transformTypeForWriting(dt arrow.DataType) arrow.DataType {
 	switch dt := dt.(type) {
-	case *arrow.StructType:
-		return arrow.StructOf(transformFieldsForWriting(dt.Fields())...)
-	case *arrow.MapType:
-		return arrow.MapOf(transformTypeForWriting(dt.KeyType()), transformTypeForWriting(dt.ItemType()))
+	case *arrow.StructType, *arrow.MapType:
+		return arrow.BinaryTypes.String
 	case arrow.ListLikeType:
 		return arrow.ListOf(transformTypeForWriting(dt.Elem()))
 	case *types.UUIDType, *types.JSONType:
@@ -37,19 +35,8 @@ func transformTypeForWriting(dt arrow.DataType) arrow.DataType {
 
 func arrowToDuckDB(dt arrow.DataType) string {
 	switch dt := dt.(type) {
-	case *arrow.StructType:
-		builder := new(strings.Builder)
-		builder.WriteString("struct(")
-		for i, field := range dt.Fields() {
-			if i > 0 {
-				builder.WriteString(", ")
-			}
-			builder.WriteString(sanitizeID(field.Name) + " " + arrowToDuckDB(field.Type))
-		}
-		builder.WriteString(")")
-		return builder.String()
-	case *arrow.MapType:
-		return "map(" + arrowToDuckDB(dt.KeyType()) + ", " + arrowToDuckDB(dt.ItemType()) + ")"
+	case *arrow.StructType, *arrow.MapType:
+		return "json"
 	case arrow.ListLikeType:
 		return arrowToDuckDB(dt.Elem()) + "[]"
 	case *arrow.BooleanType:

plugins/destination/duckdb/go.mod
@@ -68,7 +68,7 @@ require (
 	github.com/kataras/sitemap v0.0.6 // indirect
 	github.com/kataras/tunnel v0.0.4 // indirect
 	github.com/klauspost/asmfmt v1.3.2 // indirect
-	github.com/klauspost/compress v1.17.5 // indirect
+	github.com/klauspost/compress v1.17.6 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
 	github.com/labstack/echo/v4 v4.11.4 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
@@ -113,15 +113,15 @@ require (
 	go.opentelemetry.io/otel/trace v1.20.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
 	golang.org/x/arch v0.6.0 // indirect
-	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/exp v0.0.0-20240119083558-1b970713d09a // indirect
-	golang.org/x/mod v0.14.0 // indirect
-	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/mod v0.15.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sync v0.6.0 // indirect
-	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
-	golang.org/x/tools v0.17.0 // indirect
+	golang.org/x/tools v0.18.0 // indirect
 	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240125205218-1f4bbc51befe // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240125205218-1f4bbc51befe // indirect
@@ -134,3 +134,5 @@ require (
 
 // github.com/cloudquery/jsonschema @ cqmain
 replace github.com/invopop/jsonschema => github.com/cloudquery/jsonschema v0.0.0-20240202134451-d771afde32fb
+
+replace github.com/marcboeker/go-duckdb => github.com/taniabogatsch/go-duckdb v0.0.0-20240220143428-bc9d8238dc19

commit 88e97d9c78bc06536a00b2bdd421880260260384

chore: Re-enable no-migrate option for DuckDB (#16798)
This was broken in https://github.com/cloudquery/cloudquery/pull/16668

Chore, because it wasn't released.

plugins/destination/duckdb/client/client.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"runtime"
 	"strings"
+	"sync"
 
 	internalPlugin "github.com/cloudquery/cloudquery/plugins/destination/duckdb/resources/plugin"
 	"github.com/cloudquery/plugin-sdk/v4/plugin"
@@ -26,10 +27,13 @@ type Client struct {
 	db        *sql.DB
 	conn      driver.Conn // used in Appender
 
-	logger   zerolog.Logger
-	spec     Spec
-	writer   *batchwriter.BatchWriter
-	dbTables schema.Tables // current state of tables, used in Appender
+	logger zerolog.Logger
+	spec   Spec
+	writer *batchwriter.BatchWriter
+
+	// current state of tables, used in Appender
+	dbTablesMu *sync.RWMutex
+	dbTables   map[string]*schema.Table
 }
 
 var _ plugin.Client = (*Client)(nil)
@@ -37,7 +41,9 @@ var _ plugin.Client = (*Client)(nil)
 func New(ctx context.Context, logger zerolog.Logger, spec []byte, _ plugin.NewClientOptions) (plugin.Client, error) {
 	var err error
 	c := &Client{
-		logger: logger.With().Str("module", "duckdb-dest").Logger(),
+		logger:     logger.With().Str("module", "duckdb-dest").Logger(),
+		dbTablesMu: &sync.RWMutex{},
+		dbTables:   make(map[string]*schema.Table),
 	}
 	if err := json.Unmarshal(spec, &c.spec); err != nil {
 		return nil, fmt.Errorf("failed to unmarshal spec: %w", err)

plugins/destination/duckdb/client/migrate.go
@@ -28,35 +28,6 @@ type tableInfo struct {
 	columns []columnInfo
 }
 
-func (c *Client) duckDBTables(ctx context.Context, tables schema.Tables) (schema.Tables, error) {
-	var schemaTables schema.Tables
-	for _, table := range tables {
-		info, err := c.getTableInfo(ctx, table.Name)
-		if err != nil {
-			return nil, err
-		}
-		if info == nil {
-			continue
-		}
-		columns := make(schema.ColumnList, len(info.columns))
-		for i, col := range info.columns {
-			columns[i] = schema.Column{
-				Name:       col.name,
-				Type:       duckDBToArrow(col.typ),
-				NotNull:    col.notNull,
-				PrimaryKey: col.pk,
-				Unique:     col.unique,
-			}
-		}
-		schemaTables = append(schemaTables, &schema.Table{
-			Name:    table.Name,
-			Columns: columns,
-		})
-	}
-
-	return schemaTables, nil
-}
-
 func (*Client) normalizeColumns(tables schema.Tables) schema.Tables {
 	var normalized schema.Tables
 	for _, table := range tables {
@@ -131,14 +102,17 @@ func (c *Client) MigrateTables(ctx context.Context, msgs message.WriteMigrateTab
 		tables[i] = msg.Table
 	}
 
-	duckdbTables, err := c.duckDBTables(ctx, tables)
-	if err != nil {
-		return err
+	duckdbTables := make(schema.Tables, 0, len(tables))
+	for _, table := range tables {
+		t, err := c.getTableInfo(ctx, table.Name)
+		if err != nil {
+			return err
+		}
+		if t != nil {
+			duckdbTables = append(duckdbTables, t)
+			c.setTableInfoCache(t)
+		}
 	}
-	c.dbTables = duckdbTables
-	defer func() {
-		c.dbTables = c.dbTables.FlattenTables()
-	}()
 
 	normalizedTables := c.normalizeColumns(tables)
 	normalizedTablesSafeMode := make(schema.Tables, 0, len(normalizedTables))
@@ -169,13 +143,12 @@ func (c *Client) MigrateTables(ctx context.Context, msgs message.WriteMigrateTab
 			if err := c.createTableIfNotExist(ctx, table.Name, table); err != nil {
 				return err
 			}
-			c.dbTables = nil // force refresh
 			continue
 		}
 
 		changes := table.GetChanges(duckdb)
 		if len(changes) > 0 {
-			c.dbTables = nil // force refresh
+			c.removeTableInfoCache(table.Name)
 		}
 		if c.canAutoMigrate(changes) {
 			c.logger.Info().Str("table", table.Name).Msg("Table exists, auto-migrating")
@@ -187,13 +160,6 @@ func (c *Client) MigrateTables(ctx context.Context, msgs message.WriteMigrateTab
 			if err := c.recreateTable(ctx, table); err != nil {
 				return err
 			}
-		}
-	}
-
-	if c.dbTables == nil {
-		c.dbTables, err = c.duckDBTables(ctx, tables)
-		if err != nil {
-			return fmt.Errorf("failed to get duckdb tables after migrations: %w", err)
 		}
 	}
 
@@ -274,7 +240,7 @@ func (c *Client) isColumnUnique(ctx context.Context, tableName string, columName
 	return false, nil
 }
 
-func (c *Client) getTableInfo(ctx context.Context, tableName string) (*tableInfo, error) {
+func (c *Client) getTableInfo(ctx context.Context, tableName string) (*schema.Table, error) {
 	info := tableInfo{}
 	rows, err := c.db.QueryContext(ctx, fmt.Sprintf(sqlTableInfo, tableName))
 	if err != nil {
@@ -311,5 +277,54 @@ func (c *Client) getTableInfo(ctx context.Context, tableName string) (*tableInfo
 		// Table doesn't exist
 		return nil, nil
 	}
-	return &info, nil
+
+	columns := make(schema.ColumnList, len(info.columns))
+	for i, col := range info.columns {
+		columns[i] = schema.Column{
+			Name:       col.name,
+			Type:       duckDBToArrow(col.typ),
+			NotNull:    col.notNull,
+			PrimaryKey: col.pk,
+			Unique:     col.unique,
+		}
+	}
+
+	return &schema.Table{
+		Name:    tableName,
+		Columns: columns,
+	}, nil
+}
+
+func (c *Client) removeTableInfoCache(tableName string) {
+	c.dbTablesMu.Lock()
+	delete(c.dbTables, tableName)
+	c.dbTablesMu.Unlock()
+}
+
+func (c *Client) setTableInfoCache(table *schema.Table) {
+	c.dbTablesMu.Lock()
+	c.dbTables[table.Name] = table
+	c.dbTablesMu.Unlock()
+}
+
+func (c *Client) getCacheTableInfo(ctx context.Context, tableName string) (*schema.Table, error) {
+	c.dbTablesMu.RLock()
+	duckdbTableInfo := c.dbTables[tableName]
+	c.dbTablesMu.RUnlock()
+	if duckdbTableInfo != nil {
+		return duckdbTableInfo, nil
+	}
+
+	t, err := c.getTableInfo(ctx, tableName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get table metadata: %w", err)
+	}
+	if t == nil {
+		return nil, fmt.Errorf("table not found")
+	}
+
+	c.dbTablesMu.Lock()
+	c.dbTables[tableName] = t
+	c.dbTablesMu.Unlock()
+	return t, nil
 }

plugins/destination/duckdb/client/write.go
@@ -129,7 +129,7 @@ func (c *Client) Write(ctx context.Context, msgs <-chan message.WriteMessage) er
 	return nil
 }
 
-func (c *Client) appendRows(table *schema.Table, msgs message.WriteInserts) (retErr error) {
+func (c *Client) appendRows(ctx context.Context, table *schema.Table, msgs message.WriteInserts) (retErr error) {
 	if c.conn == nil {
 		// connecting before MigrateTable results in appender creation failure
 		var err error
@@ -150,9 +150,9 @@ func (c *Client) appendRows(table *schema.Table, msgs message.WriteInserts) (ret
 		}
 	}()
 
-	tableState := c.dbTables.Get(table.Name)
-	if tableState == nil {
-		return fmt.Errorf("table %s not found in duckdb", table.Name) // should never happen as appender would've failed
+	tableState, err := c.getCacheTableInfo(ctx, table.Name)
+	if err != nil {
+		return fmt.Errorf("table %s not found in duckdb: %w", table.Name, err) // should never happen as appender would've failed
 	}
 
 	fields := msgs[0].Record.Schema().Fields()
@@ -180,7 +180,7 @@ func (c *Client) WriteTableBatch(ctx context.Context, name string, msgs message.
 	table := msgs[0].GetTable()
 
 	if useAppender && len(table.PrimaryKeys()) == 0 {
-		return c.appendRows(table, msgs)
+		return c.appendRows(ctx, table, msgs)
 	}
 
 	writeStart := time.Now()