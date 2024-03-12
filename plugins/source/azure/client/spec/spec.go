85de3acffa9364a81ae4a2cdd73d4b7272c9d160
candiduslynx <candiduslynx@gmail.com>
2024-03-12 17:29:28 +0200

    feat: Add examples to the spec JSON schema

--- plugins/source/azure/client/spec/spec.go
+++ plugins/source/azure/client/spec/spec.go
@@ -12,15 +12,15 @@ import (
 type Spec struct {
 	// Specify which subscriptions to sync data from.
 	// Empty means all visible subscriptions.
-	Subscriptions []string `json:"subscriptions" jsonschema:"minLength=1,uniqueItems=true"`
+	Subscriptions []string `json:"subscriptions" jsonschema:"minLength=1,uniqueItems=true,example=subscription_id"`
 
 	// A list of subscription IDs that CloudQuery will skip syncing.
 	// This is useful if CloudQuery is discovering the list of subscription IDs and there are some subscriptions that you want to not even attempt syncing.
-	SkipSubscriptions []string `json:"skip_subscriptions" jsonschema:"minLength=1,uniqueItems=true"`
+	SkipSubscriptions []string `json:"skip_subscriptions" jsonschema:"minLength=1,uniqueItems=true,example=subscription_id"`
 
 	// The name of the cloud environment to use.
 	// See the [Azure CLI documentation](https://learn.microsoft.com/en-us/cli/azure/manage-clouds-azure-cli) for more information.
-	CloudName string `json:"cloud_name" jsonschema:"minLength=1"`
+	CloudName string `json:"cloud_name" jsonschema:"minLength=1,example=cloud_name"`
 
 	// Enabling this setting will force all `id` column values to be lowercase.
 	// This is useful to avoid case sensitivity and uniqueness issues around the `id` primary keys.
@@ -29,11 +29,11 @@ type Spec struct {
 	// An OIDC token can be used to authenticate with Azure instead of `AZURE_CLIENT_SECRET`.
 	// This is useful for Azure AD workload identity federation.
 	// When using this option, the `AZURE_CLIENT_ID` and `AZURE_TENANT_ID` environment variables must be set.
-	OIDCToken string `json:"oidc_token" jsonschema:"minLength=1"`
+	OIDCToken string `json:"oidc_token" jsonschema:"minLength=1,example=oidc_token"`
 
 	// The best effort maximum number of Go routines to use.
 	// Lower this number to reduce memory usage.
-	Concurrency int `json:"concurrency" jsonschema:"minimum=1,default=50000"`
+	Concurrency int `json:"concurrency" jsonschema:"minimum=1,default=50000,example=50000"`
 
 	// During initialization the Azure source plugin discovers all resource groups
 	// and enabled resource providers per subscription, to be used later on during the sync process.
@@ -41,7 +41,7 @@ type Spec struct {
 	// This setting controls the maximum number of concurrent requests to the Azure API during discovery.
 	// Only accounts with many subscriptions should require modifying this setting,
 	// to either lower it to avoid network errors, or to increase it to speed up the discovery process.
-	DiscoveryConcurrency int `json:"discovery_concurrency" jsonschema:"minimum=1,default=400"`
+	DiscoveryConcurrency int `json:"discovery_concurrency" jsonschema:"minimum=1,default=400,example=400"`
 
 	// Retry options to pass to the Azure Go SDK, see more details
 	// [here](https://github.com/Azure/azure-sdk-for-go/blob/f951bf52fb68cbb978b7b95d41147693c1863366/sdk/azcore/policy/policy.go#L86).
