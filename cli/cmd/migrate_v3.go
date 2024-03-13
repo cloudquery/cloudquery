910503cbe3d4548347963109ba288d2e40ebe3b7
bbernays <ben@bernays.me>
2024-03-13 13:12:10 -0500

    Update migrate_v3.go

--- cli/cmd/migrate_v3.go
+++ cli/cmd/migrate_v3.go
@@ -36,9 +36,7 @@ func migrateConnectionV3(ctx context.Context, sourceClient *managedplugin.Client
 		}
 		if destinationSpecs[i].WriteMode == specs.WriteModeAppend {
 			opts = append(opts, transformer.WithRemovePKs(), transformer.WithRemovePKs())
-			if sourceSpec.DeterministicCQID {
-				opts = append(opts, transformer.WithRemoveUniqueConstraints())
-			}
+			opts = append(opts, transformer.WithRemoveUniqueConstraints())
 		} else if destinationSpecs[i].PKMode == specs.PKModeCQID {
 			opts = append(opts, transformer.WithRemovePKs())
 			opts = append(opts, transformer.WithCQIDPrimaryKey())
