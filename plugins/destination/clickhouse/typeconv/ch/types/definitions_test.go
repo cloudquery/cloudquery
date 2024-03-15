d3ad79518bd9536cfbaa41b84a49ae68baf8422c
candiduslynx <candiduslynx@gmail.com>
2024-03-15 10:54:00 +0200

    tests

--- plugins/destination/clickhouse/typeconv/ch/types/definitions_test.go
+++ plugins/destination/clickhouse/typeconv/ch/types/definitions_test.go
@@ -26,6 +26,10 @@ func TestFieldType(t *testing.T) {
 		{dataType: &arrow.FixedSizeBinaryType{ByteWidth: 125}, expected: "FixedString(125)"},
 		{dataType: new(arrow.Date32Type), expected: "Date32"},
 		{dataType: new(arrow.Date64Type), expected: "DateTime"},
+		{dataType: &arrow.Time32Type{Unit: arrow.Second}, expected: "DateTime64(0)"},
+		{dataType: &arrow.Time32Type{Unit: arrow.Millisecond}, expected: "DateTime64(3)"},
+		{dataType: &arrow.Time64Type{Unit: arrow.Microsecond}, expected: "DateTime64(6)"},
+		{dataType: &arrow.Time64Type{Unit: arrow.Nanosecond}, expected: "DateTime64(9)"},
 	} {
 		ensureDefinition(t, tc)
 	}
