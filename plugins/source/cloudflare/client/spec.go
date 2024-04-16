33df4fd45a3fef0199695fdbfd9c485453e71f46
Erez Rokah <erezrokah@users.noreply.github.com>
2024-04-16 13:25:58 +0100

    fix: Require either `api_token` or `api_key,api_email` in schema (#17605)
    #### Summary
    
    Related to https://github.com/cloudquery/cloudquery-issues/issues/1515 (internal issue). This aligns the JSON schema with the [plugin requirements](https://hub.cloudquery.io/plugins/source/cloudquery/cloudflare/latest/docs#configuration-cloudflare-spec) to have either `api_token` or `api_key` and `api_email` required.
    
    This is not a breaking change since we only log a warning on schema failures.
    
    Technically you can configure the plugin only using a `CLOUDFLARE_API_TOKEN` env variable or (`CLOUDFLARE_API_KEY, CLOUDFLARE_EMAIL`) but that's not the standard in our plugins and we should remove it

--- plugins/source/cloudflare/client/spec.go
+++ plugins/source/cloudflare/client/spec.go
@@ -1,23 +1,34 @@
 package client
 
-import _ "embed"
+import (
+	_ "embed"
+
+	"github.com/invopop/jsonschema"
+)
 
 const (
 	defaultConcurrency = 10000
 )
 
 type Spec struct {
-	Token       string   `json:"api_token,omitempty"`
-	ApiKey      string   `json:"api_key,omitempty"`
-	ApiEmail    string   `json:"api_email,omitempty"`
+	Token       string   `json:"api_token,omitempty" jsonschema:"minLength=1,example=${CLOUDFLARE_API_TOKEN}"`
+	ApiKey      string   `json:"api_key,omitempty" jsonschema:"minLength=1,example=${CLOUDFLARE_API_KEY}"`
+	ApiEmail    string   `json:"api_email,omitempty" jsonschema:"minLength=1,example=${CLOUDFLARE_EMAIL}"`
 	Accounts    []string `json:"accounts,omitempty"`
 	Zones       []string `json:"zones,omitempty"`
-	Concurrency int      `json:"concurrency,omitempty"`
+	Concurrency int      `json:"concurrency,omitempty" jsonschema:"default=10000"`
 }
 
 func (s *Spec) SetDefaults() {
 	if s.Concurrency == 0 {
 		s.Concurrency = defaultConcurrency
+	}
+}
+
+func (Spec) JSONSchemaExtend(sc *jsonschema.Schema) {
+	sc.OneOf = []*jsonschema.Schema{
+		{Required: []string{"api_token"}},
+		{Required: []string{"api_key", "api_email"}},
 	}
 }
 
