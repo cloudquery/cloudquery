44e90b0128b3f0cbf605eb0909b1606df6faf07b
candiduslynx <candiduslynx@gmail.com>
2024-04-18 21:17:54 +0300

    ret linux/arm64

--- plugins/destination/sqlite/main.go
+++ plugins/destination/sqlite/main.go
@@ -22,6 +22,7 @@ func main() {
 		client.New,
 		plugin.WithBuildTargets([]plugin.BuildTarget{
 			{OS: plugin.GoOSLinux, Arch: plugin.GoArchAmd64, CGO: true},
+			{OS: plugin.GoOSLinux, Arch: plugin.GoArchArm64, CGO: true},
 			{OS: plugin.GoOSWindows, Arch: plugin.GoArchAmd64, CGO: true},
 			{OS: plugin.GoOSDarwin, Arch: plugin.GoArchAmd64, CGO: true},
 			{OS: plugin.GoOSDarwin, Arch: plugin.GoArchArm64, CGO: true},
