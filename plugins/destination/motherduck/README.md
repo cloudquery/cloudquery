07fc4a8957940b980edb420d3bfbda46741beb82
Kemal <223029+disq@users.noreply.github.com>
2024-05-16 09:24:40 +0100

    chore: Remove Motherduck plugin (#17991)
    This was never published, `DuckDB` plugin is used to connect to MotherDuck. 🦆

--- plugins/destination/motherduck/README.md
+++
@@ -1,7 +0,0 @@
-# CloudQuery MotherDuck Destination Plugin
-
-This destination plugin lets you sync data from a CloudQuery source to MotherDuck.
-
-## Links
-
-- [User Guide](https://cloudquery.io/docs/plugins/destinations/motherduck/overview)
--- plugins/destination/motherduck/resources/plugin/plugin.go
+++
@@ -1,9 +0,0 @@
-package plugin
-
-// Don't move this file to a different package, it's used by Go releaser to embed the version in the binary.
-var (
-	Name    = "motherduck"
-	Kind    = "destination"
-	Team    = "cloudquery"
-	Version = "development"
-)
