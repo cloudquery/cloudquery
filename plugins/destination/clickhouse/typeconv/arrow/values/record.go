a8e7200322cc4b501aae1bff0a2f334f5c1d41cb
candiduslynx <candiduslynx@gmail.com>
2024-05-28 18:45:20 +0300

    refactor: Return multi-row records on read

--- plugins/destination/clickhouse/typeconv/arrow/values/record.go
+++ plugins/destination/clickhouse/typeconv/arrow/values/record.go
@@ -1,24 +1,14 @@
 package values
 
 import (
-	"fmt"
-
-	"github.com/apache/arrow/go/v16/arrow"
 	"github.com/apache/arrow/go/v16/arrow/array"
-	"github.com/apache/arrow/go/v16/arrow/memory"
 )
 
-func Record(sc *arrow.Schema, data []any) (arrow.Record, error) {
-	if len(data) != len(sc.Fields()) {
-		return nil, fmt.Errorf("mismatching field amount: have %d, want %d", len(data), len(sc.Fields()))
-	}
-
-	builder := array.NewRecordBuilder(memory.DefaultAllocator, sc)
+func AppendToRecordBuilder(builder *array.RecordBuilder, data []any) error {
 	for i, builder := range builder.Fields() {
 		if err := buildValue(builder, data[i]); err != nil {
-			return nil, err
+			return err
 		}
 	}
-
-	return builder.NewRecord(), nil
+	return nil
 }
