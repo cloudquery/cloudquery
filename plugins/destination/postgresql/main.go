fc4ff27d37f9250b4cf912474073169406cb01fa
Ștefan Muraru <muraru.stefaan@gmail.com>
2024-06-06 14:21:31 +0300

    feat: Remove logging of error events to Sentry in plugins (#18165)

--- plugins/destination/postgresql/main.go
+++ plugins/destination/postgresql/main.go
@@ -11,10 +11,6 @@ import (
 	"github.com/cloudquery/plugin-sdk/v4/serve"
 )
 
-const (
-	sentryDSN = "https://19d1257d36854a51b17c06614e76dc2d@o1396617.ingest.sentry.io/4503896817336320"
-)
-
 func main() {
 	p := pluginSDK.NewPlugin(plugin.Name, plugin.Version, client.New,
 		pluginSDK.WithKind(plugin.Kind),
@@ -22,12 +18,8 @@ func main() {
 		pluginSDK.WithJSONSchema(spec.JSONSchema),
 		pluginSDK.WithConnectionTester(client.ConnectionTester),
 	)
-	server := serve.Plugin(p,
-		serve.WithPluginSentryDSN(sentryDSN),
-		serve.WithDestinationV0V1Server(),
-	)
-	err := server.Serve(context.Background())
-	if err != nil {
+
+	if err := serve.Plugin(p, serve.WithDestinationV0V1Server()).Serve(context.Background()); err != nil {
 		log.Fatalf("failed to serve plugin: %v", err)
 	}
 }
