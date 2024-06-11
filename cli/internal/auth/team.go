b78e3e2994cb443ce241f3de50028335e433227a
erezrokah <erezrokah@users.noreply.github.com>
2024-06-11 18:32:08 +0100

    feat: When a config file is missing, prompt the user to select run `cloudquery switch <team>`

--- cli/internal/auth/team.go
+++ cli/internal/auth/team.go
@@ -2,15 +2,30 @@ package auth
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	cqapi "github.com/cloudquery/cloudquery-api-go"
 	"github.com/cloudquery/cloudquery-api-go/auth"
 	"github.com/cloudquery/cloudquery-api-go/config"
 	"github.com/cloudquery/cloudquery/cli/internal/api"
+	"github.com/cloudquery/cloudquery/cli/internal/team"
 )
+
+func getAvailableUserTeams(ctx context.Context, token auth.Token) []string {
+	cl, err := team.NewClient(token.Value)
+	if err != nil {
+		return nil
+	}
+	teams, err := cl.ListAllTeams(ctx)
+	if err != nil {
+		return nil
+	}
+	return teams
+}
 
 // GetTeamForToken returns the team for the given token
 // If the token is a bearer token, we need to get the team from the configuration.
@@ -19,6 +34,14 @@ func GetTeamForToken(ctx context.Context, token auth.Token) (string, error) {
 	switch token.Type {
 	case auth.BearerToken:
 		team, err := config.GetValue("team")
+		if errors.Is(err, os.ErrNotExist) {
+			teams := getAvailableUserTeams(ctx, token)
+			message := "your current team is not set.\n\nTo set your current team, run `cloudquery switch <team>`"
+			if len(teams) > 0 {
+				message = fmt.Sprintf("your current team is not set.\n\nAvailable teams: %s.\n\nTo set your current team, run `cloudquery switch <team>`", strings.Join(teams, ", "))
+			}
+			return "", fmt.Errorf(message)
+		}
 		if err != nil {
 			return "", fmt.Errorf("failed to get team from config: %w", err)
 		}
