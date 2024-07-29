479ce4ddf9fba1f1df87848023d30b01e2feb209
erezrokah <erezrokah@users.noreply.github.com>
2024-07-29 14:33:54 +0100

    feat: Add `PermissionsNeeded` to tables schema

--- cli/internal/docs/json.go
+++ cli/internal/docs/json.go
@@ -10,13 +10,14 @@ import (
 )
 
 type jsonTable struct {
-	Name          string       `json:"name"`
-	Title         string       `json:"title"`
-	Description   string       `json:"description"`
-	Columns       []jsonColumn `json:"columns"`
-	Relations     []jsonTable  `json:"relations"`
-	IsPaid        bool         `json:"is_paid,omitempty"`
-	IsIncremental bool         `json:"is_incremental,omitempty"`
+	Name              string       `json:"name"`
+	Title             string       `json:"title"`
+	Description       string       `json:"description"`
+	Columns           []jsonColumn `json:"columns"`
+	Relations         []jsonTable  `json:"relations"`
+	IsPaid            bool         `json:"is_paid,omitempty"`
+	IsIncremental     bool         `json:"is_incremental,omitempty"`
+	PermissionsNeeded []string     `json:"permissions_needed,omitempty"`
 }
 
 type jsonColumn struct {
@@ -59,13 +60,14 @@ func (g *Generator) jsonifyTables(tables schema.Tables) []jsonTable {
 			}
 		}
 		jsonTables[i] = jsonTable{
-			Name:          table.Name,
-			Title:         table.Title,
-			Description:   table.Description,
-			Columns:       jsonColumns,
-			IsPaid:        table.IsPaid,
-			IsIncremental: table.IsIncremental,
-			Relations:     g.jsonifyTables(table.Relations),
+			Name:              table.Name,
+			Title:             table.Title,
+			Description:       table.Description,
+			Columns:           jsonColumns,
+			IsPaid:            table.IsPaid,
+			IsIncremental:     table.IsIncremental,
+			Relations:         g.jsonifyTables(table.Relations),
+			PermissionsNeeded: table.PermissionsNeeded,
 		}
 	}
 	return jsonTables
