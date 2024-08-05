c427c6bdd0af685a44f35197db0d22e3a937d77e
Jack Gluskin <jack@cloudquery.io>
2024-08-05 16:52:35 +0100

    Convert PostgreSQL connection string to secret

--- plugins/destination/postgresql/cloud-config-ui/src/utils/generateConnectionUrl.ts
+++ plugins/destination/postgresql/cloud-config-ui/src/utils/generateConnectionUrl.ts
@@ -1,15 +1,9 @@
 import { FormValues } from './formSchema';
 
-export function generateConnectionUrl(values: FormValues, replaceWithEnvs: boolean): string {
-  let { username } = values;
-
-  if (replaceWithEnvs) {
-    username = replaceWithEnvs ? (values.username ? '${username}' : '') : values.username;
-  }
-
+export function generateConnectionUrl(values: FormValues): string {
   const password = values.password ? '${password}' : '';
 
-  let finalUrl = `dbtype='postgresql' user='${username}' password='${password}' host='${escapeSingleQuotesAndBackslashes(values.host)}' dbname='${escapeSingleQuotesAndBackslashes(values.database)}'`;
+  let finalUrl = `dbtype='postgresql' user='${values.username}' password='${password}' host='${escapeSingleQuotesAndBackslashes(values.host)}' dbname='${escapeSingleQuotesAndBackslashes(values.database)}'`;
 
   if (values.port) {
     finalUrl += ` port='${values.port}'`;
