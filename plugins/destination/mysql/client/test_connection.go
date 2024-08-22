64b23a3e3fa351417420233f944f1bc46a6f4812
Kemal Hadimli <disq@users.noreply.github.com>
2024-08-22 17:24:21 +0300

    Don't need the errors.As

--- plugins/destination/mysql/client/test_connection.go
+++ plugins/destination/mysql/client/test_connection.go
@@ -2,7 +2,6 @@ package client
 
 import (
 	"context"
-	"errors"
 
 	"github.com/cloudquery/plugin-sdk/v4/plugin"
 	"github.com/rs/zerolog"
@@ -11,10 +10,6 @@ import (
 func NewConnectionTester(createClientFn func(context.Context, zerolog.Logger, []byte, plugin.NewClientOptions) (plugin.Client, error)) plugin.ConnectionTester {
 	return func(ctx context.Context, logger zerolog.Logger, spec []byte) error {
 		_, err := createClientFn(ctx, logger, spec, plugin.NewClientOptions{})
-		var tcErr *plugin.TestConnError
-		if errors.As(err, &tcErr) {
-			return tcErr
-		}
 		return err
 	}
 }
