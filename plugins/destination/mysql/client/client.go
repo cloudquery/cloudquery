fb59a592d4e9966b72288603fc2c25fe84864a8b
Kemal <223029+disq@users.noreply.github.com>
2024-08-22 19:06:48 +0300

    fix: Better test connection error codes (#18985)
    ~Still not great, but a small improvement to get more error codes out.~ I think this should cover most cases now.

--- plugins/destination/mysql/client/client.go
+++ plugins/destination/mysql/client/client.go
@@ -6,14 +6,14 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"net"
 	"strings"
 	"time"
 
 	"github.com/cloudquery/plugin-sdk/v4/plugin"
 	"github.com/cloudquery/plugin-sdk/v4/writers/batchwriter"
+	"github.com/go-sql-driver/mysql"
 	"github.com/rs/zerolog"
-
-	mysql "github.com/go-sql-driver/mysql"
 )
 
 type ServerType int64
@@ -38,19 +38,17 @@ type Client struct {
 	maxIndexLength int
 }
 
-var errValidateConnectionFailed = errors.New("failed to validate mysql connection")
-
 func New(ctx context.Context, logger zerolog.Logger, spec []byte, _ plugin.NewClientOptions) (plugin.Client, error) {
 	c := &Client{logger: logger.With().Str("module", "mysql").Logger()}
 	var err error
 
 	if err := json.Unmarshal(spec, &c.spec); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal spec: %w", err)
+		return nil, plugin.NewTestConnError(codeInvalidSpec, err)
 	}
 
 	c.spec.SetDefaults()
 	if err := c.spec.Validate(); err != nil {
-		return nil, err
+		return nil, plugin.NewTestConnError(codeInvalidSpec, err)
 	}
 	c.writer, err = batchwriter.New(c, batchwriter.WithLogger(c.logger), batchwriter.WithBatchSize(c.spec.BatchSize), batchwriter.WithBatchSizeBytes(c.spec.BatchSizeBytes))
 	if err != nil {
@@ -59,7 +57,7 @@ func New(ctx context.Context, logger zerolog.Logger, spec []byte, _ plugin.NewCl
 
 	dsn, err := mysql.ParseDSN(c.spec.ConnectionString)
 	if err != nil {
-		return nil, fmt.Errorf("invalid MySQL connection string: %w", err)
+		return nil, plugin.NewTestConnError("INVALID_DSN", err)
 	}
 	if dsn.Params == nil {
 		dsn.Params = map[string]string{}
@@ -67,7 +65,7 @@ func New(ctx context.Context, logger zerolog.Logger, spec []byte, _ plugin.NewCl
 	dsn.Params["parseTime"] = "true"
 	db, err := sql.Open("mysql", dsn.FormatDSN())
 	if err != nil {
-		return nil, fmt.Errorf("failed to open mysql connection: %w", err)
+		return nil, plugin.NewTestConnError(codeConnectFailed, err)
 	}
 
 	db.SetConnMaxLifetime(time.Minute * 3)
@@ -76,7 +74,7 @@ func New(ctx context.Context, logger zerolog.Logger, spec []byte, _ plugin.NewCl
 	c.db = db
 
 	if err := c.validateConnection(ctx); err != nil {
-		return nil, errors.Join(errValidateConnectionFailed, err)
+		return nil, fmt.Errorf("failed to validate connection: %w", err)
 	}
 
 	if err := c.getVersion(ctx); err != nil {
@@ -91,16 +89,19 @@ func New(ctx context.Context, logger zerolog.Logger, spec []byte, _ plugin.NewCl
 func (c *Client) validateConnection(ctx context.Context) error {
 	rows, err := c.db.QueryContext(ctx, "select database()")
 	if err != nil {
-		return err
+		return categorizeError(err, "QUERY_DATABASE_FAILED")
 	}
 	defer rows.Close()
 	for rows.Next() {
 		var name *string
 		if err := rows.Scan(&name); err != nil {
-			return err
+			return categorizeError(err, "SCAN_DATABASE_FAILED")
 		}
 		if name == nil {
-			return fmt.Errorf("default database is not selected. Update connection string to include database name")
+			return plugin.NewTestConnError(
+				"DEFAULT_DATABASE_FAILED",
+				errors.New("default database is not selected. Update connection string to include database name"),
+			)
 		}
 	}
 	return nil
@@ -109,13 +110,13 @@ func (c *Client) validateConnection(ctx context.Context) error {
 func (c *Client) getVersion(ctx context.Context) error {
 	rows, err := c.db.QueryContext(ctx, "SELECT VERSION()")
 	if err != nil {
-		return err
+		return categorizeError(err, "QUERY_VERSION_FAILED")
 	}
 	defer rows.Close()
 	for rows.Next() {
 		var versionString *string
 		if err := rows.Scan(&versionString); err != nil {
-			return err
+			return categorizeError(err, "SCAN_VERSION_FAILED")
 		}
 		if strings.Contains(*versionString, "-MariaDB") {
 			c.serverType = ServerTypeMariaDB
@@ -152,4 +153,26 @@ func (c *Client) Close(ctx context.Context) error {
 		return fmt.Errorf("failed to close writer: %w", err)
 	}
 	return c.db.Close()
+}
+
+func categorizeError(err error, defaultCode string) error {
+	var opErr *net.OpError
+	if errors.As(err, &opErr) {
+		return plugin.NewTestConnError("UNREACHABLE", err)
+	}
+	var myErr *mysql.MySQLError
+	if errors.As(err, &myErr) {
+		switch myErr.Number {
+		case 1045:
+			return plugin.NewTestConnError("ACCESS_DENIED", err)
+		case 1049:
+			return plugin.NewTestConnError("UNKNOWN_DATABASE", err)
+		default:
+			if myErr.SQLState != [5]byte{} {
+				return plugin.NewTestConnError(fmt.Sprintf("MYSQL_ERROR_%d_%s", myErr.Number, myErr.SQLState), err)
+			}
+			return plugin.NewTestConnError(fmt.Sprintf("MYSQL_ERROR_%d", myErr.Number), err)
+		}
+	}
+	return plugin.NewTestConnError(defaultCode, err)
 }
