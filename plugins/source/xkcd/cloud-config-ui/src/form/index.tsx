38ea2eb1505796ffa9d1cc04e3a69ecafdf3821f
James Riley Wilburn <riley@cloudquery.io>
2024-08-26 09:48:01 -0400

    fix: XKCD plugin UI logo url

--- plugins/source/xkcd/cloud-config-ui/src/form/index.tsx
+++ plugins/source/xkcd/cloud-config-ui/src/form/index.tsx
@@ -66,7 +66,7 @@ export function Form({ initialValues }: Props) {
                 <Box display="flex" justifyContent="space-between" alignItems="center">
                   <Typography variant="h5">Configure source</Typography>
                   <Box display="flex" justifyContent="space-between" alignItems="center" gap={1.5}>
-                    <Logo src={`/images/xkcd.webp`} alt="XKCD" />
+                    <Logo src={`images/xkcd.webp`} alt="XKCD" />
                     <Typography variant="body1">XKCD</Typography>
                   </Box>
                 </Box>
