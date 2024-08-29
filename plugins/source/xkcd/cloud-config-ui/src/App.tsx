907b4fb53d19d0bb1ca598af61ca6f23e69d77e1
Chris Duflo <chris@cloudquery.io>
2024-08-29 14:58:46 -0400

    XKCD Source UI Generalization

--- plugins/source/xkcd/cloud-config-ui/src/App.tsx
+++ plugins/source/xkcd/cloud-config-ui/src/App.tsx
@@ -1,41 +1,67 @@
 import { Fragment, useMemo } from 'react';
 
 import { createThemeOptions } from '@cloudquery/cloud-ui';
-import { CloudAppMock, useFormHeightChange, useFormInit } from '@cloudquery/plugin-config-ui-lib';
+
+import {
+  CloudAppMock,
+  CloudQueryTables,
+  PluginContextProvider,
+  useFormHeightChange,
+  useFormInit,
+} from '@cloudquery/plugin-config-ui-lib';
+import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
-import Stack from '@mui/material/Stack';
 import createTheme from '@mui/material/styles/createTheme';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
 
+import config from './config';
+import tablesData from './data/__tables.json';
 import { Form } from './form';
-
 import { pluginUiMessageHandler } from './utils/messageHandler';
-import { prepareInitialValues } from './utils/prepareInitialValues';
 
 const useCloudAppMock =
   (process.env.REACT_APP_USE_CLOUD_APP_MOCK === 'true' || process.env.NODE_ENV !== 'production') &&
   window.self === window.top;
 const DevWrapper = useCloudAppMock ? CloudAppMock : Fragment;
 // eslint-disable-next-line unicorn/prefer-module
-const devWrapperProps: any = useCloudAppMock ? require('./.env.json') : undefined;
+const { plugin, ...devWrapperProps }: any = useCloudAppMock ? require('./.env.json') : {};
+
+const pluginProps = useCloudAppMock
+  ? plugin
+  : {
+      team: process.env.REACT_APP_PLUGIN_TEAM,
+      kind: process.env.REACT_APP_PLUGIN_KIND,
+      name: process.env.REACT_APP_PLUGIN_NAME,
+      version: process.env.REACT_APP_PLUGIN_VERSION,
+    };
 
 function App() {
-  const { initialValues, initialized } = useFormInit(pluginUiMessageHandler, false);
+  const { initialValues, initialized, teamName, context } = useFormInit(
+    pluginUiMessageHandler,
+    true,
+  );
+
   useFormHeightChange(pluginUiMessageHandler);
 
   const theme = useMemo(() => createTheme(createThemeOptions()), []);
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <DevWrapper {...devWrapperProps}>
-        {initialized && (
-          <Stack>
-            <Form initialValues={initialValues ? prepareInitialValues(initialValues) : undefined} />
-          </Stack>
-        )}
-      </DevWrapper>
-    </ThemeProvider>
+    <PluginContextProvider
+      config={config}
+      plugin={pluginProps}
+      teamName={teamName}
+      tablesData={tablesData as CloudQueryTables}
+      hideStepper={context === 'wizard'} // TODO: Delete after iframe deprecation
+    >
+      <Box>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <DevWrapper {...devWrapperProps}>
+            {initialized && <Form initialValues={initialValues} />}
+          </DevWrapper>
+        </ThemeProvider>
+      </Box>
+    </PluginContextProvider>
   );
 }
 
--- plugins/source/xkcd/cloud-config-ui/src/utils/formSchema.ts
+++ plugins/source/xkcd/cloud-config-ui/src/utils/formSchema.ts
@@ -1,42 +1,15 @@
-import { resetYupDefaultErrorMessages } from '@cloudquery/cloud-ui';
-import { generateName } from '@cloudquery/plugin-config-ui-lib';
-import * as yup from 'yup';
+import { FormMessagePayload } from '@cloudquery/plugin-config-ui-connector';
+import { useCoreFormSchema } from '@cloudquery/plugin-config-ui-lib';
 
-resetYupDefaultErrorMessages(yup);
+export function useFormSchema({
+  initialValues,
+}: {
+  initialValues?: FormMessagePayload['init']['initialValues'];
+}) {
+  const fields = {};
 
-export const formValidationSchema = yup.object({
-  name: yup
-    .string()
-    .default(generateName('xkcd'))
-    .matches(
-      /^[a-z](-?[\da-z]+)+$/,
-      'Name must consist of a lower case letter, followed by alphanumeric segments separated by single dashes',
-    )
-    .max(255)
-    .required(),
-  envs: yup
-    .array()
-    .of(
-      yup.object({
-        name: yup.string().default('').required(),
-        value: yup.string().default(''),
-      }),
-    )
-    .default([]),
-  spec: yup.object({}),
-  tables: yup
-    .object()
-    .test('valid tables', function (value: Record<string, true>) {
-      if (Object.keys(value || {}).filter((key) => value[key]).length === 0) {
-        return this.createError({
-          message: 'At least one table must be selected',
-          path: 'tables',
-        });
-      }
-
-      return true;
-    })
-    .default({ xkcd_comics: true }),
-});
-
-export type FormValues = yup.InferType<typeof formValidationSchema>;
+  return useCoreFormSchema({
+    initialValues,
+    fields,
+  });
+}
