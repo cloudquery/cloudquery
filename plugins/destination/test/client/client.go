7853f50bac9af237c5046ae6940b0452c78b80a8
Kemal Hadimli <disq@users.noreply.github.com>
2024-09-11 17:29:01 +0100

    feat: Add batching and more error options to test destination

--- plugins/destination/test/client/client.go
+++ plugins/destination/test/client/client.go
@@ -10,12 +10,14 @@ import (
 	"github.com/cloudquery/plugin-sdk/v4/message"
 	"github.com/cloudquery/plugin-sdk/v4/plugin"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
+	"github.com/cloudquery/plugin-sdk/v4/writers/batchwriter"
 	"github.com/rs/zerolog"
 )
 
 type Client struct {
 	logger zerolog.Logger
 	spec   Spec
+	writer *batchwriter.BatchWriter
 
 	plugin.UnimplementedSource
 }
@@ -27,11 +29,27 @@ func New(_ context.Context, logger zerolog.Logger, specBytes []byte, _ plugin.Ne
 	if err := json.Unmarshal(specBytes, &spec); err != nil {
 		return nil, fmt.Errorf("failed to unmarshal spec: %w", err)
 	}
+	spec.SetDefaults()
 
-	return &Client{
+	c := &Client{
 		logger: logger.With().Str("module", "test").Logger(),
 		spec:   spec,
-	}, nil
+	}
+
+	if spec.BatchWriter {
+		var err error
+		c.writer, err = batchwriter.New(c,
+			batchwriter.WithBatchSize(*c.spec.BatchSize),
+			batchwriter.WithBatchSizeBytes(*c.spec.BatchSizeBytes),
+			batchwriter.WithBatchTimeout(c.spec.BatchTimeout.Duration()),
+			batchwriter.WithLogger(c.logger),
+		)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return c, nil
 }
 
 func (*Client) Read(context.Context, *schema.Table, chan<- arrow.Record) error {
@@ -39,11 +57,28 @@ func (*Client) Read(context.Context, *schema.Table, chan<- arrow.Record) error {
 }
 
 //revive:disable We need to range over the channel to clear it, but revive thinks it can be removed
-func (c *Client) Write(_ context.Context, messages <-chan message.WriteMessage) error {
+func (c *Client) Write(ctx context.Context, messages <-chan message.WriteMessage) error {
+	if c.spec.BatchWriter {
+		if err := c.writer.Write(ctx, messages); err != nil {
+			return fmt.Errorf("failed to write: %w", err)
+		}
+
+		if err := c.writer.Flush(ctx); err != nil {
+			return fmt.Errorf("failed to flush messages: %w", err)
+		}
+		return nil
+	}
+
 	if c.spec.ErrorOnWrite {
 		return errors.New("error_on_write is true")
 	}
 	for m := range messages {
+		if c.spec.ErrorOnMigrate {
+			if _, ok := m.(*message.WriteMigrateTable); ok {
+				return errors.New("error_on_migrate is true")
+			}
+		}
+
 		if m, ok := m.(*message.WriteInsert); ok {
 			m.Record.Release()
 		}
@@ -51,6 +86,31 @@ func (c *Client) Write(_ context.Context, messages <-chan message.WriteMessage) 
 	return nil
 }
 
-func (*Client) Close(context.Context) error {
+func (c *Client) Close(ctx context.Context) error {
+	if c.writer != nil {
+		return c.writer.Close(ctx)
+	}
+	return nil
+}
+
+func (c *Client) WriteTableBatch(ctx context.Context, name string, msgs message.WriteInserts) error {
+	if c.spec.ErrorOnWrite {
+		return errors.New("error_on_write is true")
+	}
+	return nil
+}
+
+func (c *Client) MigrateTables(ctx context.Context, msgs message.WriteMigrateTables) error {
+	if c.spec.ErrorOnMigrate {
+		return errors.New("error_on_migrate is true")
+	}
+	return nil
+}
+
+func (c *Client) DeleteStale(ctx context.Context, msgs message.WriteDeleteStales) error {
+	return nil
+}
+
+func (c *Client) DeleteRecord(ctx context.Context, msgs message.WriteDeleteRecords) error {
 	return nil
 }
--- plugins/destination/test/client/spec.go
+++ plugins/destination/test/client/spec.go
@@ -1,11 +1,53 @@
 package client
 
-import _ "embed"
+import (
+	_ "embed"
+	"time"
+
+	"github.com/cloudquery/plugin-sdk/v4/configtype"
+)
 
 type Spec struct {
-	// If true, will return an error on write rather than consume from the channel
+	// If true, will return an error on any write message rather than consume from the channel
 	ErrorOnWrite bool `json:"error_on_write,omitempty" jsonschema:"default=false"`
+
+	// If true, will return an error on migrate table messages rather than consume from the channel
+	ErrorOnMigrate bool `json:"error_on_migrate,omitempty" jsonschema:"default=false"`
+
+	// Whether to use a BatchWriter or not.
+	BatchWriter bool `json:"batch_writer" jsonschema:"default=false"`
+
+	// Maximum number of items that may be grouped together to be written in a single write.
+	//
+	// Defaults to `10000`.
+	BatchSize *int64 `json:"batch_size" jsonschema:"minimum=1,default=10000"`
+
+	// Maximum size of items that may be grouped together to be written in a single write.
+	//
+	// Defaults to `52428800` (50 MiB).
+	BatchSizeBytes *int64 `json:"batch_size_bytes" jsonschema:"minimum=1,default=52428800"`
+
+	// Maximum interval between batch writes.
+	//
+	// Defaults to `30s`.
+	BatchTimeout *configtype.Duration `json:"batch_timeout" jsonschema:"default=30s"`
 }
 
 //go:embed schema.json
 var JSONSchema string
+
+func (s *Spec) SetDefaults() {
+	if s.BatchSize == nil {
+		s.BatchSize = ptr(int64(10000))
+	}
+	if s.BatchSizeBytes == nil {
+		s.BatchSizeBytes = ptr(int64(50 * 1024 * 1024)) // 50 MiB
+	}
+	if s.BatchTimeout == nil {
+		s.BatchTimeout = ptr(configtype.NewDuration(30 * time.Second))
+	}
+}
+
+func ptr[A any](a A) *A {
+	return &a
+}
