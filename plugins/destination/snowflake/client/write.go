444f62522dddc20068b337ae584a33ac71d9c480
Ștefan MURARU <muraru.stefaan@gmail.com>
2024-09-18 16:44:22 +0300

    fix: update join ON condition to AND, refactor building columns

--- plugins/destination/snowflake/client/write.go
+++ plugins/destination/snowflake/client/write.go
@@ -18,7 +18,7 @@ const (
 	createOrReplaceStage      = `create or replace stage cq_plugin_stage file_format = cq_plugin_json_format;`
 	putFileIntoStage          = `put 'file://%v' @cq_plugin_stage auto_compress=true`
 	copyIntoTable             = `copy into %s from '@cq_plugin_stage' files=('%s.gz') on_error = ABORT_STATEMENT file_format = (format_name = cq_plugin_json_format) match_by_column_name = case_insensitive`
-	mergeIntoTable            = `MERGE INTO %s dest USING (SELECT %s FROM @cq_plugin_stage/%s.gz t) source ON %s WHEN MATCHED THEN %s WHEN NOT MATCHED THEN %s`
+	mergeIntoTable            = `MERGE INTO %s AS dest USING (SELECT %s FROM @cq_plugin_stage/%s.gz t) AS source ON %s WHEN MATCHED THEN %s WHEN NOT MATCHED THEN %s`
 )
 
 func (c *Client) Write(ctx context.Context, msgs <-chan message.WriteMessage) error {
@@ -94,6 +94,7 @@ func (c *Client) copyIntoTable(ctx context.Context, table *schema.Table, f *os.F
 }
 
 func (c *Client) mergeIntoTable(ctx context.Context, table *schema.Table, f *os.File) error {
+	// https://docs.snowflake.com/en/sql-reference/sql/merge#syntax
 	sql := fmt.Sprintf(mergeIntoTable, table.Name, c.createColumnsList(table), escapePath(filepath.Base(f.Name())), createPrimaryKeyList(table), updateColumnsList(table), insertColumnsList(table))
 
 	if _, err := c.db.ExecContext(ctx, sql); err != nil {
@@ -106,51 +107,45 @@ func (c *Client) mergeIntoTable(ctx context.Context, table *schema.Table, f *os.
 func (c *Client) createColumnsList(table *schema.Table) string {
 	// creates a string like:
 	// $1:COL1::TEXT as COL1, $1:COL2::NUMBER as COL2, $1:COL3::TIMESTAMP_TZ as COL3
-	columnString := ""
+	columns := make([]string, 0, len(table.Columns))
 	for _, col := range table.Columns {
-		columnString += fmt.Sprintf("$1:%s::%s as %s,", col.Name, c.SchemaTypeToSnowflake(col.Type), col.Name)
+		columns = append(columns, fmt.Sprintf("$1:%s::%s as %s", col.Name, c.SchemaTypeToSnowflake(col.Type), col.Name))
 	}
-	return strings.TrimSuffix(columnString, ",")
+	return strings.Join(columns, ",")
 }
 
 func createPrimaryKeyList(table *schema.Table) string {
 	// creates a string like:
-	// source.COL1=dest.COL1, source.COL2=dest.COL2, source.COL3=dest.COL3
-	columnString := ""
+	// source.COL1=dest.COL1 AND source.COL2=dest.COL2 AND source.COL3=dest.COL3
+	columns := make([]string, 0, len(table.PrimaryKeys()))
 	for _, col := range table.PrimaryKeys() {
-		columnString += fmt.Sprintf("source.%s=dest.%s,", col, col)
+		columns = append(columns, fmt.Sprintf("source.%s=dest.%s", col, col))
 	}
-	// remove the last comma
-	return strings.TrimSuffix(columnString, ",")
+	return strings.Join(columns, " AND ")
 }
 
 func updateColumnsList(table *schema.Table) string {
-	// creates a string like:
+	// creates an update string like:
 	// UPDATE SET COL1=source.COL1, COL2=source.COL2, COL3=source.COL3
-	columnString := " UPDATE SET "
+	columns := make([]string, 0, len(table.Columns))
 	for _, col := range table.Columns {
-		columnString += fmt.Sprintf("%s=source.%s,", strings.ToUpper(col.Name), strings.ToUpper(col.Name))
+		columns = append(columns, fmt.Sprintf("%s=source.%s", strings.ToUpper(col.Name), strings.ToUpper(col.Name)))
 	}
-	// remove the last comma
-	return strings.TrimSuffix(columnString, ",")
+	return fmt.Sprintf(" UPDATE SET %s ", strings.Join(columns, ","))
 }
 
 func insertColumnsList(table *schema.Table) string {
 	// creates a string like:
 	// INSERT (COL1, COL2, COL3) VALUES (source.COL1, source.COL2, source.COL3)
-	columnString := "INSERT ("
+	names, values := make([]string, 0, len(table.Columns)), make([]string, 0, len(table.Columns))
 	for _, col := range table.Columns {
-		columnString += strings.ToUpper(col.Name) + ","
+		names = append(names, strings.ToUpper(col.Name))
+		values = append(values, "source."+strings.ToUpper(col.Name))
 	}
-	columnString = strings.TrimSuffix(columnString, ",")
-	columnString += ") VALUES ("
-	for _, col := range table.Columns {
-		columnString += "source." + strings.ToUpper(col.Name) + ","
-	}
-	columnString = strings.TrimSuffix(columnString, ",")
-	columnString += ")"
-	return columnString
+
+	return fmt.Sprintf("INSERT (%s) VALUES (%s)", strings.Join(names, ", "), strings.Join(values, ", "))
 }
+
 func (c *Client) setupWrite(ctx context.Context) error {
 	var setupErr error
 	c.setupWriteOnce.Do(func() {
