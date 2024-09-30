07172609c85af21626a7a66a4bcd9da714a5a005
Chris Duflo <chris@cloudquery.io>
2024-09-30 15:39:37 -0400

    bigquery started

---
+++ plugins/destination/bigquery/cloud-config-ui/src/hooks/useConfig.tsx
@@ -0,0 +1,176 @@
+import { useMemo } from 'react';
+
+import { FormMessagePayload } from '@cloudquery/plugin-config-ui-connector';
+import { AuthType, DestinationConfig } from '@cloudquery/plugin-config-ui-lib';
+
+import * as yup from 'yup';
+
+import { timePartitionOptions } from '../utils/constants';
+
+interface Props {
+  initialValues?: FormMessagePayload['init']['initialValues'] | undefined;
+}
+
+export const useConfig = ({ initialValues }: Props): DestinationConfig => {
+  return useMemo(
+    () => ({
+      name: 'bigquery',
+      type: 'destination',
+      label: 'BigQuery',
+      docsLink: 'https://hub.cloudquery.io/plugins/destination/cloudquery/bigquery/latest/docs',
+      iconLink: 'images/logo.webp',
+      steps: [
+        {
+          children: [
+            {
+              component: 'section',
+              title: 'Connect to your database',
+              subtitle: 'Set up a connection to your BigQuery instance.',
+              children: [
+                // {
+                //   component: 'section',
+                //   title: 'Authentication',
+                //   subtitle: 'Use an API key to authenticate with Fastly',
+                //   children: [
+                //     {
+                //       component: 'control-secret-field',
+                //       name: 'fastly_api_key',
+                //       helperText: 'Paste the API key you generated in the Fastly dashboard.',
+                //       label: 'Fastly API Key',
+                //       schema: yup
+                //         .string()
+                //         .default(initialValues?.spec?.fastly_api_key ?? '')
+                //         .required(),
+                //     },
+                //   ],
+                // },
+                {
+                  component: 'control-secret-field',
+                  name: 'project_id',
+                  helperText:
+                    'The id of the project where the destination BigQuery database resides.',
+                  label: 'Google Cloud Project ID',
+                  schema: yup
+                    .string()
+                    .default(initialValues?.spec?.project_id ?? '')
+                    .required(),
+                },
+                {
+                  component: 'control-secret-field',
+                  name: 'dataset_id',
+                  helperText:
+                    'The name of the BigQuery dataset within the project, e.g. my_dataset. This dataset needs to be created before running a sync or migration.',
+                  label: 'Google Cloud BigQuery DataSet ID',
+                  schema: yup
+                    .string()
+                    .default(initialValues?.spec?.dataset_id ?? '')
+                    .required(),
+                },
+              ],
+            },
+            {
+              component: 'collapsible-section',
+              title: 'Advanced Connection Options',
+              defaultExpanded: false,
+              shouldRender: (values: any) => values._connectionType === 'fields',
+              children: [
+                {
+                  component: 'control-text-field',
+                  name: 'dataset_location',
+                  helperText:
+                    'The data location of the BigQuery dataset. If set, will be used as the default location for job operations.',
+                  label: 'Google Cloud BigQuery DataSet Location',
+                  schema: yup
+                    .string()
+                    .default(initialValues?.spec?.dataset_location ?? '')
+                    .required(),
+                },
+                {
+                  component: 'control-select-field',
+                  name: 'time_partitioning',
+                  options: [...timePartitionOptions],
+                  helperText: (
+                    <>
+                      The time partitioning to use when creating tables. The partition time column
+                      used will always be <b>_cq_sync_time</b> so that all rows for a sync run will
+                      be partitioned on the hour/day the sync started.
+                    </>
+                  ),
+                  label: 'Time Partitioning',
+                  schema: yup
+                    .string()
+                    .oneOf(timePartitionOptions)
+                    .default(initialValues?.spec?.time_partitioning ?? 'none')
+                    .required(),
+                },
+              ],
+            },
+            {
+              component: 'collapsible-section',
+              title: 'Advanced Sync Options',
+              defaultExpanded: false,
+              children: [
+                {
+                  component: 'control-number-field',
+                  name: 'batch_size',
+                  helperText:
+                    'Maximum number of items that may be grouped together to be written in a single write. Default is 10,000.',
+                  label: 'Batch size',
+                  schema: yup
+                    .number()
+                    .integer()
+                    .default(initialValues?.spec?.batch_size ?? 10_000)
+                    .required(),
+                },
+                {
+                  component: 'control-number-field',
+                  name: 'batch_size_bytes',
+                  helperText:
+                    'Maximum size of items that may be grouped together to be written in a single write. Default is 100,000,000 = 100MB.',
+                  label: 'Batch size (bytes)',
+                  schema: yup
+                    .number()
+                    .integer()
+                    .default(initialValues?.spec?.batch_size_bytes ?? 100_000_000)
+                    .required(),
+                },
+              ],
+            },
+          ],
+          title: 'Configuration',
+        },
+      ],
+      auth: [AuthType.OTHER],
+      guide: {
+        title: 'BigQuery configuration',
+        sections: [
+          {
+            bodies: [
+              {
+                text: (
+                  <>
+                    The BigQuery destination lets you sync data from any CloudQuery source to a
+                    BigQuery&#8209;compatible database.
+                  </>
+                ),
+              },
+            ],
+          },
+          {
+            header: 'Setup guide',
+            bodies: [
+              {
+                text: `To allow CloudQuery network access to your BigQuery instance, make sure the following
+                CloudQuery IPs are in your firewall allowlist:`,
+              },
+              { code: '35.231.218.115' },
+              { code: '35.231.72.234' },
+            ],
+          },
+        ],
+      },
+    }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [initialValues],
+  );
+};
---
+++ plugins/destination/bigquery/cloud-config-ui/src/utils/prepareSubmitValues.ts
@@ -0,0 +1,29 @@
+import { PluginUiMessagePayload } from '@cloudquery/plugin-config-ui-connector';
+import { corePrepareSubmitValues } from '@cloudquery/plugin-config-ui-lib';
+
+export function prepareSubmitValues(
+  values: Record<string, any>,
+): PluginUiMessagePayload['validation_passed']['values'] {
+  const payload = corePrepareSubmitValues(values);
+
+  payload.migrateMode = values.migrateMode;
+  payload.writeMode = 'append'; // it is only option
+
+  if (values.dataset_location) {
+    payload.spec.dataset_location = values.dataset_location;
+  }
+
+  if (values.time_partitioning) {
+    payload.spec.time_partitioning = values.time_partitioning;
+  }
+
+  if (values.batch_size) {
+    payload.spec.batch_size = Number(values.batch_size);
+  }
+
+  if (values.batch_size_bytes) {
+    payload.spec.batch_size_bytes = Number(values.batch_size_bytes);
+  }
+
+  return payload;
+}
