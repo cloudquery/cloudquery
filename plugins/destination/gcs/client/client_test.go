6e8ed8fd1394f8307be34f807ed04c9a723297e2
Kemal Hadimli <disq@users.noreply.github.com>
2024-10-05 10:50:57 +0100

    use `assert.EventuallyWithT`

--- plugins/destination/gcs/client/client_test.go
+++ plugins/destination/gcs/client/client_test.go
@@ -17,7 +17,6 @@ import (
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 const bucket = "cq-dest-gcs"
@@ -140,17 +139,17 @@ func testPluginCustom(t *testing.T, s *spec.Spec, readErrorString string) {
 		t.Fatal(fmt.Errorf("failed to close client: %w", err))
 	}
 
-	time.Sleep(2 * time.Second)
+	assert.EventuallyWithT(t, func(c *assert.CollectT) {
+		readRecords, err := readAll(ctx, client, table)
+		if readErrorString != "" {
+			assert.ErrorContains(t, err, readErrorString)
+			return
+		}
+		assert.NoError(c, err)
 
-	readRecords, err := readAll(ctx, client, table)
-	if readErrorString != "" {
-		require.ErrorContains(t, err, readErrorString)
-		return
-	}
-
-	require.NoError(t, err)
-	totalItems := plugin.TotalRows(readRecords)
-	assert.Equalf(t, int64(2), totalItems, "expected 2 items, got %d", totalItems)
+		totalItems := plugin.TotalRows(readRecords)
+		assert.Equalf(c, int64(2), totalItems, "expected 2 items, got %d", totalItems)
+	}, 2*time.Second, 100*time.Millisecond)
 }
 
 func readAll(ctx context.Context, client plugin.Client, table *schema.Table) ([]arrow.Record, error) {
