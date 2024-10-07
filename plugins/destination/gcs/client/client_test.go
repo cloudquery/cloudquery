02cfa5fb2f0097ff9a401da73ac43e44012c55cc
Kemal <223029+disq@users.noreply.github.com>
2024-10-07 15:16:41 +0100

    chore: Run tests in parallel, wait before read (GCS) (#19318)
    Getting the GCS plugin ready for the [StreamingBatchWriter update](https://github.com/cloudquery/plugin-sdk/pull/1921). ~Incorporates https://github.com/cloudquery/filetypes/pull/579 and https://github.com/cloudquery/plugin-sdk/pull/1921~

--- plugins/destination/gcs/client/client_test.go
+++ plugins/destination/gcs/client/client_test.go
@@ -17,7 +17,6 @@ import (
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 const bucket = "cq-dest-gcs"
@@ -36,10 +35,12 @@ func TestPlugin(t *testing.T) {
 		}
 
 		t.Run("generic/"+string(ft), func(t *testing.T) {
+			t.Parallel()
 			testPlugin(t, &s)
 		})
 
 		t.Run("write/"+string(ft), func(t *testing.T) {
+			t.Parallel()
 			testPluginCustom(t, &s, "")
 		})
 	}
@@ -138,15 +139,17 @@ func testPluginCustom(t *testing.T, s *spec.Spec, readErrorString string) {
 		t.Fatal(fmt.Errorf("failed to close client: %w", err))
 	}
 
-	readRecords, err := readAll(ctx, client, table)
-	if readErrorString != "" {
-		require.ErrorContains(t, err, readErrorString)
-		return
-	}
+	assert.EventuallyWithT(t, func(c *assert.CollectT) {
+		readRecords, err := readAll(ctx, client, table)
+		if readErrorString != "" {
+			assert.ErrorContains(c, err, readErrorString)
+			return
+		}
+		assert.NoError(c, err)
 
-	require.NoError(t, err)
-	totalItems := plugin.TotalRows(readRecords)
-	assert.Equalf(t, int64(2), totalItems, "expected 2 items, got %d", totalItems)
+		totalItems := plugin.TotalRows(readRecords)
+		assert.Equalf(c, int64(2), totalItems, "expected 2 items, got %d", totalItems)
+	}, 2*time.Second, 100*time.Millisecond)
 }
 
 func readAll(ctx context.Context, client plugin.Client, table *schema.Table) ([]arrow.Record, error) {
