6abbf40f3aa3dfdb00383e00cc429986ee20cd72
Chris Duflo <chris@cloudquery.io>
2024-10-08 14:11:51 -0400

    chore: HackerNews v4 upgrade

--- plugins/source/hackernews/cloud-config-ui/src/App.tsx
+++ plugins/source/hackernews/cloud-config-ui/src/App.tsx
@@ -1,39 +1,17 @@
-import { Fragment, useState, useEffect } from 'react';
+import React from 'react';
 
-import {
-  CloudAppMock,
-  CloudQueryTables,
-  ConfigUIForm,
-  PluginContextProvider,
-  useFormInit,
-} from '@cloudquery/plugin-config-ui-lib';
+import { ConfigUIForm, PluginContextProvider, useFormInit } from '@cloudquery/plugin-config-ui-lib';
+
+import { DevWrapper } from '@cloudquery/plugin-config-ui-lib/components/devWrapper';
 
 import { useConfig } from './hooks/useConfig';
+import { envJson } from './utils/envJson';
 import { pluginUiMessageHandler } from './utils/messageHandler';
 import { prepareSubmitValues } from './utils/prepareSubmitValues';
 
-const useCloudAppMock =
-  (process.env.REACT_APP_USE_CLOUD_APP_MOCK === 'true' || process.env.NODE_ENV !== 'production') &&
-  window.self === window.top;
-const DevWrapper = useCloudAppMock ? CloudAppMock : Fragment;
-// eslint-disable-next-line unicorn/prefer-module
-const { plugin, ...devWrapperProps }: any = useCloudAppMock ? require('./.env.json') : {};
-
-const pluginProps = useCloudAppMock
-  ? plugin
-  : {
-      team: process.env.REACT_APP_PLUGIN_TEAM,
-      kind: process.env.REACT_APP_PLUGIN_KIND,
-      name: process.env.REACT_APP_PLUGIN_NAME,
-      version: process.env.REACT_APP_PLUGIN_VERSION,
-    };
+const getTablesData = () => import('./data/__tables.json');
 
 function App() {
-  const [tablesData, setTablesData] = useState<CloudQueryTables | undefined>();
-  useEffect(() => {
-    import('./data/__tables.json').then(({ default: data }) => setTablesData(data));
-  }, []);
-
   const { initialValues, initialized, teamName, context } = useFormInit(
     pluginUiMessageHandler,
     true,
@@ -42,19 +20,18 @@ function App() {
   const config = useConfig({ initialValues });
 
   return (
-    <PluginContextProvider
-      config={config}
-      plugin={pluginProps}
-      teamName={teamName}
-      tablesData={tablesData as CloudQueryTables}
-      hideStepper={context === 'wizard'} // TODO: Delete after iframe deprecation
-      pluginUiMessageHandler={pluginUiMessageHandler}
-      initialValues={initialValues}
-    >
-      <DevWrapper {...devWrapperProps}>
+    <DevWrapper {...envJson}>
+      <PluginContextProvider
+        config={config}
+        teamName={teamName}
+        getTablesData={getTablesData}
+        hideStepper={context === 'wizard'} // TODO: Delete after iframe deprecation
+        pluginUiMessageHandler={pluginUiMessageHandler}
+        initialValues={initialValues}
+      >
         {initialized && <ConfigUIForm prepareSubmitValues={prepareSubmitValues} />}
-      </DevWrapper>
-    </PluginContextProvider>
+      </PluginContextProvider>
+    </DevWrapper>
   );
 }
 
