dbb8c0c1e8bc6d283331c7f026e09edd37601780
Erez Rokah <erezrokah@users.noreply.github.com>
2024-10-21 09:36:31 +0100

    fix: Stop progress bar when the context is done (#19449)
    #### Summary
    
    Related to https://github.com/cloudquery/cloudquery-issues/issues/2591 and the bug can been seen in the output of that issue:
    
    ```
    Loading spec(s) from examples/azure_to_postgresql.yml
    Starting sync for: azure (cloudquery/azure@v13.0.0) -> [postgresql (cloudquery/postgresql@v8.6.3)]
    / Syncing resources... (23/-, 13 resources/s) [2s] Error: failed to sync v3 source azure: EOF
    - Syncing resources... (23/-, 10 resources/s) [2s] %   
    ```
    
    There's an additional progress bar print after the error was printed. We should instead stop the progress bar as soon as possible when context is done.

--- cli/cmd/sync_v3.go
+++ cli/cmd/sync_v3.go
@@ -342,24 +342,6 @@ func syncConnectionV3(ctx context.Context, syncOptions syncV3Options) (syncErr e
 		)
 	}
 
-	// Add a ticker to update the progress bar every 100ms
-	t := time.NewTicker(100 * time.Millisecond)
-	newResources := int64(0)
-	defer t.Stop()
-	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				change := atomic.SwapInt64(&newResources, 0)
-				_ = bar.Add(int(change))
-				return
-			case <-t.C:
-				change := atomic.SwapInt64(&newResources, 0)
-				_ = bar.Add(int(change))
-			}
-		}
-	}()
-
 	isStateBackendEnabled := sourceSpec.BackendOptions != nil && sourceSpec.BackendOptions.TableName != ""
 
 	// Read from the sync stream and write to all destinations.
@@ -449,6 +431,27 @@ func syncConnectionV3(ctx context.Context, syncOptions syncV3Options) (syncErr e
 		eg.Go(pipeline.RunBlocking) // each transformer runs in its own goroutine
 		pipelineByDestinationName[destinationName] = pipeline
 	}
+
+	// Add a ticker to update the progress bar every 100ms
+	t := time.NewTicker(100 * time.Millisecond)
+	newResources := int64(0)
+	go func() {
+		for {
+			select {
+			case <-gctx.Done():
+				t.Stop()
+				change := atomic.SwapInt64(&newResources, 0)
+				_ = bar.Add(int(change))
+				if err := bar.Finish(); err != nil {
+					log.Warn().Err(err).Msg("Failed to finish progress bar")
+				}
+				return
+			case <-t.C:
+				change := atomic.SwapInt64(&newResources, 0)
+				_ = bar.Add(int(change))
+			}
+		}
+	}()
 
 	eg.Go(func() error {
 		// Close all transformation pipelines when the source is done
@@ -634,10 +637,6 @@ func syncConnectionV3(ctx context.Context, syncOptions syncV3Options) (syncErr e
 		}
 	}
 
-	err = bar.Finish()
-	if err != nil {
-		log.Warn().Err(err).Msg("Failed to finish progress bar")
-	}
 	atomic.StoreInt64(&isComplete, 1)
 	syncTimeTook = time.Since(syncTime)
 	exitReason = ExitReasonCompleted
