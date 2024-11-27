0f69b2102e909b1e9cd0315a19824bf1b5e49b0a
erezrokah <erezrokah@users.noreply.github.com>
2024-11-27 18:44:15 +0000

    chore: Remove docs site

--- website/content/hub.ts
+++
@@ -1,6 +0,0 @@
-export type Plugin = {
-  id: string;
-  name: string;
-};
-
-export type Plugins = Plugin[];
--- website/utils/versions.ts
+++
@@ -1,8 +0,0 @@
-export const getLatestVersion = (type, name) => {
-  try {
-    const { latest: version } = require(`../versions/${type}-${name}.json`);
-    return version.split("-")[3];
-  } catch (e) {
-    return "Unpublished";
-  }
-};
