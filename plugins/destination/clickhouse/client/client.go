a80c00052257b2979ecc818c3384875c88705711
Marcel Tyszkiewicz <marcel.tyszkiewicz+code@gmail.com>
2025-05-20 09:25:51 +0200

    bump CH to 24.8.1

--- plugins/destination/clickhouse/client/client.go
+++ plugins/destination/clickhouse/client/client.go
@@ -70,7 +70,7 @@ func New(_ context.Context, logger zerolog.Logger, specBytes []byte, _ plugin.Ne
 		return nil, fmt.Errorf("failed to verify server version %w", err)
 	}
 
-	minVer := proto.Version{Major: 22, Minor: 1, Patch: 2}
+	minVer := proto.Version{Major: 24, Minor: 8, Patch: 1}
 	if !proto.CheckMinVersion(minVer, ver.Version) {
 		defer conn.Close()
 		return nil, fmt.Errorf("server version is %s, minimum version supported is %s", ver.Version, minVer)
