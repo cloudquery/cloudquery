b0ef2bc2eecd7a6dff0f9d6427a44aaaa9ddaced
Kemal Hadimli <disq@users.noreply.github.com>
2025-06-09 10:25:34 +0100

    fix: Enable S3 destination test connections

--- plugins/destination/s3/client/test_connection.go
+++ plugins/destination/s3/client/test_connection.go
@@ -2,9 +2,13 @@ package client
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
+	"fmt"
 
+	"github.com/cloudquery/cloudquery/plugins/destination/s3/v7/client/spec"
 	"github.com/cloudquery/plugin-sdk/v4/plugin"
+	"github.com/google/uuid"
 	"github.com/rs/zerolog"
 )
 
@@ -17,7 +21,27 @@ const (
 
 func NewConnectionTester(createClientFn NewClientFn) plugin.ConnectionTester {
 	return func(ctx context.Context, logger zerolog.Logger, specBytes []byte) error {
-		_, err := createClientFn(ctx, logger, specBytes, plugin.NewClientOptions{})
+		{
+			s := &spec.Spec{}
+			err := json.Unmarshal(specBytes, &s)
+			if err != nil {
+				return fmt.Errorf("failed to unmarshal s3 spec: %w", err)
+			}
+			if err := s.Validate(); err != nil {
+				return err
+			}
+			s.SetDefaults()
+			b := true
+			s.TestWrite = &b // Force test write to be enabled for connection testing
+			specBytes, err = json.Marshal(s)
+			if err != nil {
+				return fmt.Errorf("failed to marshal s3 spec: %w", err)
+			}
+		}
+
+		_, err := createClientFn(ctx, logger, specBytes, plugin.NewClientOptions{
+			InvocationID: uuid.NewString(),
+		})
 		if err == nil {
 			return nil
 		}
