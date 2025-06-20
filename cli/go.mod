146a279a3505bd25981a56dbaa4519a96e3d042e
przechrzta <w.przechrzta@gmail.com>
2025-06-20 12:51:12 +0200

    feat: Configures otel log collector (#20895)
    <!-- 🎉 Thank you for making CloudQuery awesome by submitting a PR 🎉 -->
    
    #### Summary
    
    Adds support for open telemetry collector.
    
    
    Tested on local Tilt cluster
    
    Example otel records
    ```
    ───────
    Timestamp:          2025-06-18 07:09:04.000000000
    TimestampTime:      2025-06-18 07:09:04
    TraceId:
    SpanId:
    TraceFlags:         0
    SeverityText:       info
    SeverityNumber:     9
    ServiceName:        cloudquery-cli
    Body:               table sync finished
    ResourceSchemaUrl:  https://opentelemetry.io/schemas/1.26.0
    ResourceAttributes: {'service.name':'cloudquery-cli','telemetry.sdk.language':'go','telemetry.sdk.name':'opentelemetry','telemetry.sdk.version':'1.36.0'}
    ScopeSchemaUrl:
    ScopeName:          cloudquery
    ScopeVersion:
    ScopeAttributes:    {}
    LogAttributes:      {'client':'project:cq-playground','duration_ms':'2274.9195','errors':'0','invocation_id':'a219e90f-c8f2-4f55-84f1-c99fcacb9b69','module':'gcp-source','resources':'35','table':'gcp_storage_buckets'}
    
    Row 55:
    ───────
    Timestamp:          2025-06-18 07:09:08.000000000
    TimestampTime:      2025-06-18 07:09:08
    TraceId:
    SpanId:
    TraceFlags:         0
    SeverityText:       info
    SeverityNumber:     9
    ServiceName:        cloudquery-cli
    Body:               started call
    ResourceSchemaUrl:  https://opentelemetry.io/schemas/1.26.0
    ResourceAttributes: {'service.name':'cloudquery-cli','telemetry.sdk.language':'go','telemetry.sdk.name':'opentelemetry','telemetry.sdk.version':'1.36.0'}
    ScopeSchemaUrl:
    ScopeName:          cloudquery
    ScopeVersion:
    ScopeAttributes:    {}
    LogAttributes:      {'grpc.component':'server','grpc.method':'Transform','grpc.method_type':'bidi_stream','grpc.service':'cloudquery.plugin.v3.Plugin','grpc.start_time':'2025-06-18T09:09:01+02:00','grpc.time_ms':'6126.349','invocation_id':'a219e90f-c8f2-4f55-84f1-c99fcacb9b69','module':'basic-transformer','peer.address':'','protocol':'grpc'}
    
    Row 56:
    ```
    
    <!--
    Explain what problem this PR addresses
    -->
    
    <!--
    Use the following steps to ensure your PR is ready to be reviewed
    
    - [ ] Read the [contribution
    guidelines](https://github.com/cloudquery/cloudquery/blob/main/CONTRIBUTING.md)
    🧑‍🎓
    - [ ] Run `make lint` to ensure the proposed changes follow the coding
    style 🚨 (install golangci-lint
    [here](https://golangci-lint.run/usage/install/#local-installation))
    - [ ] Run `make test` to ensure the proposed changes pass the tests 🧪
    - [ ] If changing a source plugin run `make gen` to ensure docs are up
    to date 📝
    - [ ] Ensure the status checks below are successful ✅
    --->

--- cli/go.mod
+++ cli/go.mod
@@ -39,13 +39,18 @@ require (
 	go.opentelemetry.io/collector/pdata v1.22.0
 	go.opentelemetry.io/collector/receiver v0.116.0
 	go.opentelemetry.io/collector/receiver/otlpreceiver v0.116.0
-	go.opentelemetry.io/otel/metric v1.34.0
-	go.opentelemetry.io/otel/trace v1.34.0
+	go.opentelemetry.io/otel v1.36.0
+	go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp v0.12.2
+	go.opentelemetry.io/otel/log v0.12.2
+	go.opentelemetry.io/otel/metric v1.36.0
+	go.opentelemetry.io/otel/sdk v1.36.0
+	go.opentelemetry.io/otel/sdk/log v0.12.2
+	go.opentelemetry.io/otel/trace v1.36.0
 	go.uber.org/zap v1.27.0
 	golang.org/x/exp v0.0.0-20250408133849-7e4ce0ab07d0
-	golang.org/x/net v0.39.0
-	golang.org/x/sync v0.13.0
-	golang.org/x/term v0.31.0
+	golang.org/x/net v0.40.0
+	golang.org/x/sync v0.14.0
+	golang.org/x/term v0.32.0
 	google.golang.org/grpc v1.72.1
 	google.golang.org/protobuf v1.36.6
 	gopkg.in/yaml.v3 v3.0.1
@@ -61,6 +66,7 @@ require (
 	github.com/bahlo/generic-list-go v0.2.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/cenkalti/backoff/v5 v5.0.2 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.6 // indirect
@@ -79,6 +85,7 @@ require (
 	github.com/golang/snappy v1.0.0 // indirect
 	github.com/google/flatbuffers v25.2.10+incompatible // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.7 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
@@ -142,14 +149,15 @@ require (
 	go.opentelemetry.io/collector/receiver/xreceiver v0.116.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.56.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.56.0 // indirect
-	go.opentelemetry.io/otel v1.34.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.6.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/mod v0.24.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
-	golang.org/x/text v0.24.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 	golang.org/x/tools v0.32.0 // indirect
 	golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250519155744-55703ea1f237 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250519155744-55703ea1f237 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
 
---
+++ cli/internal/otel/opentelemetry.go
@@ -0,0 +1,189 @@
+package otel
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/rs/zerolog"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp"
+	otellog "go.opentelemetry.io/otel/log"
+	logglobal "go.opentelemetry.io/otel/log/global"
+	"go.opentelemetry.io/otel/sdk/log"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
+)
+
+// newResource returns a resource describing this application.
+func newResource() *resource.Resource {
+	def := resource.Default()
+	schemaURL := def.SchemaURL()
+	if schemaURL == "" {
+		schemaURL = semconv.SchemaURL
+	}
+	r, err := resource.Merge(
+		def,
+		resource.NewWithAttributes(
+			schemaURL,
+			semconv.ServiceName("cloudquery-cli"),
+		),
+	)
+	if err != nil {
+		panic(err)
+	}
+	return r
+}
+
+type Config struct {
+	Endpoint string
+	Insecure bool
+}
+
+func getLogsProcessor(ctx context.Context, opts *Config) (*log.BatchProcessor, error) {
+	if opts.Endpoint == "" {
+		return nil, nil
+	}
+
+	logOptions := []otlploghttp.Option{
+		otlploghttp.WithEndpoint(opts.Endpoint),
+		otlploghttp.WithCompression(otlploghttp.GzipCompression),
+	}
+	if opts.Insecure {
+		logOptions = append(logOptions, otlploghttp.WithInsecure())
+	}
+
+	exporter, err := otlploghttp.New(ctx, logOptions...)
+	if err != nil {
+		return nil, fmt.Errorf("creating OTLP log exporter: %w", err)
+	}
+
+	processor := log.NewBatchProcessor(exporter)
+	return processor, nil
+}
+
+func SetupOtel(ctx context.Context, logger zerolog.Logger, opts *Config) (shutdown func(), err error) {
+	logsProcessor, err := getLogsProcessor(ctx, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	pluginResource := newResource()
+	lp := log.NewLoggerProvider(
+		log.WithProcessor(logsProcessor),
+		log.WithResource(pluginResource),
+	)
+
+	otel.SetErrorHandler(otel.ErrorHandlerFunc(func(err error) {
+		logger.Warn().Err(err).Msg("otel error")
+	}))
+
+	logglobal.SetLoggerProvider(lp)
+	shutdown = func() {
+		if err := lp.Shutdown(context.Background()); err != nil {
+			logger.Error().Err(err).Msg("failed to shutdown OTLP logger provider")
+		}
+	}
+
+	return shutdown, nil
+}
+
+type otelLoggerHook struct {
+	otellog.Logger
+	ctx          context.Context
+	invocationId string
+}
+
+func (h *otelLoggerHook) Run(e *zerolog.Event, level zerolog.Level, message string) {
+	record := otellog.Record{}
+	record.SetTimestamp(time.Now().UTC())
+	record.SetSeverity(otellogSeverity(level))
+	record.SetSeverityText(level.String())
+	record.SetBody(otellog.StringValue(message))
+	// See https://github.com/rs/zerolog/issues/493, this is ugly but it works
+	// At the moment there's no way to get the log fields from the event, so we use reflection to get the buffer and parse it
+	// TODO: Remove this if https://github.com/rs/zerolog/pull/682 is merged
+	logData := make(map[string]any)
+	eventBuffer := fmt.Sprintf("%s}", reflect.ValueOf(e).Elem().FieldByName("buf"))
+	err := json.Unmarshal([]byte(eventBuffer), &logData)
+	if err == nil {
+		recordAttributes := make([]otellog.KeyValue, 0, len(logData))
+		for k, v := range logData {
+			if k == "message" {
+				record.SetBody(otellog.StringValue(fmt.Sprintf("%v", v)))
+				continue
+			}
+
+			if k == "level" {
+				continue
+			}
+			if k == "time" {
+				eventTimestamp, ok := v.(string)
+				if !ok {
+					continue
+				}
+				t, err := time.Parse(time.RFC3339Nano, eventTimestamp)
+				if err == nil {
+					record.SetTimestamp(t)
+					continue
+				}
+			}
+			var attributeValue otellog.Value
+			switch v := v.(type) {
+			case string:
+				attributeValue = otellog.StringValue(v)
+			case int:
+				attributeValue = otellog.IntValue(v)
+			case int64:
+				attributeValue = otellog.Int64Value(v)
+			case float64:
+				attributeValue = otellog.Float64Value(v)
+			case bool:
+				attributeValue = otellog.BoolValue(v)
+			case []byte:
+				attributeValue = otellog.BytesValue(v)
+			default:
+				attributeValue = otellog.StringValue(fmt.Sprintf("%v", v))
+			}
+			recordAttributes = append(recordAttributes, otellog.KeyValue{
+				Key:   k,
+				Value: attributeValue,
+			})
+		}
+		recordAttributes = append(recordAttributes, otellog.KeyValue{
+			Key:   "invocation_id",
+			Value: otellog.StringValue(h.invocationId)},
+		)
+		record.AddAttributes(recordAttributes...)
+	}
+
+	h.Emit(h.ctx, record)
+}
+
+func otellogSeverity(level zerolog.Level) otellog.Severity {
+	switch level {
+	case zerolog.DebugLevel:
+		return otellog.SeverityDebug
+	case zerolog.InfoLevel:
+		return otellog.SeverityInfo
+	case zerolog.WarnLevel:
+		return otellog.SeverityWarn
+	case zerolog.ErrorLevel:
+		return otellog.SeverityError
+	case zerolog.FatalLevel:
+		return otellog.SeverityFatal2
+	case zerolog.PanicLevel:
+		return otellog.SeverityFatal1
+	case zerolog.TraceLevel:
+		return otellog.SeverityTrace
+	default:
+		return otellog.SeverityUndefined
+	}
+}
+
+func NewOTELLoggerHook(invocationId string) zerolog.Hook {
+	return &otelLoggerHook{logglobal.GetLoggerProvider().Logger("cloudquery"), context.Background(), invocationId}
+}
