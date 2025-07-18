d9e5eb09b0647f3f5b5612ecc0e3075658a382d9
Przemyslaw Stepien <przemyslaw@cloudquery.io>
2025-07-18 13:00:16 +0200

    fix main_fips.go

--- plugins/source/test/main_fips.go
+++ plugins/source/test/main_fips.go
@@ -6,7 +6,6 @@ package main
 
 import (
 	"context"
-	"crypto/fips140"
 	"log"
 
 	"github.com/cloudquery/cloudquery/plugins/source/test/v4/resources/plugin"
@@ -15,7 +14,8 @@ import (
 
 func main() {
 	log.Printf("FIPS enabled: %t", fips140.Enabled())
-	if err := serve.Plugin(plugin.Plugin()).Serve(context.Background()); err != nil {
+	p := serve.Plugin(plugin.Plugin())
+	if err := p.Serve(context.Background()); err != nil {
 		log.Fatalf("failed to serve plugin: %v", err)
 	}
 }
