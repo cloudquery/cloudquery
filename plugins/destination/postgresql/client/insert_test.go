5ba5134f90f7077234619cf080ce91c2d7125d38
Stuart Murray <10003354+stoovon@users.noreply.github.com>
2025-08-26 13:12:55 +0100

    test: Implement TestConcurrentSyncsAgainstSameTable
    This does not currently overwrite rows, but instead merely adds
    new ones. It will not fully reproduce the deadlock unless we can
    overwrite rows.

--- plugins/destination/postgresql/client/insert_test.go
+++ plugins/destination/postgresql/client/insert_test.go
@@ -2,12 +2,27 @@ package client
 
 import (
 	"context"
+	"fmt"
 	"testing"
+	"time"
 
+	"github.com/apache/arrow-go/v18/arrow"
+	"github.com/apache/arrow-go/v18/arrow/array"
+	"github.com/apache/arrow-go/v18/arrow/memory"
 	"github.com/cloudquery/cloudquery/plugins/destination/postgresql/v8/client/spec"
+	internalPlugin "github.com/cloudquery/cloudquery/plugins/destination/postgresql/v8/resources/plugin"
+	"github.com/cloudquery/plugin-sdk/v4/message"
+	"github.com/cloudquery/plugin-sdk/v4/plugin"
+	"github.com/cloudquery/plugin-sdk/v4/schema"
+	sdkTypes "github.com/cloudquery/plugin-sdk/v4/types"
+	"github.com/goccy/go-json"
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
 )
 
 type MockBatchResults struct {
@@ -104,4 +119,91 @@ func TestClient_flushBatch(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestConcurrentSyncsAgainstSameTable(t *testing.T) {
+	const syncConcurrency = 50 // Caution: With significantly higher numbers we see connection issues running against Postgres in Tilt.
+	const rounds = 99
+	ctx := context.Background()
+	group, _ := errgroup.WithContext(ctx)
+	randomUUIDString := uuid.New().String()
+	tableName := "k8s_core_namespaces_" + randomUUIDString
+
+	table := &schema.Table{
+		Name: tableName,
+		Columns: []schema.Column{
+			schema.CqIDColumn,
+			schema.CqSourceNameColumn,
+			schema.CqSyncTimeColumn,
+		},
+	}
+	// Create the table
+	p := plugin.NewPlugin("postgresql",
+		internalPlugin.Version,
+		New,
+		plugin.WithJSONSchema(spec.JSONSchema),
+	)
+	s := &spec.Spec{ConnectionString: getTestConnection()}
+	b, err := json.Marshal(s)
+	require.NoError(t, err)
+	err = p.Init(ctx, b, plugin.NewClientOptions{})
+	require.NoError(t, err)
+	if err := p.WriteAll(ctx, []message.WriteMessage{&message.WriteMigrateTable{Table: table}}); err != nil {
+		t.Fatal(fmt.Errorf("failed to create table: %w", err))
+	}
+
+	for i := range syncConcurrency {
+		group.Go(func() error {
+			// Simulate a sync job against the same table
+			syncContext := context.Background()
+			p := plugin.NewPlugin("postgresql",
+				internalPlugin.Version,
+				New,
+				plugin.WithJSONSchema(spec.JSONSchema),
+			)
+			p.SetLogger(zerolog.New(zerolog.NewTestWriter(t)).Level(zerolog.WarnLevel))
+			s := &spec.Spec{ConnectionString: getTestConnection()}
+			b, err := json.Marshal(s)
+			require.NoError(t, err)
+			err = p.Init(syncContext, b, plugin.NewClientOptions{})
+			require.NoError(t, err)
+			if err := p.WriteAll(syncContext, []message.WriteMessage{&message.WriteMigrateTable{Table: table}}); err != nil {
+				t.Fatal(fmt.Errorf("failed to create table: %w", err))
+			}
+
+			for j := range rounds {
+				jobIndexAsString := fmt.Sprintf("%04d%02d", i, j)
+				randomUUIDStringWithLastCharacterReplaced := randomUUIDString[:len(randomUUIDString)-len(jobIndexAsString)] + jobIndexAsString
+				bldr := array.NewRecordBuilder(memory.DefaultAllocator, table.ToArrowSchema())
+				bldr.Field(0).(*sdkTypes.UUIDBuilder).Append(uuid.MustParse(randomUUIDStringWithLastCharacterReplaced))
+				bldr.Field(1).(*array.StringBuilder).Append("source")
+				bldr.Field(2).(*array.TimestampBuilder).Append(arrow.Timestamp(time.Now().UnixMicro()))
+				record := bldr.NewRecord()
+
+				if err := p.WriteAll(syncContext, []message.WriteMessage{&message.WriteInsert{
+					Record: record,
+				}}); err != nil {
+					t.Fatal(fmt.Errorf("failed to insert record: %w", err))
+				}
+			}
+
+			return nil
+		})
+	}
+
+	require.NoError(t, group.Wait())
+
+	ch := make(chan arrow.Record)
+	go func() {
+		defer close(ch)
+		err = p.Read(ctx, table, ch)
+	}()
+
+	numRows := 0
+	for record := range ch {
+		numRows += int(record.NumRows())
+	}
+
+	require.Equal(t, syncConcurrency*rounds, numRows)
+	require.NoError(t, err)
 }
