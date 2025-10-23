d0696e1b535cc084dd105ff19be7237daf107e0c
Muraru Ștefan <muraru.stefaan@gmail.com>
2025-10-23 12:02:02 +0300

    feat: publish FIPS versions for Bigquery destination plugin

--- plugins/destination/bigquery/main_fips.go
+++ plugins/destination/bigquery/main_fips.go
@@ -1,9 +1,12 @@
-//go:build !fipsEnabled
+//go:build fipsEnabled
+
+//go:debug fips140=on
 
 package main
 
 import (
 	"context"
+	"crypto/fips140"
 	"log"
 
 	"github.com/cloudquery/cloudquery/plugins/destination/bigquery/v4/client"
@@ -13,8 +16,10 @@ import (
 )
 
 func main() {
+	log.Printf("FIPS enabled: %t", fips140.Enabled())
+
 	p := plugin.NewPlugin(internalPlugin.Name, internalPlugin.Version, client.New,
-		plugin.WithBuildTargets(plugin.DefaultBuildTargets),
+		plugin.WithBuildTargets(buildTargets()),
 		plugin.WithKind(internalPlugin.Kind),
 		plugin.WithTeam(internalPlugin.Team),
 		plugin.WithJSONSchema(client.JSONSchema),
@@ -23,4 +28,15 @@ func main() {
 	if err := serve.Plugin(p, serve.WithDestinationV0V1Server()).Serve(context.Background()); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func buildTargets() []plugin.BuildTarget {
+	// default build targets for FIPS builds
+	// fipsEnabled is used to enable FIPS mode
+	targets := make([]plugin.BuildTarget, len(plugin.DefaultBuildTargets))
+	for i := range plugin.DefaultBuildTargets {
+		targets[i] = plugin.DefaultBuildTargets[i]
+		targets[i].Tags = append(targets[i].Tags, "fipsEnabled")
+	}
+	return targets
 }
