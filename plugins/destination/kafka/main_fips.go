88d56659fbd1b609477655791ba8bbd3c9fd96ac
Muraru Ștefan <muraru.stefaan@gmail.com>
2025-10-23 13:12:36 +0300

    feat: publish FIPS versions for Kafka destination plugin

--- plugins/destination/kafka/main_fips.go
+++ plugins/destination/kafka/main_fips.go
@@ -1,9 +1,12 @@
-//go:build !fipsEnabled
+//go:build fipsEnabled
+
+//go:debug fips140=on
 
 package main
 
 import (
 	"context"
+	"crypto/fips140"
 	"log"
 
 	"github.com/cloudquery/cloudquery/plugins/destination/kafka/v5/client"
@@ -14,13 +17,26 @@ import (
 )
 
 func main() {
+	log.Printf("FIPS enabled: %t", fips140.Enabled())
+
 	p := plugin.NewPlugin(internalPlugin.Name, internalPlugin.Version, client.New,
+		plugin.WithBuildTargets(buildTargets()),
 		plugin.WithKind(internalPlugin.Kind),
 		plugin.WithTeam(internalPlugin.Team),
 		plugin.WithJSONSchema(spec.JSONSchema),
 	)
-
 	if err := serve.Plugin(p, serve.WithDestinationV0V1Server()).Serve(context.Background()); err != nil {
-		log.Fatalf("failed to serve plugin: %v", err)
+		log.Fatal(err)
 	}
+}
+
+func buildTargets() []plugin.BuildTarget {
+	// default build targets for FIPS builds
+	// fipsEnabled is used to enable FIPS mode
+	targets := make([]plugin.BuildTarget, len(plugin.DefaultBuildTargets))
+	for i := range plugin.DefaultBuildTargets {
+		targets[i] = plugin.DefaultBuildTargets[i]
+		targets[i].Tags = append(targets[i].Tags, "fipsEnabled")
+	}
+	return targets
 }
