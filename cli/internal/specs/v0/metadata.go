4162c062c635670256c6c671d1da975a0c1fbfea
Ben Bernays <ben@bernays.me>
2026-03-16 10:37:35 -0500

    fix: Remove plugin version when not needed (#22084)
    Currently if a user uses the local, grpc, or docker registry, and specifies a Version, the logs will all report the plugin version as if it is coming from the plugin itself. This can create confusion for customers as they will see the summary log include the user set version, but in actuality the version that is running is not guarenteed to be the specified version because the plugin that is run is based on the path attribute

--- cli/internal/specs/v0/metadata.go
+++ cli/internal/specs/v0/metadata.go
@@ -42,8 +42,12 @@ func (m Metadata) VersionString() string {
 		return fmt.Sprintf("%s (%s@%s)", m.Name, m.Path, m.Version)
 	case RegistryLocal, RegistryGRPC:
 		return fmt.Sprintf("%s (%s@%s)", m.Name, m.Registry, m.Path)
-	default:
+	case RegistryDocker:
+		return fmt.Sprintf("%s@%s (%s)", m.Name, m.Registry, m.Path)
+	case RegistryGitHub:
 		return fmt.Sprintf("%s@%s (%s@%s)", m.Name, m.Registry, m.Path, m.Version)
+	default:
+		return fmt.Sprintf("%s@%s (%s)", m.Name, m.Registry, m.Path)
 	}
 }
 
@@ -83,6 +87,8 @@ func (m *Metadata) Validate() error {
 		if !strings.HasPrefix(m.Version, "v") {
 			return errors.New("version must start with v")
 		}
+	} else {
+		m.Version = "" // ignore version if registry doesn't need it, to avoid confusion
 	}
 
 	return nil
